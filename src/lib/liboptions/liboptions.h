fc7cb633c02c12692320f173fc67e935c7a4db92
rking <rking@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2008-06-09 13:57:37 +0000

    brought over more current liboptions from psi3
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@26 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: eabd39658c2c59fc3e3acc48fa308fcb582aa505

--- src/lib/liboptions/liboptions.h
+++ src/lib/liboptions/liboptions.h
@@ -14,18 +14,19 @@
 
 void options_init();
 void options_close();
+void options_read();
 void options_print();
 
-void        options_add_bool(char* cstr_option,bool bool_default);
-void        options_add_int(char* cstr_option,int int_default);
-void        options_add_double(char* cstr_option,double double_default);
-void        options_add_str(char* cstr_option,char* cstr_default);
-void        options_add_str_with_choices(char* cstr_option,char* cstr_default,char* cstr_choices);
+void        options_add_bool(const char* cstr_option,bool bool_default);
+void        options_add_int(const char* cstr_option,int int_default);
+void        options_add_double(const char* cstr_option,double double_default);
+void        options_add_str(const char* cstr_option,const char* cstr_default);
+void        options_add_str_with_choices(const char* cstr_option,const char* cstr_default,const char* cstr_choices);
 
-bool        options_get_bool(char* cstr_option);
-int         options_get_int(char* cstr_option);
-double      options_get_double(char* cstr_option);
-std::string options_get_str(char* cstr_option);
+bool        options_get_bool(const char* cstr_option);
+int         options_get_int(const char* cstr_option);
+double      options_get_double(const char* cstr_option);
+std::string options_get_str(const char* cstr_option);
 
 typedef struct 
 {
@@ -62,21 +63,21 @@ public:
   void        print();
   void        read_options();
 
-  void        add_bool_option(char* cstr_option,bool bool_default);
-  void        add_int_option(char* cstr_option,int int_default);
-  void        add_double_option(char* cstr_option,double double_default);
-  void        add_str_option(char* cstr_option,char* cstr_default);
-  void        add_str_option_with_choices(char* cstr_option,char* cstr_default,char* cstr_choices);
+  void        add_bool_option(const char* cstr_option,bool bool_default);
+  void        add_int_option(const char* cstr_option,int int_default);
+  void        add_double_option(const char* cstr_option,double double_default);
+  void        add_str_option(const char* cstr_option,const char* cstr_default);
+  void        add_str_option_with_choices(const char* cstr_option,const char* cstr_default,const char* cstr_choices);
 
-  bool        get_bool_option(char* cstr_option);
-  int         get_int_option(char* cstr_option);
-  double      get_double_option(char* cstr_option);
-  std::string get_str_option(char* cstr_option);
+  bool        get_bool_option(const char* cstr_option);
+  int         get_int_option(const char* cstr_option);
+  double      get_double_option(const char* cstr_option);
+  std::string get_str_option(const char* cstr_option);
 
-  void        set_bool_option(char* cstr_option, bool bool_value);
-  void        set_int_option(char* cstr_option,int int_value);
-  void        set_double_option(char* cstr_option,double double_value);
-  void        set_str_option(char* cstr_option,char* cstr_value);
+  void        set_bool_option(const char* cstr_option, bool bool_value);
+  void        set_int_option(const char* cstr_option,int int_value);
+  void        set_double_option(const char* cstr_option,double double_value);
+  void        set_str_option(const char* cstr_option,const char* cstr_value);
 private:
   void read_bool(BoolOptionsMap::iterator& it);
   void read_int(IntOptionsMap::iterator& it);
