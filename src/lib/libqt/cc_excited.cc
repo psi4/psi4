f4cbf943026a0cb64df7bbeedfe6b33a8e664a05
frank <frank@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2009-09-26 14:49:02 +0000

    Duplicated cc_wfn, ci_wfn, and cc_excited to handle a std:: string input.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@129 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libqt/cc_excited.cc
+++ src/lib/libqt/cc_excited.cc
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <psifiles.h>
 
 namespace psi {
@@ -30,6 +31,32 @@ int cc_excited(char *wfn)
   }
   else if ( !strcmp(wfn, "EOM_CCSD") || !strcmp(wfn, "LEOM_CCSD") ||
             !strcmp(wfn, "EOM_CC2")  || !strcmp(wfn, "EOM_CC3") ) {
+    return 1;
+  }
+  else {
+    printf("Invalid value of input keyword WFN: %s\n", wfn);
+    exit(PSI_RETURN_FAILURE);
+  }
+}
+
+/*!
+** cc_excited(): This function takes a WFN string and returns 1 if the WFN
+** is an excited-state method and 0 if the WFN is a ground-state method.
+**
+** \param wfn = wavefunction string
+**
+** Returns: 1 if an excited state method, else 0
+** \ingroup QT
+*/
+int cc_excited(std::string wfn)
+{
+  if ( (wfn =="CCSD")   || (wfn =="CCSD_T") || (wfn =="BCCD")
+    || (wfn =="BCCD_T") || (wfn =="CC2")    || (wfn =="CC3")
+    || (wfn =="CCSD_MVD") ) {
+    return 0;
+  }
+  else if ( (wfn =="EOM_CCSD") || (wfn =="LEOM_CCSD") ||
+            (wfn =="EOM_CC2")  || (wfn =="EOM_CC3") ) {
     return 1;
   }
   else {
--- src/lib/libqt/ci_wfn.cc
+++ src/lib/libqt/ci_wfn.cc
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <psifiles.h>
 
 namespace psi {
@@ -28,6 +29,31 @@ int ci_wfn(char *wfn)
       strcmp(wfn, "CASSCF")==0 || strcmp(wfn, "RASSCF")==0 ||
       strcmp(wfn, "DETCI")==0 || strcmp(wfn, "MCSCF")==0 ||
       strcmp(wfn, "OOCCD")==0 || strcmp(wfn,"ZAPTN")==0) 
+  {
+    return(1);
+  }
+  else  {
+    return 0;
+  }
+}
+
+/*!
+** ci_wfn(): Examine the wavefunction type and return 1 if a CI/MCSCF-type,
+** otherwise 0
+**
+** \param wfn = wavefunction string
+**
+** Returns: 1 if a CI/MCSCF-type wavefunction, otherwise 0
+**
+** \ingroup QT
+*/
+int ci_wfn(std::string wfn)
+{
+
+  if ((wfn == "CI")    || (wfn == "DETCAS") ||
+      (wfn =="CASSCF") || (wfn =="RASSCF")  ||
+      (wfn =="DETCI")  || (wfn =="MCSCF")   ||
+      (wfn =="OOCCD")  || (wfn == "ZAPTN"))
   {
     return(1);
   }
