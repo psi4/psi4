c0f1363408ec69701f35bb4259846e948a3ad7ec
crawdad <crawdad@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2009-12-28 18:31:25 +0000

    updated psi3 clean function to skip the checkpoint file. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@413 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 97983cee37a0094e85ebfb45b629ebd121c265d6

--- src/bin/psi4/clean.cc
+++ src/bin/psi4/clean.cc
@@ -22,7 +22,11 @@ void psiclean(void) {
   ip_cwk_add(const_cast<char*>(":PSI"));
   ip_set_uppercase(1);
 
-  for(int i=0; i < PSIO_MAXUNIT; i++) {
+  for(int i=0; i < 32; i++) { // skip the checkpoint file by default
+    psio_open(i, PSIO_OPEN_OLD);
+    psio_close(i, 0);
+  }
+  for(int i=33; i < PSIO_MAXUNIT; i++) {
     psio_open(i, PSIO_OPEN_OLD);
     psio_close(i, 0);
   }
