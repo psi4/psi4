aca1d229e2cbe2205d4a8d4018982fd2d070e420
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-01-14 18:14:03 +0000

    python should now initialize itself
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@431 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/bin/psi4/psi4.cc
+++ src/bin/psi4/psi4.cc
@@ -13,6 +13,7 @@
 #include <libciomr/libciomr.h>
 #include <liboptions/liboptions.h>
 #include <libparallel/parallel.h>
+#include <libscript/script.h>
 #include <physconst.h>
 
 #include <molecular_system.h>
@@ -24,23 +25,23 @@
 
 namespace psi { 
 
-  int psi_start(int argc, char *argv[]);
-  int psi_stop(FILE* infile, FILE* outfile, char* psi_file_prefix);
-  void print_version(FILE *);
-  void set_memory(FILE *infile, FILE *outfile);
-  int psi4_driver(Options & options, int argc, char *argv[]);
-  void psiclean(void);
+    int psi_start(int argc, char *argv[]);
+    int psi_stop(FILE* infile, FILE* outfile, char* psi_file_prefix);
+    void print_version(FILE *);
+    void set_memory(FILE *infile, FILE *outfile);
+    int psi4_driver(Options & options, int argc, char *argv[]);
+    void psiclean(void);
 
-  int read_options(const std::string &name, Options & options);
-  void read_atom_basis(char ** & atom_basis, int num_atoms);
+    int read_options(const std::string &name, Options & options);
+    void read_atom_basis(char ** & atom_basis, int num_atoms);
 
-  PSIO *psio = NULL;
-  std::map<std::string, PsiReturnType(*)(Options &, int argc, char *argv[])> dispatch_table;
+    PSIO *psio = NULL;
+    std::map<std::string, PsiReturnType(*)(Options &, int argc, char *argv[])> dispatch_table;
 
   // These are global variable for the number of processes and
   // id for each process
-  int nprocs;
-  int myid;
+    int nprocs;
+    int myid;
 }
 
 // This is the ONLY main function in PSI
@@ -53,6 +54,10 @@ int main(int argc, char *argv[])
 
     // Create the global world communicator
     Communicator::world = shared_ptr<Communicator>(new MPICommunicator(MPI_COMM_WORLD));
+
+    // Create the scripting object
+    Script::language = shared_ptr<Script>(new Python());
+    Script::language->initialize();
     
     // Needed until codes are converted to using Communicator::world
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -60,63 +65,26 @@ int main(int argc, char *argv[])
 
     psi_start(argc, argv);
 
-    if(Communicator::world->me() && !clean_only) print_version(outfile);
+    if(Communicator::world->me()==0 && !clean_only) print_version(outfile);
+
+    set_memory(infile, outfile);
 
     psio_init();
     psio_ipv1_config();
 
     if(clean_only) {
-      psiclean();
-      exit(EXIT_SUCCESS);
+        psiclean();
+        exit(EXIT_SUCCESS);
     }
-   
-    set_memory(infile, outfile);
 
     Options options;
-   
+
     psi4_driver(options, argc, argv);
-/*
+    psi_stop(infile, outfile, psi_file_prefix);
 
-       // if read from input.dat, scale.  Elsewhere always use au internally
-     std::string units = options.get_str("UNITS");
-     double conv_factor;
-     if (units == "BOHR" || units == "AU")
-       conv_factor = 1.0;
-     else if (units == "ANGSTROMS" || units == "ANGSTROM")
-       conv_factor = 1.0 / _bohr2angstroms;
-   
-     Molecular_system molecules(conv_factor);  // by default, reads geometry from input.dat
-     molecules.print(); fflush(outfile);
-     options.clear();
-   
-     try { read_atom_basis(atom_basis, molecules.get_natoms()); }
-     catch (const char * s) {
-       fprintf(outfile,"Unable to determine basis set:\n\t %s\n",s);
-       fprintf(stderr, "Unable to determine basis set:\n\t %s\n",s);
-       abort();
-     }
-   
-     for (int i=0; i<molecules.get_num_atoms(); ++i)
-       strcpy(atom_basis[i],"DZ");
-     read_options("INPUT", options);
-     module.set_prgid("INPUT");
-     input::input(options,atom_basis,molecules);
-     options.clear();
+    Script::language->finalize();
+    MPI_Finalize();
 
-     read_options("CINTS", options);
-     module.set_prgid("CINTS");
-     CINTS::cints(options,argc, argv);
-     module.set_prgid("CSCF");
-     cscf::cscf(argc, argv);
-     options.clear();
-     module.set_prgid("PSICLEAN");
-     psiclean::psiclean(argc, argv);
-*/
-
-   psi_stop(infile, outfile, psi_file_prefix);
-
-   MPI_Finalize();
-
-  // This needs to be changed a return value from the processed script
-  return EXIT_SUCCESS;
+    // This needs to be changed a return value from the processed script
+    return EXIT_SUCCESS;
 }
--- src/lib/libscript/script.cc
+++ src/lib/libscript/script.cc
@@ -1,6 +1,9 @@
 #include "script.h"
 
 using namespace psi;
+using namespace boost;
+
+shared_ptr<Script> Script::language;
 
 Script::Script()
 {
