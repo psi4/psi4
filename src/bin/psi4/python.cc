afe5df5d95238d9412e91b429f203c8ac7bb913a
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-01-14 18:14:48 +0000

    python finally working, but requires modification of makefiles to work
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@441 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/bin/psi4/python.cc
+++ src/bin/psi4/python.cc
@@ -1,8 +1,10 @@
 // Include Python's header file.
 // Use "python-config --includes" to determine this location.
 #include <cstdio>
-#include <Python.h>
+#include <boost/python.hpp>
+//#include <Python.h>
 #include <psiconfig.h>
+#include <sstream>
 #include "script.h"
 
 using namespace psi;
@@ -13,47 +15,75 @@ namespace psi {
     extern FILE *outfile;
 }
 
-static PyObject *
-py_psi_version(PyObject *self, PyObject *args)
+bool py_psi_input()
 {
-    return Py_BuildValue("s", PSI_VERSION);
+    // Need to modify input to not take argc and argv
+    // And Options object to be global.
+    // input::input3();
+    return true;
 }
 
-static PyObject *
-py_psi_input(PyObject *self, PyObject *args)
+char const* py_psi_version()
 {
-//    int result = input::input(options, 0, NULL);
-    return Py_BuildValue("i", 0);
+    return PSI_VERSION;
 }
 
-static PyObject *
-py_psi_clean(PyObject *self, PyObject *args)
+void py_psi_clean()
 {
     psiclean();
-    return Py_BuildValue("i", 0);
 }
 
-static PyMethodDef PsiMethods[] = {
-    { "version", py_psi_version, METH_NOARGS, "Obtain version information from PSI."},
-    { "input",   py_psi_input,   METH_NOARGS, "Run the input module."},
-    { "clean",   py_psi_clean,   METH_NOARGS, "Run the psiclean module."},
-    { NULL, NULL, 0, NULL } /* Sentinel */
-};
-
-static struct PyModuleDef PsiModule = {
-    PyModuleDef_HEAD_INIT,
-    "psi",   /* name of the module */
-    NULL,    /* something with documentation */
-    -1,      /* size of the per-interpreter state of the module,
-                or -1 if the module keeps state in global variables. */
-    PsiMethods
-};
-
-PyMODINIT_FUNC
-PyInit_psi()
+BOOST_PYTHON_MODULE(psi)
 {
-    return PyModule_Create(&PsiModule);
+    using namespace boost::python;
+    def("version", py_psi_version);
+    def("clean", py_psi_clean);
+
+    // modules
+    def("input",py_psi_input);
 }
+
+//static PyObject *
+//py_psi_version(PyObject *self, PyObject *args)
+//{
+//    return Py_BuildValue("s", PSI_VERSION);
+//}
+//
+//static PyObject *
+//py_psi_input(PyObject *self, PyObject *args)
+//{
+////    int result = input::input(options, 0, NULL);
+//    return Py_BuildValue("i", 0);
+//}
+//
+//static PyObject *
+//py_psi_clean(PyObject *self, PyObject *args)
+//{
+//    psiclean();
+//    return Py_BuildValue("i", 0);
+//}
+//
+//static PyMethodDef PsiMethods[] = {
+//    { "version", py_psi_version, METH_NOARGS, "Obtain version information from PSI."},
+//    { "input",   py_psi_input,   METH_NOARGS, "Run the input module."},
+//    { "clean",   py_psi_clean,   METH_NOARGS, "Run the psiclean module."},
+//    { NULL, NULL, 0, NULL } /* Sentinel */
+//};
+//
+//static struct PyModuleDef PsiModule = {
+//    PyModuleDef_HEAD_INIT,
+//    "psi",   /* name of the module */
+//    NULL,    /* something with documentation */
+//    -1,      /* size of the per-interpreter state of the module,
+//                or -1 if the module keeps state in global variables. */
+//    PsiMethods
+//};
+//
+//PyMODINIT_FUNC
+//PyInit_psi()
+//{
+//    return PyModule_Create(&PsiModule);
+//}
 
 Python::Python() : Script()
 {
@@ -67,26 +97,50 @@ Python::~Python()
 
 void Python::initialize()
 {
-    PyImport_AppendInittab("psi", PyInit_psi);
-    Py_SetProgramName(L"psi");    
-    Py_Initialize();
-    PyImport_ImportModule("psi");
 }
 
 void Python::finalize()
 {
-    Py_Finalize();
 }
 
 void Python::run(FILE *input)
 {
+    using namespace boost::python;
+    char *s;
     if (input == NULL)
         return;
     if (!Py_IsInitialized()) {
-        initialize();
+        s = strdup("psi");
+//        PyImport_AppendInittab(s, initpsi);
+        Py_Initialize();
+        #if PY_VERSION_HEX >= 0x03000000
+        Py_SetProgramName(L"psi");
+        #else
+        Py_SetProgramName(s);
+        #endif
     }
     if (Py_IsInitialized()) {
-        PyRun_AnyFile(input, "User Input File");
+        char line[256];
+        std::stringstream file;
+        while(fgets(line, sizeof(line), input)) {
+            file << line;
+        }
+        printf("Input file:\n%s", file.str().c_str());
+        str strStartScript(file.str().c_str());
+
+        try {
+            PyImport_AppendInittab(s, initpsi);
+            object objectMain(handle<>(borrowed(PyImport_AddModule("__main__"))));
+            object objectDict = objectMain.attr("__dict__");
+            s = strdup("import psi; from psi import *;");
+            PyRun_SimpleString(s);
+            
+            object objectScriptInit = exec( strStartScript, objectDict, objectDict );
+        }
+        catch (error_already_set const& e)
+        {
+            PyErr_Print();
+        }
     } else {
         fprintf(stderr, "Unable to run Python input file.\n");
         return;
