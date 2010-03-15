63276755a00a116112e7e8094506af143498a2f6
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-03-15 18:20:19 +0000

    fixed bug in LocalCommunicator
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@548 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libparallel/local.cc
+++ src/lib/libparallel/local.cc
@@ -41,35 +41,35 @@ void LocalCommunicator::raw_recv(int sender, void *data, int nbyte)
 
 void LocalCommunicator::sum(double *data, int n, double *receive_buffer, int target)
 {
-    if (target != -1) {
+    if (receive_buffer != 0) {
         ::memcpy(receive_buffer, data, sizeof(double) * n);
     }
 }
 
 void LocalCommunicator::sum(unsigned int *data, int n, unsigned int *receive_buffer, int target)
 {
-    if (target != -1) {
+    if (receive_buffer != 0) {
         ::memcpy(receive_buffer, data, sizeof(unsigned int) * n);
     }
 }
 
 void LocalCommunicator::sum(int *data, int n, int *receive_buffer, int target)
 {
-    if (target != -1) {
+    if (receive_buffer != 0) {
         ::memcpy(receive_buffer, data, sizeof(int) * n);
     }
 }
 
 void LocalCommunicator::sum(char *data, int n, char *receive_buffer, int target)
 {
-    if (target != -1) {
-        ::memcpy(receive_buffer, data, sizeof(char) *n );
+    if (receive_buffer != 0) {
+        ::memcpy(receive_buffer, data, sizeof(char) * n);
     }
 }
 
 void LocalCommunicator::sum(long *data, int n, long *receive_buffer, int target)
 {
-    if (target != -1) {
+    if (receive_buffer != 0) {
         ::memcpy(receive_buffer, data, sizeof(long) * n);
     }
 }
