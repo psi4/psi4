e41efdb3538e5bb161eb292bae3806f9898fb3c5
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-06-25 22:46:17 +0000

    cleaned up fprintf in libparallel to not segfault if Communicator::world in 0
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@703 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libparallel/fprintf.cc
+++ src/lib/libparallel/fprintf.cc
@@ -6,18 +6,33 @@ using namespace psi;
 
 extern "C" {
 
-int fprintf(FILE * __restrict __stream, const char * __restrict __format, ...)
-{
-    va_list args;
-    va_start(args, __format);
-    int status = 0;
+    void p_fprintf(FILE * __restrict __stream, const char * __restrict __format, ...)
+    {
+        va_list args;
+        va_start(args, __format);
+        int status = 0;
 
-    if (Communicator::world->me() == 0) {
         status = vfprintf(__stream, __format, args);
+
+        va_end(args);
     }
 
-    va_end(args);
-    return status;
-}
+    int fprintf(FILE * __restrict __stream, const char * __restrict __format, ...)
+    {
+        va_list args;
+        va_start(args, __format);
+        int status = 0;
+
+        if (Communicator::world.get() != NULL && Communicator::world->me() == 0) {
+            status = vfprintf(__stream, __format, args);
+        }
+        else if (Communicator::world.get() == NULL) {
+            p_fprintf(stderr, "Communicator object does not exist.\n");
+            status = vfprintf(__stream, __format, args);
+        }
+
+        va_end(args);
+        return status;
+    }
 
 }
