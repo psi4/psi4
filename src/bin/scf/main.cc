bea4b146a69a310b0c936315ef6dfb19bce81dd7
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-07-08 22:03:01 +0000

    removed getenv call from input3, using Process::environment
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@718 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 4cfa4a44cbe6186f998eed8f01c14784289eea79

--- src/bin/scf/main.cc
+++ src/bin/scf/main.cc
@@ -21,31 +21,29 @@
 
 namespace psi { namespace scf {
 
-std::string to_string(const int val);   // In matrix.cpp
-
 PsiReturnType scf(Options & options, int /* argc */, char * /* argv */[])
 {
     tstart();
-    
+
     Wavefunction::initialize_singletons();
-    
+
     shared_ptr<PSIO> psio(new PSIO);
     psiopp_ipv1_config(psio);
-    
+
     shared_ptr<Chkpt> chkpt(new Chkpt(psio, PSIO_OPEN_OLD));
-    
+
     // Initialize the psi3 timer library.
     timer_init();
 
     // Compute the Hartree-Fock energy
     HFEnergy hf(options, psio, chkpt);
     /* double hf_energy = */ hf.compute_energy();
-    
-    // Shut down psi. 
+
+    // Shut down psi.
     timer_done();
 
     tstop();
-    
+
     return Success;
 }
 
