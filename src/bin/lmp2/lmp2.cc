08c18ddd1ee1da3809a9c3ac9b4446b31299e594
bmintz <bmintz@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-08-24 17:49:09 +0000

    checking commit in eclipse
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@824 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 5e5491d8ea132dac9fc438a4094b5bfc721cbd17

--- src/bin/lmp2/lmp2.cc
+++ src/bin/lmp2/lmp2.cc
@@ -1,10 +1,10 @@
 /*! \defgroup LMP2 lmp2: LMP2 Evaluation of Energy */
 
 /*!
-** \file
-** \ingroup LMP2
-** \LMP2 evaluation of energy
-*/
+ ** \file
+ ** \ingroup LMP2
+ ** \LMP2 evaluation of energy
+ */
 
 #include <libciomr/libciomr.h>
 #include <libqt/qt.h>
@@ -12,9 +12,9 @@
 #include <libmints/mints.h>
 #include "globals.h"
 
-namespace psi{
+namespace psi {
 
-namespace lmp2{
+namespace lmp2 {
 
 PsiReturnType lmp2(Options &options) {
 
@@ -22,107 +22,107 @@ PsiReturnType lmp2(Options &options) {
     psiopp_ipv1_config(psio_obj);
     shared_ptr<Chkpt> chkpt_obj(new Chkpt(psio_obj, PSIO_OPEN_OLD));
 
-  if(Communicator::world->me() == 0) {
-    tstart();
-    fprintf(outfile, "\n**************************** Begin LMP2 *********************************\n\n\n");
+    if (Communicator::world->me() == 0) {
+        tstart();
+        fprintf(
+                outfile,
+                "\n**************************** Begin LMP2 *********************************\n\n\n");
 
+        fprintf(outfile, "\t\t\t*************************\n");
+        fprintf(outfile, "\t\t\t*                       *\n");
+        if (options.get_bool("RI_LMP2")) {
+            fprintf(outfile, "\t\t\t*       DF-LMP2         *\n");
+        } else {
+            fprintf(outfile, "\t\t\t*         LMP2          *\n");
+        }
+        fprintf(outfile, "\t\t\t*                       *\n");
+        fprintf(outfile, "\t\t\t*************************\n");
+        fprintf(outfile, "\t\t\tRunning on %d processors\n",
+                Communicator::world->nproc());
+        fflush(outfile);
 
-    fprintf(outfile, "\t\t\t*************************\n");
-    fprintf(outfile, "\t\t\t*                       *\n");
-  if(options.get_bool("RI_LMP2")){
-    fprintf(outfile, "\t\t\t*       DF-LMP2         *\n");
-  }
-  else {
-    fprintf(outfile, "\t\t\t*         LMP2          *\n");
-  }
-    fprintf(outfile, "\t\t\t*                       *\n");
-    fprintf(outfile, "\t\t\t*************************\n");
-    fprintf(outfile, "\t\t\tRunning on %d processors\n", Communicator::world->nproc());
-    fflush(outfile);
+    }
 
+    /** Compute the LMP2 energy **/
+    {
+        LMP2 lmp2_obj(psio_obj, chkpt_obj);
+        timer_init();
 
-  }
+        if (Communicator::world->me() == 0)
+            timer_on("GETPARAMS");
+        lmp2_obj.get_params(options);
+        if (Communicator::world->me() == 0)
+            timer_off("GETPARAMS");
 
-  /** Compute the LMP2 energy **/
-  {
-  LMP2 lmp2_obj(psio_obj, chkpt_obj);
-  timer_init();
+        if (Communicator::world->me() == 0)
+            timer_on("GETMOINFO");
+        lmp2_obj.get_moinfo();
+        if (Communicator::world->me() == 0)
+            timer_off("GETMOINFO");
 
-  if(Communicator::world->me() == 0)
-    timer_on("GETPARAMS");
-  lmp2_obj.get_params(options);
-  if(Communicator::world->me() == 0)
-    timer_off("GETPARAMS");
+        if (Communicator::world->me() == 0)
+            timer_on("OPDM");
+        lmp2_obj.opdm();
+        if (Communicator::world->me() == 0)
+            timer_off("OPDM");
 
-  if(Communicator::world->me() == 0)
-    timer_on("GETMOINFO");
-  lmp2_obj.get_moinfo();
-  if(Communicator::world->me() == 0)
-    timer_off("GETMOINFO");
+        if (Communicator::world->me() == 0)
+            timer_on("LOCALIZE");
+        lmp2_obj.localize();
+        if (Communicator::world->me() == 0)
+            timer_off("LOCALIZE");
 
-  if(Communicator::world->me() == 0)
-    timer_on("OPDM");
-  lmp2_obj.opdm();
-  if(Communicator::world->me() == 0)
-    timer_off("OPDM");
+        if (Communicator::world->me() == 0)
+            timer_on("GETFOCK");
+        lmp2_obj.get_fock();
+        if (Communicator::world->me() == 0)
+            timer_off("GETFOCK");
 
-  if(Communicator::world->me() == 0)
-    timer_on("LOCALIZE");
-  lmp2_obj.localize();
-  if(Communicator::world->me() == 0)
-    timer_off("LOCALIZE");
+        if (Communicator::world->me() == 0)
+            timer_on("DOMAIN");
+        lmp2_obj.domains();
+        if (Communicator::world->me() == 0)
+            timer_off("DOMAIN");
 
-  if(Communicator::world->me() == 0)
-    timer_on("GETFOCK");
-  lmp2_obj.get_fock();
-  if(Communicator::world->me() == 0)
-    timer_off("GETFOCK");
+        if (Communicator::world->me() == 0)
+            timer_on("PROJECT");
+        lmp2_obj.projection();
+        if (Communicator::world->me() == 0)
+            timer_off("PROJECT");
 
-  if(Communicator::world->me() == 0)
-    timer_on("DOMAIN");
-  lmp2_obj.domains();
-  if(Communicator::world->me() == 0)
-    timer_off("DOMAIN");
+        if (options.get_bool("RI_LMP2")) {
+            lmp2_obj.direct_df_transformation3();
+        } else {
+            if (Communicator::world->me() == 0)
+                timer_on("TRANS");
+            lmp2_obj.direct_transformation();
+            if (Communicator::world->me() == 0)
+                timer_off("TRANS");
+        }
 
-  if(Communicator::world->me() == 0)
-    timer_on("PROJECT");
-  lmp2_obj.projection();
-  if(Communicator::world->me() == 0)
-    timer_off("PROJECT");
+        lmp2_obj.allocate_T();
 
-  if(options.get_bool("RI_LMP2")){
-    lmp2_obj.direct_df_transformation3();
-  }
-  else {
-    if(Communicator::world->me() == 0)
-        timer_on("TRANS");
-    lmp2_obj.direct_transformation();
-    if(Communicator::world->me() == 0)
-        timer_off("TRANS");
-  }
+        if (Communicator::world->me() == 0)
+            timer_on("ITERATE");
+        lmp2_obj.iterate();
+        if (Communicator::world->me() == 0)
+            timer_off("ITERATE");
 
+    }
+    /** LMP2 complete **/
 
-  lmp2_obj.allocate_T();
+    timer_done();
+    if (Communicator::world->me() == 0) {
+        fprintf(
+                outfile,
+                "\n**************************** End of LMP2 *********************************\n");
 
-  if(Communicator::world->me() == 0)
-    timer_on("ITERATE");
-  lmp2_obj.iterate();
-  if(Communicator::world->me() == 0)
-    timer_off("ITERATE");
+        tstop();
+    }
 
-
-  }
-  /** LMP2 complete **/
-
-  timer_done();
-  if(Communicator::world->me() == 0) {
-    fprintf(outfile, "\n**************************** End of LMP2 *********************************\n");
-
-    tstop();
-  }
-
-  return Success;
+    return Success;
 }
 
-}}
+}
+}
 
