ce688405dca32af8d127296a96b849ee3e5a8c68
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2010-11-10 21:44:21 +0000

    initial start of 3c overlap ints
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1015 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: a1613f306902e62106a307da8adbe250fdcaba0f

--- src/lib/libmints/osrecur.h
+++ src/lib/libmints/osrecur.h
@@ -11,29 +11,29 @@ class ObaraSaikaTwoCenterRecursion
 {
     int max_am1_;
     int max_am2_;
-    
+
     double **x_;
     double **y_;
     double **z_;
-    
+
     // No default constructor
     ObaraSaikaTwoCenterRecursion();
     // No assignment operator
     ObaraSaikaTwoCenterRecursion& operator=(const ObaraSaikaTwoCenterRecursion&);
-    
+
 public:
     /// Constructor, max_am1 and max_am2 are the max angular momentum on center 1 and 2.
     /// Needed to allocate enough memory.
     ObaraSaikaTwoCenterRecursion(int max_am1, int max_am2);
     ~ObaraSaikaTwoCenterRecursion();
-    
+
     /// Returns the x recursion matrix.
     double **x() const { return x_; }
     /// Returns the y recursion matrix.
     double **y() const { return y_; }
     /// Returns the z recursion matrix.
     double **z() const { return z_; }
-    
+
     /// Computes the recursion matrices for the data provided.
     void compute(double PA[3], double PB[3], double gamma, int am1, int am2);
 };
@@ -48,20 +48,20 @@ class ObaraSaikaTwoCenterMIRecursion
     int max_am1_;
     int max_am2_;
     int max_m_;
-    
+
     double ***x_;
     double ***y_;
     double ***z_;
-    
+
     // No default constructor
     ObaraSaikaTwoCenterMIRecursion();
     // No assignment operator
     ObaraSaikaTwoCenterMIRecursion& operator=(const ObaraSaikaTwoCenterMIRecursion&);
-    
+
 public:
     ObaraSaikaTwoCenterMIRecursion(int max_am1, int max_am2, int max_m);
     ~ObaraSaikaTwoCenterMIRecursion();
-    
+
     double ***x() const { return x_; }
     double ***y() const { return y_; }
     double ***z() const { return z_; }
@@ -78,13 +78,13 @@ protected:
     int max_am1_;
     int max_am2_;
     int size_;
-    
+
     double ***vi_;
 
     // Forms Fm(U) from A20 (OS 1986)
     void calculate_f(double *F, int n, double t);
-    
-private:    
+
+private:
     // No default constructor
     ObaraSaikaTwoCenterVIRecursion();
     // No assignment operator
@@ -98,7 +98,7 @@ public:
 
     /// Returns the potential integral 3D matrix
     double ***vi() const { return vi_; }
-    
+
     /// Computes the potential integral 3D matrix using the data provided.
     virtual void compute(double PA[3], double PB[3], double PC[3], double zeta, int am1, int am2);
 };
@@ -113,21 +113,21 @@ protected:
     double ***vx_;
     double ***vy_;
     double ***vz_;
-    
+
 private:
     // No default constructor();
     ObaraSaikaTwoCenterVIDerivRecursion();
     // No assignment operator
     ObaraSaikaTwoCenterVIDerivRecursion& operator=(const ObaraSaikaTwoCenterVIDerivRecursion&);
-    
+
 public:
     ObaraSaikaTwoCenterVIDerivRecursion(int max_am1, int max_am2);
     virtual ~ObaraSaikaTwoCenterVIDerivRecursion();
-    
+
     double ***vx() const { return vx_; }
     double ***vy() const { return vy_; }
     double ***vz() const { return vz_; }
-    
+
     virtual void compute(double PA[3], double PB[3], double PC[3], double zeta, int am1, int am2);
 };
 
@@ -141,21 +141,21 @@ protected:
     double ***ex_;
     double ***ey_;
     double ***ez_;
-    
+
 private:
     // No default constructor
     ObaraSaikaTwoCenterElectricField();
     // No assignment operator
     ObaraSaikaTwoCenterElectricField& operator=(const ObaraSaikaTwoCenterElectricField&);
-    
+
 public:
     ObaraSaikaTwoCenterElectricField(int max_am1, int max_am2);
     virtual ~ObaraSaikaTwoCenterElectricField();
-    
+
     double ***ex() const { return ex_; }
     double ***ey() const { return ey_; }
     double ***ez() const { return ez_; }
-    
+
     virtual void compute(double PA[3], double PB[3], double PC[3], double zeta, int am1, int am2);
 };
 
@@ -172,22 +172,60 @@ protected:
     double ***exy_;
     double ***exz_;
     double ***eyz_;
-    
+
 private:
     // No default constructor
     ObaraSaikaTwoCenterElectricFieldGradient();
     // No assignment operator
     ObaraSaikaTwoCenterElectricFieldGradient& operator=(const ObaraSaikaTwoCenterElectricFieldGradient&);
-    
+
 public:
     ObaraSaikaTwoCenterElectricFieldGradient(int max_am1, int max_am2);
     virtual ~ObaraSaikaTwoCenterElectricFieldGradient();
-    
+
     double ***ex() const { return ex_; }
     double ***ey() const { return ey_; }
     double ***ez() const { return ez_; }
-    
+
     virtual void compute(double PA[3], double PB[3], double PC[3], double zeta, int am1, int am2);
+};
+
+/*! \ingroup MINTS
+    \class ObaraSaikaThreeCenterRecursion
+ */
+class ObaraSaikaThreeCenterRecursion
+{
+    int max_am1_;
+    int max_am2_;
+    int max_am3_;
+
+    double ***x_;
+    double ***y_;
+    double ***z_;
+
+    // No default constructor
+    ObaraSaikaThreeCenterRecursion();
+
+    // No assignment operator
+    ObaraSaikaThreeCenterRecursion& operator=(const ObaraSaikaThreeCenterRecursion&);
+
+public:
+    ObaraSaikaThreeCenterRecursion(int max_am1, int max_am2, int max_am3);
+    ~ObaraSaikaThreeCenterRecursion();
+
+    /// Returns the x recursion matrix.
+    const double ***x() const { return x_; }
+    /// Returns the y recursion matrix.
+    const double ***y() const { return y_; }
+    /// Returns the z recursion matrix.
+    const double ***z() const { return z_; }
+
+    /// Computes the recursion matrices for the data provided.
+    void compute(double GA[3],
+                 double GB[3],
+                 double GC[3],
+                 double gamma,
+                 int amA, int amB, int amC);
 };
 
 }
