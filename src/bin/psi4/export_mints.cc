405b848b86ef1c5df7a4185fa13634beaf145756
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-02-03 17:22:09 +0000

    more python wrappings for matrix and matrix factory
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1173 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: d9b11287b501bc1d686dc9da62cbaa3912d1cb37

--- src/bin/psi4/export_mints.cc
+++ src/bin/psi4/export_mints.cc
@@ -1,23 +1,39 @@
+#include <string>
 #include <boost/python.hpp>
 #include <libmints/mints.h>
 
+using namespace boost;
 using namespace boost::python;
 using namespace psi;
 
+shared_ptr<MatrixFactory> get_matrix_factory()
+{
+    // We need a valid molecule with a valid point group to create a matrix factory.
+    shared_ptr<Molecule> molecule = Process::environment.molecule();
+    if (!molecule) {
+        fprintf(outfile, "  Active molecule not set!");
+        throw PSIEXCEPTION("Active molecule not set!");
+    }
+    if (!molecule->point_group()) {
+        fprintf(outfile, "  Active molecule does not have point group set!");
+        throw PSIEXCEPTION("Active molecule does not have point group set!");
+    }
+
+    // Read in the basis set
+    shared_ptr<BasisSetParser> parser(new Gaussian94BasisSetParser(Process::environment.options.get_str("BASIS_PATH")));
+    shared_ptr<BasisSet> basis = BasisSet::construct(parser, molecule, Process::environment.options.get_str("BASIS"));
+    shared_ptr<IntegralFactory> fact(new IntegralFactory(basis, basis, basis, basis));
+    shared_ptr<SOBasisSet> sobasis(new SOBasisSet(basis, fact));
+    const Dimension& dim = sobasis->dimension();
+
+    shared_ptr<MatrixFactory> matfac(new MatrixFactory);
+    matfac->init_with(dim, dim);
+
+    return matfac;
+}
+
 void export_mints()
 {
-    class_<Matrix, shared_ptr<Matrix> >( "Matrix").
-        def(init<int, int>()).
-        def("get", &Matrix::get).
-        def("set", &Matrix::set_python).
-        def("set_name", &Matrix::set_name).
-        def("print_out", &Matrix::print_out).
-        def("rows", &Matrix::rowdim).
-        def("cols", &Matrix::coldim).
-        def("nirreps", &Matrix::nirreps).
-        def("__getitem__", &Matrix::pyget).
-        def("__setitem__", &Matrix::pyset);
-
     class_<Vector, shared_ptr<Vector> >( "Vector").
         def(init<int>()).
         def("get", &Vector::get).
@@ -33,6 +49,57 @@ void export_mints()
         def("print_out", &IntVector::print_out).
         def("dim", &IntVector::dim).
         def("nirreps", &IntVector::nirreps);
+
+    typedef void (Matrix::*matrix_multiply)(bool, bool, double, boost::shared_ptr<Matrix>, boost::shared_ptr<Matrix>, double);
+    typedef void (Matrix::*matrix_diagonalize)(boost::shared_ptr<Matrix>, boost::shared_ptr<Vector>);
+    typedef void (Matrix::*matrix_one)(boost::shared_ptr<Matrix>);
+    typedef double (Matrix::*double_matrix_one)(boost::shared_ptr<Matrix>);
+    typedef void (Matrix::*matrix_two)(boost::shared_ptr<Matrix>, boost::shared_ptr<Matrix>);
+
+    class_<Matrix, shared_ptr<Matrix> >("Matrix").
+        def(init<int, int>()).
+        def("get", &Matrix::get).
+        def("set", &Matrix::set_python).
+        def("set_name", &Matrix::set_name).
+        def("print_out", &Matrix::print_out).
+        def("rows", &Matrix::rowdim).
+        def("cols", &Matrix::coldim).
+        def("nirreps", &Matrix::nirreps).
+        def("identity", &Matrix::identity).
+        def("zero", &Matrix::zero).
+        def("zero_diagonal", &Matrix::zero_diagonal).
+        def("trace", &Matrix::trace).
+//            def("transpose", &Matrix::transpose).
+        def("add", matrix_one(&Matrix::add)).
+        def("subtract", matrix_one(&Matrix::subtract)).
+        def("accumulate_product", matrix_two(&Matrix::accumulate_product)).
+        def("scale", &Matrix::scale).
+        def("sum_of_squares", &Matrix::sum_of_squares).
+        def("rms", &Matrix::rms).
+        def("scale_row", &Matrix::scale_row).
+        def("scale_column", &Matrix::scale_column).
+        def("transform", matrix_one(&Matrix::transform)).
+        def("transform", matrix_two(&Matrix::transform)).
+        def("transform", matrix_one(&Matrix::back_transform)).
+        def("back_transform", matrix_two(&Matrix::back_transform)).
+        def("vector_dot", double_matrix_one(&Matrix::vector_dot)).
+        def("gemm", matrix_multiply(&Matrix::gemm)).
+        def("diagonalize", matrix_diagonalize(&Matrix::diagonalize)).
+        def("cholesky_factorize", &Matrix::cholesky_factorize).
+        def("invert", &Matrix::invert).
+        def("get", &Matrix::get).
+        def("set", &Matrix::set_python).
+        def("__getitem__", &Matrix::pyget).
+        def("__setitem__", &Matrix::pyset);
+
+    typedef shared_ptr<Matrix> (MatrixFactory::*create_shared_matrix)();
+    typedef shared_ptr<Matrix> (MatrixFactory::*create_shared_matrix_name)(std::string);
+
+    class_<MatrixFactory, shared_ptr<MatrixFactory> >("MatrixFactory").
+            def("shared_object", &get_matrix_factory).
+            staticmethod("shared_object").
+            def("create_matrix", create_shared_matrix(&MatrixFactory::create_shared_matrix)).
+            def("create_matrix", create_shared_matrix_name(&MatrixFactory::create_shared_matrix));
 
     class_<MintsHelper, shared_ptr<MintsHelper> >("MintsHelper").
         def("ao_overlap", &MintsHelper::ao_overlap).
@@ -128,7 +195,7 @@ void export_mints()
         def("set_ghost_fragments", &Molecule::set_ghost_fragments).
         def("set_ghost_fragment", &Molecule::set_ghost_fragment).
         def("atom_at_position", &Molecule::atom_at_position1).
-        def("print_to_output", &Molecule::print).
+        def("print_out", &Molecule::print).
         def("nuclear_repulsion_energy", &Molecule::nuclear_repulsion_energy).
         def("reorient", &Molecule::reorient).
         def("find_point_group", &Molecule::find_point_group).
