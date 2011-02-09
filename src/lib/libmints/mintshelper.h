0b72f5d4b911542dd4f2439e094d6903d3596839
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-02-09 16:11:48 +0000

    refactored IWLWriter to reduce the number of function calls it makes
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1232 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: a7f25281443a011a9b8f186f3f4fa05b4642bf5c

--- src/lib/libmints/mintshelper.h
+++ src/lib/libmints/mintshelper.h
@@ -12,17 +12,42 @@ class Options;
 class IWLWriter {
     IWL& writeto_;
     size_t count_;
+    int& current_buffer_count_;
 
-    public:
+    Label *plabel_;
+    Value *pvalue_;
+public:
 
-
-    IWLWriter(IWL& writeto) : writeto_(writeto), count_(0)
-      { }
+    IWLWriter(IWL& writeto) : writeto_(writeto), count_(0),
+        current_buffer_count_(writeto_.index())
+    {
+        plabel_ = writeto_.labels();
+        pvalue_ = writeto_.values();
+    }
 
     void operator()(int i, int j, int k, int l, int , int , int , int , int , int , int , int , double value)
     {
-        writeto_.write_value(i, j, k, l, value, 0, NULL, 0);
+        int current_label_position = 4*current_buffer_count_;
+
+        // Save the labels
+        plabel_[current_label_position++] = i;
+        plabel_[current_label_position++] = j;
+        plabel_[current_label_position++] = k;
+        plabel_[current_label_position]   = l;
+
+        // Save the value
+        pvalue_[current_buffer_count_++] = value;
+
+        // Increment overall counter
         count_++;
+
+        // If our IWL buffer is full dump to disk.
+        if (current_buffer_count_ == writeto_.ints_per_buffer()) {
+            writeto_.last_buffer() = 0;
+            writeto_.buffer_count() = current_buffer_count_;
+            writeto_.put();
+            current_buffer_count_ = 0;
+        }
     }
 
     size_t count() const { return count_; }
@@ -34,47 +59,47 @@ class IWLWriter {
 **/
 class MintsHelper {
 
-    private:
-        /// The Options reference for basis sets and things
-        Options& options_;
-        /// Uniqueness forcer until SOShellCombinationsIterator is complete
-        static int determine_unique_shell_quartets(int usii, int usjj, int uskk, int usll,
-                                           int* usi_arr,
-                                           int* usj_arr,
-                                           int* usk_arr,
-                                           int* usl_arr);
-    public:
-        /// Constructor, just lines references up
-        MintsHelper(Options&);
-        /// Constructor, uses globals
-        MintsHelper();
-        /// Destructor, does nothing
-        ~MintsHelper();
-        /// Molecular integrals (just like cints used to do)
-        void integrals();
-        /// One electron integrals (just like oeints used to do)
-        void one_electron_integrals();
-        /// Derivative integrals (not implemented)
-        void integral_gradients();
-        /// Hessian integrals (not implemented)
-        void integral_hessians();
-        /// AO Overlap Integrals
-        shared_ptr<Matrix> ao_overlap();
-        /// AO Kinetic Integrals
-        shared_ptr<Matrix> ao_kinetic();
-        /// AO Potential Integrals
-        shared_ptr<Matrix> ao_potential();
-        /// AO ERI Integrals (Full matrix, not recommended for large systems)
-        shared_ptr<Matrix> ao_eri();
-        /// AO ERI Omega Integrals (Full matrix, not recommended for large systems)
-        shared_ptr<Matrix> ao_erf_eri(double omega, double alpha, double beta);
+private:
+    /// The Options reference for basis sets and things
+    Options& options_;
+    /// Uniqueness forcer until SOShellCombinationsIterator is complete
+    static int determine_unique_shell_quartets(int usii, int usjj, int uskk, int usll,
+                                               int* usi_arr,
+                                               int* usj_arr,
+                                               int* usk_arr,
+                                               int* usl_arr);
+public:
+    /// Constructor, just lines references up
+    MintsHelper(Options&);
+    /// Constructor, uses globals
+    MintsHelper();
+    /// Destructor, does nothing
+    ~MintsHelper();
+    /// Molecular integrals (just like cints used to do)
+    void integrals();
+    /// One electron integrals (just like oeints used to do)
+    void one_electron_integrals();
+    /// Derivative integrals (not implemented)
+    void integral_gradients();
+    /// Hessian integrals (not implemented)
+    void integral_hessians();
+    /// AO Overlap Integrals
+    shared_ptr<Matrix> ao_overlap();
+    /// AO Kinetic Integrals
+    shared_ptr<Matrix> ao_kinetic();
+    /// AO Potential Integrals
+    shared_ptr<Matrix> ao_potential();
+    /// AO ERI Integrals (Full matrix, not recommended for large systems)
+    shared_ptr<Matrix> ao_eri();
+    /// AO ERI Omega Integrals (Full matrix, not recommended for large systems)
+    shared_ptr<Matrix> ao_erf_eri(double omega, double alpha, double beta);
 
-        /// AO Overlap Integrals
-        shared_ptr<Matrix> so_overlap();
-        /// AO Kinetic Integrals
-        shared_ptr<Matrix> so_kinetic();
-        /// AO Potential Integrals
-        shared_ptr<Matrix> so_potential();
+    /// AO Overlap Integrals
+    shared_ptr<Matrix> so_overlap();
+    /// AO Kinetic Integrals
+    shared_ptr<Matrix> so_kinetic();
+    /// AO Potential Integrals
+    shared_ptr<Matrix> so_potential();
 };
 
 }
