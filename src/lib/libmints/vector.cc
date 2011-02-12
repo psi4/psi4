300c1556fbd9b98d9ab5ed821b8933f4f446495c
rparrish <rparrish@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-02-12 21:55:13 +0000

    Added names to Vector and IntVector
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1247 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 2ac3a07026bbacd92acffece0dcad513ff78da41

--- src/lib/libmints/vector.cc
+++ src/lib/libmints/vector.cc
@@ -10,6 +10,7 @@ Vector::Vector() {
     vector_ = NULL;
     dimpi_ = NULL;
     nirrep_ = 0;
+    name_ = "";
 }
 
 Vector::Vector(const Vector& c) {
@@ -20,6 +21,7 @@ Vector::Vector(const Vector& c) {
         dimpi_[h] = c.dimpi_[h];
     alloc();
     copy_from(c.vector_);
+    name_ = c.name_;
 }
 
 Vector::Vector(int nirreps, int *dimpi) {
@@ -36,6 +38,23 @@ Vector::Vector(int dim) {
     dimpi_ = new int[nirrep_];
     dimpi_[0] = dim;
     alloc();
+}
+Vector::Vector(const std::string& name, int nirreps, int *dimpi) {
+    vector_ = NULL;
+    nirrep_ = nirreps;
+    dimpi_ = new int[nirrep_];
+    for (int h=0; h<nirrep_; ++h)
+        dimpi_[h] = dimpi[h];
+    alloc();
+    name_ = name;
+}
+Vector::Vector(const std::string& name, int dim) {
+    vector_ = NULL;
+    nirrep_ = 1;
+    dimpi_ = new int[nirrep_];
+    dimpi_[0] = dim;
+    alloc();
+    name = name_;
 }
 
 Vector::~Vector() {
@@ -126,6 +145,7 @@ void Vector::set(double *vec) {
 
 void Vector::print(FILE *out) {
     int h;
+    fprintf(out, "\n # %s #\n", name_.c_str());
     for (h=0; h<nirrep_; ++h) {
         fprintf(out, " Irrep: %d\n", h+1);
         for (int i=0; i<dimpi_[h]; ++i)
