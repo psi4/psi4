48e565610f17e57cf086a6909542522ec06fd973
rparrish <rparrish@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-02-13 03:39:24 +0000

    CC-DF stuff
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1260 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: db9c275d95a894f1bb805fce8d1f53fa4f606569

--- src/lib/libdfcc_solver/mp2.cc
+++ src/lib/libdfcc_solver/mp2.cc
@@ -21,6 +21,18 @@ double MP2::compute_energy()
 
 void MP2::print_header()
 {
+    fprintf(outfile, "\t********************************************************\n");
+    fprintf(outfile, "\t*                                                      *\n");
+    fprintf(outfile, "\t*                        DF-MP2                        *\n");
+    fprintf(outfile, "\t*    2nd-Order Density-Fitted Moller-Plesset Theory    *\n");
+    fprintf(outfile, "\t*        with Laplace and Pseudospectral Grids         *\n");
+    fprintf(outfile, "\t*                                                      *\n");
+    fprintf(outfile, "\t*            Rob Parrish and Ed Hohenstein             *\n");
+    fprintf(outfile, "\t*                                                      *\n");
+    fprintf(outfile, "\t********************************************************\n");
+
+    //CC::print_header();
+            
 }
 
 }}
