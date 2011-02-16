ddc4f70f188dedd26bd0ba4d928edcaa6bc2e637
andysim <andysim@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-02-16 20:26:47 +0000

    New and improved parser.  More flexible input syntax, soon to be documented on the Pswiki...
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1280 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- lib/python/input.py
+++ lib/python/input.py
@@ -8,59 +8,67 @@ def bad_option_syntax(line):
     print 'Unsupported syntax:\n\n%s\n\n' % (line)
     exit()
 
-def process_option(matchobj, option_string, line):
-    spaces = matchobj.group(1)
-    key   = matchobj.group(2).upper()
-    value = matchobj.group(3).strip()
-    temp = ""
-    if re.match(r'^[-]?\d*\.?\d+$', value) or re.match(r'^\[.*\]$', value):
+def process_option(spaces, module, key, value, line):
+    key    = key.upper()
+    value  = value.strip()
+    temp   = ""
+ 
+    global_options = False 
+    module = module.upper()
+    if(module == "GLOBALS" or module == "GLOBAL" or module == "" or module.isspace()):
+        global_options = True
+    
+    # Start by assuming that we're setting a local option
+    command_string = "PsiMod.set_local_option(\"%s\", \"%s\", " % (module, key)
+    if(global_options):
+        # If it's really a global, we need slightly different syntax
+        command_string = "PsiMod.set_global_option(\"%s\", " % (key)
+
+    if re.match(r'^[-]?\d*\.?\d+$', value) or re.match(r'^\[.*\]$', value.upper()):
         # This is a number
-        temp ='PsiMod.%s("%s", %s)' % (option_string, key, value)
+        temp ='%s %s)' % (command_string, value)
     elif re.match(r'^\$', value):
         # Assume the user has set the variable in value
-        temp = 'PsiMod.%s("%s", %s)' % (option_string, key, value[1:])
+        temp = '%s %s)' % (command_string, value[1:])
     else:
         # This must contain only alphanumeric (plus -,* for basis sets) or it's bad
         if re.match(r'^[-*\w]+$', value):
-            temp = 'PsiMod.%s("%s", "%s")' % (option_string, key, value)
+            temp = '%s "%s")' % (command_string, value.upper())
         else:
             bad_option_syntax(line)
     return spaces + temp + "\n"
 
-def process_global_command(matchobj):
-    return process_option(matchobj, "set_global_option", matchobj.group(0))
-
 def process_set_command(matchobj):
-    return process_option(matchobj, "set_option", matchobj.group(0)) 
+    result = ""
+    module_string = ""
+    if(matchobj.group(2)):
+        module_string = matchobj.group(2)
+    for module in module_string.split(","):
+        result = result + process_option(matchobj.group(1), module, matchobj.group(3), matchobj.group(4), matchobj.group(0))
+    return result
 
-def process_options(matchobj, command_string):
+def process_set_commands(matchobj):
     spaces = matchobj.group(1)
-    module = matchobj.group(2)
     commands = matchobj.group(3)
     command_lines = re.split('\n', commands)
     map(lambda x: x.strip(), command_lines)
-    set_command = "set_global_option"
-    result = []
-    if module != "":
-        set_command = "set_option"
-        result.append('%sPsiMod.set_default_options_for_module("%s")\n' % (spaces, module.upper()))
-    for line in command_lines:
-        # Ignore blank/empty lines
-        if (not line or line.isspace()):
-            continue
-        matchobj = re.match(r'^\s*()(\w+)[\s=]+(.*?)$', line)
-        # Is the syntax correct? If so, process the line 
-        if matchobj:
-            result.append(process_option(matchobj, command_string,  line))
-        else:
-            bad_option_syntax(line)
-    return (spaces).join(result)
+    result = ""
+    module_string = ""
+    if(matchobj.group(2)):
+        module_string = matchobj.group(2)
+    for module in module_string.split(","):
+        for line in command_lines:
+            # Ignore blank/empty lines
+            if (not line or line.isspace()):
+                continue
+            matchobj = re.match(r'^\s*(\w+)[\s=]+(.*?)$', line)
+            # Is the syntax correct? If so, process the line 
+            if matchobj:
+                result = result + process_option(spaces, module, matchobj.group(1), matchobj.group(2), line)
+            else:
+                bad_option_syntax(line)
+    return result
 
-def process_global_commands(matchobj):
-    return process_options(matchobj, "set_global_option")
-
-def process_set_commands(matchobj):
-    return process_options(matchobj, "set_option")
 
 def process_molecule_command(matchobj):
     spaces = matchobj.group(1)
@@ -137,30 +145,21 @@ def process_input(raw_input):
     blankline = re.compile(r'^\s*$')
     temp = re.sub(blankline, '', temp, re.MULTILINE)
  
-    # Then remove repeated newlines
-    multiplenewlines = re.compile(r'\n+')
-    temp = re.sub(multiplenewlines, '\n', temp)
-
     # Process all "set name? { ... }"
-    #set_commands = re.compile(r'^(\s*?)set\s*(\w*?)\s*\{(.*?)\}', re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    set_commands = re.compile(r'^(\s*?)set\s*([\w\.]*?)\s*\{(.*?)\}', re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    set_commands = re.compile(r'^(\s*?)set\s+([-,\w]*?)[\s=]*\{(.*?)\}', re.MULTILINE | re.DOTALL | re.IGNORECASE)
     temp = re.sub(set_commands, process_set_commands, temp)
 
-    # Process all individual "set key value"
-    set_command = re.compile(r'(\s*?)set\s+(\w+)[\s=]+(.*?)($|#.*)', re.MULTILINE | re.IGNORECASE)
+    # Process all individual "set (module_list) key  {[value_list] or $value or value}"
+    set_command = re.compile(r'^(\s*?)set\s+(?:([-,\w]+)\s+)?(\w+)[\s=]+((\[.*\])|(\$?[-\.\w]+))\s*$', re.MULTILINE | re.IGNORECASE)
     temp = re.sub(set_command, process_set_command, temp)
-
-    # Process all "global(s) { ... }"
-    global_commands = re.compile(r'^(\s*?)()globals?\s*\{(.*?)\}', re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    temp = re.sub(global_commands, process_global_commands, temp)
-
-    # Process all individual "global key value"
-    global_command = re.compile(r'^(\s*?)globals?\s+(\w+)[\s=]+(.*?)(\s*)$', re.MULTILINE | re.IGNORECASE)
-    temp = re.sub(global_command, process_global_command, temp)
 
     # Process "molecule name? { ... }"
     molecule = re.compile(r'^(\s*?)molecule\s*(\w*?)\s*\{(.*?)\}', re.MULTILINE | re.DOTALL | re.IGNORECASE)
     temp = re.sub(molecule, process_molecule_command, temp)
+
+    # Then remove repeated newlines
+    multiplenewlines = re.compile(r'\n+')
+    temp = re.sub(multiplenewlines, '\n', temp)
 
     # Process " extract"
     extract = re.compile(r'(\s*?)(\w+)\s*=\s*\w+\.extract_subsets.*', re.IGNORECASE)
@@ -207,23 +206,26 @@ R = .9
 }
 
 #this is a comment
-globals {
+set globals {
     RI_BASIS_SCF STO-3G
 }
 
-global ncycles = 5
-
-set scf {
-    SCF_TYPE DF # test this too
+set scf,ccsd  = {
+    print 1
     DOCC [3, 0, 1, 1]
     DIIS on
 }
 
-    globals freeze_core true
+    set globals freeze_core true
+    set global freeze_core = true
 
-set docc =  [2, 0, 1, 1]
-set docc   [2, 0, 1, 1]
-    set docc   [2, 0, 1, 1]
+    set global  docc   [2, 0, 1, 1]
+    set  ss  = 3.0
+    set  ss   3.0
+    set scf socc  [23]
+    set scf,ccsd docc  [34,43]
+    set dostuff   1
+    set globals,ccsd do_more_stuff $foo
 
     set mp2 {
         print  5
