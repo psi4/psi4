99dbbf3aaf4cbea8cfdc7a15507d9c2406167064
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-03-08 19:10:11 +0000

    code cleanup
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1494 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 3b7c33fc4a95b7e234b8753918a242ac134c4253

--- src/lib/libscf_solver/rohf.cc
+++ src/lib/libscf_solver/rohf.cc
@@ -59,21 +59,11 @@ void ROHF::common_init()
     epsilon_a_ = SharedVector(factory_->create_vector());
     epsilon_b_ = epsilon_a_;
 
-//    pk_ = NULL;
-//    k_ = NULL;
-
     fprintf(outfile, "  DIIS %s.\n\n", diis_enabled_ ? "enabled" : "disabled");
-
-//    if (scf_type_ == "PK")
-//        allocate_PK();
 }
 
 void ROHF::finalize()
 {
-//    if (pk_)
-//        delete[](pk_);
-//    if (k_)
-//        delete[](k_);
     Fc_.reset();
     Fo_.reset();
     Dc_old_.reset();
@@ -277,44 +267,6 @@ bool ROHF::test_convergency()
         return false;
 }
 
-#if 0
-void ROHF::allocate_PK()
-{
-    // Figure out how many pair combinations yield A1 symmetry (done in above loop)
-    //   num_pair_combinations_of_A1 = ioff[_opi[0]] + ioff[_opi[1]] + ioff[_opi[2]] + ...
-    // Allocate memory for the PK matrix (using a vector)
-    if (pk_size_ < (memory_ / sizeof(double) / 2)) {
-        pk_ = new double[pk_size_];
-        k_ = new double[pk_size_];
-
-        if (pk_ == NULL || k_ == NULL) {
-            fprintf(outfile, "  Insufficient free system memory for in-core PK implementation.\n");
-            fprintf(outfile, "  Switching to out-of-core algorithm.\n");
-            scf_type_ = "OUT_OF_CORE";
-        } else {
-            // Zero out PK and K
-            memset(pk_, 0, pk_size_*sizeof(double));
-            memset(k_, 0, pk_size_*sizeof(double));
-
-            fprintf(outfile,
-                "  Allocated %lu elements (%lu pairs) for PK. (%5f MiB)\n",
-                (unsigned long)pk_size_, (unsigned long)pk_pairs_, pk_size_ * 8.0 / 1048576.0);
-            fprintf(outfile,
-                "  Allocated %lu elements (%lu pairs) for K.  (%5f MiB)\n\n",
-                (unsigned long)pk_size_, (unsigned long)pk_pairs_, pk_size_ * 8.0 / 1048576.0);
-        }
-    } else {
-        fprintf(outfile,
-                "  Insufficient memory for in-core PK implementation.\n");
-        fprintf(outfile,
-                "  Would need %lu elements of double memory. (%5f MiB)\n",
-                (unsigned long)pk_size_*2, pk_size_ * 8.0 / 1048576.0 * 2.0);
-        fprintf(outfile, "  Switching to out-of-core algorithm.\n");
-        scf_type_ = "OUT_OF_CORE";
-    }
-}
-
-#endif
 void ROHF::form_initialF()
 {
     // Form the initial Fock matrix, closed and open variants
@@ -382,7 +334,7 @@ void ROHF::form_F()
             }
         }
     }
-#ifdef _DEBUG
+
     if (debug_) {
         Fc_->print(outfile);
         Fo_->print(outfile);
@@ -390,7 +342,6 @@ void ROHF::form_F()
         Fot->print(outfile);
         Feff_->print(outfile);
     }
-#endif
 }
 
 void ROHF::form_C()
@@ -469,123 +420,8 @@ double ROHF::compute_E() {
     return Etotal;
 }
 
-#if 0
-void ROHF::form_PK() {
-    // struct iwlbuf ERIIN;
-    int ilsti, nbuf;
-    int i, j, k, l;
-    int ii, jj, kk, ll;
-    int is, js, ks, ls;
-    int fi;
-    size_t bra, ket, braket;
-    int idx;
-    int counter = 0;
-    double value;
-
-    // PK zeroed out during allocation
-    fprintf(outfile, "  Forming PK and K matrices.\n");
-    fflush(outfile);
-
-    IWL ERIIN(psio_.get(), PSIF_SO_TEI, 0.0, 1, 1);
-
-    do {
-        ilsti = ERIIN.last_buffer();
-        nbuf = ERIIN.buffer_count();
-
-        fi = 0;
-        for (idx=0; idx<nbuf; ++idx) {
-            i = ERIIN.labels()[fi] > 0 ? ERIIN.labels()[fi] : -ERIIN.labels()[fi];
-            j = ERIIN.labels()[fi+1];
-            k = ERIIN.labels()[fi+2];
-            l = ERIIN.labels()[fi+3];
-            value = ERIIN.values()[idx];
-            fi += 4;
-
-            // Get the symmetries
-            is = so2symblk_[i];
-            js = so2symblk_[j];
-            ks = so2symblk_[k];
-            ls = so2symblk_[l];
-
-            // Get the offset of the SO index in its symblock
-            ii = so2index_[i];
-            jj = so2index_[j];
-            kk = so2index_[k];
-            ll = so2index_[l];
-
-            // J
-            if ((is == js) && (ks == ls)) {
-                bra = INDEX2(ii, jj);
-                ket = INDEX2(kk, ll);
-                // pk_symoffset_ corrects for the symmetry offset in the pk_ vector
-                braket = INDEX2(bra + pk_symoffset_[is], ket + pk_symoffset_[ks]);
-                pk_[braket] += value;
-
-                // K/2 (2nd sort)
-                if ((ii != jj) && (kk != ll)) {
-                    if ((is == ls) && (js == ks)) {
-                        bra = INDEX2(ii, ll);
-                        ket = INDEX2(jj, kk);
-                        braket = INDEX2(bra + pk_symoffset_[is], ket + pk_symoffset_[js]);
-                        if ((ii == ll) || (jj == kk)) {
-                            pk_[braket] -= 0.5 * value;
-                            k_[braket] -= 0.5 * value;
-                        } else {
-                            pk_[braket] -= 0.25 * value;
-                            k_[braket] -= 0.25 * value;
-                        }
-                    }
-                }
-            }
-
-            // K/2 (1st sort)
-            if ((is == ks) && (js == ls)) {
-                bra = INDEX2(ii, kk);
-                ket = INDEX2(jj, ll);
-                braket = INDEX2(bra + pk_symoffset_[is], ket + pk_symoffset_[js]);
-                if ((ii == kk) || (jj == ll)) {
-                    pk_[braket] -= 0.5 * value;
-                    k_[braket] -= 0.5 * value;
-                } else {
-                    pk_[braket] -= 0.25 * value;
-                    k_[braket] -= 0.25 * value;
-                }
-            }
-            counter++;
-        }
-
-        if (!ilsti)
-            ERIIN.fetch();
-    } while (!ilsti);
-
-    // Going out of scope will close the buffer
-    // iwl_buf_close(&ERIIN, 1);
-
-    // After stage two is complete, the elements of P must be halved for the case IJ=KL.
-    for (size_t ij=0; ij < pk_pairs_; ++ij) {
-        pk_[INDEX2(ij,ij)] *= 0.5;
-        k_[INDEX2(ij,ij)] *= 0.5;
-    }
-
-    fprintf(outfile, "  Processed %d two-electron integrals.\n", counter);
-#ifdef _DEBUG
-    if (debug_) {
-        fprintf(outfile, "pk_:\n");
-        print_array(pk_, pk_pairs_, outfile);
-        fprintf(outfile, "k_:\n");
-        print_array(k_, pk_pairs_, outfile);
-    }
-#endif
-}
-
-#endif
 void ROHF::form_G()
 {
-//    if (scf_type_ == "PK") {
-//        form_G_from_PK();
-//        return;
-//    }
-
     /*
      * If we define
      *
@@ -633,133 +469,5 @@ void ROHF::form_G()
     Go_->subtract(Ko_);
 
 }
-
-#if 0
-void ROHF::form_G_from_PK()
-{
-    int nirreps = factory_->nirrep();
-    int *opi = factory_->rowspi();
-    size_t ij;
-    double *Do_vector = new double[pk_pairs_];
-    double *Dc_vector = new double[pk_pairs_];
-    double *Gc_vector = new double[pk_pairs_];
-    double *Go_vector = new double[pk_pairs_];
-
-    Gc_->zero();
-    Go_->zero();
-
-    memset(Do_vector, 0, sizeof(double) * pk_pairs_);
-    memset(Dc_vector, 0, sizeof(double) * pk_pairs_);
-    memset(Gc_vector, 0, sizeof(double) * pk_pairs_);
-    memset(Go_vector, 0, sizeof(double) * pk_pairs_);
-
-    ij=0;
-    for (int h=0; h<nirreps; ++h) {
-        for (int p=0; p<opi[h]; ++p) {
-            for (int q=0; q<=p; ++q) {
-                if (p != q) {
-                    Dc_vector[ij] = 2.0 * Dc_->get(h, p, q);
-                    Do_vector[ij] = 2.0 * Do_->get(h, p, q);
-                } else {
-                    Dc_vector[ij] = Dc_->get(h, p, q);
-                    Do_vector[ij] = Do_->get(h, p, q);
-                }
-                ij++;
-            }
-        }
-    }
-
-#ifdef _DEBUG
-    if (debug_) {
-        fprintf(outfile, "PK: ij = %lu\n", (unsigned long)ij);
-        fflush(outfile);
-        Dc_->print(outfile);
-        fprintf(outfile, "PK: Dc vector:\n");
-        for (ij=0; ij<pk_pairs_; ++ij)
-            fprintf(outfile, "PK: Dc vector [%lu] = %20.14f\n", (unsigned long)ij, Dc_vector[ij]);
-        Do_->print(outfile);
-        fprintf(outfile, "PK: Do vector:\n");
-        for (ij=0; ij<pk_pairs_; ++ij)
-            fprintf(outfile, "PK: Do vector [%lu] = %20.14f\n", (unsigned long)ij, Do_vector[ij]);
-    }
-#endif
-
-    /*
-     * This code goes through the densities (Dc_ and Do_), PK, and K to form
-     * two G matrices. One G matrix is for Fc_ and the other for Fo_.
-     * See derivation notebook for equations.
-     */
-    double Gc_pq, Dc_pq;
-    double Go_pq, Do_pq;
-    double* Dc_rs;
-    double* Gc_rs;
-    double* Do_rs;
-    double* Go_rs;
-    int pq, rs;
-    double* PK_block = pk_;
-    double* K_block = k_;
-    int ts_pairs = pk_pairs_;
-    for (pq = 0; pq < ts_pairs; ++pq) {
-        Gc_pq = 0.0;
-        Dc_pq = Dc_vector[pq];
-        Dc_rs = &Dc_vector[0];
-        Gc_rs = &Gc_vector[0];
-        Go_pq = 0.0;
-        Do_pq = Do_vector[pq];
-        Do_rs = &Do_vector[0];
-        Go_rs = &Go_vector[0];
-        for (rs = 0; rs <= pq; ++rs) {
-            // D_{rs}^{c} * PK_{pqrs}         Also found in RHF
-            Gc_pq  += *PK_block * (*Dc_rs);
-            *Gc_rs += *PK_block * Dc_pq;
-            // D_{rs}^{o} * PK_{pqrs} / 2     Yes, open D adds to closed G
-            Gc_pq  += *PK_block * (*Do_rs) * 0.5;
-            *Gc_rs += *PK_block * Do_pq    * 0.5;
-            // D_{rs}^{c} * PK_{pqrs} / 2     Yes, closed D adds to open G
-            Go_pq  += *PK_block * (*Dc_rs) * 0.5;
-            *Go_rs += *PK_block * Dc_pq    * 0.5;
-            // D_{rs}^{o} * (PK_{pqrs} + K_{pqrs}) / 4
-            Go_pq  += (*PK_block + *K_block) * (*Do_rs) * 0.25;
-            *Go_rs += (*PK_block + *K_block) * Do_pq    * 0.25;
-            ++Dc_rs;
-            ++Gc_rs;
-            ++Do_rs;
-            ++Go_rs;
-            ++PK_block;
-            ++K_block;
-        }
-        Gc_vector[pq] += Gc_pq;
-        Go_vector[pq] += Go_pq;
-    }
-
-    // Convert G to a matrix
-    ij = 0;
-    for (int h = 0; h < nirreps; ++h) {
-        for (int p = 0; p < opi[h]; ++p) {
-            for (int q = 0; q <= p; ++q) {
-                Gc_->set(h, p, q, 2.0 * Gc_vector[ij]);
-                Gc_->set(h, q, p, 2.0 * Gc_vector[ij]);
-                Go_->set(h, p, q, 2.0 * Go_vector[ij]);
-                Go_->set(h, q, p, 2.0 * Go_vector[ij]);
-                ij++;
-            }
-        }
-    }
-
-#ifdef _DEBUG
-    if (debug_) {
-        fprintf(outfile, "Gc from PK:\n");
-        Gc_->print(outfile);
-        fprintf(outfile, "Go from PK:\n");
-        Go_->print(outfile);
-    }
-#endif
-
-    delete[](Dc_vector);
-    delete[](Do_vector);
-    delete[](Gc_vector);
-    delete[](Go_vector);
-}
-#endif
 
 }}
