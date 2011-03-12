193834ee1e72ebc0bf65cee39f8d78228b36fd52
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-03-12 14:06:11 +0000

    nabla integrals added, not tested
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1536 6a214465-e6f0-41bc-91e6-de896bb96b5e

---
+++ src/lib/libmints/nabla.h
@@ -0,0 +1,43 @@
+#ifndef _psi_src_lib_libmints_nabla_h_
+#define _psi_src_lib_libmints_nabla_h_
+
+#include <boost/shared_ptr.hpp>
+
+namespace psi {
+
+    class BasisSet;
+    class GaussianShell;
+    class ObaraSaikaTwoCenterRecursion;
+    class OneBodyAOInt;
+    class IntegralFactory;
+    class SphericalTransform;
+    class SimpleMatrix;
+
+/*! \ingroup MINTS
+ *  \class DipoleInt
+ *  \brief Computes dipole integrals.
+ *
+ * Use an IntegralFactory to create this object. */
+class NablaInt : public OneBodyAOInt
+{
+    //! Obara and Saika recursion object to be used.
+    ObaraSaikaTwoCenterRecursion overlap_recur_;
+
+    //! Computes the dipole between two gaussian shells.
+    void compute_pair(const boost::shared_ptr<GaussianShell>&, const boost::shared_ptr<GaussianShell>&);
+    //! Computes the dipole derivative between two gaussian shells.
+//    void compute_pair_deriv1(const boost::shared_ptr<GaussianShell>&, const boost::shared_ptr<GaussianShell>&);
+
+public:
+    //! Constructor. Do not call directly use an IntegralFactory.
+    NablaInt(std::vector<SphericalTransform>&, boost::shared_ptr<BasisSet>, boost::shared_ptr<BasisSet>, int deriv=0);
+    //! Virtual destructor
+    virtual ~NablaInt();
+
+    //! Does the method provide first derivatives?
+    bool has_deriv1() { return true; }
+};
+
+}
+
+#endif
