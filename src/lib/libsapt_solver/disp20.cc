0a7bd7314fb93898589ffe431b2f68df09cb2f6b
hohenstein <hohenstein@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-03-14 04:06:39 +0000

    Added Laplace transformed dispersion to SAPT code
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1572 6a214465-e6f0-41bc-91e6-de896bb96b5e

---
+++ src/lib/libsapt_solver/disp20.cc
@@ -0,0 +1,28 @@
+#include "sapt0.h"
+
+namespace psi { namespace sapt {
+
+void SAPT0::disp20()
+{
+  shared_ptr<Vector> evals_aoccA(new Vector(aoccA_));
+  shared_ptr<Vector> evals_virA(new Vector(nvirA_));
+  shared_ptr<Vector> evals_aoccB(new Vector(aoccB_));
+  shared_ptr<Vector> evals_virB(new Vector(nvirB_));
+
+  for (int a=0; a<aoccA_; a++)
+    evals_aoccA->set(0,a,evalsA_[a+foccA_]);
+  for (int r=0; r<nvirA_; r++)
+    evals_virA->set(0,r,evalsA_[r+noccA_]);
+  for (int b=0; b<aoccB_; b++)
+    evals_aoccB->set(0,b,evalsB_[b+foccB_]);
+  for (int s=0; s<nvirB_; s++)
+    evals_virB->set(0,s,evalsB_[s+noccB_]);
+
+  denom_ = shared_ptr<SAPTLaplaceDenominator>(new SAPTLaplaceDenominator(
+    evals_aoccA,evals_virA,evals_aoccB,evals_virB,
+    options_.get_double("DENOMINATOR_DELTA"),debug_));
+
+  denom_->debug();
+}
+
+}}
