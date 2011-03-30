8a6391bcc3cbb1d569ffb809b583d146a6633d20
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-03-30 11:39:16 +0000

    my compiler doesn't like this
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1656 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libsapt_solver/sapt.cc
+++ src/lib/libsapt_solver/sapt.cc
@@ -59,7 +59,7 @@ void SAPT::initialize()
   // TODO: Verify that the basis assign does not messs this up
   if (options_.get_str("RI_BASIS_SAPT") == "") {
       basisset_->molecule()->set_basis_all_atoms(options_.get_str("BASIS") + "-RI", "RI_BASIS_SAPT");
-      fprintf(outfile, ("    No auxiliary basis selected, defaulting to " + options_.get_str("BASIS") + "-RI\n\n").c_str()); 
+      fprintf(outfile, "    No auxiliary basis selected, defaulting to %s-RI\n\n", options_.get_str("BASIS").c_str()); 
   }
 
   ribasis_ = shared_ptr<BasisSet>(BasisSet::construct(parser, molecule_, 
