psi4/psi4

commit 6826520ecb3338721771ed68431b50211b67fcbf

Some printing/summary for MP2C
git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1707 6a214465-e6f0-41bc-91e6-de896bb96b5e

Former-commit-id: a31ce4bc0e51d0c9342c9ef359be14c5794fb0f4

src/bin/psi4/python.cc
@@ -446,6 +446,13 @@ double py_psi_get_variable(const std::string & key)
     return Process::environment.globals[uppercase_key];
 }
 
+void py_psi_set_variable(const std::string & key, double val)
+{
+    string uppercase_key = key;
+    transform(uppercase_key.begin(), uppercase_key.end(), uppercase_key.begin(), ::toupper);
+    Process::environment.globals[uppercase_key] = val;
+}
+
 void py_psi_set_memory(unsigned long int mem)
 {
     Process::environment.set_memory(mem);
@@ -541,7 +548,8 @@ BOOST_PYTHON_MODULE(PsiMod)
     def("get_option", py_psi_get_option);
     def("get_global_option", py_psi_get_global_option);
 
-    def("get_variable", py_psi_get_variable);
+    def("set_variable", py_psi_set_variable);
+    def("set_variable", py_psi_set_variable);
     def("print_variables", py_psi_print_variable_map);
 
     def("add_user_basis_file", py_psi_add_user_specified_basis_file);

lib/python/driver.py
@@ -8,7 +8,7 @@ procedures = {'energy' : {
             'dfmp2'         : run_dfmp2,
             'dfcc'          : run_dfcc,
             'sapt0'         : run_sapt,
-            'sapt_dft'      : run_sapt,
+            'mp2c'          : run_mp2c,
             'ccsd'          : run_ccsd,
             'ccsd(t)'       : run_ccsd_t,
             'detci'         : run_detci

lib/python/proc.py
@@ -242,6 +242,85 @@ def run_dfcc(name, **kwargs):
     e_dfcc = PsiMod.dfcc()
     return e_dfcc
 
+def run_mp2c(name, **kwargs):
+
+    molecule = PsiMod.get_active_molecule()
+    if (kwargs.has_key('molecule')):
+        molecule = kwargs.pop('molecule')
+
+    if not molecule:
+        raise ValueNotSet("no molecule found")
+
+    molecule.update_geometry()
+    monomerA = molecule.extract_subsets(1,2)
+    monomerA.set_name("monomerA")
+    monomerB = molecule.extract_subsets(2,1)
+    monomerB.set_name("monomerB")
+
+    ri = PsiMod.get_option('SCF_TYPE')
+    ri_ints_io = PsiMod.get_option('RI_INTS_IO')
+
+    PsiMod.IO.set_default_namespace("dimer")
+    PsiMod.set_local_option("SCF","SAPT","2-dimer")
+    PsiMod.print_out("\n")
+    banner('Dimer HF')
+    PsiMod.print_out("\n")
+    PsiMod.set_global_option('RI_INTS_IO','SAVE')
+    e_dimer = scf_helper('RHF',**kwargs)
+    PsiMod.print_out("\n")
+    banner('Dimer DFMP2')
+    PsiMod.print_out("\n")
+    e_dimer_mp2 = PsiMod.dfmp2()
+    PsiMod.set_global_option('RI_INTS_IO','LOAD')
+
+    activate(monomerA)
+    if (ri == "DF"):
+        PsiMod.IO.change_file_namespace(97,"dimer","monomerA")
+    PsiMod.IO.set_default_namespace("monomerA")
+    PsiMod.set_local_option("SCF","SAPT","2-monomer_A")
+    PsiMod.print_out("\n")
+    banner('Monomer A HF')
+    PsiMod.print_out("\n")
+    e_monomerA = scf_helper('RHF',**kwargs)
+    PsiMod.print_out("\n")
+    banner('Monomer A DFMP2')
+    PsiMod.print_out("\n")
+    e_monomerA_mp2 = PsiMod.dfmp2()
+
+    activate(monomerB)
+    if (ri == "DF"):
+        PsiMod.IO.change_file_namespace(97,"monomerA","monomerB")
+    PsiMod.IO.set_default_namespace("monomerB")
+    PsiMod.set_local_option("SCF","SAPT","2-monomer_B")
+    PsiMod.print_out("\n")
+    banner('Monomer B HF')
+    PsiMod.print_out("\n")
+    e_monomerB = scf_helper('RHF',**kwargs)
+    PsiMod.print_out("\n")
+    banner('Monomer B DFMP2')
+    PsiMod.print_out("\n")
+    e_monomerB_mp2 = PsiMod.dfmp2()
+    PsiMod.set_global_option('RI_INTS_IO',ri_ints_io)
+
+    PsiMod.IO.change_file_namespace(121,"monomerA","dimer")
+    PsiMod.IO.change_file_namespace(122,"monomerB","dimer")
+
+    activate(molecule)
+    PsiMod.IO.set_default_namespace("dimer")
+    PsiMod.set_local_option("SAPT","E_CONVERGE",10)
+    PsiMod.set_local_option("SAPT","D_CONVERGE",10)
+    PsiMod.set_local_option("SAPT","SAPT_LEVEL","MP2C")
+    PsiMod.print_out("\n")
+    banner("MP2C")
+    PsiMod.print_out("\n")
+
+    PsiMod.set_variable("MP2C DIMER MP2 ENERGY",e_dimer_mp2)
+    PsiMod.set_variable("MP2C MONOMER A MP2 ENERGY",e_monomerA_mp2)
+    PsiMod.set_variable("MP2C MONOMER B MP2 ENERGY",e_monomerB_mp2)
+    
+    e_sapt = PsiMod.sapt()
+    return e_sapt
+
 def run_sapt(name, **kwargs):
 
     molecule = PsiMod.get_active_molecule()

src/bin/psi4/read_options.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-07adfd8de31d78e98b4a055015acbfbcec999948
+3fc79fdfe98fa4c5dca41213f75685ff42dcaeb4

src/bin/sapt/wrapper.cc
@@ -25,10 +25,13 @@ PsiReturnType sapt(Options & options)
   if (options.get_str("SAPT_LEVEL") == "SAPT0") {
     SAPT0 sapt(options, psio, chkpt);
     sapt.compute_energy();
-  } else if (options.get_str("SAPT_LEVEL") == "SAPT_DFT") {
-    SAPT_DFT sapt(options, psio, chkpt);
+  } else if (options.get_str("SAPT_LEVEL") == "MP2C") {
+    MP2C sapt(options, psio, chkpt);
     sapt.compute_energy();
+  } else {
+    throw PSIEXCEPTION("Unrecognized SAPT type");
   }
+
 
   // Shut down psi.
   timer_done();

src/lib/libsapt_solver/sapt0.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-2386eeb13b9f8ef9a9a8e925c89fc102a7cff2c7
+28a672950c6ec20d71262a4dcd8e41927f8749fe

src/lib/libsapt_solver/sapt_dft.cc
@@ -5,17 +5,19 @@ using namespace boost;
 
 namespace psi { namespace sapt {
 
-SAPT_DFT::SAPT_DFT(Options& options, shared_ptr<PSIO> psio, shared_ptr<Chkpt> chkpt)
+MP2C::MP2C(Options& options, shared_ptr<PSIO> psio, shared_ptr<Chkpt> chkpt)
     : SAPT0(options, psio, chkpt)
 {
 }
 
-SAPT_DFT::~SAPT_DFT()
+MP2C::~MP2C()
 {
 }
 
-double SAPT_DFT::compute_energy()
+double MP2C::compute_energy()
 {
+    print_header();
+
     form_quadrature();
     form_J();
     form_W();
@@ -35,6 +37,10 @@ double SAPT_DFT::compute_energy()
     double E_UCHF = 0.0;
     double E_TDDFT = 0.0;
     double E_DeltaMP2C = 0.0;
+    double E_MP2_int = Process::environment.globals["MP2C DIMER MP2 ENERGY"] -
+                       Process::environment.globals["MP2C MONOMER A MP2 ENERGY"] -
+                       Process::environment.globals["MP2C MONOMER B MP2 ENERGY"];
+    double E_MP2C_int = 0.0;
 
     int n = 0;
     for (quad_->reset(); !quad_->isDone(); quad_->nextPoint() ) {
@@ -55,6 +61,7 @@ double SAPT_DFT::compute_energy()
     }
 
     E_DeltaMP2C = E_TDDFT - E_UCHF; 
+    E_MP2C_int = E_MP2_int + E_DeltaMP2C; 
 
     fprintf(outfile, "  ------------------------------------------------------------------------------\n");       
     fprintf(outfile, "   @ UCHF Dispersion Energy:  %18.12f [mH] %18.12f [kcal]\n", E_UCHF*1000.0, \
@@ -63,13 +70,26 @@ double SAPT_DFT::compute_energy()
         E_TDDFT*627.509);   
     fprintf(outfile, "   @ Delta MP2C Energy:       %18.12f [mH] %18.12f [kcal]\n", E_DeltaMP2C*1000.0, \
         E_DeltaMP2C*627.509);   
-    fprintf(outfile, "  ------------------------------------------------------------------------------\n");       
+    fprintf(outfile, "   @ MP20 Interaction Energy: %18.12f [mH] %18.12f [kcal]\n", E_MP2_int*1000.0, \
+        E_MP2_int*627.509);   
+    fprintf(outfile, "   @ MP2C Interaction Energy: %18.12f [mH] %18.12f [kcal]\n", E_MP2C_int*1000.0, \
+        E_MP2C_int*627.509);   
+    fprintf(outfile, "  ------------------------------------------------------------------------------\n\n");       
+
+    fprintf(outfile, "  --\"We're looking for a bug no one's ever seen before...some kinda smart bug.\"\n");
+    
     fflush(outfile); 
+
+    Process::environment.globals["MP2C UCHF ENERGY"] = E_UCHF; 
+    Process::environment.globals["MP2C TDDFT ENERGY"] = E_TDDFT; 
+    Process::environment.globals["MP2C DELTA MP2C ENERGY"] = E_DeltaMP2C; 
+    Process::environment.globals["MP2C MP2 ENERGY"] = E_MP2_int; 
+    Process::environment.globals["MP2C MP2C ENERGY"] = E_MP2C_int; 
  
-    return E_DeltaMP2C; 
+    return E_MP2C_int; 
 }
 
-void SAPT_DFT::form_quadrature()
+void MP2C::form_quadrature()
 {
     quad_ = shared_ptr<Quadrature>(new ChebyshevIIQuadrature(options_.get_int("OMEGA_POINTS"),
         options_.get_double("OMEGA_CENTER"))); 
@@ -79,7 +99,7 @@ void SAPT_DFT::form_quadrature()
     }
 }
 
-void SAPT_DFT::form_J()
+void MP2C::form_J()
 {
     int naux = ribasis_->nbf();
 
@@ -126,7 +146,7 @@ void SAPT_DFT::form_J()
     }
 }
 
-void SAPT_DFT::form_W()
+void MP2C::form_W()
 {
     int naux = ribasis_->nbf();
 
@@ -440,7 +460,7 @@ void SAPT_DFT::form_W()
     }
 }
 
-void SAPT_DFT::form_X0()
+void MP2C::form_X0()
 {
     int NA = ribasis_->nbf(); 
     int NB = ribasis_->nbf(); 
@@ -652,7 +672,7 @@ void SAPT_DFT::form_X0()
     LiaB.reset();
 }
 
-void SAPT_DFT::form_XC()
+void MP2C::form_XC()
 {
     int naux = ribasis_->nbf();
     int* piv = new int[naux];
@@ -689,7 +709,7 @@ void SAPT_DFT::form_XC()
         error |= C_DGETRI(naux,T1p[0],naux,piv,work,lwork);
 
         if (error != 0)
-            throw PSIEXCEPTION("SAPT_DFT::form_XC: LU inverse failed in Dyson Equation");
+            throw PSIEXCEPTION("MP2C::form_XC: LU inverse failed in Dyson Equation");
 
         if (debug_) 
             T1->print();
@@ -722,7 +742,7 @@ void SAPT_DFT::form_XC()
         error |= C_DGETRI(naux,T1p[0],naux,piv,work,lwork);
 
         if (error != 0)
-            throw PSIEXCEPTION("SAPT_DFT::form_XC: LU inverse failed in Dyson Equation");
+            throw PSIEXCEPTION("MP2C::form_XC: LU inverse failed in Dyson Equation");
 
         if (debug_) 
             T1->print();
@@ -738,7 +758,7 @@ void SAPT_DFT::form_XC()
     delete[] work;
 }
 
-std::vector<double> SAPT_DFT::casimirPolder()
+std::vector<double> MP2C::casimirPolder()
 {
     int naux = ribasis_->nbf(); 
 
@@ -760,12 +780,21 @@ std::vector<double> SAPT_DFT::casimirPolder()
     return E;
 }
 
-void SAPT_DFT::print_header()
+void MP2C::print_header()
 {
-  fprintf(outfile,"         SAPT-DFT  \n");
-  fprintf(outfile,"      Ed Hohenstein\n"); 
-  fprintf(outfile,"       Rob Parish\n") ;
-  fprintf(outfile,"     13 April 2011\n") ;
+  int nthread = 1;
+  #ifdef _OPENMP
+      nthread = omp_get_max_threads();
+  #endif
+
+  fprintf(outfile, "\n");
+  fprintf(outfile, "         ---------------------------------------------------------\n");
+  fprintf(outfile, "                                   MP2C\n");
+  fprintf(outfile, "                     by Rob Parrish, and Ed Hohenstein\n");
+  fprintf(outfile, "                      %3d Threads, %6ld MiB Core\n", nthread, memory_ / 1000000L);
+  fprintf(outfile, "         ---------------------------------------------------------\n");
+  fprintf(outfile, "\n");
+
   fprintf(outfile,"\n");
   fprintf(outfile,"    Orbital Information\n");
   fprintf(outfile,"  -----------------------\n");
@@ -782,7 +811,7 @@ void SAPT_DFT::print_header()
   fflush(outfile);
 }
 
-void SAPT_DFT::print_results()
+void MP2C::print_results()
 {
 }
 

src/lib/libsapt_solver/sapt_dft.h
@@ -9,7 +9,7 @@ class Quadrature;
 
 namespace sapt {
 
-class SAPT_DFT : public SAPT0 {
+class MP2C : public SAPT0 {
 
 private:
 
@@ -52,8 +52,8 @@ protected:
 
 
 public:
-    SAPT_DFT(Options& options, boost::shared_ptr<PSIO> psio, boost::shared_ptr<Chkpt> chkpt);
-    virtual ~SAPT_DFT();
+    MP2C(Options& options, boost::shared_ptr<PSIO> psio, boost::shared_ptr<Chkpt> chkpt);
+    virtual ~MP2C();
 
     virtual double compute_energy();
 };

commit 1e4a6e5ae7b1c63662111c976816d3082662f81c

Fixed what Rob broke in r1707
git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1718 6a214465-e6f0-41bc-91e6-de896bb96b5e

src/bin/psi4/python.cc
@@ -548,7 +548,7 @@ BOOST_PYTHON_MODULE(PsiMod)
     def("get_option", py_psi_get_option);
     def("get_global_option", py_psi_get_global_option);
 
-    def("set_variable", py_psi_set_variable);
+    def("get_variable", py_psi_get_variable);
     def("set_variable", py_psi_set_variable);
     def("print_variables", py_psi_print_variable_map);
 