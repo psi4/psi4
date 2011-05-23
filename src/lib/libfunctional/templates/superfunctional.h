21d0f858d42dacec2eba6ffa9bc0aad60efbb3c5
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-05-23 15:30:05 +0000

    stuff
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1794 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 246d162d2686d6e91b65d2a1958e4ca57e430f0f

--- src/lib/libfunctional/templates/superfunctional.h
+++ src/lib/libfunctional/templates/superfunctional.h
@@ -14,10 +14,6 @@
 #include <string>
 #include <vector>
 
-using namespace psi;
-using namespace boost;
-using namespace std;
-
 namespace psi {
 
 class Properties;
@@ -28,7 +24,7 @@ class SuperFunctional {
     public:
 
         static boost::shared_ptr<SuperFunctional> createSuperFunctional(const std::string & alias, int npoints = 5000, int deriv = 1);
-        static boost::shared_ptr<SuperFunctional> buildSuperFunctional(const std::string & build, int npoints = 5000, int deriv = 1);        
+        static boost::shared_ptr<SuperFunctional> buildSuperFunctional(const std::string & build, int npoints = 5000, int deriv = 1);
         static std::string availableSuperFunctionals();
         static std::vector<std::string> availableNames();
         static std::string testSuperFunctionals();
@@ -36,39 +32,39 @@ class SuperFunctional {
         SuperFunctional(int npoints = 5000, int deriv = 1);
         ~SuperFunctional();
 
-        std::string getName() const { return name_; }        
-        std::string getDescription() const { return description_; }        
-        std::string getCitation() const { return citation_; }        
+        std::string getName() const { return name_; }
+        std::string getDescription() const { return description_; }
+        std::string getCitation() const { return citation_; }
         void setName(const std::string & name) { name_ = name; }
         void setDescription(const std::string & description) { description_ = description; }
         void setCitation(const std::string & citation) { citation_ = citation; }
 
-        std::string getComposition();        
-        
-        bool isGGA();    
-        bool isMeta();    
-        
-        bool isHybrid() { return  exact_exchange_ > 0.0; }  
-        bool isDoubleHybrid() { return pt2_ > 0.0; }  
-        bool isRangeCorrected() { return omega_ > 0.0; }  
-        bool isDashD() { return dashD_weight_ > 0.0; }  
+        std::string getComposition();
+
+        bool isGGA();
+        bool isMeta();
+
+        bool isHybrid() { return  exact_exchange_ > 0.0; }
+        bool isDoubleHybrid() { return pt2_ > 0.0; }
+        bool isRangeCorrected() { return omega_ > 0.0; }
+        bool isDashD() { return dashD_weight_ > 0.0; }
 
         void setParameter(const std::string & functional, const std::string & param, double val);
-        
-        std::string testSuperFunctional(shared_ptr<Properties> props);
 
-        double getExactExchange() { return exact_exchange_; } 
+        std::string testSuperFunctional(boost::shared_ptr<Properties> props);
+
+        double getExactExchange() { return exact_exchange_; }
         void setExactExchange(double exch) {exact_exchange_ = exch; }
-     
-        double getPT2() { return pt2_; } 
+
+        double getPT2() { return pt2_; }
         void setPT2(double pt2) {pt2_ = pt2; }
 
-        double getOmega() { return omega_; } 
+        double getOmega() { return omega_; }
         void setOmega(double omega) {omega_ = omega; }
 
         double getDashDWeight() { return dashD_weight_; }
         boost::shared_ptr<Dispersion> getDashD() { return dashD_; }
-        void setDashD(shared_ptr<Dispersion> disp, double weight);    
+        void setDashD(boost::shared_ptr<Dispersion> disp, double weight);
 
         void setNPoints(int npoints) { reallocate(npoints,deriv_); }
         void setDeriv(int deriv) { reallocate(npoints_,deriv); }
@@ -85,28 +81,28 @@ class SuperFunctional {
         boost::shared_ptr<Functional> getFunctional(int index) { return functionals_[index].first; }
         double getWeight(int index) { return functionals_[index].second; }
         int size() { return functionals_.size(); }
-    
-        double* getFunctionalValue() const { return functional_; }       
-        double* getV_RhoA() const { return v_rho_a_; }       
-        double* getV_RhoB() const { return v_rho_b_; }       
-        double* getV_RhoA_RhoA() const { return v_rho_a_rho_a_; }       
-        double* getV_RhoA_RhoB() const { return v_rho_a_rho_b_; }       
-        double* getV_RhoB_RhoB() const { return v_rho_b_rho_b_; }       
-        double* getV_GammaAA() const { return v_gamma_aa_; }       
-        double* getV_GammaAB() const { return v_gamma_ab_; }       
-        double* getV_GammaBB() const { return v_gamma_bb_; }       
-        double* getV_RhoA_GammaAA() const { return v_rho_a_gamma_aa_; }       
-        double* getV_RhoA_GammaAB() const { return v_rho_a_gamma_ab_; }       
-        double* getV_RhoA_GammaBB() const { return v_rho_a_gamma_bb_; }       
-        double* getV_RhoB_GammaAA() const { return v_rho_b_gamma_aa_; }       
-        double* getV_RhoB_GammaAB() const { return v_rho_b_gamma_ab_; }       
-        double* getV_RhoB_GammaBB() const { return v_rho_b_gamma_bb_; }       
-        double* getV_GammaAA_GammaAA() const { return v_gamma_aa_gamma_aa_; }       
-        double* getV_GammaAA_GammaAB() const { return v_gamma_aa_gamma_ab_; }       
-        double* getV_GammaAA_GammaBB() const { return v_gamma_aa_gamma_bb_; }       
-        double* getV_GammaAB_GammaAB() const { return v_gamma_ab_gamma_ab_; }       
-        double* getV_GammaAB_GammaBB() const { return v_gamma_ab_gamma_bb_; }       
-        double* getV_GammaBB_GammaBB() const { return v_gamma_bb_gamma_bb_; }       
+
+        double* getFunctionalValue() const { return functional_; }
+        double* getV_RhoA() const { return v_rho_a_; }
+        double* getV_RhoB() const { return v_rho_b_; }
+        double* getV_RhoA_RhoA() const { return v_rho_a_rho_a_; }
+        double* getV_RhoA_RhoB() const { return v_rho_a_rho_b_; }
+        double* getV_RhoB_RhoB() const { return v_rho_b_rho_b_; }
+        double* getV_GammaAA() const { return v_gamma_aa_; }
+        double* getV_GammaAB() const { return v_gamma_ab_; }
+        double* getV_GammaBB() const { return v_gamma_bb_; }
+        double* getV_RhoA_GammaAA() const { return v_rho_a_gamma_aa_; }
+        double* getV_RhoA_GammaAB() const { return v_rho_a_gamma_ab_; }
+        double* getV_RhoA_GammaBB() const { return v_rho_a_gamma_bb_; }
+        double* getV_RhoB_GammaAA() const { return v_rho_b_gamma_aa_; }
+        double* getV_RhoB_GammaAB() const { return v_rho_b_gamma_ab_; }
+        double* getV_RhoB_GammaBB() const { return v_rho_b_gamma_bb_; }
+        double* getV_GammaAA_GammaAA() const { return v_gamma_aa_gamma_aa_; }
+        double* getV_GammaAA_GammaAB() const { return v_gamma_aa_gamma_ab_; }
+        double* getV_GammaAA_GammaBB() const { return v_gamma_aa_gamma_bb_; }
+        double* getV_GammaAB_GammaAB() const { return v_gamma_ab_gamma_ab_; }
+        double* getV_GammaAB_GammaBB() const { return v_gamma_ab_gamma_bb_; }
+        double* getV_GammaBB_GammaBB() const { return v_gamma_bb_gamma_bb_; }
         double* getV_TauA() const { return v_tau_a_; }
         double* getV_TauB() const { return v_tau_b_; }
         double* getV_RhoA_TauA() const { return v_rho_a_tau_a_; }
@@ -122,13 +118,13 @@ class SuperFunctional {
         double* getV_TauA_TauA() const { return v_tau_a_tau_a_; }
         double* getV_TauA_TauB() const { return v_tau_a_tau_b_; }
         double* getV_TauB_TauB() const { return v_tau_b_tau_b_; }
-     
+
     protected:
-        
+
         void reallocate(int npoints, int deriv);
         void release();
         void allocate();
- 
+
         std::vector<std::pair<boost::shared_ptr<Functional>, double> > functionals_;
 
         std::string name_;
@@ -177,7 +173,7 @@ class SuperFunctional {
         double* v_tau_a_tau_a_;
         double* v_tau_a_tau_b_;
         double* v_tau_b_tau_b_;
-        
+
         int npoints_;
         int deriv_;
         bool oldGGA_;
@@ -187,4 +183,4 @@ class SuperFunctional {
 
 }}
 
-#endif 
+#endif
