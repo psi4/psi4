73c6f2b991ef083976dea66e646a495fd860ee48
rking <rking@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-06-03 18:29:21 +0000

    forgot to check in this file which takes backward steps if opt goes awry
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1823 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: eb2185af545fe625b7aaa4539c914cbcee60d067

---
+++ src/bin/optking/molecule_backstep.cc
@@ -0,0 +1,127 @@
+/*! \file molecule.cc
+    \ingroup optking
+    \brief molecule class (really, molecular system class)
+*/
+
+#include "molecule.h"
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
+#include "linear_algebra.h"
+#include "print.h"
+#include "atom_data.h"
+#include "physconst.h"
+
+#define EXTERN
+#include "globals.h"
+
+namespace opt {
+
+// Update OPT_DATA to go back one step and try again with a smaller step.
+// The current step data is replaced.
+// For now lets just divide the step size by one half.
+//
+// OPT_DATA contains:
+//  left unchanged : Nintco, Ncart
+//  left unchanged : Hessian (throw bypasses updating)
+//  left unchanged : rfo_eigenvector (a unit vector)
+//
+//  decrease by 1 : iteration (iteration if number of stored steps in opt_data
+//                  - at least for now
+//  increase by 1 : consecutive_backsteps
+//
+//  std::vector<STEP_DATA *> steps;
+//  Modify steps[Nsteps-2]. Then erase steps[Nsteps-1] (current step).
+//  
+//  Last step data unchanged:
+//   f_q
+//   geom
+//   energy
+//   unit_step
+//   dq_gradient
+//   dq_hessian
+//
+//   dq_norm = dq_norm (last) /2
+//   DE_predicted  : call DE_nr_energy or DE_rfo_energy (dq_norm)
+//
+
+// compute change in energy according to quadratic approximation
+inline double DE_nr_energy(double step, double grad, double hess) {
+  return (step * grad + 0.5 * step * step * hess);
+}
+
+// compute change in energy according to RFO approximation
+inline double DE_rfo_energy(double rfo_t, double rfo_g, double rfo_h) {
+  return (rfo_t * rfo_g + 0.5 * rfo_t * rfo_t * rfo_h)/(1 + rfo_t*rfo_t);
+}
+
+void MOLECULE::backstep(void) {
+
+  fprintf(outfile,"\tRe-doing last optimization step - smaller this time.");
+
+  p_Opt_data->erase_last_step();
+
+  p_Opt_data->decrement_iteration();
+  p_Opt_data->increment_consecutive_backsteps();
+
+  int Nsteps = p_Opt_data->nsteps();
+  int Nintco = g_nintco();
+
+  double *dq = p_Opt_data->g_dq_pointer(Nsteps-1);
+  for (int i=0; i<Nintco; ++i)
+    dq[i] /= 2;
+  double dq_norm = sqrt(array_dot(dq, dq, Nintco));
+
+  double *rfo_u  = p_Opt_data->g_rfo_eigenvector_pointer();
+  double dq_grad = p_Opt_data->g_dq_gradient(Nsteps-1);
+  double dq_hess = p_Opt_data->g_dq_hessian(Nsteps-1);
+
+  double DE_projected;
+  if (Opt_params.step_type == OPT_PARAMS::NR)
+    DE_projected = DE_nr_energy(dq_norm, dq_grad, dq_hess);
+  else if (Opt_params.step_type == OPT_PARAMS::RFO)
+    DE_projected = DE_rfo_energy(dq_norm, dq_grad, dq_hess);
+
+  fprintf(outfile, "\tNewly projected energy change : %20.10lf\n", DE_projected);
+
+  // do displacements for each fragment separately
+  for (int f=0; f<fragments.size(); ++f) {
+    if (fragments[f]->is_frozen() || Opt_params.freeze_intrafragment) {
+      fprintf(outfile,"\tDisplacements for frozen fragment %d skipped.\n", f+1);
+      continue;
+    }
+    fragments[f]->displace(&(dq[g_intco_offset(f)]), true, g_intco_offset(f));
+  }
+
+  // do displacements for interfragment coordinates
+  double *q_target;
+  for (int I=0; I<interfragments.size(); ++I) {
+
+    q_target = interfragments[I]->intco_values();
+    for (int i=0; i<interfragments[I]->g_nintco(); ++i)
+      q_target[i] += dq[g_interfragment_intco_offset(I) + i];
+
+    interfragments[I]->orient_fragment(q_target);
+
+    free_array(q_target);
+  }
+
+#if defined(OPTKING_PACKAGE_QCHEM)
+  // fix rotation matrix for rotations in QCHEM EFP code
+  for (int I=0; I<efp_fragments.size(); ++I)
+    efp_fragments[I]->displace( I, &(dq[g_efp_fragment_intco_offset(I)]) );
+#endif
+
+  symmetrize_geom(); // now symmetrize the geometry for next step
+
+  // save values in step data
+  p_Opt_data->save_step_info(DE_projected, rfo_u, dq_norm, dq_grad, dq_hess);
+
+  free_array(rfo_u);
+  fflush(outfile);
+
+} // end take RFO step
+
+}
