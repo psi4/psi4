2d99f5b0bb581a3b764c1cfec5e260a30c26641a
rparrish <rparrish@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-06-03 21:44:56 +0000

    Some epic pseudospectral/DFT grid work
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1826 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: c19fdff6da7cf0e14fc644b46762144339c852b1

--- src/lib/lib3index/3index.h
+++ src/lib/lib3index/3index.h
@@ -3,7 +3,7 @@
 
 #include <psi4-dec.h>
 #include <psiconfig.h>
-#include <libmints/matrix.h>
+#include <libmints/vector3.h>
 
 namespace psi {
 
@@ -14,6 +14,7 @@ class Vector;
 class IntVector;
 class GridBlock;
 class PseudospectralInt;
+class Vector3;
 
 class FittingMetric {
 
@@ -92,14 +93,12 @@ class PseudoGrid {
 protected:
     /// The grid (in a Jeff Bridges voice)
     boost::shared_ptr<GridBlock> grid_;
-    /// Name (ie: cc-pVQZ-ultrafine)
-    std::string name_;
     /// The molecule the grid is build on
     boost::shared_ptr<Molecule> molecule_;
 
 public:
     /// Construct a grid object from a molecule and name, then parse (or maybe autogen)
-    PseudoGrid(boost::shared_ptr<Molecule> mol, const std::string& name);
+    PseudoGrid(boost::shared_ptr<Molecule> mol);
     /// Destructor, frees grid memory if created
     virtual ~PseudoGrid();
 
@@ -107,8 +106,232 @@ public:
     boost::shared_ptr<GridBlock> getBlock() const { return grid_; }
     /// Parse grid given full file path to G94-style grid file
     void parse(const std::string& file);
+    /// Build the grid automatically from quadratures
+    void build();
 
 };
+
+class SphericalGrid {
+
+protected:
+    /// Master map of order to number of points 
+    static std::map<int, int> lebedev_order_to_points_;
+    /// Helper function to generate lebedev reccurence points
+    static int lebedevReccurence(int, int, double, double, double, SphericalGrid* leb);
+    /// Helper to build Lebedev grid
+    static boost::shared_ptr<SphericalGrid> buildLebedevGrid(int order);
+
+    /// Scheme
+    std::string scheme_;
+    /// Number of points
+    int npoints_;
+    /// Spherical harmonic order
+    int order_;
+    /// x coordinates on unit sphere 
+    double* x_;
+    /// y coordinates on unit sphere 
+    double* y_;
+    /// z coordinates on unit sphere 
+    double* z_;
+    /// weights  on unit sphere (normalized to 4\pi)
+    double* w_;
+
+public:
+    SphericalGrid();
+    virtual ~SphericalGrid();   
+    static boost::shared_ptr<SphericalGrid> buildGrid(const std::string& scheme, int order);
+    static std::map<int, int> lebedevOrdersToPoints() { return lebedev_order_to_points_; }
+
+    /// Number of grid points
+    int npoints() const { return npoints_; } 
+    /// Order of quadrature 
+    int order() const { return order_; } 
+    /// Scheme used to build the quadrature
+    std::string scheme() const { return scheme_; }
+    /// Print a trace of this spherical quadrature
+    virtual void print(FILE* out = outfile); 
+    
+    /// The x points. You do not own this 
+    double* x() const { return x_; }
+    /// The y points. You do not own this 
+    double* y() const { return y_; }
+    /// The z points. You do not own this 
+    double* z() const { return z_; }
+    /// The weights, normalized to 4\pi on Omega. You do not own this 
+    double* w() const { return w_; }
+
+};
+
+class RadialGrid {
+
+protected:
+    /// Scheme
+    std::string scheme_;
+    /// Number of points
+    int npoints_;
+    /// r coordinates
+    double* r_;
+    /// weights without r^2 (1D)
+    double* w_x_;
+    /// weights with r^2 (3D)
+    double* w_r_;
+
+    static boost::shared_ptr<RadialGrid> buildTreutlerGrid(int n, double xi, double alpha = 0.6);
+    static boost::shared_ptr<RadialGrid> buildBeckeGrid(int n, double xi);
+    static boost::shared_ptr<RadialGrid> buildMuraGrid(int n, double xi);
+    static boost::shared_ptr<RadialGrid> buildEMGrid(int n, double xi);
+    static boost::shared_ptr<RadialGrid> buildMultiExpGrid(int n, double xi);
+public:
+    RadialGrid();
+    virtual ~RadialGrid();   
+
+    /// Build the radial grid
+    static boost::shared_ptr<RadialGrid> buildGrid(const std::string& scheme, int n, double xi);
+
+    /// Number of grid points
+    int npoints() const { return npoints_; } 
+    /// Scheme used to build the quadrature
+    std::string scheme() const { return scheme_; }
+    /// Print a trace of this radial quadrature
+    virtual void print(FILE* out = outfile); 
+    
+    /// The r points. You do not own this 
+    double* r() const { return r_; }
+    /// The weights, without the r^2 element. You do not own this
+    double* w_x() const { return w_x_; }
+    /// The weights, with the r^2 element. You do not own this
+    double* w_r() const { return w_r_; }
+         
+};
+
+class AtomicGrid {
+
+protected:
+    /// The radial quadrature
+    boost::shared_ptr<RadialGrid> radial_;
+    /// The vector of spherical grids (unit spheres)  
+    std::vector<boost::shared_ptr<SphericalGrid> > sphericals_;
+    /// The rotation matrix to bring this grid to standard orientation
+    boost::shared_ptr<Matrix> rotation_;
+    /// The center of this atom
+    Vector3 center_;
+
+    /// Total points for this atom
+    int npoints_;
+    /// Full x points. 
+    double* x_;
+    /// Full y points. 
+    double* y_;
+    /// Full z points. 
+    double* z_;
+    /// Full weights
+    double* w_;
+
+public:
+    AtomicGrid();
+    virtual ~AtomicGrid();   
+
+    void buildGrid(const Vector3& center,
+              boost::shared_ptr<Matrix> rotation,
+              boost::shared_ptr<RadialGrid> radial,          
+              std::vector<boost::shared_ptr<SphericalGrid> > spheres); 
+    
+    /// Number of grid points
+    int npoints() const { return npoints_; } 
+    /// Print a trace of this atomic quadrature
+    virtual void print(FILE* out = outfile); 
+    
+    /// The x points. You do not own this 
+    double* x() const { return x_; }
+    /// The y points. You do not own this 
+    double* y() const { return y_; }
+    /// The z points. You do not own this 
+    double* z() const { return z_; }
+    /// The weights, normalized to 1 on R3. You do not own this 
+    double* w() const { return w_; }
+};
+
+class MolecularGrid {
+
+protected:
+    /// Bragg-Slater Radii
+    static std::vector<double> BSRadii_;
+    /// Helper to get BS Radii
+    static void getBSRadii(); 
+
+    /// Scheme
+    std::string scheme_;
+    /// The vector of atomic quadratures
+    std::vector<boost::shared_ptr<AtomicGrid> > atoms_;
+    /// The molecule this grid is built on
+    boost::shared_ptr<Molecule> molecule_;
+    
+    /// Total points for this molecule 
+    int npoints_;
+    /// Full x points. 
+    double* x_;
+    /// Full y points. 
+    double* y_;
+    /// Full z points. 
+    double* z_;
+    /// Full weights
+    double* w_;
+    
+    /// Helpers for weights
+    void applyNaiveWeights();
+    void applyBeckeWeights();
+    void applyTreutlerWeights();
+    void applyStratmannWeights();
+
+    // Helper for the first three types
+    void applyStandardWeights(boost::shared_ptr<Matrix> chi);
+
+public:
+    MolecularGrid(boost::shared_ptr<Molecule> molecule);
+    virtual ~MolecularGrid();   
+
+    /// Build the grid 
+    void buildGrid(std::vector<boost::shared_ptr<AtomicGrid> > atoms,
+                   const std::string& scheme);
+
+    /// Number of grid points
+    int npoints() const { return npoints_; } 
+    /// Print a trace of this molecular quadrature
+    virtual void print(FILE* out = outfile); 
+    
+    /// The x points. You do not own this 
+    double* x() const { return x_; }
+    /// The y points. You do not own this 
+    double* y() const { return y_; }
+    /// The z points. You do not own this 
+    double* z() const { return z_; }
+    /// The weights, normalized to 1 on R3. You do not own this 
+    double* w() const { return w_; }
+};
+
+class PseudospectralGrid : public MolecularGrid {
+
+protected:
+    /// The primary basis
+    boost::shared_ptr<BasisSet> primary_;
+    /// The dealias basis 
+    boost::shared_ptr<BasisSet> dealias_;
+    /// The Options object
+    Options& options_;   
+ 
+public:
+    PseudospectralGrid(boost::shared_ptr<Molecule> molecule,
+                       boost::shared_ptr<BasisSet> primary_, 
+                       boost::shared_ptr<BasisSet> dealias_,
+                       Options& options); 
+    virtual ~PseudospectralGrid();
+
+};
+
+#if 0
+class DFTGrid : public MolecularGrid {
+}; 
+#endif
 
 class SchwarzSieve {
 
@@ -213,7 +436,7 @@ class TensorChunk {
         // Pointer to the chunk
         boost::shared_ptr<Matrix> chunk() const { return chunk_; }
         // Pointer to the chunk pointer
-        double** chunk_pointer() const { return chunk_->pointer(); }
+        double** chunk_pointer() const;
         // read in the chunk corresponding to block_number
         // If you need read-only access, and the tensor is small enough to
         // fit in core under the memory provided (nblock is 1), setting
@@ -447,7 +670,7 @@ public:
     // Pointer to the chunk (P|
     boost::shared_ptr<Matrix> chunk() const { return Aia_; }
     // Pointer to the chunk pointer
-    double** chunk_pointer() const { return Aia_->pointer(); }
+    double** chunk_pointer() const;
     // Compute the desired block number
     void compute_block(int block_number);
     // The starting slow index of the current block
