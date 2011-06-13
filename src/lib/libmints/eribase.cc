2083a85a00c289c1a6c3e54b8f052b3a1ca4f97c
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-06-13 16:41:53 +0000

    ok, changes I made didn't work
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1866 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libmints/eribase.cc
+++ src/lib/libmints/eribase.cc
@@ -791,14 +791,9 @@ void TwoElectronInt::compute_quartet(int sh1, int sh2, int sh3, int sh4)
     else {
         // Old version - without ShellPair - STILL USED BY RI CODES
         for (int p1=0; p1<nprim1; ++p1) {
-            max_p2 = (sh1 == sh2) ? p1+1 : nprim2;
-
             double a1 = s1->exp(p1);
             double c1 = s1->coef(p1);
-
-            for (int p2=0; p2<max_p2; ++p2) {
-                m = (1 + (sh1 == sh2 && p1 != p2));
-
+            for (int p2=0; p2<nprim2; ++p2) {
                 double a2 = s2->exp(p2);
                 double c2 = s2->coef(p2);
                 double zeta = a1 + a2;
@@ -821,13 +816,9 @@ void TwoElectronInt::compute_quartet(int sh1, int sh2, int sh3, int sh4)
                 double Sab = pow(M_PI*ooz, 3.0/2.0) * exp(-a1*a2*ooz*AB2) * c1 * c2;
 
                 for (int p3=0; p3<nprim3; ++p3) {
-                    max_p4 = (sh3 == sh4) ? p3+1 : nprim4;
-
                     double a3 = s3->exp(p3);
                     double c3 = s3->coef(p3);
-
-                    for (int p4=0; p4<max_p4; ++p4) {
-                        n = m * (1 + (sh3 == sh4 && p3 != p4));
+                    for (int p4=0; p4<nprim4; ++p4) {
 
                         double a4 = s4->exp(p4);
                         double c4 = s4->coef(p4);
