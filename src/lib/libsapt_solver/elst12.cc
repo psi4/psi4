ae740e2f15a699b363f17dfdae767b2ad2b84e0c
hohenstein <hohenstein@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-07-07 15:08:46 +0000

    Added exchange terms to SAPT2
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1952 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/lib/libsapt_solver/elst12.cc
+++ src/lib/libsapt_solver/elst12.cc
@@ -7,16 +7,20 @@ void SAPT2::elst12()
   double e_elst120 = elst120(wBAA_,wBRR_,CHFA_,PSIF_SAPT_AMPS,
     "pAA Density Matrix","pRR Density Matrix","Y2 AR Amplitudes",
     foccA_,noccA_,nvirA_);
+
+  if (debug_) {
+    fprintf(outfile,"    Elst120,r           = %18.12lf H\n",e_elst120);
+  }
+
   double e_elst102 = elst120(wABB_,wASS_,CHFB_,PSIF_SAPT_AMPS,
     "pBB Density Matrix","pSS Density Matrix","Y2 BS Amplitudes",
     foccB_,noccB_,nvirB_);
 
-  e_elst12_ = e_elst120 + e_elst102;
-
   if (debug_) {
-    fprintf(outfile,"    Elst120,r           = %18.12lf H\n",e_elst120);
-    fprintf(outfile,"    Elst102,r           = %18.12lf H\n",e_elst102);
+    fprintf(outfile,"    Elst102,r           = %18.12lf H\n\n",e_elst102);
   }
+
+  e_elst12_ = e_elst120 + e_elst102;
 
   if (print_) {
     fprintf(outfile,"    Elst12,r            = %18.12lf H\n",e_elst12_);
@@ -42,20 +46,26 @@ double SAPT2::elst120(double **wBAA, double **wBRR, double **CHFA, int ampfile,
   psio_->read_entry(ampfile,Ylabel,(char *) yAR[0],
     sizeof(double)*aoccA*nvirA);
 
-  double elst = 0.0;
+  double e1 = 0.0, e2 = 0.0, e3 = 0.0;
 
   for (int a=0; a<aoccA; a++) {
-    elst -= 2.0*C_DDOT(aoccA,pAA[a],1,&(wBAA[a+foccA][foccA]),1);
+    e1 -= 2.0*C_DDOT(aoccA,pAA[a],1,&(wBAA[a+foccA][foccA]),1);
   }
 
-  elst += 2.0*C_DDOT(nvirA*nvirA,pRR[0],1,wBRR[0],1);
-  elst += 4.0*C_DDOT(aoccA*nvirA,yAR[0],1,CHFA[foccA],1);
+  e2 += 2.0*C_DDOT(nvirA*nvirA,pRR[0],1,wBRR[0],1);
+  e3 += 4.0*C_DDOT(aoccA*nvirA,yAR[0],1,CHFA[foccA],1);
 
   free_block(pAA);
   free_block(pRR);
   free_block(yAR);
 
-  return(elst);
+  if (debug_) {
+    fprintf(outfile,"\n    Elst12_1            = %18.12lf H\n",e1);
+    fprintf(outfile,"    Elst12_2            = %18.12lf H\n",e2);
+    fprintf(outfile,"    Elst12_3            = %18.12lf H\n",e3);
+  }
+
+  return(e1+e2+e3);
 }
 
 }}
