c950b136e183d0af33a455aff407be64addfff1a
jturney <jturney@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-07-07 00:50:23 +0000

    finished MADNESS integration
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1950 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 51c36257b67f1eef7c82f64a7ae68c5a3574d912

--- src/lib/libparallel/mad.cc
+++ src/lib/libparallel/mad.cc
@@ -8,102 +8,86 @@ using namespace boost;
 
 #if HAVE_MADNESS == 1
 
-MadCommunicator::MadCommunicator(boost::shared_ptr<madness::World> madness_world, const std::string &communicator) :
-Communicator(communicator), madworld_(madness_world) {
+MadCommunicator::MadCommunicator(const int &argc, char **argv) :
+    Communicator()
+{
+
+    // Madness is initialized in the communicator constructor
+    // Initialize madness and create a madness world communicator
+    madness::initialize(argc, argv);
+
+    madworld_ = SharedMadWorld(new madness::World(MPI::COMM_WORLD));
+
     me_ = madworld_->rank();
     nproc_ = madworld_->nproc();
+    nthread_ = madworld_->taskq.nthread();
+    communicator_ = "MADNESS";
 
-    // set the number of threads in the thread pool
-    int shift = 0;
-    char *cnthread = getenv("MAD_NUM_THREADS");
-    // MAD_NUM_THREADS is total no. of application threads whereas
-    // POOL_NTHREAD is just the number in the pool (one less)
-    if (cnthread) shift = -1;
-    if (cnthread == 0) {
-        cnthread = getenv("POOL_NTHREAD");
-        shift = 1;
-    }
+    thread_id_.clear();
 
-    if (cnthread) {
-        int result = sscanf(cnthread, "%d", &nthread_);
-        nthread_ += shift;
-    }
-    else {
-#ifdef _SC_NPROCESSORS_CONF
-        int ncpu = sysconf(_SC_NPROCESSORS_CONF);
-#elif defined(HC_NCPU)
-        int mib[2]={CTL_HW,HW_NCPU}, ncpu;
-        size_t len = sizeof(ncpu);
-#else
-        int ncpu=1;
-#endif
-        if (ncpu < 2) ncpu = 2;
-        nthread_ = ncpu - 1; // One less than # physical processors
-    }
-
-    std::cout << "nthread = " << nthread_ << std::endl;
-    thread_count=0;
-    mutex_ = boost::shared_ptr<madness::Spinlock>(get_mutex());
-    while (thread_count < nthread_) {
-        madworld_->taskq.add(*this, &MadCommunicator::set_thread_id);
-    }
-
-
-    std::cout << "waiting at the sync" << std::endl;
-    sync();
-    std::cout << "Proc " << me_ << " finished getting thread ids.  They are:" << std::endl;
-
-    std::map<pthread_t, int>::iterator it;
-    for (it=thread_id_.begin(); it != thread_id_.end(); it++)
-        std::cout << "  [" << (*it).first << ", " << (*it).second << "]" << std::endl;
-
-    sync();
-}
-
-madness::Void MadCommunicator::set_thread_id() {
-
-    mutex_->lock();
-
-    pthread_t tid;
-    std::map<pthread_t, int>::iterator it;
-
-    tid = pthread_self();
-    for (it=thread_id_.begin(); it != thread_id_.end(); it++) {
-        if (tid == (*it).first) {
-           mutex_->unlock();
-           sleep(1);
-           return madness::None;
+    // If we have threads in the thread pool, this gets all of the pthread ids from madness, and
+    // creates a map of pthread ids to a unique integer value (i.e. 0, 1, 2, ...)
+    if (Process::environment("MAD_NUM_THREADS") != "1" && Process::environment("POOL_NTHREAD") != "0") {
+        pthread_t *id = madworld_->taskq.thread_id();
+        for (int i=0; i < nthread_; i++) {
+            thread_id_.insert( std::pair<pthread_t, int>(id[i], i) );
         }
+        free(id);
     }
-    thread_id_.insert( std::pair<pthread_t, int>(tid,thread_count) );
-    thread_count++;
-    mutex_->unlock();
+    // We need to add the master thread because it can perform tasks that are in the task queue
+    thread_id_.insert( std::pair<pthread_t, int>(pthread_self(), nthread_));
+    // Add one to the total number of threads for the master thread
+    nthread_++;
 
-    return madness::None;
 }
+
+MadCommunicator::MadCommunicator(const MadCommunicator &copy) :
+    Communicator(), me_(copy.me_), nproc_(copy.nproc_),
+    nthread_(copy.nthread_), communicator_(copy.communicator_),
+    madworld_(copy.madworld_), thread_id_(copy.thread_id_)
+{ }
+
+MadCommunicator& MadCommunicator::operator =(const MadCommunicator& other)
+{
+    if (this != &other) {
+        me_   = other.me_;
+        nproc_ = other.nproc_;
+        nthread_ = other.nthread_;
+        madworld_ = other.madworld_;
+        thread_id_ = other.thread_id_;
+    }
+    return *this;
+}
+
+
 
 MadCommunicator::~MadCommunicator()
-    {}
+{   }
 
-void MadCommunicator::sync() {
+void MadCommunicator::sync()
+{
     madworld_->gop.fence();
 }
 
-void MadCommunicator::raw_send(const void* data, int nbyte, int target){
+void MadCommunicator::raw_send(const void* data, int nbyte, int target)
+{
     madworld_->mpi.Isend(data, nbyte, MPI_BYTE, target, 0);
 }
 
-void MadCommunicator::raw_recv(void* data, int nbyte, int sender) {
+void MadCommunicator::raw_recv(void* data, int nbyte, int sender)
+{
     madworld_->mpi.Irecv(data, nbyte, MPI_BYTE, sender, 0);
 }
 
-void MadCommunicator::raw_bcast(void* data, int nbyte, int broadcaster) {
+void MadCommunicator::raw_bcast(void* data, int nbyte, int broadcaster)
+{
     madworld_->gop.broadcast(data, nbyte, broadcaster);
 }
 
 
 #define SUMMEMBER(T) \
-void MadCommunicator::raw_sum(T *data, int nelem, T *receive_buffer, int target) { \
+void MadCommunicator::raw_sum(T *data, int nelem, T *receive_buffer, int target) \
+{ \
     madworld_->gop.sum(data, nelem); \
 }
 
@@ -113,7 +97,8 @@ SUMMEMBER(int)
 SUMMEMBER(char)
 SUMMEMBER(long)
 
-void MadCommunicator::print(FILE *out) const {
+void MadCommunicator::print(FILE *out) const
+{
     if (me() == 0) {
         fprintf(out, "\n    Using MadCommunicator (Number of procs = %d)\n", nproc());
         fprintf(out, "                          (Number of threads in pool = %d)\n\n", nthread_);
