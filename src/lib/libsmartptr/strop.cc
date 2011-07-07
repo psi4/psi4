a51edf1b4faae2ac6ac2dc429ed7306af4f227aa
andysim <andysim@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-07-07 20:19:44 +0000

    Removed old YETI/Smartptr files.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@1954 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: e364504c86d51abed12a8f6d3afd145e51d153d9

--- src/lib/libsmartptr/strop.cc
+++
@@ -1,8 +0,0 @@
-#include <boost/algorithm/string.hpp>
-#include "strop.h"
-
-void
-smartptr::split(std::vector<std::string>& values, const std::string& line, const char* tokens)
-{
-    boost::split(values, line, boost::is_any_of(tokens), boost::algorithm::token_compress_on);
-}
