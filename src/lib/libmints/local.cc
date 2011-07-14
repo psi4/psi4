407b54b02e1add94361931a2ec163e8df715d5b1
rparrish <rparrish@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-07-14 21:02:43 +0000

    Work on Local
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@2010 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 4b209005215e13e3939406449b4f07729bc21fdd

--- src/lib/libmints/local.cc
+++ src/lib/libmints/local.cc
@@ -12,6 +12,10 @@ Local::Local(boost::shared_ptr<BasisSet> basisset, boost::shared_ptr<Matrix> C_U
     basisset_(basisset), C_USO_(C_USO), print_(0), debug_(0)
 {
 }
+Local::Local(boost::shared_ptr<BasisSet> basisset, boost::shared_ptr<BasisSet> auxiliary, boost::shared_ptr<Matrix> C_USO) :
+    basisset_(basisset), auxiliary_(auxiliary), C_USO_(C_USO), print_(0), debug_(0)
+{
+}
 Local::~Local()
 {
 }
@@ -24,6 +28,12 @@ void Local::common_init()
 
     C_AO_ = C_AO();
     L_AO_ = C_AO_;
+
+    int nso = basisset_->nbf();
+    X_ = boost::shared_ptr<Matrix>(new Matrix("S^-1/2",nso,nso));
+    boost::shared_ptr<OneBodyAOInt> Sint(integral->ao_overlap());
+    Sint->compute(X_);
+    X_->power(-1.0/2.0);
 }
 boost::shared_ptr<Matrix> Local::C_USO()
 {
@@ -64,20 +74,20 @@ boost::shared_ptr<Matrix> Local::AO2USO()
 {
     return AO2USO_;
 }
-boost::shared_ptr<Matrix> Local::localize(const std::string& algorithm, double conv)
+void Local::localize(const std::string& algorithm, double conv)
 {
     if (algorithm == "CHOLESKY")
-        return localize_cholesky(conv);    
+        localize_cholesky(conv);    
     else if (algorithm == "CHOLESKY")
-        return localize_pipek_mezey(conv);    
+        localize_pipek_mezey(conv);    
     else if (algorithm == "BOYS")
-        return localize_boys(conv);    
+        localize_boys(conv);    
     else if (algorithm == "ER")
-        return localize_er(conv);    
+        localize_er(conv);    
     else 
         throw PSIEXCEPTION("Localization algorithm not recognized");
 }
-boost::shared_ptr<Matrix> Local::localize_cholesky(double conv) 
+void Local::localize_cholesky(double conv) 
 {
     if (print_) {
         fprintf(outfile, "  ==> Localization: Cholesky <==\n\n");
@@ -99,30 +109,237 @@ boost::shared_ptr<Matrix> Local::localize_cholesky(double conv)
     for (int i = 0; i < nmo; i++) {
         C_DCOPY(nso, &Dp[0][i], nso, &Lp[0][i], nmo);
     }
-
-    return L_AO_;
 }
-boost::shared_ptr<Matrix> Local::localize_pipek_mezey(double conv) 
+void Local::localize_pipek_mezey(double conv) 
 {
     throw FeatureNotImplemented("psi::Local","localize_pipek_mezey",__FILE__,__LINE__);
     L_AO_ = boost::shared_ptr<Matrix>(C_AO()->clone());
 
-    return L_AO_;
 }
-boost::shared_ptr<Matrix> Local::localize_boys(double conv) 
+void Local::localize_boys(double conv) 
 {
     throw FeatureNotImplemented("psi::Local","localize_boys",__FILE__,__LINE__);
     L_AO_ = boost::shared_ptr<Matrix>(C_AO()->clone());
 
-    return L_AO_;
 }
-boost::shared_ptr<Matrix> Local::localize_er(double conv) 
+void Local::localize_er(double conv) 
 {
     throw FeatureNotImplemented("psi::Local","localize_er",__FILE__,__LINE__);
     L_AO_ = boost::shared_ptr<Matrix>(C_AO()->clone());
 
-    return L_AO_;
 }
+boost::shared_ptr<Matrix> Local::lowdin_charges(boost::shared_ptr<Matrix> C)
+{
+    boost::shared_ptr<Molecule> molecule = basisset_->molecule(); 
+    int nmo = C->colspi()[0];
+    int nso = C->rowspi()[0];
+    int natom = molecule->natom();
+    boost::shared_ptr<Matrix> Q(new Matrix("Q: Gross Lowdin charges (nmo x natom)", nmo, natom));
+
+    boost::shared_ptr<Matrix> XC(new Matrix("XC", nso, nmo));
+
+    double** Cp  = C->pointer();
+    double** Xp  = X_->pointer();
+    double** XCp = XC->pointer();
+
+    C_DGEMM('N','N',nso,nmo,nso,1.0,Xp[0],nso,Cp[0],nmo,0.0,XCp[0],nmo);
+
+    double** Qp = Q->pointer();
+    for (int i = 0; i < nmo; i++) {
+        for (int m = 0; m < nso; m++) {
+            int atom = basisset_->shell_to_center(basisset_->function_to_shell(m));
+            Qp[i][atom] += XCp[m][i] * XCp[m][i];
+        }
+    }
+
+    return Q;
+}
+void Local::compute_boughton_pulay_domains(double Qcutoff) 
+{
+    boost::shared_ptr<Molecule> molecule = basisset_->molecule(); 
+    int nmo = L_AO_->colspi()[0];
+    int nso = L_AO_->rowspi()[0];
+    int natom = molecule->natom();
+
+    // Build Lowdin charges for current local coefficients
+    Q_ = lowdin_charges(L_AO_);
+    double** Qp = Q_->pointer();    
+
+    // Clear domains
+    domains_.clear();
+    auxiliary_domains_.clear();
+
+    // Build each domain
+    for (int i = 0; i < nmo; i++) {
+        std::set<int> total_atoms;
+
+        // TODO
+
+        domains_.push_back(OrbitalDomain::buildOrbitalDomain(basisset_, total_atoms));
+        if (auxiliary_.get() != NULL) 
+            auxiliary_domains_.push_back(OrbitalDomain::buildOrbitalDomain(auxiliary_, total_atoms));
+    } 
+}
+void Local::compute_polly_domains(double Qcutoff, double Rext, double Qcheck) 
+{
+    boost::shared_ptr<Molecule> molecule = basisset_->molecule(); 
+    int nmo = L_AO_->colspi()[0];
+    int nso = L_AO_->rowspi()[0];
+    int natom = molecule->natom();
+
+    // Build Lowdin charges for current local coefficients
+    Q_ = lowdin_charges(L_AO_);
+    double** Qp = Q_->pointer();    
+
+    // Clear domains
+    domains_.clear();
+    auxiliary_domains_.clear();
+
+    // Build each domain
+    for (int i = 0; i < nmo; i++) {
+        std::set<int> charge_atoms;
+        std::set<int> range_atoms;
+        std::set<int> total_atoms;
+
+        // Add atoms if they are greater than the charge cutoff
+        for (int A = 0; A < natom; A++) {
+            if (Qp[i][A] > Qcutoff) {
+                charge_atoms.insert(A);
+            }
+        }
+
+        // Add atoms if they are inside the extended range parameter
+        for (std::set<int>::const_iterator it = charge_atoms.begin();
+            it != charge_atoms.end(); it++) {
+            Vector3 v = molecule->xyz(*it);
+            for (int A = 0; A < natom; A++) {
+                if (v.distance(molecule->xyz(A)) < Rext) {
+                    range_atoms.insert(A);
+                }
+            }
+        }
+
+        // Merge the two lists        
+        for (std::set<int>::const_iterator it = charge_atoms.begin();
+            it != charge_atoms.end(); it++) {
+            total_atoms.insert((*it));
+        }
+        for (std::set<int>::const_iterator it = range_atoms.begin();
+            it != range_atoms.end(); it++) {
+            total_atoms.insert((*it));
+        }
+
+        // Check total charge to be sure the domain did not sneak by
+        double charge_check = 0.0;
+        for (std::set<int>::const_iterator it = total_atoms.begin();
+            it != total_atoms.end(); it++) {
+            charge_check += Qp[i][(*it)];
+        }
+
+        // If the domain sneaks by, add all atoms to the domain
+        if (charge_check < Qcheck) {
+            total_atoms.clear();
+            for (int A = 0; A < natom; A++) {
+                total_atoms.insert(A);
+            }
+        }
+
+        domains_.push_back(OrbitalDomain::buildOrbitalDomain(basisset_, total_atoms));
+        if (auxiliary_.get() != NULL) 
+            auxiliary_domains_.push_back(OrbitalDomain::buildOrbitalDomain(auxiliary_, total_atoms));
+    } 
+}
+OrbitalDomain::OrbitalDomain()
+{
+}
+OrbitalDomain::~OrbitalDomain()
+{
+}
+void OrbitalDomain::print(FILE* out, int label)
+{
+    fprintf(out, "  => Orbital Domain %d: %ld Atoms <=\n\n", label, atoms_.size());
+
+    fprintf(out, "    Atoms Local to Global:\n");
+    for (int A = 0; A < atoms_local_to_global_.size(); A++) {
+        fprintf(out, "    %4d -> %4d\n", A, atoms_local_to_global_[A]);
+    } 
+    fprintf(out, "    \n");
+
+    fprintf(out, "    Atoms Global to Local:\n");
+    for (int A = 0; A < atoms_global_to_local_.size(); A++) {
+        int a = atoms_local_to_global_[A];
+        fprintf(out, "    %4d -> %4d\n", a, atoms_global_to_local_[a]);
+    } 
+    fprintf(out, "    \n");
+
+    fprintf(out, "    Shells Local to Global:\n");
+    for (int A = 0; A < shells_local_to_global_.size(); A++) {
+        fprintf(out, "    %4d -> %4d\n", A, shells_local_to_global_[A]);
+    } 
+    fprintf(out, "    \n");
+
+    fprintf(out, "    Shells Global to Local:\n");
+    for (int A = 0; A < shells_global_to_local_.size(); A++) {
+        int a = shells_local_to_global_[A];
+        fprintf(out, "    %4d -> %4d\n", a, shells_global_to_local_[a]);
+    } 
+    fprintf(out, "    \n");
+
+    fprintf(out, "    Functions Local to Global:\n");
+    for (int A = 0; A < functions_local_to_global_.size(); A++) {
+        fprintf(out, "    %4d -> %4d\n", A, functions_local_to_global_[A]);
+    } 
+    fprintf(out, "    \n");
+
+    fprintf(out, "    Functions Global to Local:\n");
+    for (int A = 0; A < functions_global_to_local_.size(); A++) {
+        int a = functions_local_to_global_[A];
+        fprintf(out, "    %4d -> %4d\n", a, functions_global_to_local_[a]);
+    } 
+    fprintf(out, "    \n");
+}
+boost::shared_ptr<OrbitalDomain> OrbitalDomain::buildOrbitalDomain(boost::shared_ptr<BasisSet> basis, const std::set<int>& atoms)
+{
+    boost::shared_ptr<OrbitalDomain> domain(new OrbitalDomain());
+    domain->atoms_ = atoms;
+   
+    boost::shared_ptr<Molecule> molecule = basis->molecule();
+    int natom = molecule->natom();
+    int nso = basis->nbf();
+    int nshell = basis->nshell(); 
+
+    for (std::set<int>::const_iterator it = atoms.begin();
+        it != atoms.end(); it++) {
+        domain->atoms_local_to_global_.push_back((*it));
+    }
+
+    std::sort(domain->atoms_local_to_global_.begin(), domain->atoms_local_to_global_.end());
+
+    for (int A = 0; A < atoms.size(); A++) {
+        domain->atoms_global_to_local_[domain->atoms_local_to_global_[A]] = A;
+    } 
+
+    int shell_counter = 0;
+    int fun_counter = 0;
+    for (int M = 0; M < nshell; M++) {
+        int atom = basis->shell_to_center(M);
+        if (!atoms.count(atom)) continue;
+   
+        domain->shells_local_to_global_.push_back(M);
+        domain->shells_global_to_local_[M] = shell_counter++;
+
+        int mstart = basis->shell(M)->function_index();
+        int nM = basis->shell(M)->nfunction();
+
+        for (int om = 0; om < nM; om++) {
+            domain->functions_local_to_global_.push_back(om + mstart);
+            domain->functions_global_to_local_[om + mstart] = fun_counter++;
+        } 
+    }
+
+    return domain;
+}
+
 
 }
 
