fe016e0b34e909950fa63b042c57ca507dc45315
ssh2 <ssh2@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-08-15 03:16:08 +0000

    Attempts to fix Tau Squared in the DCFT code. Tau Squared correction is still different from the spin-orbital code...
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@2093 6a214465-e6f0-41bc-91e6-de896bb96b5e
    
    Former-commit-id: 518d9b1338c2e3b64fca2037359d4f78a95a155a

--- src/bin/dcft/dcft_tau.cc
+++ src/bin/dcft/dcft_tau.cc
@@ -166,7 +166,7 @@ DCFTSolver::compute_tau_squared()
 {
 
     dpdfile2 T_OO, T_oo, T_VV, T_vv;
-    dpdfile2 TT_OO, TT_oo, TT_VV, TT_vv;
+//    dpdfile2 TT_OO, TT_oo, TT_VV, TT_vv;
 
     dpd_file2_init(&T_OO, PSIF_DCFT_DPD, 0,
                   _ints->DPD_ID('O'), _ints->DPD_ID('O'), "Tau <O|O>");
@@ -177,23 +177,23 @@ DCFTSolver::compute_tau_squared()
     dpd_file2_init(&T_vv, PSIF_DCFT_DPD, 0,
                   _ints->DPD_ID('v'), _ints->DPD_ID('v'), "Tau <v|v>");
 
-    dpd_file2_init(&TT_OO, PSIF_DCFT_DPD, 0,
-                  _ints->DPD_ID('O'), _ints->DPD_ID('O'), "Tau^2 <O|O>");
-    dpd_file2_init(&TT_oo, PSIF_DCFT_DPD, 0,
-                  _ints->DPD_ID('o'), _ints->DPD_ID('o'), "Tau^2 <o|o>");
-    dpd_file2_init(&TT_VV, PSIF_DCFT_DPD, 0,
-                  _ints->DPD_ID('V'), _ints->DPD_ID('V'), "Tau^2 <V|V>");
-    dpd_file2_init(&TT_vv, PSIF_DCFT_DPD, 0,
-                  _ints->DPD_ID('v'), _ints->DPD_ID('v'), "Tau^2 <v|v>");
+//    dpd_file2_init(&TT_OO, PSIF_DCFT_DPD, 0,
+//                  _ints->DPD_ID('O'), _ints->DPD_ID('O'), "Tau^2 <O|O>");
+//    dpd_file2_init(&TT_oo, PSIF_DCFT_DPD, 0,
+//                  _ints->DPD_ID('o'), _ints->DPD_ID('o'), "Tau^2 <o|o>");
+//    dpd_file2_init(&TT_VV, PSIF_DCFT_DPD, 0,
+//                  _ints->DPD_ID('V'), _ints->DPD_ID('V'), "Tau^2 <V|V>");
+//    dpd_file2_init(&TT_vv, PSIF_DCFT_DPD, 0,
+//                  _ints->DPD_ID('v'), _ints->DPD_ID('v'), "Tau^2 <v|v>");
 
     dpd_file2_mat_init(&T_OO);
     dpd_file2_mat_init(&T_oo);
     dpd_file2_mat_init(&T_VV);
     dpd_file2_mat_init(&T_vv);
-    dpd_file2_mat_init(&TT_OO);
-    dpd_file2_mat_init(&TT_oo);
-    dpd_file2_mat_init(&TT_VV);
-    dpd_file2_mat_init(&TT_vv);
+//    dpd_file2_mat_init(&TT_OO);
+//    dpd_file2_mat_init(&TT_oo);
+//    dpd_file2_mat_init(&TT_VV);
+//    dpd_file2_mat_init(&TT_vv);
     dpd_file2_mat_rd(&T_OO);
     dpd_file2_mat_rd(&T_oo);
     dpd_file2_mat_rd(&T_VV);
@@ -207,94 +207,125 @@ DCFTSolver::compute_tau_squared()
         // Alpha occupied
         for(int i = 0 ; i < naoccpi_[h]; ++i){
             for(int j = 0 ; j < naoccpi_[h]; ++j){
-                TT_OO.matrix[h][i][j] = (-1.0) * T_OO.matrix[h][i][j] * T_OO.matrix[h][i][j];
+                a_tautau_->set(h, i, j, (-1.0) * T_OO.matrix[h][i][j] * T_OO.matrix[h][i][j]);
             }
         }
         // Beta occupied
         for(int i = 0 ; i < nboccpi_[h]; ++i){
             for(int j = 0 ; j < nboccpi_[h]; ++j){
-                TT_oo.matrix[h][i][j] = (-1.0) * T_oo.matrix[h][i][j] * T_oo.matrix[h][i][j];
+                b_tautau_->set(h, i, j, (-1.0) * T_oo.matrix[h][i][j] * T_oo.matrix[h][i][j]);
             }
         }
         // Alpha virtual
         for(int i = 0 ; i < navirpi_[h]; ++i){
             for(int j = 0 ; j < navirpi_[h]; ++j){
-                TT_VV.matrix[h][i][j] = T_VV.matrix[h][i][j] * T_VV.matrix[h][i][j];
+                a_tautau_->set(h, naoccpi_[h] + i, naoccpi_[h] + j, T_VV.matrix[h][i][j] * T_VV.matrix[h][i][j]);
             }
         }
         // Beta virtual
         for(int i = 0 ; i < nbvirpi_[h]; ++i){
             for(int j = 0 ; j < nbvirpi_[h]; ++j){
-                TT_vv.matrix[h][i][j] = T_vv.matrix[h][i][j] * T_vv.matrix[h][i][j];
+                b_tautau_->set(h, nboccpi_[h] + i, nboccpi_[h] + j, T_vv.matrix[h][i][j] * T_vv.matrix[h][i][j]);
             }
         }
+
+//        // Alpha occupied
+//        for(int i = 0 ; i < naoccpi_[h]; ++i){
+//            for(int j = 0 ; j < naoccpi_[h]; ++j){
+//                TT_OO.matrix[h][i][j] = (-1.0) * T_OO.matrix[h][i][j] * T_OO.matrix[h][i][j];
+//            }
+//        }
+//        // Beta occupied
+//        for(int i = 0 ; i < nboccpi_[h]; ++i){
+//            for(int j = 0 ; j < nboccpi_[h]; ++j){
+//                TT_oo.matrix[h][i][j] = (-1.0) * T_oo.matrix[h][i][j] * T_oo.matrix[h][i][j];
+//            }
+//        }
+//        // Alpha virtual
+//        for(int i = 0 ; i < navirpi_[h]; ++i){
+//            for(int j = 0 ; j < navirpi_[h]; ++j){
+//                TT_VV.matrix[h][i][j] = T_VV.matrix[h][i][j] * T_VV.matrix[h][i][j];
+//            }
+//        }
+//        // Beta virtual
+//        for(int i = 0 ; i < nbvirpi_[h]; ++i){
+//            for(int j = 0 ; j < nbvirpi_[h]; ++j){
+//                TT_vv.matrix[h][i][j] = T_vv.matrix[h][i][j] * T_vv.matrix[h][i][j];
+//            }
+//        }
     }
 
-    a_tautau_->zero();
-    b_tautau_->zero();
+//    a_tautau_->zero();
+//    b_tautau_->zero();
 
-    for(int h = 0; h < nirrep_; ++h){
-        if(nsopi_[h] == 0) continue;
+//    for(int h = 0; h < nirrep_; ++h){
+//        if(nsopi_[h] == 0) continue;
 
-        double **temp = block_matrix(nsopi_[h], nsopi_[h]);
-        double **paOccC = aocc_c_->pointer(h);
-        double **pbOccC = bocc_c_->pointer(h);
-        double **paVirC = avir_c_->pointer(h);
-        double **pbVirC = bvir_c_->pointer(h);
-        double **pa_tautau_ = a_tautau_->pointer(h);
-        double **pb_tautau_ = b_tautau_->pointer(h);
+//        double **temp = block_matrix(nsopi_[h], nsopi_[h]);
+//        double **paOccC = aocc_c_->pointer(h);
+//        double **pbOccC = bocc_c_->pointer(h);
+//        double **paVirC = avir_c_->pointer(h);
+//        double **pbVirC = bvir_c_->pointer(h);
+//        double **pa_tautau_ = a_tautau_->pointer(h);
+//        double **pb_tautau_ = b_tautau_->pointer(h);
 
-        /*
-         * Backtransform the Tau^2 correction to the AO basis
-         * tau^2Trans = C moTau^2 Ct
-         */
+//        /*
+//         * Backtransform the Tau^2 correction to the AO basis
+//         * tau^2Trans = C moTau^2 Ct
+//         */
 
-        // Alpha occupied
-        if(naoccpi_[h] && nsopi_[h]){
-            C_DGEMM('n', 'n', nsopi_[h], naoccpi_[h], naoccpi_[h], 1.0, paOccC[0], naoccpi_[h],
-                    TT_OO.matrix[h][0], naoccpi_[h], 0.0, temp[0], nsopi_[h]);
-            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], naoccpi_[h], 1.0, temp[0], nsopi_[h],
-                    paOccC[0], naoccpi_[h], 1.0, pa_tautau_[0], nsopi_[h]);
-        }
-        // Beta occupied
-        if(nboccpi_[h] && nsopi_[h]){
-            C_DGEMM('n', 'n', nsopi_[h], nboccpi_[h], nboccpi_[h], 1.0, pbOccC[0], nboccpi_[h],
-                    TT_oo.matrix[h][0], nboccpi_[h], 0.0, temp[0], nsopi_[h]);
-            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], nboccpi_[h], 1.0, temp[0], nsopi_[h],
-                    pbOccC[0], nboccpi_[h], 1.0, pb_tautau_[0], nsopi_[h]);
-        }
-        // Alpha virtual
-        if(navirpi_[h] && nsopi_[h]){
-            C_DGEMM('n', 'n', nsopi_[h], navirpi_[h], navirpi_[h], 1.0, paVirC[0], navirpi_[h],
-                    TT_VV.matrix[h][0], navirpi_[h], 0.0, temp[0], nsopi_[h]);
-            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], navirpi_[h], 1.0, temp[0], nsopi_[h],
-                    paVirC[0], navirpi_[h], 1.0, pa_tautau_[0], nsopi_[h]);
-        }
-        // Beta virtual
-        if(nbvirpi_[h] && nsopi_[h]){
-            C_DGEMM('n', 'n', nsopi_[h], nbvirpi_[h], nbvirpi_[h], 1.0, pbVirC[0], nbvirpi_[h],
-                    TT_vv.matrix[h][0], nbvirpi_[h], 0.0, temp[0], nsopi_[h]);
-            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], nbvirpi_[h], 1.0, temp[0], nsopi_[h],
-                    pbVirC[0], nbvirpi_[h], 1.0, pb_tautau_[0], nsopi_[h]);
-        }
+//        // Alpha occupied
+//        if(naoccpi_[h] && nsopi_[h]){
+//            C_DGEMM('n', 'n', nsopi_[h], naoccpi_[h], naoccpi_[h], 1.0, paOccC[0], naoccpi_[h],
+//                    TT_OO.matrix[h][0], naoccpi_[h], 0.0, temp[0], nsopi_[h]);
+//            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], naoccpi_[h], 1.0, temp[0], nsopi_[h],
+//                    paOccC[0], naoccpi_[h], 1.0, pa_tautau_[0], nsopi_[h]);
+//        }
+//        // Beta occupied
+//        if(nboccpi_[h] && nsopi_[h]){
+//            C_DGEMM('n', 'n', nsopi_[h], nboccpi_[h], nboccpi_[h], 1.0, pbOccC[0], nboccpi_[h],
+//                    TT_oo.matrix[h][0], nboccpi_[h], 0.0, temp[0], nsopi_[h]);
+//            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], nboccpi_[h], 1.0, temp[0], nsopi_[h],
+//                    pbOccC[0], nboccpi_[h], 1.0, pb_tautau_[0], nsopi_[h]);
+//        }
+//        // Alpha virtual
+//        if(navirpi_[h] && nsopi_[h]){
+//            C_DGEMM('n', 'n', nsopi_[h], navirpi_[h], navirpi_[h], 1.0, paVirC[0], navirpi_[h],
+//                    TT_VV.matrix[h][0], navirpi_[h], 0.0, temp[0], nsopi_[h]);
+//            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], navirpi_[h], 1.0, temp[0], nsopi_[h],
+//                    paVirC[0], navirpi_[h], 1.0, pa_tautau_[0], nsopi_[h]);
+//        }
+//        // Beta virtual
+//        if(nbvirpi_[h] && nsopi_[h]){
+//            C_DGEMM('n', 'n', nsopi_[h], nbvirpi_[h], nbvirpi_[h], 1.0, pbVirC[0], nbvirpi_[h],
+//                    TT_vv.matrix[h][0], nbvirpi_[h], 0.0, temp[0], nsopi_[h]);
+//            C_DGEMM('n', 't', nsopi_[h], nsopi_[h], nbvirpi_[h], 1.0, temp[0], nsopi_[h],
+//                    pbVirC[0], nbvirpi_[h], 1.0, pb_tautau_[0], nsopi_[h]);
+//        }
 
-        free_block(temp);
-    }
+//        free_block(temp);
+//    }
 
-    dpd_file2_mat_wrt(&TT_OO);
-    dpd_file2_mat_wrt(&TT_oo);
-    dpd_file2_mat_wrt(&TT_VV);
-    dpd_file2_mat_wrt(&TT_vv);
+//    dpd_file2_mat_wrt(&TT_OO);
+//    dpd_file2_mat_wrt(&TT_oo);
+//    dpd_file2_mat_wrt(&TT_VV);
+//    dpd_file2_mat_wrt(&TT_vv);
+
+    dpd_file2_print(&T_OO, outfile);
+    dpd_file2_print(&T_VV, outfile);
+
+//    dpd_file2_print(&TT_OO, outfile);
+//    dpd_file2_print(&TT_VV, outfile);
 
     dpd_file2_close(&T_OO);
     dpd_file2_close(&T_oo);
     dpd_file2_close(&T_VV);
     dpd_file2_close(&T_vv);
 
-    dpd_file2_close(&TT_OO);
-    dpd_file2_close(&TT_oo);
-    dpd_file2_close(&TT_VV);
-    dpd_file2_close(&TT_vv);
+//    dpd_file2_close(&TT_OO);
+//    dpd_file2_close(&TT_oo);
+//    dpd_file2_close(&TT_VV);
+//    dpd_file2_close(&TT_vv);
 }
 
 /**
