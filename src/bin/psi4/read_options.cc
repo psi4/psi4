f9a9f144835dfecf5f35459c75a811d90838d071
loriab <loriab@6a214465-e6f0-41bc-91e6-de896bb96b5e>
2011-08-20 03:49:46 +0000

    Incorporating documentation scripts into Makefile system and crude userman basis section
    git-svn-id: https://sirius.chem.vt.edu/svn/psi4/trunk@2118 6a214465-e6f0-41bc-91e6-de896bb96b5e

--- src/bin/psi4/read_options.cc
+++ src/bin/psi4/read_options.cc
@@ -1921,7 +1921,7 @@ int read_options(const std::string &name, Options & options, bool suppress_print
       options.add_bool("TEST_DERIVATIVE_B", false);
       /*- Read Cartesian Hessian -*/
       options.add_bool("READ_CARTESIAN_H", false);
-      /*- Define IRC step size in bohr(amu)^1/2 -*/
+      /*- Define IRC step size in bohr(amu)$^1/2$ -*/
       options.add_double("IRC_STEP_SIZE", 0.2);
       /*- Define IRC mapping direction {FORWARD, BACKWARD} -*/
       options.add_str("IRC_DIRECTION", "FORWARD", "FORWARD BACKWARD");
