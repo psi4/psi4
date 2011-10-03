963f1bb0534cb1529678d7c04a3b252928ecea4a
Justin Turney <justin.turney@gmail.com>
2011-10-03 13:52:20 -0400

    Adds projection code to MOIndexSpace using Werner's approach from Mol. Phys. 102, 21-22, 2311

--- src/lib/libmints/moindexspace.cc
+++ src/lib/libmints/moindexspace.cc
@@ -9,13 +9,13 @@ MOIndexSpace::MOIndexSpace(const std::string& name,
                            const boost::shared_ptr<BasisSet>& basis,
                            const boost::shared_ptr<IntegralFactory>& ints)
     : nirrep_(full_C->nirrep()), name_(name), C_(full_C),
-      evals_(evals), basis_(basis), ints_(ints)
+      evals_(evals), basis_(basis), ints_(ints), dim_(full_C->colspi())
 {
 }
 
 MOIndexSpace::MOIndexSpace(const std::string &name, const boost::shared_ptr<Wavefunction> &wave)
     : nirrep_(wave->nirrep()), name_(name), C_(wave->Ca()), evals_(wave->epsilon_a()),
-      basis_(wave->basisset()), ints_(wave->integral())
+      basis_(wave->basisset()), ints_(wave->integral()), dim_(wave->Ca()->colspi())
 {
 }
 
@@ -54,21 +54,54 @@ const Dimension& MOIndexSpace::dim() const
     return dim_;
 }
 
-boost::shared_ptr<Matrix> MOIndexSpace::transform(const MOIndexSpace& space2, const MOIndexSpace& space1)
+MOIndexSpace MOIndexSpace::transform(const MOIndexSpace& A, const boost::shared_ptr<BasisSet>& B)
 {
-    boost::shared_ptr<Matrix> S21 = overlap(space2, space1);
+    boost::shared_ptr<Matrix> SBA = overlap(B, A.basis());
+    SBA->set_name("Sba");
+    boost::shared_ptr<Matrix> SBB = overlap(B, B);
+    SBB->set_name("SBB");
 
-    fprintf(outfile, "MOIndexSpace::transform: This doesn't work.\n");
+    // Follows Werner's method from Mol. Phys. 102, 21-22, 2311
+    // just like HF::dualBasisProjection
 
-//    // Invert S21
-//    S21->general_invert();
+    // 1. Invert SBB
+    SBB->invert();
+    SBB->set_name("SBB^-1");
 
-//    S21->set_name("Transformation Matrix between space1 and space2");
+    // 2. Form T
+    boost::shared_ptr<Matrix> I = Matrix::create("I = SAB SBB SBA", SBA->colspi(), SBA->colspi());
+    I->transform(SBB, SBA);
 
-    return S21;
+    boost::shared_ptr<Matrix> T = Matrix::create("T", A.dim(), A.dim());
+    T->transform(I, A.C());
+    I.reset(); // release memory
+
+    // 3. Form T^{-1/2}
+    T->power(-0.5);
+
+    // 4. Cb = [Sbb]^-1 Sba] Ca T^{-1/2}
+    // 4a. Ca T^{-1/2}
+    boost::shared_ptr<Matrix> CaT = Matrix::create("Ca*T^{-1/2}", A.C()->rowspi(), A.C()->colspi());
+    CaT->gemm(false, false, 1.0, A.C(), T, 0.0);
+
+    // 4b. Sba * 4a
+    boost::shared_ptr<Matrix> SbaCaT = Matrix::create("SbaCaT", SBB->rowspi(), A.C()->colspi());
+    SbaCaT->gemm(false, false, 1.0, SBA, CaT, 0.0);
+
+    // 4c. [Sbb]^-1 * 4b
+    boost::shared_ptr<Matrix> Cb = Matrix::create("Cb", SBB->rowspi(), A.C()->colspi());
+    Cb->gemm(false, false, 1.0, SBB, SbaCaT, 0.0);
+
+    boost::shared_ptr<IntegralFactory> i(new IntegralFactory(B, B, B, B));
+
+    return MOIndexSpace("Ca transformed into Cb",
+                        Cb,
+                        A.evals(),
+                        B,
+                        i);
 }
 
-boost::shared_ptr<Matrix> MOIndexSpace::overlap(const MOIndexSpace &space2, const MOIndexSpace &space1)
+boost::shared_ptr<Matrix> MOIndexSpace::overlap(const MOIndexSpace &space1, const MOIndexSpace &space2)
 {
     IntegralFactory mix_ints(space1.basis(), space2.basis(), space1.basis(), space2.basis());
 
@@ -79,13 +112,24 @@ boost::shared_ptr<Matrix> MOIndexSpace::overlap(const MOIndexSpace &space2, cons
     S->compute(Smat);
     delete S;
 
-    boost::shared_ptr<Matrix> V1 = space1.C();
-    boost::shared_ptr<Matrix> V2 = space2.C();
+    return Smat;
+}
 
-    boost::shared_ptr<Matrix> result(new Matrix("Overlap between space1 and space2", V1->colspi(), V2->colspi()));
-    result->transform(V1, Smat, V2);
+boost::shared_ptr<Matrix> MOIndexSpace::overlap(const boost::shared_ptr<BasisSet>& basis1,
+                                                const boost::shared_ptr<BasisSet>& basis2)
+{
+    IntegralFactory mix_ints(basis1, basis2);
+    SOBasisSet sobasis1(basis1, &mix_ints);
+    SOBasisSet sobasis2(basis2, &mix_ints);
 
-    return result;
+    boost::shared_ptr<Matrix> Smat(new Matrix("Overlap between space1 and space2",
+                                              sobasis1.dimension(), sobasis2.dimension()));
+
+    OneBodySOInt *S = mix_ints.so_overlap();
+    S->compute(Smat);
+    delete S;
+
+    return Smat;
 }
 
 } // namespace psi
