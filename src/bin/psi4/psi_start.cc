95cac0cb1cdb6516ff2e12166ca17d280f339fb5
Justin Turney <justin.turney@gmail.com>
2011-10-27 17:46:44 -0400

    Adds more to psi4 --help describing --new-plugin

--- src/bin/psi4/psi_start.cc
+++ src/bin/psi4/psi_start.cc
@@ -261,10 +261,14 @@ void print_usage(void)
     printf(" -o  --output filename    Redirect output elsewhere. Default: output.dat\n");
     printf(" -m  --messy              Leave temporary files after the run is completed.\n");
     printf(" -n  --nthread            Number of threads to use (overrides OMP_NUM_THREADS)\n");
-    printf("     --new-plugin name    Creates a new directory with files for writing a new plugin.\n");
+    printf("     --new-plugin name    Creates a new directory with files for writing a\n"
+           "                          new plugin. You can specify an additional argument\n"
+           "                          that specifies a template to use, for example:\n"
+           "                              --new-plugin name +mointegrals\n");
     printf(" -p  --prefix prefix      Prefix name for psi files. Default: psi\n");
     printf(" -v  --verbose            Print a lot of information.\n");
-    printf(" -d  --debug              Flush the outfile at every fprintf. Default: true iff --with-debug.\n");
+    printf(" -d  --debug              Flush the outfile at every fprintf.\n"
+           "                          Default: true iff --with-debug.\n");
     printf(" -V  --version            Print version information.\n");
     printf(" -w  --wipe               Clean out your scratch area.\n");
 
