9c70b2f5af38a1e7433062467a3ed8163ec988b0
rparrish <robparrish@gmail.com>
2011-11-06 14:09:05 -0500

    Work on standardizing frozen_docc/frozen_uocc
    Former-commit-id: 7a0ecc282c216666da300c38968a69d1d13238b7

--- src/lib/libmints/molecule.h
+++ src/lib/libmints/molecule.h
@@ -239,7 +239,7 @@ public:
     void set_basis_by_label(const std::string& label, const std::string& name, const std::string& type="BASIS");
 
     /// Number of frozen core for molecule given freezing state
-    int nfrozen_core(const std::string& depth="");
+    int nfrozen_core(const std::string& depth = "");
 
     /// @{
     /// Tests to see of an atom is at the passed position with a given tolerance
