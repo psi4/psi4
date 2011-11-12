04f813c12488e1955bfedbe7236a0e179bc043a2
Justin Turney <justin.turney@gmail.com>
2011-11-12 13:15:16 -0500

    Modifies the electric field integrals to use the VI recursion code; Adds electric field gradient integrals; both are untested

--- src/lib/libmints/electricfield.cc
+++ src/lib/libmints/electricfield.cc
@@ -12,7 +12,8 @@ using namespace psi;
 using namespace std;
 
 ElectricFieldInt::ElectricFieldInt(vector<SphericalTransform>& spherical_transforms, boost::shared_ptr<BasisSet> bs1, boost::shared_ptr<BasisSet> bs2, int nderiv) :
-    OneBodyAOInt(spherical_transforms, bs1, bs2, nderiv), efield_recur_(bs1->max_am(), bs2->max_am()), natom_(bs1->molecule()->natom())
+    OneBodyAOInt(spherical_transforms, bs1, bs2, nderiv),
+    efield_recur_(bs1->max_am()+2, bs2->max_am()+2), natom_(bs1->molecule()->natom())
 {
     int maxam1 = bs1_->max_am();
     int maxam2 = bs2_->max_am();
@@ -20,9 +21,16 @@ ElectricFieldInt::ElectricFieldInt(vector<SphericalTransform>& spherical_transfo
     int maxnao1 = INT_NCART(maxam1);
     int maxnao2 = INT_NCART(maxam2);
 
-    buffer_ = new double[3*natom_*maxnao1*maxnao2];
-
-    set_chunks(3*natom_);
+    if (nderiv == 0) {
+        buffer_ = new double[3*natom_*maxnao1*maxnao2];
+        set_chunks(3*natom_);
+    }
+    else if (nderiv == 1) {
+        buffer_ = new double[6*natom_*maxnao1*maxnao2];
+        set_chunks(6*natom_);
+    }
+    else
+        throw FeatureNotImplemented("LibMints", "ElectricFieldInts called with deriv > 1",  __FILE__, __LINE__);
 }
 
 ElectricFieldInt::~ElectricFieldInt()
@@ -63,17 +71,12 @@ void ElectricFieldInt::compute_pair(const boost::shared_ptr<GaussianShell>& s1,
     AB2 += (A[0] - B[0]) * (A[0] - B[0]);
     AB2 += (A[1] - B[1]) * (A[1] - B[1]);
     AB2 += (A[2] - B[2]) * (A[2] - B[2]);
-    double AB = sqrt(AB2);
 
-    memset(buffer_, 0, 3 * natom_ * s1->ncartesian() * s2->ncartesian() * sizeof(double));
+    memset(buffer_, 0, 3 * natom_ * size * sizeof(double));
 
-    double ***ex = efield_recur_.ex();
-    double ***ey = efield_recur_.ey();
-    double ***ez = efield_recur_.ez();
-
-//    double ***ex = efield_recur_.vx();
-//    double ***ey = efield_recur_.vy();
-//    double ***ez = efield_recur_.vz();
+    double ***ex = efield_recur_.vx();
+    double ***ey = efield_recur_.vy();
+    double ***ez = efield_recur_.vz();
 
     for (int p1=0; p1<nprim1; ++p1) {
         double a1 = s1->exp(p1);
@@ -104,7 +107,6 @@ void ElectricFieldInt::compute_pair(const boost::shared_ptr<GaussianShell>& s1,
                 int atomoffset = atom * 3 * size;
 
                 double PC[3];
-                double Z = (double)bs1_->molecule()->Z(atom);
                 Vector3 C = bs1_->molecule()->xyz(atom);
 
                 PC[0] = P[0] - C[0];
@@ -136,7 +138,124 @@ void ElectricFieldInt::compute_pair(const boost::shared_ptr<GaussianShell>& s1,
                                 buffer_[atomoffset+ao12+ydisp] += ey[iind][jind][0] * over_pf;
                                 buffer_[atomoffset+ao12+zdisp] += ez[iind][jind][0] * over_pf;
 
-//                                fprintf(outfile, "final: over_pf = %10.8lf AIX[%d][%d][0] = %10.8lf\tAIY = %10.8lf\tAIZ = %10.8lf\n", over_pf, iind, jind, ex[iind][jind][0], ey[iind][jind][0], ez[iind][jind][0]);
+                                ao12++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ElectricFieldInt::compute_pair_deriv1(const boost::shared_ptr<GaussianShell>& s1,
+                                           const boost::shared_ptr<GaussianShell>& s2)
+{
+    int ao12;
+    int am1 = s1->am();
+    int am2 = s2->am();
+    int nprim1 = s1->nprimitive();
+    int nprim2 = s2->nprimitive();
+    double A[3], B[3];
+    A[0] = s1->center()[0];
+    A[1] = s1->center()[1];
+    A[2] = s1->center()[2];
+    B[0] = s2->center()[0];
+    B[1] = s2->center()[1];
+    B[2] = s2->center()[2];
+
+    int izm = 1;
+    int iym = am1 + 1;
+    int ixm = iym * iym;
+    int jzm = 1;
+    int jym = am2 + 1;
+    int jxm = jym * jym;
+
+    // Not sure if these are needed.
+    int size =  INT_NCART(am1) * INT_NCART(am2);
+    int xydisp = size;
+    int xzdisp = xydisp + size;
+    int yydisp = xzdisp + size;
+    int yzdisp = yydisp + size;
+    int zzdisp = yzdisp + size;
+
+    // compute intermediates
+    double AB2 = 0.0;
+    AB2 += (A[0] - B[0]) * (A[0] - B[0]);
+    AB2 += (A[1] - B[1]) * (A[1] - B[1]);
+    AB2 += (A[2] - B[2]) * (A[2] - B[2]);
+
+    memset(buffer_, 0, 6 * natom_ * size * sizeof(double));
+
+    double ***exx = efield_recur_.vxx();
+    double ***exy = efield_recur_.vxy();
+    double ***exz = efield_recur_.vxz();
+    double ***eyy = efield_recur_.vyy();
+    double ***eyz = efield_recur_.vyz();
+    double ***ezz = efield_recur_.vzz();
+
+    for (int p1=0; p1<nprim1; ++p1) {
+        double a1 = s1->exp(p1);
+        double c1 = s1->coef(p1);
+        for (int p2=0; p2<nprim2; ++p2) {
+            double a2 = s2->exp(p2);
+            double c2 = s2->coef(p2);
+            double gamma = a1 + a2;
+            double oog = 1.0 / gamma;
+
+            double PA[3], PB[3];
+            double P[3];
+
+            P[0] = (a1*A[0] + a2*B[0])*oog;
+            P[1] = (a1*A[1] + a2*B[1])*oog;
+            P[2] = (a1*A[2] + a2*B[2])*oog;
+            PA[0] = P[0] - A[0];
+            PA[1] = P[1] - A[1];
+            PA[2] = P[2] - A[2];
+            PB[0] = P[0] - B[0];
+            PB[1] = P[1] - B[1];
+            PB[2] = P[2] - B[2];
+
+            double over_pf = exp(-a1*a2*AB2*oog) * sqrt(M_PI*oog) * M_PI * oog * c1 * c2;
+
+            // Loop over atoms of basis set 1 (only works if bs1_ and bs2_ are on the same molecule
+            for (int atom=0; atom<bs1_->molecule()->natom(); ++atom) {
+                int atomoffset = atom * 3 * size;
+
+                double PC[3];
+                Vector3 C = bs1_->molecule()->xyz(atom);
+
+                PC[0] = P[0] - C[0];
+                PC[1] = P[1] - C[1];
+                PC[2] = P[2] - C[2];
+
+                // Get recursive
+                efield_recur_.compute(PA, PB, PC, gamma, am1, am2);
+
+                // Gather contributions.
+                ao12 = 0;
+                for (int ii = 0; ii <= am1; ++ii) {
+                    int l1 = am1 - ii;
+                    for (int jj = 0; jj <= ii; ++jj) {
+                        int m1 = ii - jj;
+                        int n1 = jj;
+
+                        for (int kk = 0; kk <= am2; ++kk) {
+                            int l2 = am2 - kk;
+                            for (int ll = 0; ll <= kk; ++ll) {
+                                int m2 = kk - ll;
+                                int n2 = ll;
+
+                                // Compute location in the recursion
+                                int iind = l1 * ixm + m1 * iym + n1 * izm;
+                                int jind = l2 * jxm + m2 * jym + n2 * jzm;
+
+                                buffer_[atomoffset+ao12]        += exx[iind][jind][0] * over_pf;
+                                buffer_[atomoffset+ao12+xydisp] += exy[iind][jind][0] * over_pf;
+                                buffer_[atomoffset+ao12+xzdisp] += exz[iind][jind][0] * over_pf;
+                                buffer_[atomoffset+ao12+yydisp] += eyy[iind][jind][0] * over_pf;
+                                buffer_[atomoffset+ao12+yzdisp] += eyz[iind][jind][0] * over_pf;
+                                buffer_[atomoffset+ao12+zzdisp] += ezz[iind][jind][0] * over_pf;
 
                                 ao12++;
                             }
--- src/lib/libmints/electricfield.h
+++ src/lib/libmints/electricfield.h
@@ -5,7 +5,7 @@ namespace psi {
 
     class BasisSet;
     class GaussianShell;
-    class ObaraSaikaTwoCenterRecursion;
+    class ObaraSaikaTwoCenterVIDeriv2Recursion;
     class OneBodyAOInt;
     class IntegralFactory;
     class SphericalTransform;
@@ -19,8 +19,7 @@ namespace psi {
 class ElectricFieldInt : public OneBodyAOInt
 {
     //! Obara and Saika recursion object to be used.
-    ObaraSaikaTwoCenterElectricField efield_recur_;                 // Both ElectricField and VIDeriv should give the same result
-//    ObaraSaikaTwoCenterVIDerivRecursion efield_recur_;
+    ObaraSaikaTwoCenterVIDeriv2Recursion efield_recur_;
 
     //! Number of atoms.
     int natom_;
@@ -28,26 +27,17 @@ class ElectricFieldInt : public OneBodyAOInt
     //! Computes the electric field between two gaussian shells.
     void compute_pair(const boost::shared_ptr<GaussianShell>&, const boost::shared_ptr<GaussianShell>&);
 
+    //! Computes the electric field gradient between two gaussian shells.
+    void compute_pair_deriv1(const boost::shared_ptr<GaussianShell>&, const boost::shared_ptr<GaussianShell>&);
+
 public:
     //! Constructor. Do not call directly use an IntegralFactory.
     ElectricFieldInt(std::vector<SphericalTransform>&, boost::shared_ptr<BasisSet>, boost::shared_ptr<BasisSet>, int deriv=0);
     //! Virtual destructor
     virtual ~ElectricFieldInt();
 
-    //! Compute dipole derivative between two shells, result stored in buffer_.
-    //void compute_shell_deriv1(int, int);
-
-    /** Compute all dipole integrals and store them in an array of matrices.
-     *  @param result Contains the dipole moment integrals. Order is [mu_x, mu_y, mu_].
-     */
-//    void compute(std::vector<boost::shared_ptr<SimpleMatrix> > &result);
-    /** Compute all dipole derivatives and store them in an array of matrices.
-     *  @param result Contains the dipole moment derivative integrals. Order is [mu_x(Aix,Aiy,Aiz...An), mu_y..., mu_z...]
-     */
-    //void compute_deriv1(std::vector<boost::shared_ptr<SimpleMatrix> > &result);
-
     //! Does the method provide first derivatives?
-    bool has_deriv1() { return false; }
+    bool has_deriv1() { return true; }
 };
 
 }
