7b3742a2917a4d6fed4031cfcd9c04eb97835169
Daniel Crawford <crawdad@vt.edu>
2011-11-24 00:47:34 -0500

    Finished initial conversion of CCEOM to PSI4.  Adding test case cc12.
    Former-commit-id: 64f35c8fba47d81f17589695875e111fb7a456b2

--- src/bin/cceom/get_eom_params.cc
+++ src/bin/cceom/get_eom_params.cc
@@ -4,11 +4,11 @@
 */
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
+#include <string>
 #include <cmath>
-#include <libipv1/ip_lib.h>
 #include <libciomr/libciomr.h>
 #include <libchkpt/chkpt.h>
+#include <psi4-dec.h>
 #include "MOInfo.h"
 #include "Params.h"
 #include "Local.h"
@@ -19,154 +19,88 @@ namespace psi { namespace cceom {
 
 void get_eom_params(Options &options)
 {
-  int errcod, i, j, sym, iconv,exist, state_irrep, c_irrep;
-
+  // Number of excited states per irrep
   chkpt_init(PSIO_OPEN_OLD);
-  if (chkpt_rd_override_occ()) {
-    eom_params.states_per_irrep = chkpt_rd_statespi();
+  if (chkpt_rd_override_occ()) eom_params.states_per_irrep = chkpt_rd_statespi();
+  else if (options.exists("STATES_PER_IRREP")) {
+    if(options["STATES_PER_IRREP"].size() != moinfo.nirreps) 
+      throw PsiException("STATES_PER_IRREP is wrong size. Should be number of irreps.", __FILE__, __LINE__);
+    eom_params.states_per_irrep = new int[moinfo.nirreps];
+    for (int h=0; h < moinfo.nirreps; ++h)
+      eom_params.states_per_irrep[h] = options["STATES_PER_IRREP"][h].to_integer();
   }
-  else if (ip_exist("STATES_PER_IRREP",0)) {
-    eom_params.states_per_irrep = (int *) malloc(moinfo.nirreps * sizeof(int));
-    ip_count("STATES_PER_IRREP", &i, 0);
-    if (i != moinfo.nirreps) {
-      fprintf(outfile,"Dim. of states_per_irrep vector must be %d\n", moinfo.nirreps) ;
-      exit(0);
-    }
-    for (i=0;i<moinfo.nirreps;++i)
-      errcod = ip_data("STATES_PER_IRREP","%d",&(eom_params.states_per_irrep[i]),1,i);
-  }
-  else { fprintf(outfile,"Must have states_per_irrep vector in input.\n"); exit(PSI_RETURN_FAILURE); } 
+  else throw PsiException("Must provide states_per_irrep vector in input.", __FILE__, __LINE__);
   chkpt_close();
 
-
-  eom_params.cs_per_irrep = (int *) malloc(moinfo.nirreps * sizeof(int));
+  // Number of guess vectors per irrep
+  eom_params.cs_per_irrep = new int[moinfo.nirreps];
   eom_params.number_of_states = 0;
-  for (state_irrep=0; state_irrep<moinfo.nirreps; ++state_irrep) {
+  for (int state_irrep = 0; state_irrep < moinfo.nirreps; ++state_irrep) {
     eom_params.cs_per_irrep[state_irrep^moinfo.sym] = eom_params.states_per_irrep[state_irrep];
-	eom_params.number_of_states += eom_params.states_per_irrep[state_irrep];
+    eom_params.number_of_states += eom_params.states_per_irrep[state_irrep];
   }
-  eom_params.state_energies = (double*) malloc(eom_params.number_of_states * sizeof(double));
+  eom_params.state_energies = new double[eom_params.number_of_states];
 
   eom_params.max_iter = 80 * moinfo.nirreps;
-  errcod = ip_data("MAX_ITER","%d",&(eom_params.max_iter),0);
+  eom_params.max_iter = options.get_int("MAXITER");
 
-  /* Use prop_sym and prop_root only to determine what energy to write the file32 */
-  if (ip_exist("PROP_SYM",0)) {
-    ip_data("PROP_SYM","%d",&(eom_params.prop_sym),0);
+  // Use prop_sym and prop_root only to determine what energy to write to chkpt
+  if (options.exists("PROP_SYM")) {
+    eom_params.prop_sym = options.get_int("PROP_SYM");
     eom_params.prop_sym = (eom_params.prop_sym - 1)^moinfo.sym;
   }
   else {
-    for (i=0;i<moinfo.nirreps;++i)
-      if (eom_params.states_per_irrep[i])
-        eom_params.prop_sym = i^moinfo.sym;
+    for (int i = 0;i < moinfo.nirreps; ++i)
+      if (eom_params.states_per_irrep[i]) eom_params.prop_sym = i^moinfo.sym;
   }
-  if (ip_exist("PROP_ROOT",0)) {
-    ip_data("PROP_ROOT","%d",&(eom_params.prop_root),0);
-    if (eom_params.prop_root > eom_params.states_per_irrep[eom_params.prop_sym^moinfo.sym]) {
-      fprintf(outfile,"prop_root is too big\n");
-      exit(1);
-    }
+  if (options.exists("PROP_ROOT")) {
+    eom_params.prop_root = options.get_int("PROP_ROOT");
+    if (eom_params.prop_root > eom_params.states_per_irrep[eom_params.prop_sym^moinfo.sym])
+      throw PsiException("Value of prop_root is too large.", __FILE__, __LINE__);
   }
-  else {
-    eom_params.prop_root = eom_params.states_per_irrep[eom_params.prop_sym^moinfo.sym];
-  }
+  else eom_params.prop_root = eom_params.states_per_irrep[eom_params.prop_sym^moinfo.sym];
   --eom_params.prop_root;
 
   // by default for CC3 use follow_root to root-following based on CCSD soln
-  if ( (!strcmp(params.wfn,"EOM_CC3")) && (eom_params.prop_root != 0) ) {
-    eom_params.follow_root = true;
-  }
-  // allow user to explicitly turn off following which may help in bizarre cases
+  if (params.wfn == "EOM_CC3" && eom_params.prop_root != 0) eom_params.follow_root = true;
+  // allow user to explicitly turn off root-following which may help in bizarre cases
   // in this case prop_root is used to determine which residuals are used
-  if (ip_exist("CC3_FOLLOW_ROOT",0)) {
-    i = 0;
-    errcod = ip_boolean("CC3_FOLLOW_ROOT",&(i),0);
-    if (errcod == IPE_OK) eom_params.follow_root = i;
-  }
+  if (options.exists("CC3_FOLLOW_ROOT")) eom_params.follow_root = options.get_bool("CC3_FOLLOW_ROOT");
 
   /* so far, all R's are always kept so this is not used */
   eom_params.save_all = 0;
 
   eom_params.mult = 1;
-  eom_params.rhf_triplets = 0;
-  errcod = ip_data("RHF_TRIPLETS","%d",&(eom_params.rhf_triplets),0);
-  if (eom_params.rhf_triplets != 0) eom_params.mult = 3;
+  eom_params.rhf_triplets = options.get_bool("RHF_TRIPLETS");
+  if (eom_params.rhf_triplets) eom_params.mult = 3;
 
-  eom_params.excitation_range = 2;
-  errcod = ip_data("EXCITATION_RANGE","%d",&(eom_params.excitation_range),0);
-
-  eom_params.print_singles = 0;
-  errcod = ip_data("PRINT_SINGLES","%d",&(eom_params.print_singles),0);
-
-  eom_params.vectors_per_root_SS = 5;
-  errcod = ip_data("VECTORS_PER_ROOT_SS","%d",&(eom_params.vectors_per_root_SS),0);
-
-  eom_params.vectors_per_root = 12;
-  errcod = ip_data("VECTORS_PER_ROOT","%d",&(eom_params.vectors_per_root),0);
-
+  eom_params.excitation_range = options.get_int("EXCITATION_RANGE");
+  eom_params.print_singles = options["PRINT_SINGLES"].to_integer();
+  eom_params.vectors_per_root_SS = options.get_int("VECTORS_PER_ROOT_SS");
+  eom_params.vectors_per_root = options.get_int("VECTORS_PER_ROOT");
   eom_params.vectors_cc3 = eom_params.prop_root + 9;
-  errcod = ip_data("VECTORS_CC3","%d",&(eom_params.vectors_cc3),0);
+  eom_params.vectors_cc3 = options.get_int("VECTORS_CC3");
   if (eom_params.vectors_cc3 > eom_params.vectors_per_root)
     eom_params.vectors_cc3 = eom_params.vectors_per_root;
 
-  eom_params.collapse_with_last = 1;
-  errcod = ip_boolean("COLLAPSE_WITH_LAST",&(eom_params.collapse_with_last),0);
-
-  eom_params.complex_tol = 1E-12;
-  errcod = ip_data("COMPLEX_TOL","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.complex_tol = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.residual_tol = 1E-6;
-  errcod = ip_data("RESIDUAL_TOL","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.residual_tol = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.residual_tol_SS = 1E-6;
-  errcod = ip_data("RESIDUAL_TOL_SS","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.residual_tol_SS = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.eval_tol = 1E-8;
-  errcod = ip_data("EVAL_TOL","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.eval_tol = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.eval_tol_SS = 1E-6;
-  errcod = ip_data("EVAL_TOL_SS","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.eval_tol_SS = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.amps_to_print = 5;
-  errcod = ip_data("AMPS_TO_PRINT","%d",&i,0);
-  if(errcod == IPE_OK) eom_params.amps_to_print = i;
-
-  eom_params.schmidt_add_residual_tol = 1E-3;
-  errcod = ip_data("SCHMIDT_ADD_RESIDUAL_TOL","%d",&(iconv),0);
-  if(errcod == IPE_OK) eom_params.schmidt_add_residual_tol = 1.0*pow(10.0,(double) -iconv);
-
-  eom_params.skip_diagSS = 0;
-  i = 0;
-  errcod = ip_boolean("SKIP_DIAGSS",&(i),0);
-  if (i) eom_params.skip_diagSS = 1;
-
-  eom_params.restart_eom_cc3 = 0;
-  i = 0;
-  errcod = ip_boolean("RESTART_EOM_CC3",&(i),0);
-  if (i) eom_params.restart_eom_cc3 = 1;
-
+  eom_params.collapse_with_last = options.get_bool("COLLAPSE_WITH_LAST");
+  eom_params.complex_tol = options.get_double("COMPLEX_TOL");
+  eom_params.residual_tol = options.get_double("RESIDUAL_TOL");
+  eom_params.residual_tol_SS = options.get_double("RESIDUAL_TOL_SS");
+  eom_params.eval_tol = options.get_double("EVAL_TOL");
+  eom_params.eval_tol_SS = options.get_double("EVAL_TOL_SS");
+  eom_params.amps_to_print = options.get_int("AMPS_TO_PRINT");
+  eom_params.schmidt_add_residual_tol = options.get_double("SCHMIDT_ADD_RESIDUAL_TOL");
+  eom_params.skip_diagSS = options["SKIP_DIAGSS"].to_integer();
+  eom_params.restart_eom_cc3 = options["RESTART_EOM_CC3"].to_integer();
   eom_params.max_iter_SS = 500;
-
-  if(eom_params.guess == NULL) { /* we didn't use the cmdline arg --reuse */
-    if(ip_exist("EOM_GUESS",0))
-      errcod = ip_string("EOM_GUESS", &(eom_params.guess), 0);
-    else
-      eom_params.guess = strdup("SINGLES");
-  }
+  eom_params.guess = options.get_str("EOM_GUESS");
 
   fprintf(outfile, "\n\tCCEOM parameters:\n");
   fprintf(outfile, "\t-----------------\n");
   fprintf(outfile, "\tStates sought per irrep     =");
-  if(strcmp(eom_params.guess,"INPUT")) {
-    for (i=0;i<moinfo.nirreps;++i) fprintf(outfile, " %s %d,", moinfo.irr_labs[i],
-					   eom_params.states_per_irrep[i]);
-  }
-  else fprintf(outfile, " USER INPUT");
+  for(int i = 0; i < moinfo.nirreps; ++i) 
+    fprintf(outfile, " %s %d,", moinfo.irr_labs[i], eom_params.states_per_irrep[i]);
 
   fprintf(outfile,"\n");
   fprintf(outfile, "\tMax. number of iterations   = %5d\n", eom_params.max_iter);
@@ -180,13 +114,11 @@ void get_eom_params(Options &options)
   fprintf(outfile, "\tComplex tolerance           = %5.1e\n", eom_params.complex_tol);
   fprintf(outfile, "\tRoot for properties         = %5d\n", eom_params.prop_root + 1);
   fprintf(outfile, "\tSym of state for properties = %6s\n", moinfo.irr_labs[eom_params.prop_sym]);
-  fprintf(outfile, "\tGuess vectors taken from    = %s\n", eom_params.guess);
+  fprintf(outfile, "\tGuess vectors taken from    = %s\n", eom_params.guess.c_str());
   fprintf(outfile, "\tRestart EOM CC3             = %s\n", eom_params.restart_eom_cc3?"YES":"NO");
   fprintf(outfile, "\tCollapse with last vector   = %s\n", eom_params.collapse_with_last ? "YES":"NO");
-  if (eom_params.follow_root)
-    fprintf(outfile, "\tRoot following for CC3 turned on.\n");
+  if (eom_params.follow_root) fprintf(outfile, "\tRoot following for CC3 turned on.\n");
   fprintf(outfile, "\n\n");
 }
-
 
 }} // namespace psi::cceom
