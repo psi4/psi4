80bceac39845b0a86f6390a2be96c32a67d2527d
David Sherrill <sherrill@gatech.edu>
2011-11-29 17:36:03 -0500

    Add new RASCI test case
    Former-commit-id: 04c955c36d45afcbae974cc318c8cdae2054736b

--- src/bin/detci/structs.h
+++ src/bin/detci/structs.h
@@ -310,6 +310,7 @@ struct calcinfo {
    int **ras_orbs[4];    /* ras_orbs[ras][irr][cnt] gives an orbital number */
    int max_orbs_per_irrep; /* maximum orbials per irrep fzv not included */
    int max_pop_per_irrep;/* maximum populated orbitals per irrep fzv included */
+   int sigma_initialized; /* has sigma_init been called yet? */
    };
 
 
