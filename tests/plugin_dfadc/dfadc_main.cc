c8336ca2c4ddad683ed7e7968e7a5a0a431c0e9e
Masaaki Saitow <masaaki@osiris2.lan>
2012-01-02 23:30:58 +0900

    OpenMP macros are optimized in order to achieve better granularity.
    Former-commit-id: e34cf6fcfd2457b0e5e95baf7d40b4e1cf02e528

--- tests/plugin_dfadc/dfadc_main.cc
+++ tests/plugin_dfadc/dfadc_main.cc
@@ -20,20 +20,24 @@ read_options(std::string name, Options &options){
         options.add_int("MEMORY", 1000);
         /*- The Reference -*/
         options.add_str("REFERENCE", "");
+        /*- The DF basis -*/
+        options.add_str("RI_BASIS_ADC", "");
         /*- The convergence criterion for pole searching step -*/
         options.add_int("NEWTON_CONV", 7);
         /*- The maximum numbers of the pole searching iteration  -*/
         options.add_int("POLE_MAX", 20);
         /*- Maximu iteration number in simultaneous expansion method -*/
-        options.add_int("SEM_MAX", 30);
+        options.add_int("SEM_MAX", 100);
         /*- The cutoff norm of residual vector in SEM step -*/
         options.add_int("NORM_TOL", 6);
         /*- Indicates whether three-virtual integral is processed by semi-direct algorithm -*/
         options.add_int("CUTOFF", 2);
         /*- Partial renormalization scheme for the ground state wavefunction -*/
         options.add_bool("DO_PR", false);
-        /*- The DF basis -*/
-        options.add_str("RI_BASIS_ADC", "");
+        /*- Whether convergence of residual norm is necessary or not -*/ 
+        options.add_bool("DO_RESIDUAL", true);
+        /*- Initial and the maximum dimension of the Ritz space -*/
+        options.add("DIM_RITZ", new ArrayType());
     }
 }
 
