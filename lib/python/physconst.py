1aaf3f0ba42f98ed908cefbcfb80f56ddd885463
Justin Turney <justin.turney@gmail.com>
2012-01-18 16:11:32 -0500

    Adds physical constants Python file
    Former-commit-id: c835257feb7483b03690380bf3742bf3321dc925

---
+++ lib/python/physconst.py
@@ -0,0 +1,25 @@
+# These values are taken from physconst.h
+
+h                   = 6.6260755E-34  # J s
+c                   = 2.99792458E8   # m s^-1
+kb                  = 1.380658E-23   # J K^-1
+R                   = 8.314510       # J K^-1 mol^-1
+bohr2angstroms      = 0.529177249
+bohr2m              = 0.529177249E-10
+bohr2cm             = 0.529177249E-8
+amu2g               = 1.6605402E-24
+amu2kg              = 1.6605402E-27
+au2kg               = 5.485799110E-4
+hartree2J           = 4.35974381E-18
+hartree2aJ          = 4.35974381
+cal2J               = 4.184
+dipmom_au2si        = 8.47835791E-30
+dipmom_au2debye     = 2.54175
+dipmom_debye2si     = 3.33564E-30
+c_au                = 137.0359895
+hartree2ev          = 27.211396
+hartree2wavenumbers = 219474.6313710
+e0                  = 8.854187816E-12  # vacuum permittivity
+na                  = 6.022136736E23   # Avagadro's number
+me                  = 9.10938188E-31   # Electron rest mass
+
