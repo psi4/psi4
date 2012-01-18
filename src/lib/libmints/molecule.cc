c5a4b7cd45f3f9cc0fec4847d5b4312063620fa4
Justin Turney <justin.turney@gmail.com>
2012-01-18 12:25:15 -0500

    Removes code I thought I needed.

--- src/lib/libmints/molecule.cc
+++ src/lib/libmints/molecule.cc
@@ -44,27 +44,6 @@ using namespace boost;
 
 namespace {
 const double dzero = 0.0;
-
-// templated version of my_equal so it could work with both char and wchar_t
-template<typename charT>
-struct my_equal {
-    my_equal( const std::locale& loc ) : loc_(loc) {}
-    bool operator()(charT ch1, charT ch2) {
-        return std::toupper(ch1, loc_) == std::toupper(ch2, loc_);
-    }
-private:
-    const std::locale& loc_;
-};
-
-// find substring (case insensitive)
-template<typename T>
-int ci_find_substr( const T& str1, const T& str2, const std::locale& loc = std::locale() )
-{
-    T::const_iterator it = std::search( str1.begin(), str1.end(),
-        str2.begin(), str2.end(), my_equal<T::value_type>(loc) );
-    if ( it != str1.end() ) return it - str1.begin();
-    else return -1; // not found
-}
 }
 
 // the third parameter of from_string() should be
