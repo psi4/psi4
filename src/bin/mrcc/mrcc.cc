4519d4b808d1e0eced024ad74ebc59f5f31bf6b1
Justin Turney <justin.turney@gmail.com>
2012-01-20 18:28:23 -0500

    RHF/ROHF/UHF interface with MRCC working
    Former-commit-id: ce2b6ff50c8c8e0d01778462ed26cd13b969a497

--- src/bin/mrcc/mrcc.cc
+++ src/bin/mrcc/mrcc.cc
@@ -15,6 +15,54 @@
 
 namespace psi{ namespace mrcc {
 
+void write_oei_to_disk(FILE* fort55, SharedMatrix moH)
+{
+    // Walk through moH and save the non-zero values
+    int offset = 0;
+    for (int h=0; h<moH->nirrep(); ++h) {
+        for (int m=0; m<moH->rowdim(h); ++m) {
+            for (int n=0; n<=m; ++n) {
+                if (fabs(moH->get(h, m, n)) > 1.0e-12) {
+                    fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", moH->get(h, m, n), m+offset+1, n+offset+1, 0, 0);
+                }
+            }
+        }
+        offset += moH->rowdim(h);
+    }
+}
+
+void write_tei_to_disk(FILE* fort55, int nirrep, dpdbuf4& K, double ints_tolerance)
+{
+    for(int h = 0; h < nirrep; ++h){
+        dpd_buf4_mat_irrep_init(&K, h);
+        dpd_buf4_mat_irrep_rd(&K, h);
+        for(int pq = 0; pq < K.params->rowtot[h]; ++pq){
+            int p = K.params->roworb[h][pq][0];
+            int q = K.params->roworb[h][pq][1];
+            for(int rs = 0; rs < K.params->coltot[h]; ++rs){
+                int r = K.params->colorb[h][rs][0];
+                int s = K.params->colorb[h][rs][1];
+
+                if (fabs(K.matrix[h][pq][rs]) > ints_tolerance)
+                    fprintf(fort55, "%28.20E%4d%4d%4d%4d\n",
+                            K.matrix[h][pq][rs], p+1, q+1, r+1, s+1);
+            }
+        }
+        dpd_buf4_mat_irrep_close(&K, h);
+    }
+}
+
+void print_dim(const std::string& name, const Dimension& dim)
+{
+    fprintf(outfile, "        %15s [ ", name.c_str());
+    for (int h=0; h<dim.n(); ++h) {
+        fprintf(outfile, "%3d", dim[h]);
+        if (h != dim.n()-1)
+            fprintf(outfile, ",");
+    }
+    fprintf(outfile, "]\n");
+}
+
 PsiReturnType mrcc(Options& options, int level, bool pertcc)
 {
     fprintf(outfile, "\n  MRCC input file generator.\n");
@@ -33,8 +81,18 @@ PsiReturnType mrcc(Options& options, int level, bool pertcc)
 
     int nbf = active_mopi.sum();
     int nirrep = wave->nirrep();
-    int nelectron = 2 * (docc.sum() - frzcpi.sum());
+    int nelectron = 2 * active_docc.sum() + active_socc.sum();
 
+    fprintf(outfile, "    Orbital Information:\n\n");
+    print_dim("Frozen Core", frzcpi);
+    print_dim("Active DOCC", active_docc);
+    print_dim("SOCC", active_socc);
+    print_dim("Frozen Virtual", frzvpi);
+
+    fprintf(outfile, "\n");
+    print_dim("Total MOs", active_mopi);
+
+    fprintf(outfile, "\n");
     FILE* fort55 = fopen("fort.55", "w");
     fprintf(fort55, "%22d%22d\n", nbf, nelectron);
 
@@ -53,19 +111,25 @@ PsiReturnType mrcc(Options& options, int level, bool pertcc)
     if (count % 3) // integrals start on their own line
         fprintf(fort55, "\n");
 
-    // Need to figure out this. I think it means closed shell
-    fprintf(fort55, " 150000\n");
-
     fprintf(outfile, "    Beginning integral transformation.\n");
 
     // Define the orbital space of the MO integrals we need.
     std::vector<boost::shared_ptr<MOSpace> > spaces;
     spaces.push_back(MOSpace::all);
 
-    // Create integral transformation object
-    IntegralTransform ints(wave, spaces, IntegralTransform::Restricted);
+    // Check the reference.
+    bool restricted = true;
 
-    // This actually transforms everything (OEI and TEI)
+    if (options.get_str("REFERENCE") == "UHF")
+        restricted = false;
+
+    if (pertcc && options.get_str("REFERENCE") == "ROHF") 
+        throw PSIEXCEPTION("Perturbative methods not implemented for ROHF references.");
+
+    // Create integral transformation object
+    IntegralTransform ints(wave, spaces, restricted ? IntegralTransform::Restricted : IntegralTransform::Unrestricted);
+
+    // This transforms everything (OEI and TEI)
     ints.transform_tei(MOSpace::all, MOSpace::all, MOSpace::all, MOSpace::all);
 
     // Use the IntegralTransform object's DPD instance, for convenience
@@ -74,66 +138,102 @@ PsiReturnType mrcc(Options& options, int level, bool pertcc)
     fprintf(outfile, "    Transformation complete.\n\n");
     fprintf(outfile, "  Generating fort.55 integral file...");
 
-    dpdbuf4 K;
-    _default_psio_lib_->open(PSIF_LIBTRANS_DPD, PSIO_OPEN_OLD);
-
-    // We want only the permutationally unique integrals, hence [A>=A]+, see libtrans documenation for details
-    dpd_buf4_init(&K, PSIF_LIBTRANS_DPD, 0,
-                  ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // In memory
-                  ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // On disk
-                  0,
-                  "MO Ints (AA|AA)");
-
     double ints_tolerance = options.get_double("INTS_TOLERANCE");
 
-    for(int h = 0; h < nirrep; ++h){
-        dpd_buf4_mat_irrep_init(&K, h);
-        dpd_buf4_mat_irrep_rd(&K, h);
-        for(int pq = 0; pq < K.params->rowtot[h]; ++pq){
-            int p = K.params->roworb[h][pq][0];
-            int q = K.params->roworb[h][pq][1];
-            int psym = K.params->psym[p];
-            int qsym = K.params->qsym[q];
-            for(int rs = 0; rs < K.params->coltot[h]; ++rs){
-                int r = K.params->colorb[h][rs][0];
-                int s = K.params->colorb[h][rs][1];
-                int rsym = K.params->rsym[r];
-                int ssym = K.params->ssym[s];
+    _default_psio_lib_->open(PSIF_LIBTRANS_DPD, PSIO_OPEN_OLD);
+    dpdbuf4 K;
 
-                // Print out the absolute orbital numbers and the integral value
-                if (fabs(K.matrix[h][pq][rs]) > ints_tolerance)
-                    fprintf(fort55, "%28.20E%4d%4d%4d%4d\n",
-                            K.matrix[h][pq][rs], p+1, q+1, r+1, s+1);
-            }
-        }
-        dpd_buf4_mat_irrep_close(&K, h);
+    fprintf(fort55, " 150000\n");
+
+    // RHF
+    if (restricted) {
+
+        // We want only the permutationally unique integrals, hence [A>=A]+, see libtrans documenation for details
+        dpd_buf4_init(&K, PSIF_LIBTRANS_DPD, 0,
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // In memory
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // On disk
+                      0,
+                      "MO Ints (AA|AA)");
+        write_tei_to_disk(fort55, nirrep, K, ints_tolerance);
+        dpd_buf4_close(&K);
+
+        // Load in frozen core operator, in the event of FREEZE_CORE = FALSE this is the MO OEI
+        SharedMatrix moH(new Matrix(PSIF_MO_FZC, wave->nmopi(), wave->nmopi()));
+        moH->load(_default_psio_lib_, PSIF_OEI);
+        View vmoH(moH, active_mopi, active_mopi, wave->frzcpi(), wave->frzcpi());
+        moH = vmoH();
+        write_oei_to_disk(fort55, moH);
+
+        // Print nuclear repulsion energy.
+        // Eventually needs to be changed to frozen core energy + nuclear repulsion energy
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", ints.get_frozen_core_energy() + molecule->nuclear_repulsion_energy(), 0, 0, 0, 0);
     }
-    dpd_buf4_close(&K);
+    else {
+
+        // We want only the permutationally unique integrals, hence [A>=A]+, see libtrans documenation for details
+
+        // Load up alpha alpha
+        dpd_buf4_init(&K, PSIF_LIBTRANS_DPD, 0,
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // In memory
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[A>=A]+"), // On disk
+                      0,
+                      "MO Ints (AA|AA)");
+        write_tei_to_disk(fort55, nirrep, K, ints_tolerance);
+        dpd_buf4_close(&K);
+
+        // Write out separator
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", 0.0, 0, 0, 0, 0);
+
+        // Load up beta beta
+        dpd_buf4_init(&K, PSIF_LIBTRANS_DPD, 0,
+                      ints.DPD_ID("[a>=a]+"), ints.DPD_ID("[a>=a]+"), // In memory
+                      ints.DPD_ID("[a>=a]+"), ints.DPD_ID("[a>=a]+"), // On disk
+                      0,
+                      "MO Ints (aa|aa)");
+        write_tei_to_disk(fort55, nirrep, K, ints_tolerance);
+        dpd_buf4_close(&K);
+
+        // Write out separator
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", 0.0, 0, 0, 0, 0);
+
+        // Load up alpha beta
+        dpd_buf4_init(&K, PSIF_LIBTRANS_DPD, 0,
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[a>=a]+"), // In memory
+                      ints.DPD_ID("[A>=A]+"), ints.DPD_ID("[a>=a]+"), // On disk
+                      0,
+                      "MO Ints (AA|aa)");
+        write_tei_to_disk(fort55, nirrep, K, ints_tolerance);
+        dpd_buf4_close(&K);
+
+        // Write out separator
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", 0.0, 0, 0, 0, 0);
+
+        // Load in alpha frozen core operator, in the event of FREEZE_CORE = FALSE this is the MO OEI
+        SharedMatrix moH(new Matrix(PSIF_MO_A_FZC, wave->nmopi(), wave->nmopi()));
+        moH->load(_default_psio_lib_, PSIF_OEI);
+        View vmoH(moH, active_mopi, active_mopi, wave->frzcpi(), wave->frzcpi());
+        moH = vmoH();
+        write_oei_to_disk(fort55, moH);
+
+        // Write out separator
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", 0.0, 0, 0, 0, 0);
+
+        // Load in beta frozen core operator, in the event of FREEZE_CORE = FALSE this is the MO OEI
+        SharedMatrix moHb(new Matrix(PSIF_MO_B_FZC, wave->nmopi(), wave->nmopi()));
+        moHb->load(_default_psio_lib_, PSIF_OEI);
+        View vmoHb(moHb, active_mopi, active_mopi, wave->frzcpi(), wave->frzcpi());
+        moHb = vmoHb();
+        write_oei_to_disk(fort55, moHb);
+
+        // Write out separator
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", 0.0, 0, 0, 0, 0);
+
+        // Print nuclear repulsion energy.
+        // Eventually needs to be changed to frozen core energy + nuclear repulsion energy
+        fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", ints.get_frozen_core_energy() + molecule->nuclear_repulsion_energy(), 0, 0, 0, 0);
+    }
     _default_psio_lib_->close(PSIF_LIBTRANS_DPD, PSIO_OPEN_OLD);
 
-    // Load in frozen core operator, in the event of FREEZE_CORE = FALSE this is the MO OEI
-    SharedMatrix moH(new Matrix(PSIF_MO_FZC, wave->nmopi(), wave->nmopi()));
-    moH->load(_default_psio_lib_, PSIF_OEI);
-
-    View vmoH(moH, active_mopi, active_mopi, wave->frzcpi(), wave->frzcpi());
-    moH = vmoH();
-
-    // Walk through moH and save the non-zero values
-    int offset = 0;
-    for (int h=0; h<nirrep; ++h) {
-        for (int m=0; m<active_mopi[h]; ++m) {
-            for (int n=0; n<=m; ++n) {
-                if (fabs(moH->get(h, m, n)) > 1.0e-12) {
-                    fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", moH->get(h, m, n), m+offset+1, n+offset+1, 0, 0);
-                }
-            }
-        }
-        offset += active_mopi[h];
-    }
-
-    // Print nuclear repulsion energy.
-    // Eventually needs to be changed to frozen core energy + nuclear repulsion energy
-    fprintf(fort55, "%28.20E%4d%4d%4d%4d\n", ints.get_frozen_core_energy() + molecule->nuclear_repulsion_energy(), 0, 0, 0, 0);
     fclose(fort55);
 
     fprintf(outfile, "done.\n  Generating fort.56 input file...");
@@ -160,13 +260,36 @@ PsiReturnType mrcc(Options& options, int level, bool pertcc)
     if (options["MRCC_LEVEL"].has_changed())
         level = options.get_int("MRCC_LEVEL");
 
+    int nsing = 1;
+    int closed_shell = 1;
+    int spatial_orbitals = 1;
+    int ndoub = 0;
+    if (pertcc && (options.get_str("REFERENCE") == "ROHF" || options.get_str("REFERENCE") == "UHF")) {
+        nsing = 0;
+        closed_shell = 0;
+        spatial_orbitals = 0;
+        ndoub = 1;
+    }
+    if (options["MRCC_NUM_SINGLET_ROOTS"].has_changed())
+        nsing = options.get_int("MRCC_NUM_SINGLET_ROOTS");
+
+    int symm = 0;
+    for (int h=0; h<nirrep; ++h)
+        for (int n=0; n<active_socc[h]; ++n)
+            symm ^= h;
+    symm += 1; // stupid 1 based fortran
+
     FILE* fort56 = fopen("fort.56", "w");
-    fprintf(fort56, "%6d%6d%6d%6d%6d      0     0      1     0     1     1     1     0      0      0%6d     0     0    0.00    0%6lu\n",
+    fprintf(fort56, "%6d%6d%6d%6d%6d      0     0%6d     0%6d%6d     1%6d      0      0%6d     0     0    0.00    0%6lu\n",
             level,                                           // # 1
-            options.get_int("MRCC_NUM_SINGLET_ROOTS"),       // # 2
+            nsing,                                           // # 2
             options.get_int("MRCC_NUM_TRIPLET_ROOTS"),       // # 3
             options.get_int("MRCC_RESTART"),                 // # 4
             ccmethod,                                        // # 5
+            symm,                                            // # 8
+            closed_shell,                                    // #10
+            spatial_orbitals,                                // #11
+            ndoub,                                           // #13
             e_conv,                                          // #16
             Process::environment.get_memory() / 1000 / 1000  // #21
         );
