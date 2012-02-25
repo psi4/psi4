dcdc560029d6cdaa6271dd18b5c70aed83defc88
deprince <eugene.deprince@gmail.com>
2012-02-25 14:53:32 -0500

    beginnings of clusters-in-molecule code.  the boys localization is done and could be ripped out for any other purposes.
    Former-commit-id: 117631da6aa09f7af14836fc028e267091d1ccd6

---
+++ tests/plugin_libcim/cim.h
@@ -0,0 +1,26 @@
+#ifndef CIM_H
+#define CIM_H
+namespace boost {
+template<class T> class shared_ptr;
+}
+namespace psi{
+class CIM{
+  public:
+    CIM(Options&options);
+    ~CIM();
+    void BuildClusters();
+    double **Fock;
+    int nirreps,nso,nmo,ndocc,nvirt,nfzc,nfzv,ndoccact;
+    double thresh1,thresh2,thresh3;
+    Options options_;
+
+    /*
+     * build occupied domains:
+     * central orbitals | central mo domain | environmental domain
+     */
+    int*cdomainsizes,**cdomains,*ncentral,**central;
+    int**env,*nenv;
+    void OccupiedDomains();
+};
+};
+#endif
