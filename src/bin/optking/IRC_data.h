243c50a5d45074d8f6993cffbdbcedf96efb0593
Andreas Copan <avcopan@elrond.chem>
2012-02-27 16:41:37 -0600

    Transition State Optimization Test Database Added; IRC Step Function Altered (output function added, stopping keyword added, constrained optimization algorithm improved)
    Former-commit-id: 577f22aa85440729ac497b692b299d39f28ef82a

--- src/bin/optking/IRC_data.h
+++ src/bin/optking/IRC_data.h
@@ -31,10 +31,14 @@ class IRC_POINT {
   double *f_q;         // internal coordinate forces
   double *f_x;         // cartesian coordinate forces
   double energy;       // total energy
+  double step_length;
+  double arc_length;
+  double line_length;
 
   public:
 
-    IRC_POINT(int coord_in, double *q_p_in, double *q_in, double *x_in, double *f_q_in, double *f_x_in, double E_in)
+    IRC_POINT(int coord_in, double *q_p_in, double *q_in, double *x_in, double *f_q_in,
+              double *f_x_in, double E_in, double step, double arc, double line)
     {
       coord_step = coord_in;
       q_pivot = q_p_in;
@@ -43,6 +47,9 @@ class IRC_POINT {
       f_q = f_q_in;
       f_x = f_x_in;
       energy = E_in;
+      step_length = step;
+      arc_length = arc;
+      line_length = line;
     }
 
     ~IRC_POINT() {free_array(q_pivot); free_array(q); free_array(x); free_array(f_q); free_array(f_x);} // free memory
@@ -54,20 +61,34 @@ class IRC_POINT {
     double *g_f_q(void) const { return f_q; }
     double *g_f_x(void) const { return f_x; }
     double g_energy(void) const { return energy; }
-
+    double g_step_length(void) const { return step_length; }
+    double g_arc_length(void) const { return arc_length; }
+    double g_line_length(void) const { return line_length; }
 };
 
 // IRC reaction path data
 class IRC_DATA {
   int coord_step;
   int sphere_step;
+  double step_dist;
+  double arc_dist;
+  double line_dist;
+  double arc_step_length;
 
   std::vector<IRC_POINT *> steps; 
 
   public:
+  bool go;
+  bool in_min_range;
 
     IRC_DATA() {
       sphere_step = 0;
+      go = 1;
+      in_min_range = 0;
+      step_dist = 0;
+      arc_dist = 0;
+      line_dist = 0;
+      arc_step_length = 0;
     };
 
     // free memory
@@ -86,6 +107,10 @@ class IRC_DATA {
         return steps.back()->g_coord_step() + 1;
     }
 
+    double g_energy(void) const
+    {
+      return steps[steps.size()-1]->g_energy();
+    }
     double *g_q_pivot(void) const
     {
       return steps[steps.size()-1]->g_q_pivot();
@@ -106,6 +131,10 @@ class IRC_DATA {
     {
       return steps[steps.size()-1]->g_f_x();
     }
+    double g_arc_length(void) const
+    {
+      return steps[steps.size()-1]->g_arc_length();
+    }
 
     const IRC_POINT &g_step(int index) const { return *(steps[index]); }
 
@@ -119,13 +148,16 @@ class IRC_DATA {
 
     void add_irc_point(int coord_in, double *q_p_in, double *q_in, double *x_in, double *f_q_in, double *f_x_in, double E_in)
     {
-      IRC_POINT *onepoint = new IRC_POINT(coord_in, q_p_in, q_in, x_in, f_q_in, f_x_in, E_in);
+      arc_dist += arc_step_length;
+      IRC_POINT *onepoint = new IRC_POINT(coord_in, q_p_in, q_in, x_in, f_q_in, f_x_in, E_in, step_dist, arc_dist, line_dist);
       steps.push_back(onepoint);
     }
 
     friend void MOLECULE::irc_step(void);
 
     void point_converged(opt::MOLECULE &mol);
+    void progress_report(opt::MOLECULE &mol);
+
 };
 
 }
