42b7eecb420b972957d7d725e3d1538e3d53a280
Alexander Sokolov <alex@ccqc.uga.edu>
2012-02-29 15:08:34 -0500

    Ready to build the density in DCFT Gradients
    Former-commit-id: 356a087de54c9c6c13b258280d4a88b1ffd6f6ec

--- src/bin/dcft/dcft.h
+++ src/bin/dcft/dcft.h
@@ -70,6 +70,7 @@ protected:
     void compute_gradient();
     void orbital_response_guess();
     void cumulant_response_guess();
+    void compute_density();
     //void AO_contribute(dpdfile2 *tau1_AO, dpdfile2 *tau2_AO, int p, int q,
     //        int r, int s, double value);
     bool correct_mo_phases(bool dieOnError = true);
