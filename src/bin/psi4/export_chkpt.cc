54b05db60ff19c3fec39b6db32f1e8f9f8f38fcd
Lori Burns <lori.burns@gatech.edu>
2012-02-28 23:34:20 -0500

    Enable docstrings to be extracted from PsiMod. Add "docstring" placeholders.
    Former-commit-id: ef5cb5c1a6f12920f5863d3d4091330f66de8b29

--- src/bin/psi4/export_chkpt.cc
+++ src/bin/psi4/export_chkpt.cc
@@ -8,18 +8,18 @@ using namespace psi;
 
 void export_chkpt()
 {
-    class_<Chkpt, boost::shared_ptr<Chkpt> >( "Checkpoint", init<PSIO*, int>() ).
-        add_property( "enuc", &Chkpt::rd_enuc, &Chkpt::wt_enuc).
-        add_property( "label", &Chkpt::rd_label, &Chkpt::wt_label).
-        add_property( "escf", &Chkpt::rd_escf, &Chkpt::wt_escf).
-        add_property( "eref", &Chkpt::rd_eref, &Chkpt::wt_eref).
-        add_property( "ecorr", &Chkpt::rd_ecorr, &Chkpt::wt_ecorr).
-        add_property( "efzc", &Chkpt::rd_efzc, &Chkpt::wt_efzc).
-        add_property( "etot", &Chkpt::rd_etot, &Chkpt::wt_etot).
-        add_property( "disp", &Chkpt::rd_disp, &Chkpt::wt_disp).
-        add_property( "eccsd", &Chkpt::rd_eccsd, &Chkpt::wt_eccsd).
-        add_property( "e_t", &Chkpt::rd_e_t, &Chkpt::wt_e_t).
-        add_property( "emp2", &Chkpt::rd_emp2, &Chkpt::wt_emp2).
-        def( "shared_object", &Chkpt::shared_object).
+    class_<Chkpt, boost::shared_ptr<Chkpt> >( "Checkpoint", "docstring", init<PSIO*, int>() ).
+        add_property( "enuc", &Chkpt::rd_enuc, &Chkpt::wt_enuc, "docstring").
+        add_property( "label", &Chkpt::rd_label, &Chkpt::wt_label, "docstring").
+        add_property( "escf", &Chkpt::rd_escf, &Chkpt::wt_escf, "docstring").
+        add_property( "eref", &Chkpt::rd_eref, &Chkpt::wt_eref, "docstring").
+        add_property( "ecorr", &Chkpt::rd_ecorr, &Chkpt::wt_ecorr, "docstring").
+        add_property( "efzc", &Chkpt::rd_efzc, &Chkpt::wt_efzc, "docstring").
+        add_property( "etot", &Chkpt::rd_etot, &Chkpt::wt_etot, "docstring").
+        add_property( "disp", &Chkpt::rd_disp, &Chkpt::wt_disp, "docstring").
+        add_property( "eccsd", &Chkpt::rd_eccsd, &Chkpt::wt_eccsd, "docstring").
+        add_property( "e_t", &Chkpt::rd_e_t, &Chkpt::wt_e_t, "docstring").
+        add_property( "emp2", &Chkpt::rd_emp2, &Chkpt::wt_emp2, "docstring").
+        def( "shared_object", &Chkpt::shared_object, "docstring").
         staticmethod("shared_object");
 }
