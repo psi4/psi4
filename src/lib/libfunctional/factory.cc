cfa7004eebe7ac3b72310c53952f92b9f5146421
rparrish <robparrish@gmail.com>
2012-03-23 02:30:33 -0400

    DFT Functionals now def'd in Python

--- src/lib/libfunctional/factory.cc
+++ src/lib/libfunctional/factory.cc
@@ -6,16 +6,6 @@
 
 namespace psi {
 
-boost::shared_ptr<Functional> Functional::build(const std::string& alias)
-{
-    boost::shared_ptr<Functional> fun;
-
-    if (alias == "S_X") {
-        fun = Functional::build_base("S_X"); 
-    }
-
-    return fun;
-}
 boost::shared_ptr<Functional> Functional::build_base(const std::string& alias)
 {
     Functional* fun;
@@ -25,16 +15,9 @@ boost::shared_ptr<Functional> Functional::build_base(const std::string& alias)
         x->gga_type_  = XFunctional::GGA_None;
         x->meta_type_ = XFunctional::Meta_None;
         x->sr_type_   = XFunctional::SR_None;
-
-        x->set_gga(false);
-        x->set_meta(false);
-        x->set_alpha(1.0);
-        x->set_omega(1.0);
-        x->set_name("S_X");
-        x->set_description("    Slater Exchange\n");
-        x->set_citation("    J.C. Slater, Phys. Rev., 81(3):385-390, 1951\n");
-
         fun = static_cast<Functional*>(x);
+    } else {
+        throw PSIEXCEPTION("Functional::build_base: Unrecognized base Functional.");
     }
 
     return boost::shared_ptr<Functional>(fun);
