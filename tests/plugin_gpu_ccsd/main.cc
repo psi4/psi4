7b15d368e6f6b06e438f2651a1402cc36762bd99
Eugene DePrince <deprince@Eugenes-MacBook-Pro.local>
2012-03-27 15:45:48 -0400

    changed natural orbital options to match those in the sapt code
    Former-commit-id: 9d4d0808601168d4933b175de129bb17fbf32a25

--- tests/plugin_gpu_ccsd/main.cc
+++ tests/plugin_gpu_ccsd/main.cc
@@ -31,9 +31,9 @@ read_options(std::string name, Options &options)
      /*- Compute triples contribution? */
      options.add_bool("COMPUTE_TRIPLES", true);
      /*- Use MP2 NOs to truncate virtual space for (T)? -*/
-     options.add_bool("TRIPLES_USE_NOS", false);
+     options.add_bool("NAT_ORBS", false);
      /*- Cutoff for occupation of MP2 NO orbitals in (T) -*/
-     options.add_double("VIRTUAL_CUTOFF", 1.0e-6);
+     options.add_double("OCC_TOLERANCE", 1.0e-6);
      /*- Desired number of threads. This will override OMP_NUM_THREADS in (T) -*/
      options.add_int("NUM_THREADS", 1);
      /*- Do SCS-MP2? -*/
