9a07bbe3d20c0e67e561b7e62c3b8cb6be9e2509
rparrish <robparrish@gmail.com>
2012-04-02 11:34:32 -0400

    HJS variant of wPBE implemented
    Former-commit-id: 62420efa1ffce3b45ed72b85183b2b47adcb4546

--- src/lib/libfunctional/wpbex_functional.h
+++ src/lib/libfunctional/wpbex_functional.h
@@ -10,7 +10,7 @@ void wpbe_F(double rho, double s, double omega, double* F, double* F_rho, double
 
 /** 
  * Short-range PBE functional 
- * 
+ * Following HJS notation of J. Chem. Phys., 128, 194105
  **/
 
 class wPBEXFunctional : public Functional {
@@ -22,6 +22,29 @@ protected:
     // Global types (Slater, Thomas-Fermi constants)
     double _K0_;
     double _k0_;
+    double _s0_;
+    double _pi12_;
+
+    double _s_min_tol_;
+    double _nu_min_tol_;
+
+    // HJS Parameters 
+    double _A_;
+    double _B_;
+    double _C_;
+    double _D_;
+    double _E_;
+    
+    std::vector<double> _Ha_;
+    std::vector<double> _Hb_;
+
+    // HJS or HSE?
+    bool hjs_;
+
+    // F_HJS^\omega(s,nu) kernel
+    void hjs_F(double s, double nu, double* F, double* F_s, double* F_nu);
+    // F_HSE^\omega(s,nu) kernel
+    void hse_F(double rho, double s, double omega, double* F, double* F_rho, double* F_s);
 
     // Set defaults up internally 
     
@@ -41,8 +64,10 @@ public:
     // => Computers <= //
 
     virtual void compute_functional(const std::map<std::string,SharedVector>& in, const std::map<std::string,SharedVector>& out, int npoints, int deriv, double alpha);
-
     void compute_sigma_functional(const std::map<std::string,SharedVector>& in, const std::map<std::string,SharedVector>& out, int npoints, int deriv, double alpha, bool spin);
+
+    bool hjs() const { return hjs_; }
+    void set_hjs(bool hjs) { hjs_ = hjs; }
 
 };
 
