6ff009b4cc654f0529b2c40b6090ba12cb641478
Andy Simmonett <andy.simmonett@gmail.com>
2012-04-09 11:50:14 -0400

    Made ZMatrices more numerically stable, when there are linear bond angles.

--- src/lib/libmints/coordentry.cc
+++ src/lib/libmints/coordentry.cc
@@ -184,23 +184,26 @@ ZMatrixEntry::~ZMatrixEntry()
 {
 }
 
+
 void
 ZMatrixEntry::set_coordinates(double x, double y, double z)
 {
-    coordinates_[0] = x;
-    coordinates_[1] = y;
-    coordinates_[2] = z;
+    coordinates_[0] = fabs(x) < CLEANUP_THRESH ? 0.0 : x;
+    coordinates_[1] = fabs(y) < CLEANUP_THRESH ? 0.0 : y;
+    coordinates_[2] = fabs(z) < CLEANUP_THRESH ? 0.0 : z;
 
     if(rto_ != 0){
         if(!rto_->is_computed())
              throw PSIEXCEPTION("Coordinates have been set in the wrong order");
         rval_->set(r(coordinates_, rto_->compute()));
     }
+
     if(ato_ != 0){
         if(!ato_->is_computed())
              throw PSIEXCEPTION("Coordinates have been set in the wrong order");
         aval_->set(180.0*a(coordinates_, rto_->compute(), ato_->compute())/M_PI);
     }
+
     if(dto_ != 0){
         if(!dto_->is_computed())
              throw PSIEXCEPTION("Coordinates have been set in the wrong order");
@@ -341,6 +344,7 @@ const Vector3& ZMatrixEntry::compute()
         for(int xyz = 0; xyz < 3; ++xyz)
            coordinates_[xyz] = B[xyz] + r * (eX[xyz] * sinABC * cosABCD + eY[xyz] * sinABC * sinABCD - eCB[xyz] * cosABC );
     }
+
     computed_ = true;
     return coordinates_;
 }
