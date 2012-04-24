ce19a36cbe32d0b192178f0ed2582cdbdb651468
Lori Burns <lori.burns@gatech.edu>
2012-04-24 13:23:56 -0400

    Modify script for trac feed. Hide merge commits.
    Former-commit-id: fbff05cfaae936ed1b555cc1f19a75c1329e4786

--- doc/sphinxman/source/extract_changeset.py
+++ doc/sphinxman/source/extract_changeset.py
@@ -2,7 +2,7 @@
 import os
 # Extracts last git changelog item into html for psicode
 
-gitlast = '/var/www/trac/feed/latest_trac_changeset2.txt'  # path to output file with latest changeset
+gitlast = '/var/www/trac/feed/latest_trac_changeset.txt'  # path to output file with latest changeset
 githist = '/var/www/trac/feed/history_trac_changeset.txt'  # path to output file with last Nhist changesets
 Nhist = 100
 
@@ -10,10 +10,10 @@ Nhist = 100
 thisdir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(thisdir)
 
-log_cmd = 'git log --branches=master -%s --pretty=format:\'<title> %%cr [%%h] by %%cn <br/>: %%s<br/></title>\' > %s'\
-          % (Nhist, gitlast)
+log_cmd = 'git log --branches=master -%s --no-merges --pretty=format:\'<title> %%cr [%%h] by %%cn: <br/> %%s<br/></title>\' > %s'\
+          % (Nhist, githist)
 os.system(log_cmd)
 
-log_cmd = 'git log --branches=master -1 --pretty=format:\'<title> %%cr [%%h] by %%cn <br/>: %%s<br/></title>\' > %s'\
-          % (githist)
+log_cmd = 'git log --branches=master -1 --no-merges --pretty=format:\'<title> %%cr by %%cn: <br/> %%s<br/></title>\' > %s'\
+          % (gitlast)
 os.system(log_cmd)
