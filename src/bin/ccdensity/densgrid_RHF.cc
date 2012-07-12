236c5a06aaadbb898d8a373ef5647925976d8bbe
crawdad <crawdad@vt.edu>
2012-07-12 17:16:42 -0400

    More progress on density-on-a-grid.
    Former-commit-id: fa5960b375b8ca2a5e4000a48cb95e7f218c579a

--- src/bin/ccdensity/densgrid_RHF.cc
+++ src/bin/ccdensity/densgrid_RHF.cc
@@ -5,10 +5,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "psi4-dec.h"
-#include <libmints/wavefunction.h>
-#include <libmints/basisset.h>
-#include <libmints/matrix.h>
-#include <libmints/vector.h>
+#include <libmints/mints.h>
 #include <libciomr/libciomr.h>
 #include <libpsio/psio.h>
 #include <libiwl/iwl.h>
@@ -31,25 +28,55 @@ namespace psi { namespace ccdensity {
 ** TDC, 7/2012
 */
 void compute_delta(double **delta, double x, double y, double z);
-void setup_delta(void);
-int nmo, nao; // global
+int nmo, nso, nao; // global
 double **scf, **u; // global
+boost::shared_ptr<BasisSet> basis;
+boost::shared_ptr<Wavefunction> wfn;
 
-void densgrid_RHF(void)
+void densgrid_RHF(Options& options)
 {
-  int nmo = moinfo.nmo;
-  int nfzv = moinfo.nfzv;
-  int nirreps = moinfo.nirreps;
-  int nactive = nmo - nfzv;
   double dens;
   double **D, **delta;
   double xmin, xmax, ymin, ymax, zmin, zmax;
   double xstep, ystep, zstep;
+  int *order;
+  double **scf_pitzer;
 
-  boost::shared_ptr<Molecule> molecule = Process::environment.molecule();
+  wfn = Process::environment.reference_wavefunction();
+  basis = wfn->basisset();
+
+  nao = basis->nao();
+  nso = moinfo.nso;
+  nmo = moinfo.nmo;
+  chkpt_init(PSIO_OPEN_OLD);
+  scf_pitzer = chkpt_rd_scf();
+  chkpt_close();
 
   D = moinfo.opdm; // A block matrix
-  delta = block_matrix(nactive, nactive); // Dirac delta function 
+  delta = block_matrix(nmo, nmo); // Dirac delta function 
+
+  // Set up AO->SO transformation matrix (u)
+  MintsHelper helper(options, 0);
+  SharedMatrix aotoso = helper.petite_list(true)->aotoso();
+  int *col_offset = new int[wfn->nirrep()];
+  col_offset[0] = 0;
+  for(int h=1; h < wfn->nirrep(); h++)
+    col_offset[h] = col_offset[h-1] + aotoso->coldim(h-1);
+
+  u = block_matrix(nao, nso);
+  for(int h=0; h < wfn->nirrep(); h++)
+    for(int j=0; j < aotoso->coldim(h); j++)
+      for(int i=0; i < nao; i++)
+        u[i][j+col_offset[h]] = aotoso->get(h, i, j);
+  delete[] col_offset;
+
+  /*** Arrange the SCF eigenvectors into QT ordering ***/
+  order = moinfo.pitzer2qt;
+  scf = block_matrix(nso, nmo);
+  for(int i=0; i < nmo; i++) {
+      int I = order[i];  /* Pitzer --> QT */
+      for(int j=0; j < nso; j++) scf[j][I] = scf_pitzer[j][i];
+    }
 
   xmin = -3.0; xmax = +3.0; xstep = 0.1;
   ymin = -3.0; ymax = +3.0; ystep = 0.1;
@@ -64,8 +91,8 @@ void densgrid_RHF(void)
         compute_delta(delta, x, y, z);
 
         dens = 0.0;
-        for(int i=0; i < nactive; i++)
-          for(int j=0; j < nactive; j++)
+        for(int i=0; i < nmo; i++)
+          for(int j=0; j < nmo; j++)
             dens += delta[i][j] * D[i][j];
 
       } // z
@@ -73,7 +100,7 @@ void densgrid_RHF(void)
   } // x
 
   free_block(delta);
-
+  free_block(scf);
 }
 
 void compute_delta(double **delta, double x, double y, double z)
@@ -81,23 +108,18 @@ void compute_delta(double **delta, double x, double y, double z)
   int i, j;
   double *phi_ao, *phi_so, *phi_mo;
 
-  setup_delta();
-
   phi_ao = init_array(nao);  /* AO function values */
-  phi_so = init_array(nmo);  /* SO function values */
+  phi_so = init_array(nso);  /* SO function values */
   phi_mo = init_array(nmo);  /* MO function values */
 
-  boost::shared_ptr<BasisSet> basis = Process::environment.reference_wavefunction()->basisset();
   basis->compute_phi(phi_ao, x, y, z);
 
   /*  for(i=0; i < nao; i++) printf("%d %20.10f\n", i, phi_ao[i]); */
 
   /* Transform the basis function values to the MO basis */
-  C_DGEMV('n', nmo, nao, 1.0, &(u[0][0]), nao, &(phi_ao[0]), 1,
-          0.0, &(phi_so[0]), 1);
+  C_DGEMV('t', nao, nso, 1.0, u[0], nso, phi_ao, 1, 0.0, phi_so, 1);
 
-  C_DGEMV('t', nmo, nmo, 1.0, &(scf[0][0]), nmo, &(phi_so[0]), 1,
-          0.0, &(phi_mo[0]), 1);
+  C_DGEMV('t', nmo, nso, 1.0, scf[0], nmo, phi_so, 1, 0.0, phi_mo, 1);
 
   /* for(i=0; i < nmo; i++) printf("%d %20.10f\n", i, phi_mo[i]); */
 
@@ -110,60 +132,6 @@ void compute_delta(double **delta, double x, double y, double z)
   free(phi_ao);
   free(phi_so);
   free(phi_mo);
-}
-
-void setup_delta(void)
-{
-  static int done=0;
-  int i, I, j;
-  int nirreps, nfzc, nfzv;
-  int *order, *clsdpi, *openpi, *orbspi, *fruocc, *frdocc;
-  double **scf_pitzer;
-
-  if(done) return;
-
-  chkpt_init(PSIO_OPEN_OLD);
-  nmo = chkpt_rd_nmo();
-  nao = chkpt_rd_nao();
-  nirreps = chkpt_rd_nirreps();
-  clsdpi = chkpt_rd_clsdpi();
-  openpi = chkpt_rd_openpi();
-  orbspi = chkpt_rd_orbspi();
-  scf_pitzer = chkpt_rd_scf();
-  u = chkpt_rd_usotao();
-  chkpt_close();
-
-  moinfo.frdocc = Process::environment.reference_wavefunction()->frzcpi();
-  moinfo.fruocc = Process::environment.reference_wavefunction()->frzvpi();
-
-  nfzc = nfzv = 0;
-  for(i=0; i < nirreps; i++) {
-    nfzc += frdocc[i];
-    nfzv += fruocc[i];
-  }
-
-  /*** Get the Pitzer -> QT reordering array ***/
-  order = init_int_array(nmo);
-  reorder_qt(clsdpi, openpi, frdocc, fruocc, order, orbspi, nirreps);
-
-  /*** Arrange the SCF eigenvectors into QT ordering ***/
-  scf = block_matrix(nmo, nmo);
-  for(i=0; i < nmo; i++) {
-      I = order[i];  /* Pitzer --> QT */
-      for(j=0; j < nmo; j++) scf[j][I] = scf_pitzer[j][i];
-    }
-
-  free(order);
-  free(clsdpi);
-  free(openpi);
-  free(orbspi);
-  free(fruocc);
-  free(frdocc);
-  free_block(scf_pitzer);
-
-  done = 1;
-
-  return;
 }
 
 }} // namespace psi::ccdensity
