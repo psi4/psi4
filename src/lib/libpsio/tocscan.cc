3fe35ea3948f68207a92f59730cfc29387d4824f
David Sherrill <sherrill@gatech.edu>
2012-07-23 12:49:35 -0400

    Fix problem with tests/pywrap_all; enable projection of guesses with different puream setting; add ability to check for existence of a file or a TOC entry
    Former-commit-id: 539874157ce0b9102e77531610631d107dbf29cc

--- src/lib/libpsio/tocscan.cc
+++ src/lib/libpsio/tocscan.cc
@@ -41,5 +41,37 @@ psio_tocentry*PSIO::tocscan(unsigned int unit, const char *key) {
     return _default_psio_lib_->tocscan(unit, key);
   }
 
+bool PSIO::tocentry_exists(unsigned int unit, const char *key) {
+  psio_tocentry *this_entry;
+
+  if (key == NULL)
+    return (true);
+
+  if ((strlen(key)+1) > PSIO_KEYLEN)
+    psio_error(unit, PSIO_ERROR_KEYLEN);
+
+  this_entry = psio_unit[unit].toc;
+
+  while (this_entry != NULL) {
+    if (!strcmp(this_entry->key, key))
+      return (true);
+    this_entry = this_entry->next;
+  }
+
+  return (false);
+}
+
+  /*!
+   ** PSIO_TOCSCAN(): Scans the TOC for a particular keyword and returns either
+   ** a pointer to the entry or NULL to the caller.
+   **
+   ** \ingroup PSIO
+   */
+
+  bool psio_tocentry_exists(unsigned int unit, const char *key) {
+    return _default_psio_lib_->tocentry_exists(unit, key);
+  }
+
+
 }
 
