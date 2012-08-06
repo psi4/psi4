0ff2ad34b44a70792098ab19661d0d9e238cb521
Lori Burns <lori.burns@gatech.edu>
2012-08-06 16:32:13 -0400

    Revert "Revert "sapt maintenance. (a) make "delta HF,r (2)" label the same across sapt levels since points to same value and (b) move handling default DF_BASIS_SAPT value from c-side to py-side using BasisFamily.""
    This reverts commit 85c36bc3e11e6e0c45d2bfc79c95505336533f70.

--- src/bin/psi4/read_options.cc
+++ src/bin/psi4/read_options.cc
@@ -705,7 +705,7 @@ int read_options(const std::string &name, Options & options, bool suppress_print
     options.add_str("DF_BASIS_SAPT", "");
     /*- Auxiliary basis set for SAPT Elst10 and Exch10 density fitting 
     computations, may be important if heavier elements are involved.
-    :ref:`Defaults <apdx:basisFamily>` to a RI basis. -*/
+    Defaults to |sapt__df_basis_sapt|. -*/
     options.add_str("DF_BASIS_ELST", "");
     /*- Maximum error allowed (Max error norm in Delta tensor)
     in the approximate energy denominators employed for most of the
