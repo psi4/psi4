8cdb81fab9f836d04be94a23b5809c9d605573e4
Justin Turney <justin.turney@gmail.com>
2012-10-09 16:52:40 -0400

    Flushes out more of the Wavefunction interface

--- src/bin/psi4/export_mints.cc
+++ src/bin/psi4/export_mints.cc
@@ -466,14 +466,14 @@ void export_mints()
             def("alpha_orbital_space", &Wavefunction::alpha_orbital_space, "docstring").
             def("beta_orbital_space", &Wavefunction::beta_orbital_space, "docstring").
             def("molecule", &Wavefunction::molecule, "docstring").
-            def("doccpi", &Wavefunction::doccpi, "docstring").
-            def("soccpi", &Wavefunction::soccpi, "docstring").
-            def("nsopi", &Wavefunction::nsopi, "docstring").
-            def("nmopi", &Wavefunction::nmopi, "docstring").
-            def("nalphapi", &Wavefunction::nalphapi, "docstring").
-            def("nbetapi", &Wavefunction::nbetapi, "docstring").
-            def("frzcpi", &Wavefunction::frzcpi, "docstring").
-            def("frzvpi", &Wavefunction::frzvpi, "docstring").
+            def("doccpi", &Wavefunction::doccpi, return_value_policy<copy_const_reference>(), "docstring").
+            def("soccpi", &Wavefunction::soccpi, return_value_policy<copy_const_reference>(), "docstring").
+            def("nsopi", &Wavefunction::nsopi, return_value_policy<copy_const_reference>(), "docstring").
+            def("nmopi", &Wavefunction::nmopi, return_value_policy<copy_const_reference>(), "docstring").
+            def("nalphapi", &Wavefunction::nalphapi, return_value_policy<copy_const_reference>(), "docstring").
+            def("nbetapi", &Wavefunction::nbetapi, return_value_policy<copy_const_reference>(), "docstring").
+            def("frzcpi", &Wavefunction::frzcpi, return_value_policy<copy_const_reference>(), "docstring").
+            def("frzvpi", &Wavefunction::frzvpi, return_value_policy<copy_const_reference>(), "docstring").
             def("nalpha", &Wavefunction::nalpha, "docstring").
             def("nbeta", &Wavefunction::nbeta, "docstring");
 
