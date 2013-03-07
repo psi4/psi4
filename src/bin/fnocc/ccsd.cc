d82ab3215611f218a94a8d7d921eda79c7e6d6c3
Eugene DePrince <deprince@lawn-128-61-127-253.lawn.gatech.edu>
2013-03-07 12:44:16 -0500

    tiny change to fnocc diis

--- src/bin/fnocc/ccsd.cc
+++ src/bin/fnocc/ccsd.cc
@@ -435,13 +435,25 @@ PsiReturnType CoupledCluster::CCSDIterations() {
       }
       eccsd = CheckEnergy();
 
-      if      (diis_iter <= maxdiis) diis_iter++;
+      if (diis_iter < maxdiis ) {
+         replace_diis_iter++;
+      }else {
+          double min = 1.0e9;
+          for (int j = 1; j <= (diis_iter < maxdiis ? diis_iter : maxdiis); j++) {
+              if ( fabs( diisvec[j-1] ) < min ) {
+                  min = fabs( diisvec[j-1] );
+                  replace_diis_iter = j;
+              }
+          }
+      }
+
+      if (diis_iter <= maxdiis) diis_iter++;
       //else if (replace_diis_iter < maxdiis) replace_diis_iter++;
       //else    replace_diis_iter = 1;
 
       time_t iter_stop = time(NULL);
       fprintf(outfile,"  %5i   %i %i %15.10f %15.10f %15.10f %8d\n",
-            iter,diis_iter-1,replace_diis_iter,eccsd,eccsd-Eold,nrm,(int)iter_stop-(int)iter_start);
+            iter,diis_iter-1,replace_diis_iter-1,eccsd,eccsd-Eold,nrm,(int)iter_stop-(int)iter_start);
       fflush(outfile);
       iter++;
 
@@ -2654,13 +2666,25 @@ PsiReturnType DFCoupledCluster::CCSDIterations() {
       Eold = eccsd;
       eccsd = CheckEnergy();
 
+      if (diis_iter < maxdiis ) {
+         replace_diis_iter++;
+      }else {
+          double min = 1.0e9;
+          for (int j = 1; j <= (diis_iter < maxdiis ? diis_iter : maxdiis); j++) {
+              if ( fabs( diisvec[j-1] ) < min ) {
+                  min = fabs( diisvec[j-1] );
+                  replace_diis_iter = j;
+              }
+          }
+      }
+
       if (diis_iter<=maxdiis) diis_iter++;
       //if (replace_diis_iter<maxdiis) replace_diis_iter++;
       //else replace_diis_iter = 1;
 
       time_t iter_stop = time(NULL);
       fprintf(outfile,"  %5i   %i %i %15.10f %15.10f %15.10f %8d\n",
-            iter,diis_iter-1,replace_diis_iter,eccsd,eccsd-Eold,nrm,(int)iter_stop-(int)iter_start);
+            iter,diis_iter-1,replace_diis_iter-1,eccsd,eccsd-Eold,nrm,(int)iter_stop-(int)iter_start);
       fflush(outfile);
       iter++;
       if (iter==1){
