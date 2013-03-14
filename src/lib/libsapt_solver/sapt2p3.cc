8739e32055d3b68da90bb55453105455364a8e0d
Sherrill Group Member <lori.burns@gatech.edu>
2013-03-14 17:37:34 -0400

    Alter sapt disp22 ccd label

--- src/lib/libsapt_solver/sapt2p3.cc
+++ src/lib/libsapt_solver/sapt2p3.cc
@@ -297,7 +297,7 @@ void SAPT2p3::print_results()
     fprintf(outfile,"      Disp22 (T) (CCD)    %16.8lf mH %16.8lf kcal mol^-1\n",
       e_disp22t_ccd_*1000.0,e_disp22t_ccd_*pc_hartree2kcalmol);
     if (nat_orbs_)
-      fprintf(outfile,"      Est. Disp22 (T)     %16.8lf mH %16.8lf kcal mol^-1\n",
+      fprintf(outfile,"      Est. Disp22 (T) (CCD)%15.8lf mH %16.8lf kcal mol^-1\n",
         e_est_disp22t_ccd_*1000.0,e_est_disp22t_ccd_*pc_hartree2kcalmol);
   }
   fprintf(outfile,"      Exch-Disp20         %16.8lf mH %16.8lf kcal mol^-1\n",
