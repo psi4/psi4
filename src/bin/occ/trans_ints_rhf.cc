5f3323bbbe3c173cab8c7a8e7b274b8306e1b01f
Ugur Bozkaya <ugrbzky@gmail.com>
2013-03-30 23:15:57 -0400

    I fixed the Z-vector code that I broke while I was implementing the frozen core approximation.

--- src/bin/occ/trans_ints_rhf.cc
+++ src/bin/occ/trans_ints_rhf.cc
@@ -145,6 +145,13 @@ if (wfn_type_ != "OMP2") {
       timer_off("Build Fock");
       }
 
+      else if (orb_opt_ == "FALSE") {
+         for(int h = 0; h < nirrep_; ++h){
+             for(int i = 0; i < occpiA[h]; ++i) FockA->set(h, i, i, epsilon_a_->get(h,i));
+             for(int a = 0; a < virtpiA[h]; ++a) FockA->set(h, a + occpiA[h], a + occpiA[h], epsilon_a_->get(h, a + occpiA[h]));
+         }
+      }
+
       timer_on("Build Denominators");
       if (orb_opt_ == "TRUE") denominators_rhf();
       else if (orb_opt_ == "FALSE") denominators_rmp2();
--- src/bin/occ/trans_ints_ump2.cc
+++ src/bin/occ/trans_ints_ump2.cc
@@ -150,6 +150,12 @@ void OCCWave::trans_ints_ump2()
       fock_beta();   
       timer_off("Build Fock");
       */
+         for(int h = 0; h < nirrep_; ++h){
+             for(int i = 0; i < occpiA[h]; ++i) FockA->set(h, i, i, epsilon_a_->get(h,i));
+             for(int i = 0; i < occpiB[h]; ++i) FockB->set(h, i, i, epsilon_b_->get(h,i));
+             for(int a = 0; a < virtpiA[h]; ++a) FockA->set(h, a + occpiA[h], a + occpiA[h], epsilon_a_->get(h, a + occpiA[h]));
+             for(int a = 0; a < virtpiB[h]; ++a) FockB->set(h, a + occpiB[h], a + occpiB[h], epsilon_b_->get(h, a + occpiB[h]));
+         }
 
       timer_on("Build Denominators");
       denominators_ump2();
