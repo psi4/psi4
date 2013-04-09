f34501580f27fbb89a24a837e2069b892415cdbd
Rollin King <rking@bethel.edu>
2013-04-09 15:07:04 -0500

    removed isotope data from atom_data.cc/atom_data.h - not used anyway

--- src/bin/optking/atom_data.cc
+++ src/bin/optking/atom_data.cc
@@ -3,7 +3,7 @@
     \brief atom_data.cc : initialize static class members in atom_data.h
 */
 
-#include "atom_data.h"
+#include <string>
 #include <map>
 
 namespace opt {
@@ -170,6 +170,7 @@ double Element_to_Z(std::string lbl) {
   return opt::element_to_Z_map[lbl];
 }
 
+/*
 map<string,double> create_isotope_to_mass_map() {
   map<string,double> m;
 
@@ -3481,5 +3482,6 @@ static map<string,double> isotope_to_mass_map = create_isotope_to_mass_map();
 double Isotope_to_mass(std::string lbl) {
   return opt::isotope_to_mass_map[lbl];
 }
+*/
 
 }
