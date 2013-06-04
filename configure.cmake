409904974456af3e9ceaeca71b6c1801c9e5c654
Justin Turney <justin.turney@gmail.com>
2013-06-04 17:39:12 +0200

    stuff
    Former-commit-id: 9afd8529c04d0279e9fd555a8f358cad08b89af3

--- configure.cmake
+++ configure.cmake
@@ -12,7 +12,7 @@ import os.path
 def execute(command, die_on_error = True):
     print("\tExecuting %s" % command)
     failed = 0;
-    process = subprocess.Popen(command.split())
+    process = subprocess.Popen(command)
     (stdout, stderr) = process.communicate()
     status = process.returncode
     if status:
@@ -36,16 +36,22 @@ blankstring = "No default" # This looks prettier when the user prints out the he
 # The C++ compiler
 parser.add_argument('--with-cxx',
                     metavar='= CXX',
-                    type=str, 
+                    type=str,
                     default=blankstring,
                     help='The C++ compiler to use.  If not specified the environmental variable $CXX will be used. '\
                          +'If that is not set, CMake will use the first working compiler it finds.')
-# Debug symbols 
+# CXXFLAGS flags
+parser.add_argument('--with-cxxflags',
+                    metavar='= CXXFLAGS',
+                    type=str,
+                    default=blankstring,
+                    help="Any extra flags to pass to the C++ compiler.")
+# Debug symbols
 debuggroup = parser.add_mutually_exclusive_group()
 debuggroup.add_argument('--with-debug',
                         action="store_true",
                         help='Add debug flags.')
-debuggroup.add_argument('--without-debug', 
+debuggroup.add_argument('--without-debug',
                         action="store_false",
                         help='Do not add debug flags.')
 # ERD package
@@ -53,7 +59,7 @@ erdgroup = parser.add_mutually_exclusive_group()
 erdgroup.add_argument('--with-erd',
                         action="store_true",
                         help='Add support for the ERD integral package.')
-erdgroup.add_argument('--without-erd', 
+erdgroup.add_argument('--without-erd',
                         action="store_false",
                         help='Do not use the ERD integral package.')
 # F77 symbol
@@ -66,19 +72,19 @@ parser.add_argument('--with-f77symbol',
 # Lapack
 parser.add_argument('--with-lapack',
                     metavar='= LAPACK',
-                    type=str, 
+                    type=str,
                     default=blankstring,
                     help='The flags to be passed to the linker to use BLAS and LAPACK. For modern mkl, with intel compilers, you can set this to -mkl (N.B. not -lmkl).  If omitted, CMake will try to find a working version for you.')
 # LD flags
 parser.add_argument('--with-ldflags',
                     metavar='= LDFLAGS',
-                    type=str, 
+                    type=str,
                     default=blankstring,
                     help="Any extra flags to pass to the linker (usually -Llibdir -llibname type arguments). You shouldn't need this.")
 # Libint max A.M.
 parser.add_argument('--with-max-am-eri',
                     metavar="= MAX_ANGULAR_MOMENTUM",
-                    type=int, 
+                    type=int,
                     default=5,
                     help='The maximum angular momentum level for the libint integrals package (p=1,d=2,3=f, etc.).')
 # Plugins
@@ -86,19 +92,19 @@ pluginsgroup = parser.add_mutually_exclusive_group()
 pluginsgroup.add_argument('--with-plugins',
                     action="store_false",
                     help='Compile with support for pluginss.')
-pluginsgroup.add_argument('--without-plugins', 
+pluginsgroup.add_argument('--without-plugins',
                     action="store_true",
                     help='Compile without support for plugins.')
 # Prefix
 parser.add_argument('--prefix',
                     metavar='= PREFIX',
-                    type=str, 
+                    type=str,
                     default="/usr/local/psi4",
                     help='Installation directory for Psi4')
 # Python
 parser.add_argument('--with-python',
                     metavar='= PYTHON',
-                    type=str, 
+                    type=str,
                     default=blankstring,
                     help='The Python interpreter (development version) to use.  CMake will detect one automatically, if omitted.')
 # Optimization
@@ -106,11 +112,29 @@ optgroup = parser.add_mutually_exclusive_group()
 optgroup.add_argument('--with-opt',
                     action="store_false",
                     help='Add optimization flags.')
-optgroup.add_argument('--without-opt', 
+optgroup.add_argument('--without-opt',
                     action="store_true",
                     help='Do not add optimization flags.')
 
 
+def dict_to_list(dictionary):
+    l = [ ]
+    for k in sorted(dictionary.iterkeys()):
+        s = "-D" + k + "="
+        if not dictionary[k]:
+            s += '""'
+        else:
+            if isinstance(dictionary[k], list):
+                s += " ".join(dictionary[k])
+                print l
+            elif isinstance(dictionary[k], str):
+                s += dictionary[k]
+            elif isinstance(dictionary[k], (int, float)):
+                s += str(dictionary[k])
+            else:
+                raise Exception("Unexpected keyword type: %r" % dictionary[k])
+        l.append(s)
+    return l
 
 def dict_to_string(dictionary):
     """Converts a dictionary of keywords into a string of arguments to pass to CMake"""
@@ -122,6 +146,7 @@ def dict_to_string(dictionary):
         else:
             if isinstance(dictionary[k], list):
                 string += '"' + " ".join(dictionary[k]) + '"'
+                print string
             elif isinstance(dictionary[k], str):
                 string += dictionary[k]
             elif isinstance(dictionary[k], (int, float)):
@@ -149,6 +174,8 @@ else:
 
 if args.with_debug:
     cmakeflags['CXXFLAGS'].append("-g")
+if args.with_cxxflags != blankstring:
+    cmakeflags['CXXFLAGS'].append(args.with_cxxflags)
 # LDFLAGS
 if args.with_ldflags != blankstring:
     cmakeflags['LDFLAGS'] = [args.with_ldflags]
@@ -164,5 +191,7 @@ if args.with_lapack != blankstring:
 # F77SYMBOL
 cmakeflags['F77SYMBOL'] = args.with_f77symbol
 
-execute("cmake " + scriptdir + dict_to_string(cmakeflags))
+args = ["cmake", scriptdir]
+args.extend(dict_to_list(cmakeflags))
+execute(args)
 
