15dc60a061c56812a43fdce1a28d5c355110cb57
David Hollman <david.s.hollman@gmail.com>
2013-06-05 19:04:43 -0400

    changes to grendel
    Former-commit-id: 1782b4ca08973837fbcaefc22de0ef72392687fa

--- lib/python/grendel/util/metaclasses.py
+++ lib/python/grendel/util/metaclasses.py
@@ -2,11 +2,120 @@
 """
 
 # Nothing in this module thus far should be exposed to the public interface.
-from abc import ABCMeta
-from functools import wraps
-from itertools import groupby
 
 __all__ = []
+
+#region | Immutable |
+
+class Immutable(type):
+    """
+    Metaclass for classes intended to be immutable.  Immutable classes can only
+    have attributes that are themselves immutable (or built-in types known to be
+    immutable, like str, int, float, complex, tuple, etc.).  Attributes can only
+    be set between the beginning and the end of the __init__ method (i.e. methods
+    called by __init__ can also make changes, but only if they are called from
+    __init__).  Methods intended to be called from __init__ that mutate the class
+    should have the @mutator decorator (though forgetting to do so will only
+    lead to a more confusing error message now, it may be used later).
+    All base classes of an Immutable class must themselves be immutable
+    """
+
+    IMMUTABLE_BUILTINS = (
+        str,
+        int,
+        long,
+        float,
+        complex,
+        bool,
+        slice
+    )
+
+    IMMUTABLE_SEQUENCES = (
+        frozenset,
+        tuple
+    )
+
+    #TODO support for lazily evaluated attributes
+
+    def __new__(mcs, name, bases, cls_dict):
+        # Make sure the bases are immutable
+        for base in bases:
+            if not isinstance(base, Immutable) and base is not object:
+                raise TypeError("Immutable classes can only inherit from other Immutable classes"
+                                " or 'object';  '{0}' inherits from '{1}', which is not Immutable".format(
+                    name, base.__name__
+                ))
+        #----------------------------------------#
+        # Patch __init__ to activate the immutability guard after it finishes
+        if "__init__" in cls_dict:
+            init_func = cls_dict['__init__']
+        else:
+            init_func = None
+        def __init__(self, *args, **kwargs):
+            object.__setattr__(self, "__allow_mutations__", True)
+            if init_func is not None:
+                init_func(self, *args, **kwargs)
+            if self.__class__.__name__ == name:
+                # only stop allowing mutations if we are in our
+                #   own __init__
+                self.__allow_mutations__ = False
+        if init_func is not None:
+            __init__.__doc__ = init_func.__doc__
+            __init__.__dict__.update(init_func.__dict__)
+        cls_dict['__init__'] = __init__
+        #----------------------------------------#
+        # Immutability checker
+        def is_immutable(obj):
+            if isinstance(obj, Immutable.IMMUTABLE_BUILTINS) or isinstance(type(obj), Immutable):
+                return True
+            elif isinstance(obj, Immutable.IMMUTABLE_SEQUENCES):
+                return all(is_immutable(item) for item in obj)
+            else:
+                return False
+        #----------------------------------------#
+        # Make __setattr__ raise an error
+        if "__setattr__" in cls_dict:
+            raise TypeError("Immutable classes cannot define '__setattr__'")
+        def __setattr__(self, key, value):
+            if self.__allow_mutations__:
+                # This means we're coming from __init__ or some function that __init__ calls
+                # But we still need to check if the value is immutable
+                if not is_immutable(value):
+                    raise AttributeError("Attributes of Immutable objects must be themselves Immutable.")
+                object.__setattr__(self, key, value)
+            else:
+                raise TypeError("Can't change attribute '{0}' of object of immutable type '{1}'".format(
+                    key, name
+                ))
+        cls_dict['__setattr__'] = __setattr__
+        #----------------------------------------#
+        # Make __delattr__ raise an error
+        if "__delattr__" in cls_dict:
+            raise TypeError("Immutable classes cannot define '__delattr__'")
+        def __delattr__(self, item):
+            if self.__allow_mutations__:
+                object.__delattr__(self, item)
+            else:
+                raise TypeError("Can't delete attribute '{0}' of object of immutable type '{1}'".format(
+                    item, name
+                ))
+        cls_dict['__delattr__'] = __delattr__
+        #----------------------------------------#
+        return type.__new__(mcs, name, bases, cls_dict)
+
+def mutator(func):
+    """
+    Does nothing so far
+    """
+    func.__is_mutator__ = True
+    return func
+
+#TODO StrictlyImmutable submetaclass that checks hash before and after every method call?
+#endregion
+
+#--------------------------------------------------------------------------------#
+
+#region | SubscriptableClass                                                             |
 
 # Slightly more efficient than using @operable_class, but much less general
 class SubscriptableClass(type):
@@ -27,6 +136,12 @@ class SubscriptableClass(type):
         else:
             # Otherwise, act like we're not here
             return NotImplemented
+
+#endregion
+
+#--------------------------------------------------------------------------------#
+
+#region | Commented out code                                                             |
 
 # TODO @must_call_super decorator
 #def semi_abstract_method(group):
@@ -71,6 +186,7 @@ class SubscriptableClass(type):
 #
 #
 #        return cls
+#endregion
 
 #####################
 # Dependent Imports #
--- tests/testgrendel/grendel_tests/util_tests/test_decorators.py
+++ tests/testgrendel/grendel_tests/util_tests/test_decorators.py
@@ -3,15 +3,14 @@ import unittest
 import sys
 import os
 
-# Add the directory containing 'grendel' to the sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
-
 # Add the directory containing the 'grendel_tests' package to sys.path
+from grendel.util.metaclasses import Immutable
+
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 
-from grendel.util.decorators import with_flexible_arguments
+from grendel.util.decorators import with_flexible_arguments, CachedMethod
 
-class DecoratorTest(unittest.TestCase):
+class FlexibleArgsTest(unittest.TestCase):
 
     def test_flexible_args_trivial(self):
         @with_flexible_arguments()
@@ -129,7 +128,7 @@ class DecoratorTest(unittest.TestCase):
         with self.assertRaisesRegexp(TypeError, r'flexible\(\) takes exactly 1 argument \(2 given\)'):
             foo.flexible(2, test=5)
 
-    def test_example(self):
+    def test_flecible_example(self):
         @with_flexible_arguments(
             required=[
                 ('name', 'greet', 'name_to_greet'),
@@ -154,4 +153,93 @@ class DecoratorTest(unittest.TestCase):
         )
 
 
+class FooCached(object):
 
+    def __init__(self, d):
+        self.d = d
+
+    @CachedMethod
+    def bar(self, a, b, c):
+        return ((a - b) // c) * self.d
+
+    @CachedMethod
+    def baz(self, a, b=2, c=4):
+        return (a + 2*b + 4*c) * self.d
+
+class FooImmutable(object):
+    __metaclass__ = Immutable
+
+    def __init__(self, d):
+        self.d = d
+
+    @CachedMethod
+    def bar(self, a, b):
+        return (2 * a + 4 * b) * self.d
+
+
+class CachedMethodTest(unittest.TestCase):
+
+    def test_order(self):
+        f = FooCached(3)
+        self.assertEqual(f.bar(14, 2, 4), 9)
+        self.assertEqual(f.bar(2, 14, 4), -9)
+        self.assertEqual(f.bar(4, 14, 2), -15)
+        self.assertEqual(f.bar(4, 2, 14), 0)
+        self.assertEqual(f.bar(2, 4, 14), -3)
+        self.assertEqual(len(f.bar.cache), 5)
+
+    def test_kwargs(self):
+        f = FooCached(4)
+        self.assertEqual(f.bar(2, 12, 5), -8)
+        self.assertEqual(f.bar(2, 12, c=5), -8)
+        self.assertEqual(f.bar(b=12, a=2, c=5), -8)
+        self.assertEqual(f.bar(a=2, c=5, b=12), -8)
+        self.assertEqual(len(f.bar.cache), 1)
+
+    def test_defaults(self):
+        f = FooCached(3)
+        self.assertEqual(f.baz(1), 63)
+        self.assertEqual(f.baz(3, c=1), 33)
+        self.assertEqual(f.baz(3, 2, 1), 33)
+        self.assertEqual(len(f.baz.cache), 2)
+
+    def test_immutable(self):
+        f = FooImmutable(2)
+        self.assertEqual(f.bar(3, 6), 60)
+        self.assertEqual(f.bar(a=3, b=6), 60)
+        self.assertEqual(len(f.bar.cache), 1)
+
+    def test_errors_1(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r'bar\(\) takes exactly 4 arguments \(3 given\)'):
+            f.bar(1,2)
+
+    def test_errors_2(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r'bar\(\) takes exactly 4 arguments \(5 given\)'):
+            f.bar(1,2,3,4)
+
+    def test_errors_3(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r'baz\(\) takes at least 2 arguments \(1 given\)'):
+            f.baz()
+
+    def test_errors_4(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r'baz\(\) takes at most 4 arguments \(5 given\)'):
+            f.baz(1,2,3,4)
+
+    def test_errors_5(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r"unexpected keyword argument 'f'"):
+            f.bar(1, 2, f=4)
+
+    def test_errors_6(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r"multiple values .* 'b'"):
+            f.bar(1, 2, b=4)
+
+    def test_errors_7(self):
+        f = FooCached(2)
+        with self.assertRaisesRegexp(TypeError, r"multiple values .* 'c'"):
+            f.baz(1, 2, 3, c=4)
