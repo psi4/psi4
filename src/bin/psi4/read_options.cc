140ac7c42436adb7b32b09ea4e6bed531a051d7b
Ugur Bozkaya <ugrbzky@gmail.com>
2013-07-18 19:09:39 +0300

    Checking positive definitness of OPDM for non-OO EKT computations.

--- src/bin/psi4/read_options.cc
+++ src/bin/psi4/read_options.cc
@@ -2657,6 +2657,10 @@ int read_options(const std::string &name, Options & options, bool suppress_print
     options.add_bool("EKT_EA",false);
     /*- Do optimize the orbitals?  -*/
     options.add_bool("ORB_OPT",true);
+    /*- Do consider orbital response contributions for PDMs and GFM?  -*/
+    options.add_bool("RELAXED",true);
+    /*- Do symmetrize the GFM and OPDM in the EKT computations?  -*/
+    options.add_bool("SYMMETRIZE",false);
   }
   if (name == "MRCC"|| options.read_globals()) {
       /*- MODULEDESCRIPTION Interface to MRCC program written by Mih\ |a_acute|\ ly K\ |a_acute|\ llay. -*/
