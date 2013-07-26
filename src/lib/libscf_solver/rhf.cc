0b2f0e952044b9ae7264f885709436400ad8612a
Ilya Kaliman <ilya.kaliman@gmail.com>
2013-07-26 18:50:07 -0400

    QM/EFP fully works now.
    Former-commit-id: 5bbb8e44c20543984a96efb585b8a02766ea5501

--- src/lib/libscf_solver/rhf.cc
+++ src/lib/libscf_solver/rhf.cc
@@ -206,12 +206,6 @@ void RHF::form_F()
     Fa_->copy(H_);
     Fa_->add(G_);
 
-    // add efp contribuation to Fock matrix
-    if ( Process::environment.get_efp()->get_frag_count() > 0 ) {
-        boost::shared_ptr<Matrix> Vefp = Process::environment.get_efp()->modify_Fock();
-        Fa_->add(Vefp);
-    }
-
     if (debug_) {
         Fa_->print(outfile);
         J_->print();
