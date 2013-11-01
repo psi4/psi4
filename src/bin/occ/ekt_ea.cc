bacdf63757c663e1333390c493a377794e167e57
Ugur Bozkaya <ugrbzky@gmail.com>
2013-11-02 01:12:33 +0200

    EKT properties can be requested in energy computations for non-OO methods.

--- src/bin/occ/ekt_ea.cc
+++ src/bin/occ/ekt_ea.cc
@@ -505,11 +505,13 @@ if (reference_ == "UNRESTRICTED") {
 }// if (reference_ == "UNRESTRICTED") 
 
      // For Non-OO methods
+     /*
      if (orb_opt_ == "FALSE" && reference_ == "RESTRICTED" && dertype == "FIRST") GFock->scale(2.0);  
      else if (orb_opt_ == "FALSE" && reference_ == "UNRESTRICTED" && dertype == "FIRST") {
               GFockA->scale(2.0);  
               GFockB->scale(2.0);  
      }
+     */
 
        GFock_primeA.reset();
        g1HalfA.reset();
