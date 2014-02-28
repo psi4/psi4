a0b74e78b10e8f21cc153517bba5d78cfa668a73
Andy Simmonett <andy.simmonett@gmail.com>
2014-02-28 11:41:45 -0500

    Fixed broken CCENERGY bypass SCF option.

--- lib/python/proc.py
+++ lib/python/proc.py
@@ -1030,13 +1030,14 @@ def run_ccenergy(name, **kwargs):
         pass
 
     # Bypass routine scf if user did something special to get it to converge
-    if not (('bypass_scf' in kwargs) and yes.match(str(kwargs['bypass_scf']))):
+    bypass = ('bypass_scf' in kwargs) and yes.match(str(kwargs['bypass_scf']))
+    if not bypass:
         scf_helper(name, **kwargs)
 
-        # If the scf type is DF/CD, then the AO integrals were never written to disk
-        if psi4.get_option('SCF', 'SCF_TYPE') == 'DF' or psi4.get_option('SCF', 'SCF_TYPE') == 'CD':
-            mints = psi4.MintsHelper()
-            mints.integrals()
+    # If the scf type is DF/CD, then the AO integrals were never written to disk
+    if bypass or psi4.get_option('SCF', 'SCF_TYPE') == 'DF' or psi4.get_option('SCF', 'SCF_TYPE') == 'CD':
+        mints = psi4.MintsHelper()
+        mints.integrals()
 
     psi4.transqt2()
     psi4.ccsort()
