e64c6620d459a3e47c2d30bf61751d05ab596406
Justin Turney <justin.turney@gmail.com>
2014-03-01 16:10:19 -0500

    Tag psi.clean with pid

--- src/lib/libpsio/psio.hpp
+++ src/lib/libpsio/psio.hpp
@@ -44,6 +44,8 @@ private:
     std::map<std::string, bool> files_;
     /// Set of files to retain after psiclean
     std::set<std::string> retained_files_;
+
+    std::string pid_;
 public:
     /// Default constructor (does nothing)
     PSIOManager();
@@ -77,15 +79,16 @@ public:
             */
     void set_specific_retention(int fileno, bool retain);
     /**
-            * Get the path for a specific file number
-            * \param fileno PSI4 file number
-            * \return the appropriate full path
-            */
-    bool get_specific_retention(int fileno);
-    /**
             * Inquire whether a specific file number is set to be retained
             * \param fileno PSI4 file number
             * \return keeping or not?
+            */
+    bool get_specific_retention(int fileno);
+
+    /**
+            * Get the path for a specific file number
+            * \param fileno PSI4 file number
+            * \return the appropriate full path
             */
     std::string get_file_path(int fileno);
 
