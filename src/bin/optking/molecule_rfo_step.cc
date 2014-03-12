53204138edb3a1474fc46a8bc3e925df0bdff2de
selliott <sne86684@bethel.edu>
2014-03-11 22:23:13 -0500

    iterative scheme for scaling rfo matrix added
    Former-commit-id: 75d9b22489b7a0c372c523a31624dfca0732fbcb

--- src/bin/optking/molecule_rfo_step.cc
+++ src/bin/optking/molecule_rfo_step.cc
@@ -60,125 +60,174 @@ void MOLECULE::rfo_step(void) {
   double *fq = p_Opt_data->g_forces_pointer();
   double **H = p_Opt_data->g_H_pointer();
   double *dq = p_Opt_data->g_dq_pointer();
+  double **SRFO = init_matrix(dim+1,dim+1);
+  double **rfO = init_matrix(dim+1,dim+1);
 
   // build (lower-triangle of) RFO matrix and diagonalize
   double **rfo_mat = init_matrix(dim+1, dim+1);
   for (i=0; i<dim; ++i)
     for (j=0; j<=i; ++j)
       rfo_mat[i][j] = H[i][j];
+      rfo[i][j] = H[i][j];
 
   for (i=0; i<dim; ++i)
     rfo_mat[dim][i] = - fq[i];
+    rfodim[i][j] = -fq[i];
 
   if (Opt_params.print_lvl >= 3) {
     fprintf(outfile,"RFO mat\n");
     print_matrix(outfile, rfo_mat, dim+1, dim+1);
   }
-
-  double *lambda = init_array(dim+1);
-  opt_symm_matrix_eig(rfo_mat, dim+1, lambda);
+  double *h = init_array(dim);
+  opt_symm_matrix_eig(H, dim, h);
+  double *evalsRfo = init_array(dim+1);
+  opt_symm_matrix_eig(rfo_mat, dim+1, evalsRfo);
   if (Opt_params.print_lvl >= 2) {
-    fprintf(outfile,"RFO eigenvalues/lambdas\n");
-    print_matrix(outfile, &(lambda), 1, dim+1);
+    fprintf(outfile,"RFO eigenvalues/evalsRfos\n");
+    print_matrix(outfile, &(evalsRfo), 1, dim+1);
   }
 
-  // Do intermediate normalization.  
-  // RFO paper says to scale eigenvector to make the last element equal to 1.
-  // During the course of an optimization some evects may appear that are bogus leads
-  // - the root following can avoid them. 
-  for (i=0; i<dim+1; ++i) {
-    tval = rfo_mat[i][dim];
-    if (fabs(tval) > Opt_params.rfo_normalization_min) {
-      for (j=0;j<dim+1;++j)
-        rfo_mat[i][j] /= rfo_mat[i][dim];
-    }
-  }
-  if (Opt_params.print_lvl >= 3) {
-    fprintf(outfile,"RFO eigenvectors (rows)\n");
-    print_matrix(outfile, rfo_mat, dim+1, dim+1);
-  }
 
   int rfo_root, f;
   double rfo_eval;
   double *rfo_u;      // unit vector in step direction
+  double dqtdq;
   double rfo_dqnorm;   // norm of step
   double rfo_g;         // gradient in step direction
   double rfo_h;         // hessian in step direction
   double DE_projected; // projected energy change by quadratic approximation
+  double alpha; //scaling factor
+  double** smat = init_matrix(dim+1,dim+1); //scaling matrix
+  bool symm_rfo_step = false;
+  double * rfo_old_evect = p_Opt_data->g_rfo_eigenvector_pointer();
+  double lambda;
+  double sum;
+  double analyticDerivative;
+  double trust;
+  trust = 0.001;
 
-  // *** choose which RFO eigenvector to use
-  // if not root following, then use rfo_root'th lowest eigenvalue; default is 0 (lowest)
-  if ( (!Opt_params.rfo_follow_root) || (p_Opt_data->g_iteration() == 1)) {
-    rfo_root = Opt_params.rfo_root;
-    fprintf(outfile,"\tGoing to follow RFO solution %d.\n", rfo_root+1);
+//Iterative sequence to find s
+  While (fabs(trust- Sqrt[dqtdq]) >= 10^-8) {
 
-    // Now test RFO eigenvector and make sure that it is totally symmetric.
-    bool symm_rfo_step = false;
+	//Creates scaling matrix
+	  for(i=0; i<dim+1; i++) {
+		for(j=0; j<dim+1; j++) {
+			if(i==dim+1 && j==dim+1) {
+				smat[i][j]=1
+			}
+			else if(i==j) {
+				smat[i][j]=1/alpha;
+			}
+			else {
+				smat[i][j]=0;
+			}
+		}
+	  }
+ 	 //Scales RFO matrix
+	  opt_matrix_mult(rfo, 0, smat, 0, SRFO, 0, dim+1, dim+1,1, 0);
+	  //Finds eigenvectors and eigenvalues
+	  opt_symm_matrix_eig(SRFO, dim+1, evalsSrfo);
+	
+	  // Do intermediate normalization.  
+	  // RFO paper says to scale eigenvector to make the last element equal to 1.
+	  // During the course of an optimization some evects may appear that are bogus leads
+	  // - the root following can avoid them. 
+	  for (i=0; i<dim+1; ++i) {
+	    tval = SRFO[i][dim];
+	    if (fabs(tval) > Opt_params.rfo_normalization_min) {
+	      for (j=0;j<dim+1;++j)
+	        SRFO[i][j] /= SRFO[i][dim];
+	    }
+	  }
+	  if (Opt_params.print_lvl >= 3) {
+	    fprintf(outfile,"scaled RFO eigenvectors (rows)\n");
+	    print_matrix(outfile, SRFO, dim+1, dim+1);
+	  }
 
-    while (!symm_rfo_step) {
+	  // *** choose which RFO eigenvector to use
+	  // if not root following, then use rfo_root'th lowest eigenvalue; default is 0 (lowest)
+	  if ( (!Opt_params.rfo_follow_root) || (p_Opt_data->g_iteration() == 1)) {
+	    rfo_root = Opt_params.rfo_root;
+	    fprintf(outfile,"\tGoing to follow RFO solution %d.\n", rfo_root+1);
+	
+	    // Now test RFO eigenvector and make sure that it is totally symmetric.
+	
+	    while (!symm_rfo_step) {
 
-      symm_rfo_step = intco_combo_is_symmetric(rfo_mat[rfo_root], dim);
+	      symm_rfo_step = intco_combo_is_symmetric(SRFO[rfo_root], dim);
 
-      if (!symm_rfo_step) {
-        fprintf(outfile,"\tRejecting RFO root %d because it breaks the molecular point group.\n", rfo_root+1);
-        fprintf(outfile,"\tIf you are doing an energy minimization, there may exist a lower-energy, ");
-        fprintf(outfile,"structure with less symmetry.\n");
-        ++rfo_root;
-      }
+	      if (!symm_rfo_step) {
+        	fprintf(outfile,"\tRejecting RFO root %d because it breaks the molecular point group.\n", rfo_root+1);
+	        fprintf(outfile,"\tIf you are doing an energy minimization, there may exist a lower-energy, ");
+        	fprintf(outfile,"structure with less symmetry.\n");
+	        ++rfo_root;
+	      }
 
-      if (rfo_root == dim+1) // quit in the unlikely event we've checked them all
-        break;
-    }
-  }
-  else { // do root following
-    double * rfo_old_evect = p_Opt_data->g_rfo_eigenvector_pointer();
-    tval = 0;
-    for (i=0; i<dim; ++i) { // dot only within H block, excluding rows and columns approaching 0
-      tval2 = array_dot(rfo_mat[i], rfo_old_evect,dim);
-      if (tval2 > tval) {
-        tval = tval2;
-        rfo_root = i;
-      }
-    }
-    fprintf(outfile,"RFO vector %d has maximal overlap with previous step\n", rfo_root+1);
-  }
-  p_Opt_data->set_rfo_eigenvector(rfo_mat[rfo_root]);
+	      if (rfo_root == dim+1) // quit in the unlikely event we've checked them all
+	        break;
+	    }
+	  }
+	  else { // do root following
+	    tval = 0;
+	    for (i=0; i<dim; ++i) { // dot only within H block, excluding rows and columns approaching 0
+	      tval2 = array_dot(SRFO[i], rfo_old_evect,dim);
+	      if (tval2 > tval) {
+        	tval = tval2;
+	        rfo_root = i;
+	      }
+	    }
+	    fprintf(outfile,"RFO vector %d has maximal overlap with previous step\n", rfo_root+1);
+	  }
+	  p_Opt_data->set_rfo_eigenvector(SRFO[rfo_root]);
 
-  // print out lowest energy evects
-  if (Opt_params.print_lvl >= 2) {
-    for (i=0; i<dim+1; ++i) {
-      if ((lambda[i] < 0.0) || (i <rfo_root)) {
-        fprintf(outfile,"RFO eigenvalue %d: %15.10lf (or 2*%-15.10lf)\n", i+1, lambda[i],lambda[i]/2);
-        fprintf(outfile,"eigenvector:\n");
-        print_matrix(outfile, &(rfo_mat[i]), 1, dim+1);
-      }
-    }
-  }
-  free_array(lambda);
+	  // print out lowest energy evects
+	  if (Opt_params.print_lvl >= 2) {
+	    for (i=0; i<dim+1; ++i) {
+	      if ((evalsSrfo[i] < 0.0) || (i <rfo_root)) {
+	        fprintf(outfile,"Scaled RFO eigenvalue %d: %15.10lf (or 2*%-15.10lf)\n", i+1, evalsSrfo[i],evalsSrfo[i]/2);
+	        fprintf(outfile,"eigenvector:\n");
+	        print_matrix(outfile, &(SRFO[i]), 1, dim+1);
+	      }
+	    }
+	  }
+	  free_array(evalsSrfo);
 
-  for (j=0; j<dim; ++j)
-    dq[j] = rfo_mat[rfo_root][j]; // leave out last column
+	  for (j=0; j<dim; ++j)
+	    dq[j] = SRFO[rfo_root][j]; // leave out last column
 
-  // Zero steps for frozen fragment
-  for (f=0; f<fragments.size(); ++f) {
-    if (fragments[f]->is_frozen() || Opt_params.freeze_intrafragment) {
-      fprintf(outfile,"\tZero'ing out displacements for frozen fragment %d\n", f+1);
-      for (i=0; i<fragments[f]->g_nintco(); ++i)
-        dq[ g_intco_offset(f) + i ] = 0.0;
-    }
-  }
+	//DR.KING, WE DON'T REALLY KNOW WHAT THIS FROZEN FRAGMENT IS FOR AND IF IT NEEDS ALTERING
+	  // Zero steps for frozen fragment  
+	  for (f=0; f<fragments.size(); ++f) {
+	    if (fragments[f]->is_frozen() || Opt_params.freeze_intrafragment) {
+	      fprintf(outfile,"\tZero'ing out displacements for frozen fragment %d\n", f+1);
+	      for (i=0; i<fragments[f]->g_nintco(); ++i)
+	        dq[ g_intco_offset(f) + i ] = 0.0;
+	    }
+	  }
 
-  apply_intrafragment_step_limit(dq);
-  //check_intrafragment_zero_angles(dq);
-
-  // get norm |dq| and unit vector in the step direction
-  rfo_dqnorm = sqrt( array_dot(dq, dq, dim) );
-  rfo_u = init_array(dim);
-  array_copy(rfo_mat[rfo_root], rfo_u, dim);
-  array_normalize(rfo_u, dim);
-  free_matrix(rfo_mat);
-
-  // get gradient and hessian in step direction
+	  apply_intrafragment_step_limit(dq);
+	  //check_intrafragment_zero_angles(dq);
+	
+	  // get norm |dq| and unit vector in the step direction
+	  dqtdq = array_dot(dq, dq, dim);
+	  rfo_dqnorm = sqrt( dqtdq );
+	  rfo_u = init_array(dim);
+	  array_copy(SRFO[rfo_root], rfo_u, dim);
+	  array_normalize(rfo_u, dim);
+	  free_matrix(rfo_mat);
+	  free_matrix(SRFO);
+	
+	  // find the analytical derivative
+	  lambda = array_dot(fq,dq,dim);
+	  sum = 0;
+	  for (int i=0; i<dim; i++) {
+		sum = sum + ( array_dot(H[i],fq)^2 ) / ( ( h[i]-lambda*alpha )^3 );
+	  }
+	  analyticDerivative = 2*lambda / (1+alpha*dqtdq ) * sum;
+	  alpha = alpha + 2*(trust*rfo_dqnorm - dqtdq) / analyticDerivative;
+   }
+	  
+ // get gradient and hessian in step direction
   rfo_g = -1 * array_dot(fq, rfo_u, dim);
   rfo_h = 0;
   for (i=0; i<dim; ++i)
@@ -201,8 +250,9 @@ void MOLECULE::rfo_step(void) {
   fprintf(outfile,"Step-size in mass-weighted internal coordinates: %20.10lf\n", N);
 */
 
-  // do displacements for each fragment separately
-  for (f=0; f<fragments.size(); ++f) {
+
+// do displacements for each fragment separately
+ * for (f=0; f<fragments.size(); ++f) {
     if (fragments[f]->is_frozen() || Opt_params.freeze_intrafragment) {
       fprintf(outfile,"\tDisplacements for frozen fragment %d skipped.\n", f+1);
       continue;
