a02c90fc5c12158ec7b3929f3283e8a8f396587a
Alexander Sokolov <alex@ccqc.uga.edu>
2014-06-04 09:36:11 -0400

    Three-particle correction can now be calculated using Ftilde semicanonical orbitals
    Former-commit-id: 32ccd116f8f4cb707c1d4624458d264882325ee6

--- src/bin/dcft/dcft_memory.cc
+++ src/bin/dcft/dcft_memory.cc
@@ -75,6 +75,8 @@ DCFTSolver::init()
     Fb_          = SharedMatrix(reference_wavefunction_->Fb());
     moF0a_         = SharedMatrix(new Matrix("Alpha MO F0 Matrix", nirrep_, nmopi_, nmopi_));
     moF0b_         = SharedMatrix(new Matrix("Beta MO F0 Matrix", nirrep_, nmopi_, nmopi_));
+    Ftilde_a_      = SharedMatrix(new Matrix("Alpha MO Ftilde Matrix", nirrep_, nmopi_, nmopi_));
+    Ftilde_b_      = SharedMatrix(new Matrix("Beta MO Ftilde Matrix", nirrep_, nmopi_, nmopi_));
     Ca_          = SharedMatrix(new Matrix("Alpha MO Coefficients", nirrep_, nsopi_, nsopi_));
     Cb_          = SharedMatrix(new Matrix("Beta MO Coefficients", nirrep_, nsopi_, nsopi_));
     moFa_        = SharedMatrix(new Matrix("Alpha MO Fock Matrix", nirrep_, nmopi_, nmopi_));
