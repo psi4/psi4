9161e8d9f7971d57028f5380baeb5ddf76d64491
Ryan Richard <ryanmrichard1@gmail.com>
2014-06-24 19:05:47 -0400

    LocalComm works
    Former-commit-id: 49ef31c6f4ee3371883a1e859db69b352e9fb226

--- include/process.h
+++ include/process.h
@@ -1,23 +1,23 @@
 /*
- * JKFactory: Interface and code for highly parallel J and K
- *             builds.
+ *@BEGIN LICENSE
  *
- *  Copyright (c) 2014 Ryan M. Richard
+ * PSI4: an ab initio quantum chemistry software package
  *
- *  This file is part of JKFactory.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  JKFactory is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *@END LICENSE
  */
 #ifndef PROCESS_H_
 #define PROCESS_H_
