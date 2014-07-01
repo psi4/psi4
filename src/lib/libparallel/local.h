80921bf336c4daac343ff01348d63693f1ea795e
Ryan Richard <ryanmrichard1@gmail.com>
2014-07-01 14:30:17 -0400

    Added a parallel filemanager
    Former-commit-id: 99d7673c3749016391fc3c433a93d9b393e71cfb

--- src/lib/libparallel/local.h
+++ src/lib/libparallel/local.h
@@ -29,35 +29,40 @@
 #include <omp.h>
 namespace psi {
 
-class LocalCommWrapper: public Parallel<LocalCommWrapper> {
-	private:
-		template<typename type>
-		void bcastImpl(const type* data,const int nelem, const int
-				broadcaster,const std::string& CommName="None")const{
-		}
+class LocalCommWrapper:public Parallel<LocalCommWrapper> {
+   private:
+      template <typename type>
+      void bcastImpl(const type* data, const int nelem, const int broadcaster,
+            const std::string& CommName="None") const {
+      }
+      template <typename T>
+      void bcastImpl(T& data,const int broadcaster,
+            const std::string&Comm="NONE") const {
+      }
 
-    template<typename type>
-    void all_gatherImpl(const type* localdata,const int nelem, type* target,
-          const std::string& CommName="NONE") const {
-       if(localdata!=target)
-             std::memcpy(const_cast<type*>(localdata),target,sizeof(type)*nelem);
-    }
-    friend Parallel<LocalCommWrapper>;
-	public:
-    LocalCommWrapper(const int &argc,char **argv){
-       //The next three lines are what the old local comm did
-       //the code breaks if I do not include them
-       //The way I understand this is that the number of openmp threads
-       //is getting hard-coded to 1, and somewhere in the code people are
-       //counting on this behavior...
-       omp_set_nested(0);
-       if (Process::environment("OMP_NUM_THREADS") == "")
-           Process::environment.set_n_threads(1);
-       std::cout<<"Made local comm"<<std::endl;
-    }
-}; // End of LocalCommWrapper class
+      template <typename type>
+      void all_gatherImpl(const type* localdata, const int nelem, type* target,
+            const std::string& CommName="NONE") const {
+         if (localdata!=target)
+            std::memcpy(const_cast<type*>(localdata), target,
+                  sizeof(type)*nelem);
+      }
+      friend Parallel<LocalCommWrapper> ;
+   public:
+      LocalCommWrapper(const int &argc, char **argv) {
+         //The next three lines are what the old local comm did
+         //the code breaks if I do not include them
+         //The way I understand this is that the number of openmp threads
+         //is getting hard-coded to 1, and somewhere in the code people are
+         //counting on this behavior...
+         omp_set_nested(0);
+         if (Process::environment("OMP_NUM_THREADS")=="")
+            Process::environment.set_n_threads(1);
+         std::cout<<"Made local comm"<<std::endl;
+      }
+};
+// End of LocalCommWrapper class
 
-
-} // End of namespace psi
+}// End of namespace psi
 
 #endif // End of _psi_src_lib_libparallel_local_h_
