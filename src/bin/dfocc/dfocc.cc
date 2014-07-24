20c557df6d21e69b0d5ed2d79fba68c916826b48
Ugur Bozkaya <ugrbzky@gmail.com>
2014-07-25 01:37:28 +0300

    Updating title functions.

--- src/bin/dfocc/dfocc.cc
+++ src/bin/dfocc/dfocc.cc
@@ -383,7 +383,7 @@ void DFOCC::title()
    else if (wfn_type_ == "CD-OMP2" && orb_opt_ == "TRUE") fprintf(outfile,"                      CD-OMP2 (CD-OO-MP2)   \n");
    else if (wfn_type_ == "CD-OMP2" && orb_opt_ == "FALSE") fprintf(outfile,"                       CD-MP2   \n");
    fprintf(outfile,"              Program Written by Ugur Bozkaya\n") ; 
-   fprintf(outfile,"              Latest Revision July 13, 2014\n") ;
+   fprintf(outfile,"              Latest Revision July 25, 2014\n") ;
    fprintf(outfile,"\n");
    fprintf(outfile," ============================================================================== \n");
    fprintf(outfile," ============================================================================== \n");
