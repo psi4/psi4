f145dd8dacb07b788aa4a736d5f9b35573319e82
Ryan Richard <ryanmrichard1@gmail.com>
2014-08-25 16:32:42 -0400

    Doxygen is working with CMake and fixed some license issues in libfrag
    Former-commit-id: 1a056f74ef560074ece4adb2f14bc1b1f8aa40de

--- src/lib/libfrag/MBE.h
+++ src/lib/libfrag/MBE.h
@@ -1,8 +1,23 @@
 /*
- * MBE.h
+ *@BEGIN LICENSE
  *
- *  Created on: May 15, 2014
- *      Author: richard
+ * PSI4: an ab initio quantum chemistry software package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *@END LICENSE
  */
 
 #ifndef MBE_H_
