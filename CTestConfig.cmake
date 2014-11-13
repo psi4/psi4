7b8c31ba5132f48b08f4b4b7d3c02ed37fcf7f26
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-11-13 11:17:10 +0100

    Remove configure.cmake.bak Update CTestConfig.cmake
    All available processors are used in building the project when submitting to the Dashboard
    
    
    Former-commit-id: 0a2c6f65d7f2c516e4525259550474bfea4cad9d

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,3 +11,11 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "repo.theochem.kth.se")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Psi")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+if(DEFINED ENV{CTEST_MAKE_NUM_PROCS})
+    set(MAKECOMMAND "make -j$ENV{CTEST_MAKE_NUM_PROCS}" CACHE STRING "Custom make command")
+endif()
+
+# total allowed time for all tests in seconds
+# see http://www.itk.org/Wiki/CTest:Using_CTEST_and_CDASH_without_CMAKE#General_CTest_settings
+set(CTEST_TIMEOUT           "21600")
