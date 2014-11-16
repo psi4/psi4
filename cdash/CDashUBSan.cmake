0d6c046dd7b59448cefb7942ea2a8c7a4220afbb
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-11-16 22:28:29 +0100

    Valgrind and sanitizers in place. Have to test on stallo
    Former-commit-id: 759e4523771269d647cb0b47a5181810c0f4403c

---
+++ cdash/CDashUBSan.cmake
@@ -0,0 +1,30 @@
+#
+# CTest script for undefined behaviour sanitizer (UBSan) memcheck build and submission to dashboard 
+# Warning: the correct flags are to be set through a separate (and prior) call to
+# the setup script!
+#
+# Written by Roberto Di Remigio November 2014 
+#
+
+find_program(HOSTNAME_COMMAND NAMES hostname)
+execute_process(COMMAND ${HOSTNAME_COMMAND} 
+    OUTPUT_VARIABLE CTEST_SITE
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+include(ProcessorCount)
+ProcessorCount(NCORES)
+if(NOT NCORES EQUAL 0)
+    set(CTEST_BUILD_FLAGS -j${NCORES})
+else()
+    set(NCORES 1)
+endif()
+
+set(CTEST_MEMORYCHECK_TYPE "UndefinedBehaviorSanitizer")
+ctest_start(Experimental)
+ctest_configure()
+ctest_build()
+ctest_test(PARALLEL_LEVEL ${NCORES})
+ctest_memcheck(PARALLEL_LEVEL ${NCORES})
+ctest_submit()
+
+# vim:et:sw=4:ts=4:
--- cmake/ConfigTesting.cmake
+++ cmake/ConfigTesting.cmake
@@ -1,5 +1,32 @@
 include(ConfigFramework)
 
+if(ENABLE_MEMCHECK)
+   find_program(VALGRIND_EXECUTABLE NAMES valgrind)
+   if(VALGRIND_EXECUTABLE)
+      set(MEMORYCHECK_COMMAND ${VALGRIND_EXECUTABLE})
+      set(CTEST_MEMORYCHECK_COMMAND ${VALGRIND_EXECUTABLE})
+      set(MEMORYCHECK_COMMAND_OPTIONS "--leak-check=full")
+      set(MEMORYCHECK_SUPPRESSIONS_FILE
+          ${CMAKE_BINARY_DIR}/valgrind-suppressions.txt)
+   endif()
+   mark_as_advanced(VALGRIND_EXECUTABLE)
+endif()   
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/CTestConfig.cmake)
+   include(CTest)
+   if(EXISTS ${CMAKE_SOURCE_DIR}/cdash)
+      set(DASHBOARD_DIR ${CMAKE_SOURCE_DIR}/cdash)
+      add_subdirectory(cdash)
+   endif()
+endif()
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/CTestCustom.cmake.in)
+   configure_file(${CMAKE_SOURCE_DIR}/CTestCustom.cmake.in
+       ${CMAKE_BINARY_DIR}/CTestCustom.cmake @ONLY)
+endif()
+
+enable_testing()
+
 # set cdash buildname
 set(BUILDNAME
     "${CMAKE_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}-${CMAKE_CXX_COMPILER_ID}-${BLAS_TYPE}-${CMAKE_BUILD_TYPE}"
@@ -14,9 +41,5 @@ set(DART_TESTING_TIMEOUT
     "Set timeout in seconds for every single test"
     )
 
-include(CTest)
-set(DASHBOARD_DIR ${CMAKE_SOURCE_DIR}/cdash)
-add_subdirectory(cdash)
-enable_testing()
 # This must come last!!
 add_subdirectory(tests)
