3ca664d4a57f0b960da4222837a4f080d3cea20e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2014-11-19 16:48:37 +0100

    Updated CTest scripts for dynamic analysis
    Former-commit-id: f62e918de2fd465038dbe44cb08dd744df12c709

--- cdash/CDashValgrind.cmake
+++ cdash/CDashValgrind.cmake
@@ -4,13 +4,16 @@
 # Written by Roberto Di Remigio November 2014 
 #
 
-message("CTEST_SOURCE_DIRECTORY : ${CTEST_SOURCE_DIRECTORY}")
-message("CTEST_BINARY_DIRECTORY : ${CTEST_BINARY_DIRECTORY}")
+set(PROJECT_NAME "Psi")
+set(PROJECT_REPOSITORY "git@github.com:robertodr/psi4.git") 
+set(CTEST_COMMAND ctest)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_NAME "RDR-gcc4.9.1-asan")
+set(CTEST_SITE "stallo.uit.no")
 
-find_program(HOSTNAME_COMMAND NAMES hostname)
-execute_process(COMMAND ${HOSTNAME_COMMAND} 
-    OUTPUT_VARIABLE CTEST_SITE
-    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+find_program(CTEST_GIT_COMMAND NAMES git)
+set(CTEST_UPDATE_COMMAND ${CTEST_GIT_COMMAND})
+
 
 include(ProcessorCount)
 ProcessorCount(NCORES)
@@ -20,12 +23,49 @@ else()
     set(NCORES 1)
 endif()
 
+# Defaults
+set(BRANCH master)
+set(TEST_MODEL Experimental)
+
+# Initialize directories
+set(CTEST_SOURCE_DIRECTORY $ENV{CTEST_SOURCE_DIRECTORY})
+set(CTEST_BINARY_DIRECTORY $ENV{CTEST_BINARY_DIRECTORY})
+# Fresh clone of repository and checkout right branch
+if(NOT EXISTS ${CTEST_SOURCE_DIRECTORY})
+    execute_process(COMMAND ${CTEST_GIT_COMMAND} 
+        clone ${PROJECT_REPOSITORY} ${CTEST_SOURCE_DIRECTORY}
+        OUTPUT_QUIET
+        )
+    if(NOT (${BRANCH} STREQUAL master))
+        execute_process(COMMAND ${CTEST_GIT_COMMAND} checkout 
+            -b ${BRANCH} origin/${BRANCH}
+            WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+            OUTPUT_QUIET
+            )
+    endif()
+endif()
+
+# Run the setup script
+macro(setup_cmake_env)
+    set(CTEST_CONFIGURE_COMMAND "cmake  -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DENABLE_MPI=OFF -DENABLE_SGI_MPT=OFF -DENABLE_OMP=ON -DENABLE_VECTORIZATION=OFF -DENABLE_CSR=OFF -DENABLE_SCALAPACK=OFF -DENABLE_SCALASCA=OFF -DENABLE_UNIT_TESTS=OFF -DENABLE_STATIC_LINKING=OFF -DENABLE_PLUGINS=ON -DENABLE_LIBERD=OFF -DENABLE_GPU_DFCC=OFF -DENABLE_DUMMY_PLUGIN=OFF -DENABLE_CXX11_SUPPORT=ON -DLIBINT_OPT_AM=5 -DENABLE_MEMCHECK=ON -DCMAKE_INSTALL_PREFIX=/usr/local/psi4 -DCMAKE_BUILD_TYPE=debug ${CTEST_SOURCE_DIRECTORY}")
+endmacro()    
+
+# Run the dashboard
+macro(run_dashboard)
+    ctest_configure()
+    ctest_build()
+    ctest_test(PARALLEL_LEVEL ${NCORES})
+    ctest_memcheck(PARALLEL_LEVEL ${NCORES})
+endmacro()
+
+# This is the actual run
+setup_cmake_env()
+ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 set(CTEST_MEMORYCHECK_TYPE "Valgrind")
-ctest_start(Experimental)
-ctest_configure()
-ctest_build()
-ctest_test(PARALLEL_LEVEL ${NCORES})
-ctest_memcheck(PARALLEL_LEVEL ${NCORES})
+ctest_start(${TEST_MODEL})
+ctest_update()
+run_dashboard()
 ctest_submit()
 
 # vim:et:sw=4:ts=4:
