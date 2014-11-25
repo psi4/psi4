0781c3a67a87b45f9cc3ccda05310feeac6fbfb3
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-11-25 12:26:30 +0100

    New Find*.cmake modules added for detection of libdl, libm, librt and libutil. Refactor (again) Python detection.
    Detection of system libraries is more CMake idiomatic and use the FindPackageHandleStandardArgs standard module.
    Python detection has been moved out of FindPython.cmake (which was not a Find*.cmake module) to the root CMakeLists.txt
    The necessary functions have been moved to GenericMacros.cmake
    
    
    Former-commit-id: ed93516b422ae00d4f91a1d43142a6db164d8de3

--- cmake/FindDL.cmake
+++ cmake/FindDL.cmake
@@ -1,30 +1,60 @@
-# - Find libdl
-# Find the native LIBDL includes and library
+# PSI4: an ab initio quantum chemistry software package
 #
-#  LIBDL_INCLUDE_DIR - where to find dlfcn.h, etc.
+# Copyright (c) 2007-2013 The PSI4 Developers.
+#
+# The copyrights for code used from other parties are included in
+# the corresponding files.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# - Find libdl
+# Find the native libdl includes and library
+#
+#  LIBDL_INCLUDE_DIR - where to find dlfcn.h
 #  LIBDL_LIBRARIES   - List of libraries when using libdl.
 #  LIBDL_FOUND       - True if libdl found.
 
-
-IF (LIBDL_INCLUDE_DIR)
+if(LIBDL_INCLUDE_DIR)
   # Already in cache, be silent
-  SET(LIBDL_FIND_QUIETLY TRUE)
-ENDIF (LIBDL_INCLUDE_DIR)
+  set(LIBDL_FIND_QUIETLY TRUE)
+endif(LIBDL_INCLUDE_DIR)
 
-FIND_PATH(LIBDL_INCLUDE_DIR dlfcn.h)
+find_path(LIBDL_INCLUDE_DIR dlfcn.h
+	  PATHS
+	  /usr/include
+	  /usr/local/include
+	  )
 
-SET(LIBDL_NAMES dl libdl ltdl libltdl)
-FIND_LIBRARY(LIBDL_LIBRARY NAMES ${LIBDL_NAMES} )
+set(LIBDL_NAMES dl libdl ltdl libltdl)
+find_library(LIBDL_LIBRARY 
+	     NAMES ${LIBDL_NAMES}
+	     PATHS
+	     /usr/lib
+	     /usr/local/lib
+	     /lib
+	     )
 
 # handle the QUIETLY and REQUIRED arguments and set LIBDL_FOUND to TRUE if 
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibDL DEFAULT_MSG LIBDL_LIBRARY LIBDL_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libdl DEFAULT_MSG LIBDL_LIBRARY LIBDL_INCLUDE_DIR)
 
-IF(LIBDL_FOUND)
-  SET( LIBDL_LIBRARIES ${LIBDL_LIBRARY} )
-ELSE(LIBDL_FOUND)
-  SET( LIBDL_LIBRARIES )
-ENDIF(LIBDL_FOUND)
+if(LIBDL_FOUND)
+  set(LIBDL_LIBRARIES ${LIBDL_LIBRARY})
+else(LIBDL_FOUND)
+  set(LIBDL_LIBRARIES)
+endif(LIBDL_FOUND)
 
-MARK_AS_ADVANCED( LIBDL_LIBRARY LIBDL_INCLUDE_DIR )
+mark_as_advanced(LIBDL_LIBRARY LIBDL_INCLUDE_DIR)
