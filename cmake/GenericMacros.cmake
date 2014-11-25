8f7d9c09c096d2eb3a2e54fc234c24eee0293740
Roberto Di Remigio <roberto.diremigio@gmail.com>
2014-11-25 14:42:49 +0100

    Fixed Python problems on stallo
    Former-commit-id: 92d7a77d16fcab7d7b324b91f0bd9978535213ce

--- cmake/GenericMacros.cmake
+++ cmake/GenericMacros.cmake
@@ -38,66 +38,6 @@ macro(init_FCMangle)
         file(APPEND ${PROJECT_BINARY_DIR}/include/FCMangle.h ${contents})
 endmacro()
 
-function(get_Python_version_string pyVersion) 
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                            "import sys; sys.stdout.write(';'.join([str(x) for x in sys.version_info[:3]]))"
-                    OUTPUT_VARIABLE _VERSION
-                    RESULT_VARIABLE _PYTHON_VERSION_RESULT
-                    ERROR_QUIET)
-    if(NOT _PYTHON_VERSION_RESULT)
-        string(REPLACE ";" "." pyVersion "${_VERSION}")
-        list(GET _VERSION 0 PYTHON_VERSION_MAJOR)
-        list(GET _VERSION 1 PYTHON_VERSION_MINOR)
-        list(GET _VERSION 2 PYTHON_VERSION_PATCH)
-        if(PYTHON_VERSION_PATCH EQUAL 0)
-            # it's called "Python 2.7", not "2.7.0"
-            string(REGEX REPLACE "\\.0$" "" pyVersion "${pyVersion}")
-        endif()
-    else()
-        # sys.version predates sys.version_info, so use that
-        execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import sys; sys.stdout.write(sys.version)"
-                        OUTPUT_VARIABLE _VERSION
-                        RESULT_VARIABLE _PYTHON_VERSION_RESULT
-                        ERROR_QUIET)
-        if(NOT _PYTHON_VERSION_RESULT)
-            string(REGEX REPLACE " .*" "" pyVersion "${_VERSION}")
-            string(REGEX REPLACE "^([0-9]+)\\.[0-9]+.*" "\\1" PYTHON_VERSION_MAJOR "${pyVersion}")
-            string(REGEX REPLACE "^[0-9]+\\.([0-9])+.*" "\\1" PYTHON_VERSION_MINOR "${pyVersion}")
-            if(pyVersion MATCHES "^[0-9]+\\.[0-9]+\\.[0-9]+.*")
-                string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" PYTHON_VERSION_PATCH "${pyVersion}")
-            else()
-                set(PYTHON_VERSION_PATCH "0")
-            endif()
-        else()
-            # sys.version was first documented for Python 1.5, so assume
-            # this is older.
-            set(pyVersion "1.4")
-            set(PYTHON_VERSION_MAJOR "1")
-            set(PYTHON_VERSION_MAJOR "4")
-            set(PYTHON_VERSION_MAJOR "0")
-        endif()
-    endif()
-    set(pyVersion "${pyVersion}" PARENT_SCOPE)
-    unset(_PYTHON_VERSION_RESULT)
-    unset(_VERSION)
-endfunction()
-
-function(set_python_hints pyIncDir pyLibs)
-    # Copied from https://bitbucket.org/fenics-project/dolfin
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                      "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_inc())"
-	        OUTPUT_VARIABLE pyIncDir 
-		RESULT_VARIABLE _PYTHON_INCLUDE_RESULT
-	       )
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                     "import os, sys, inspect; sys.stdout.write(os.path.split(os.path.split(inspect.getfile(inspect))[0])[0])"
-	        OUTPUT_VARIABLE pyLibs 
-		RESULT_VARIABLE _PYTHON_LIBRARY_RESULT
-	       )
-    set(pyIncDir "${pyIncDir}" PARENT_SCOPE)
-    set(pyLibs   "${pyLibs}" PARENT_SCOPE)
-endfunction()
-
 function(test_fortran_mangling CODE PREFIX ISUPPER POSTFIX FLAGS DOC SUB RESULT)
   if(ISUPPER)
     string(TOUPPER "${SUB}" sub)
