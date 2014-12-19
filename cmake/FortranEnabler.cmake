840da82d774ceb4d71f998a0c06189dd264f19c8
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-12-19 21:57:52 +0100

    Start work on segregating Fortran

---
+++ cmake/FortranEnabler.cmake
@@ -0,0 +1,16 @@
+# Determine Fortran name mangling, used for external linking
+if ((${CMAKE_SYSTEM_NAME} MATCHES "Darwin") AND (CMAKE_SYSTEM_VERSION VERSION_LESS 13))
+    # 12.5.0 broken (LAB), above should trap pre-Mavericks
+    set(CMAKE_C_FLAGS_HOLD ${CMAKE_C_FLAGS})
+    set(CMAKE_CXX_FLAGS_HOLD ${CMAKE_CXX_FLAGS})
+    message(STATUS "Trying Fortran name mangling appending -m32 flag")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+endif()
+include(FortranCInterface)
+FortranCInterface_VERIFY(CXX)
+init_FCMangle()
+if ((${CMAKE_SYSTEM_NAME} MATCHES "Darwin") AND (CMAKE_SYSTEM_VERSION VERSION_LESS 13))
+    set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS_HOLD})
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS_HOLD})
+endif()
