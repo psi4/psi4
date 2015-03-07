4b9a6dde610d5cb9b57b183a5eeb75ef2c8379c1
Lori A. Burns <lori.burns@gmail.com>
2015-03-06 20:07:03 -0500

    initial checkin of conda recipe. a hard-coded path in build.sh so can't immediately be run, in general
    Former-commit-id: 2050c9e0e5310bc3aead09a0f9e635a1faad3cee

---
+++ conda-recipe/psi4/post-link.sh
@@ -0,0 +1,7 @@
+set +x off
+echo "  Thank you for installing psi4. It is recommended that you run script"
+echo "    >>> ${PREFIX}/share/psi/scripts/setenv.py"
+echo "  to configure the runtime environment. Additional resources can be found at"
+echo "    Website: www.psicode.org"
+echo "    GitHub:  https://github.com/psi4/psi4public/wiki"
+echo "    Manual:  bit.ly/psi4manual"
