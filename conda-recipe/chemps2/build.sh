d6b15e259c76bec704bdd62b8fa72c39c72926e2
Lori A. Burns <lori.burns@gmail.com>
2015-04-17 20:08:21 -0400

    update libchemps2.so conda so as static as possible, old libc, and MKL embedded

--- conda-recipe/chemps2/build.sh
+++ conda-recipe/chemps2/build.sh
@@ -1,14 +1,16 @@
 source /theoryfs2/common/software/intel2015/bin/compilervars.sh intel64
-#export TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/psi4cmake/psi4/glibc2.12rpm
-#    -DLIBC_INTERJECT="-L${TLIBC}/usr/lib64 ${TLIBC}/lib64/libpthread.so.0 ${TLIBC}/lib64/libc.so.6" \
+export TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/psi4cmake/psi4/glibc2.12rpm
 
 mkdir build
 cd build
 cmake \
     -DCMAKE_CXX_COMPILER=icpc \
-    -DSTATIC_ONLY=ON \
-    -DENABLE_XHOST=OFF \
+    -DMKL=ON \
+    -DBUILD_DOCUMENTATION=OFF \
     -DENABLE_TESTS=OFF \
+    -DENABLE_GENERIC=ON \
+    -DENABLE_XHOST=OFF \
+    -DLIBC_INTERJECT="-L${TLIBC}/usr/lib64 ${TLIBC}/lib64/libpthread.so.0 ${TLIBC}/lib64/libc.so.6" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     ${SRC_DIR}
 make -j3  # -j${CPU_COUNT}
