5806868de190bdb86ac49548ac2cda9239fcb388
Lori A. Burns <lori.burns@gmail.com>
2015-04-21 14:29:58 -0400

    update conda chemps2 recipe to install to lib and redistribute libiomp5.so

--- conda-recipe/chemps2/build.sh
+++ conda-recipe/chemps2/build.sh
@@ -1,8 +1,11 @@
 source /theoryfs2/common/software/intel2015/bin/compilervars.sh intel64
 export TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/psi4cmake/psi4/glibc2.12rpm
+REDIST=/theoryfs2/common/software/intel2015/composer_xe_2015.0.090/compiler/lib/intel64/libiomp5.so
 
 mkdir build
 cd build
+mkdir lib
+cp ${REDIST} ${PREFIX}/lib
 cmake \
     -DCMAKE_CXX_COMPILER=icpc \
     -DMKL=ON \
@@ -12,6 +15,7 @@ cmake \
     -DENABLE_XHOST=OFF \
     -DLIBC_INTERJECT="-L${TLIBC}/usr/lib64 ${TLIBC}/lib64/libpthread.so.0 ${TLIBC}/lib64/libc.so.6" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     ${SRC_DIR}
 make -j3  # -j${CPU_COUNT}
 make install
