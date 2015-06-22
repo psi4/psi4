9866ef003bd6aaac61ff28d82cdb2c95c664e0af
Lori A. Burns <lori.burns@gmail.com>
2015-06-22 17:49:24 -0400

    more extract_changeset fixes for integration with cmake

--- doc/sphinxman/extract_changeset.py
+++ doc/sphinxman/extract_changeset.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import sys
 # Extracts last git changelog item into html for psicode
 
 gitlast = 'feed/latest_trac_changeset.txt'  # path to output file with latest changeset
@@ -7,13 +8,14 @@ githist = 'feed/history_trac_changeset.txt'  # path to output file with last Nhi
 Nhist = 100
 
 # So that this can be called from anywhere
+writedir = sys.argv[1] if len(sys.argv) == 2 else './'
 thisdir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(thisdir)
 
 log_cmd = 'git log --branches=master -%s --no-merges --pretty=format:\'<title> %%cr [%%h] by %%cn: <br/> %%s<br/></title>\' > %s'\
-          % (Nhist, githist)
+          % (Nhist, writedir + os.sep + githist)
 os.system(log_cmd)
 
 log_cmd = 'git log --branches=master -1 --no-merges --pretty=format:\'<title> %%cr by %%cn: <br/> %%s<br/></title>\' > %s'\
-          % (gitlast)
+          % (writedir + os.sep + gitlast)
 os.system(log_cmd)
