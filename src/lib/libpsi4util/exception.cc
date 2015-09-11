4a8eb4ecfe43b936f0edf408bf03357d95cd5969
Justin Turney <justin.turney@gmail.com>
2015-09-11 09:56:27 -0400

    Adds stack trace information to PsiException
    Pulls the stack trace code from PsiException2… why is there another
    PsiException class.
    
    
    Former-commit-id: d4382b1f341d08de56bf565dc3883ef940a0c53f

--- src/lib/libpsi4util/exception.cc
+++ src/lib/libpsi4util/exception.cc
@@ -21,6 +21,9 @@
  */
 
 #include <exception.h>
+#include <execinfo.h>
+#include <cxxabi.h>
+#include <vector>
 #include <sstream>
 
 #include <cstring>
@@ -28,20 +31,57 @@
 using namespace std;
 
 namespace psi {
-PsiException::PsiException(
-    string msg,
-    const char* _file,
-    int _line
-) throw() : runtime_error(msg)
+
+PsiException::PsiException(string msg,
+                           const char *_file,
+                           int _line) throw()
+        : runtime_error(msg)
 {
-    msg_ = msg;
     file_ = _file;
     line_ = _line;
+
+    std::stringstream message;
+    message << std::endl << "Fatal Error: " << msg << std::endl;
+    message << "Error occurred in file: " << file_ << " on line: " << line_ << std::endl;
+
+    std::vector<void *> Stack(5);
+    char **strings;
+    int size = backtrace(&Stack[0], 5);
+    int status = -1;
+
+    message << "The most recent " << (size < 5 ? size : 5) << " function calls were:" << std::endl << std::endl;
+    strings = backtrace_symbols(&Stack[0], size);
+
+    for (int i = 0; i < size; i++) {
+        //This part from https://panthema.net/2008/0901-stacktrace-demangled/
+        char *begin_name = NULL, *begin_offset = NULL, *end_offset = NULL;
+        for (char *p = strings[i]; *p; ++p) {
+            if (*p == '(') begin_name = p;
+            else if (*p == '+')begin_offset = p;
+            else if (*p == ')' && begin_offset) {
+                end_offset = p;
+                break;
+            }
+        }
+        if (begin_name && begin_offset && end_offset
+            && begin_name < begin_offset) {
+            *begin_name++ = '\0';
+            *begin_offset++ = '\0';
+            *end_offset = '\0';
+            char *demangledname =
+                    abi::__cxa_demangle(begin_name, 0, 0, &status);
+            if (status == 0)
+                message << demangledname << std::endl;
+            ::free(demangledname);
+        }
+    }
+
+    msg_ = message.str();
 }
 
 PsiException::PsiException(const PsiException& copy) throw()
-    : runtime_error(copy.msg_), msg_(copy.msg_), file_(strdup(copy.file_)),
-        line_(copy.line_)
+        : runtime_error(copy.msg_), msg_(copy.msg_), file_(strdup(copy.file_)),
+          line_(copy.line_)
 {
 }
 
@@ -51,7 +91,7 @@ PsiException::rewrite_msg(string msg) throw()
     msg_ = msg;
 }
 
-const char*
+const char *
 PsiException::what() const throw()
 {
     //stringstream sstr;
@@ -61,7 +101,7 @@ PsiException::what() const throw()
     return msg_.c_str();
 }
 
-const char*
+const char *
 PsiException::file() const throw()
 {
     return file_;
@@ -73,7 +113,7 @@ PsiException::line() const throw()
     return line_;
 }
 
-const char*
+const char *
 PsiException::location() const throw()
 {
     stringstream sstr;
@@ -87,83 +127,83 @@ PsiException::~PsiException() throw()
 }
 
 SanityCheckError::SanityCheckError(
-    string message,
-    const char* _file,
-    int _line
-    ) throw()
-  : PsiException(message, _file, _line)
+        string message,
+        const char *_file,
+        int _line
+) throw()
+        : PsiException(message, _file, _line)
 {
     stringstream sstr;
     sstr << "sanity check failed! " << message;
     rewrite_msg(sstr.str());
 }
 
-SanityCheckError::~SanityCheckError() throw() {}
+SanityCheckError::~SanityCheckError() throw()
+{ }
 
 SystemError::SystemError(
-    int eno,
-    const char* _file,
-    int _line
-    ) throw()
-    : PsiException("", _file, _line)
+        int eno,
+        const char *_file,
+        int _line
+) throw()
+        : PsiException("", _file, _line)
 {
     stringstream sstr;
     sstr << "SystemError:  " << strerror(eno);
     rewrite_msg(sstr.str());
 }
 
-SystemError::~SystemError() throw() {}
+SystemError::~SystemError() throw()
+{ }
 
 InputException::InputException(
-    string msg,
-    string param_name,
-    int value,
-    const char* _file,
-    int _line
+        string msg,
+        string param_name,
+        int value,
+        const char *_file,
+        int _line
 ) throw() : PsiException(msg, _file, _line)
 {
     write_input_msg<int>(msg, param_name, value);
 }
 
 InputException::InputException(
-    string msg,
-    string param_name,
-    string value,
-    const char* _file,
-    int _line
+        string msg,
+        string param_name,
+        string value,
+        const char *_file,
+        int _line
 ) throw() : PsiException(msg, _file, _line)
 {
     write_input_msg<string>(msg, param_name, value);
 }
 
 InputException::InputException(
-    string msg,
-    string param_name,
-    double value,
-    const char* _file,
-    int _line
+        string msg,
+        string param_name,
+        double value,
+        const char *_file,
+        int _line
 ) throw() : PsiException(msg, _file, _line)
 {
     write_input_msg<double>(msg, param_name, value);
 }
 
 InputException::InputException(
-    string msg,
-    string param_name,
-    const char* _file,
-    int _line
+        string msg,
+        string param_name,
+        const char *_file,
+        int _line
 ) throw() : PsiException(msg, _file, _line)
 {
     write_input_msg<string>(msg, param_name, "in input");
 }
 
-template <
-    class T
-> void
-InputException::write_input_msg(
-    string msg,
-    string param_name,
-    T value
+template<class T>
+void InputException::write_input_msg(
+        string msg,
+        string param_name,
+        T value
 ) throw()
 {
     stringstream sstr;
@@ -175,106 +215,119 @@ InputException::write_input_msg(
 
 template<class T>
 StepSizeError<T>::StepSizeError(
-    string value_name,
-    T max,
-    T actual,
-    const char* _file,
-    int _line) throw()
-    : LimitExceeded<T>(value_name + " step size", max, actual, _file, _line)
+        string value_name,
+        T max,
+        T actual,
+        const char *_file,
+        int _line) throw()
+        : LimitExceeded<T>(value_name + " step size", max, actual, _file, _line)
 {
 }
 
 template<class T>
-StepSizeError<T>::~StepSizeError() throw() {}
+StepSizeError<T>::~StepSizeError() throw()
+{ }
 
 template<class T>
 MaxIterationsExceeded<T>::MaxIterationsExceeded(
-    string routine_name,
-    T max,
-    const char* _file,
-    int _line)  throw()
-    : LimitExceeded<T>(routine_name + " iterations", max, max, _file, _line)
+        string routine_name,
+        T max,
+        const char *_file,
+        int _line)  throw()
+        : LimitExceeded<T>(routine_name + " iterations", max, max, _file, _line)
 {
 }
 
 template<class T>
-MaxIterationsExceeded<T>::~MaxIterationsExceeded() throw() {}
+MaxIterationsExceeded<T>::~MaxIterationsExceeded() throw()
+{ }
 
 template<class T>
 ConvergenceError<T>::ConvergenceError(
-    string routine_name,
-    T max,
-    double _desired_accuracy,
-    double _actual_accuracy,
-    const char* _file,
-    int _line) throw()
-    : MaxIterationsExceeded<T>(routine_name + " iterations", max, _file, _line), desired_acc_(_desired_accuracy), actual_acc_(_actual_accuracy)
+        string routine_name,
+        T max,
+        double _desired_accuracy,
+        double _actual_accuracy,
+        const char *_file,
+        int _line) throw()
+        : MaxIterationsExceeded<T>(routine_name + " iterations", max, _file, _line), desired_acc_(_desired_accuracy),
+          actual_acc_(_actual_accuracy)
 {
     stringstream sstr;
-    sstr << "could not converge " << routine_name << ".  desired " << _desired_accuracy << " but got " << _actual_accuracy << "\n";
+    sstr << "could not converge " << routine_name << ".  desired " << _desired_accuracy << " but got " <<
+    _actual_accuracy << "\n";
     sstr << LimitExceeded<T>::description();
     PsiException::rewrite_msg(sstr.str());
 }
 
 template<class T>
-ConvergenceError<T>::~ConvergenceError() throw() {}
+ConvergenceError<T>::~ConvergenceError() throw()
+{ }
 
 template<class T>
 double
-ConvergenceError<T>::desired_accuracy() const throw() {return desired_acc_;}
+ConvergenceError<T>::desired_accuracy() const throw()
+{ return desired_acc_; }
 
 template<class T>
 double
-ConvergenceError<T>::actual_accuracy() const throw() {return actual_acc_;}
+ConvergenceError<T>::actual_accuracy() const throw()
+{ return actual_acc_; }
 
 template<>
 ConvergenceError<int>::ConvergenceError(
-    string routine_name,
-    int max,
-    double _desired_accuracy,
-    double _actual_accuracy,
-    const char* _file,
-    int _line) throw()
-    : MaxIterationsExceeded<int>(routine_name + " iterations", max, _file, _line), desired_acc_(_desired_accuracy), actual_acc_(_actual_accuracy)
+        string routine_name,
+        int max,
+        double _desired_accuracy,
+        double _actual_accuracy,
+        const char *_file,
+        int _line) throw()
+        : MaxIterationsExceeded<int>(routine_name + " iterations", max, _file, _line), desired_acc_(_desired_accuracy),
+          actual_acc_(_actual_accuracy)
 {
     stringstream sstr;
-    sstr << "could not converge " << routine_name << ".  desired " << _desired_accuracy << " but got " << _actual_accuracy << "\n";
+    sstr << "could not converge " << routine_name << ".  desired " << _desired_accuracy << " but got " <<
+    _actual_accuracy << "\n";
     sstr << LimitExceeded<int>::description();
     PsiException::rewrite_msg(sstr.str());
 }
 
 template<>
-ConvergenceError<int>::~ConvergenceError() throw() {}
+ConvergenceError<int>::~ConvergenceError() throw()
+{ }
 
 template<>
 double
-ConvergenceError<int>::desired_accuracy() const throw() {return desired_acc_;}
+ConvergenceError<int>::desired_accuracy() const throw()
+{ return desired_acc_; }
 
 template<>
 double
-ConvergenceError<int>::actual_accuracy() const throw() {return actual_acc_;}
+ConvergenceError<int>::actual_accuracy() const throw()
+{ return actual_acc_; }
 
 template<class T>
 ResourceAllocationError<T>::ResourceAllocationError(
-    string resource_name,
-    T max,
-    T actual,
-    const char* _file,
-    int _line)  throw()
-    : LimitExceeded<size_t>(resource_name, max, actual, _file, _line)
+        string resource_name,
+        T max,
+        T actual,
+        const char *_file,
+        int _line)  throw()
+        : LimitExceeded<size_t>(resource_name, max, actual, _file, _line)
 {
 }
 
 template<class T>
-ResourceAllocationError<T>::~ResourceAllocationError() throw() {}
+ResourceAllocationError<T>::~ResourceAllocationError() throw()
+{ }
 
 FeatureNotImplemented::FeatureNotImplemented(
-    string module_name,
-    string feature_name,
-    const char* _file,
-    int _line
+        string module_name,
+        string feature_name,
+        const char *_file,
+        int _line
 ) throw()
- : PsiException("psi exception", _file, _line)
+        : PsiException("psi exception", _file, _line)
 {
     stringstream sstr;
     sstr << feature_name << " not implemented in " << module_name;
