psi4/psi4

commit 19741369dc24fc8ebbfaf33793854d6a00b48e95

Adds ability to send options to the sow/reap per symmetry
Former-commit-id: 0f644a09eb0e86a3c3ae55159a9d5100ec196b8d

lib/python/p4util/procutil.py
@@ -100,7 +100,7 @@ def format_molecule_for_input(mol, name=''):
     #return eval(commands)
 
 
-def format_options_for_input():
+def format_options_for_input(molecule, **kwargs):
     """Function to return a string of commands to replicate the
     current state of user-modified options. Used to capture C++
     options information for distributed (sow/reap) input files.
@@ -110,11 +110,17 @@ def format_options_for_input():
        - Does not cover local (as opposed to global) options.
 
     """
+    symmetry = molecule.find_point_group(0.00001).symbol()
     commands = ''
     commands += """\npsi4.set_memory(%s)\n\n""" % (psi4.get_memory())
     for chgdopt in psi4.get_global_option_list():
         if psi4.has_global_option_changed(chgdopt):
             chgdoptval = psi4.get_global_option(chgdopt)
+
+            if chgdopt.lower() in kwargs:
+                if symmetry in kwargs[chgdopt.lower()]:
+                    chgdoptval = kwargs[chgdopt.lower()][symmetry]
+
             if isinstance(chgdoptval, basestring):
                 commands += """psi4.set_global_option('%s', '%s')\n""" % (chgdopt, chgdoptval)
 # Next four lines were conflict between master and roa branches (TDC, 10/29/2014)

lib/python/driver.py.REMOVED.git-id
@@ -1,1 +1,1 @@
-e52689861efc8f5619ec689c100b134fb081e912
+a59da47b7ddf02f97eb03e87270a80019eb1087b

commit ddeacd9e4c15475ab701482c77911bd360d6a6c6

fix pywrap-opt-sowreap test case broken by 19741369dc24fc8ebbfaf33793854d6a00b48e95 [formerly 0f644a09eb0e86a3c3ae55159a9d5100ec196b8d]
Former-commit-id: 1072fc72f20804d4edf8e020f0b28cf272a9854e

lib/python/p4util/procutil.py
@@ -100,7 +100,7 @@ def format_molecule_for_input(mol, name=''):
     #return eval(commands)
 
 
-def format_options_for_input(molecule, **kwargs):
+def format_options_for_input(molecule=None, **kwargs):
     """Function to return a string of commands to replicate the
     current state of user-modified options. Used to capture C++
     options information for distributed (sow/reap) input files.
@@ -110,16 +110,18 @@ def format_options_for_input(molecule, **kwargs):
        - Does not cover local (as opposed to global) options.
 
     """
-    symmetry = molecule.find_point_group(0.00001).symbol()
+    if molecule is not None:
+        symmetry = molecule.find_point_group(0.00001).symbol()
     commands = ''
     commands += """\npsi4.set_memory(%s)\n\n""" % (psi4.get_memory())
     for chgdopt in psi4.get_global_option_list():
         if psi4.has_global_option_changed(chgdopt):
             chgdoptval = psi4.get_global_option(chgdopt)
 
-            if chgdopt.lower() in kwargs:
-                if symmetry in kwargs[chgdopt.lower()]:
-                    chgdoptval = kwargs[chgdopt.lower()][symmetry]
+            if molecule is not None:
+                if chgdopt.lower() in kwargs:
+                    if symmetry in kwargs[chgdopt.lower()]:
+                        chgdoptval = kwargs[chgdopt.lower()][symmetry]
 
             if isinstance(chgdoptval, basestring):
                 commands += """psi4.set_global_option('%s', '%s')\n""" % (chgdopt, chgdoptval)