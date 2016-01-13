787682833eaa1598bdbc2505338df941692239d3
Daniel Smith <malorian@me.com>
2016-01-13 13:44:42 -0500

    Wfn pass: FNOCC is done, unfortunately conventional does not work until transqt is replaced
    Former-commit-id: ce896e5a8bacaf07f39f0bd5f3bb72e11c59dbb1

--- src/bin/fnocc/coupled_pair.cc
+++ src/bin/fnocc/coupled_pair.cc
@@ -53,9 +53,8 @@ void DefineQuadraticTasks();
 void SortIntegrals(int nfzc,int nfzv,int norbs,int ndoccact,int nvirt,Options&options);
 
 CoupledPair::CoupledPair(boost::shared_ptr<Wavefunction> reference_wavefunction, Options &options):
-        CoupledCluster(reference_wavefunction,options)
+        CoupledCluster(reference_wavefunction, options)
 {
-    reference_wavefunction_ = reference_wavefunction;
     common_init();
 
     // which cepa level? 0,1,2,3
