c1c9fe1bb714a5d0c2e42f37248e8d0462ecc78c
Andy Simmonett <andy.simmonett@gmail.com>
2016-01-22 15:37:23 -0700

    Fixed build script to work with older bash.
    Former-commit-id: e32b36e8cd1e40ea77c9fdd8a502aca52c9f1f13

--- .scripts/travis_build.sh
+++ .scripts/travis_build.sh
@@ -2,7 +2,7 @@
 
 # The return code will capture an error from ANY of the functions in the pipe
 set -o pipefail
-make |& tee build.log | grep "Building"
+make 2>&1 | tee build.log | grep "Building"
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
