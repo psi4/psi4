9d3374331eb24975dbb32f9e6664669119654e91
Andy Simmonett <andy.simmonett@gmail.com>
2016-01-26 22:42:27 -0700

    Attempting to output the logs from failed runs in Travis tests.
    Former-commit-id: e01ecc0150701ba32ed2862c55c5228049297072

---
+++ .scripts/travis_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ctest -j2 -L quicktests 2>&1 | tee run.log
+
+# Now we scrape the run log, looking for any failures
+while read p; do
+  set -o pipefail
+  echo $p | grep "(Failed)"
+  if [ $? -eq 0 ]; then
+    badtest=`echo $p | awk '{print $3}'`
+    echo $badtest failed.  Here is the output:
+    cat tests/${badtest}/output.dat
+  fi
+done <run.log
