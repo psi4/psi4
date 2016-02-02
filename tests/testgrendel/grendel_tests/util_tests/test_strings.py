b35340864634e33b10869d97dfd3dc571d4be29a
Daniel Smith <malorian@me.com>
2016-02-01 19:01:17 -0500

    Wfn pass: Moved more grendel and findif to the attic

--- tests/testgrendel/grendel_tests/util_tests/test_strings.py
+++
@@ -1,9 +0,0 @@
-from grendel.gmath.tensor import Tensor
-from grendel.gmath.vector import Vector
-from grendel.util.strings import classname
-
-def test_classname():
-    assert classname(object) == "object"
-    assert classname(Tensor) == "Tensor"
-    assert classname("Torsion") == "Torsion"
-    assert classname(Vector(1,2,3)) == "Vector"
