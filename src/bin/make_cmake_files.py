8d6a1f01f6577d06b4a0bc73946114b0b77bcbdc
Daniel Smith <malorian@me.com>
2016-04-19 15:35:07 -0400

    Added boilerplate license to all files where headers were missing

--- src/bin/make_cmake_files.py
+++ src/bin/make_cmake_files.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python
-# -*- python -*-
-# -*- coding: utf-8 -*-
-# vim:filetype=python:
-# Create CMakeLists.txt template for leaf directories
-# (c) Roberto Di Remigio  <roberto.d.remigio@uit.no>
-# licensed under the GNU Lesser General Public License
+#
+# @BEGIN LICENSE
+#
+# Psi4: an open-source quantum chemistry software package
+#
+# Copyright (c) 2007-2016 The Psi4 Developers.
+#
+# The copyrights for code used from other parties are included in
+# the corresponding files.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# @END LICENSE
+#
 
 import sys
 import glob
@@ -148,4 +168,28 @@ if (not lang == 'F'):
 print('Template for {} created'.format(args.libname))
 print('Don\'t forget to fix excluded files and dependencies!!!')
 
-# vim:et:ts=4:sw=4
+N LICENSE
+#
+# Psi4: an open-source quantum chemistry software package
+#
+# Copyright (c) 2007-2016 The Psi4 Developers.
+#
+# The copyrights for code used from other parties are included in
+# the corresponding files.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# @END LICENSE
+## vim:et:ts=4:sw=4
--- src/lib/libfock/jk_independent.h
+++ src/lib/libfock/jk_independent.h
@@ -1,17 +1,29 @@
-/**
- * jk_independent.h
- * 
- * Created by Bill March, 2/25/13
+/*
+ * @BEGIN LICENSE
  *
- * This is a version of the JK class that takes two classes as template 
- * arguments and uses them to create the J and K matrices. This allows us
- * to specify two different methods for the two matrices, such as CFMM and
- * LinK.
+ * Psi4: an open-source quantum chemistry software package
  *
- * Problem with this: how do I instantiate it? I'll have to add to RHF or 
- * where ever this happens.
+ * Copyright (c) 2007-2016 The Psi4 Developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
  */
-
 
 #ifndef _jk_independent_h
 #define _jk_independent_h
