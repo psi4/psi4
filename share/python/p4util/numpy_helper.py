c14eefb329c690f8d8c8b3300704e366e46864c8
dgasmith <malorian@me.com>
2016-05-07 14:13:04 -0400

    NumPy: Adds ability to convert irreped psi4 objects to dense arrays
    Former-commit-id: 5830af54f9200da011dd043edc1306ae47c9cbc2

--- share/python/p4util/numpy_helper.py
+++ share/python/p4util/numpy_helper.py
@@ -111,19 +111,68 @@ def array_to_matrix(self, arr, name="New Matrix"):
         ret_view[:] = arr
         return ret
 
-def to_array(matrix, copy=True):
+def to_array(matrix, copy=True, dense=False):
     """
     Converts a Psi4 Matrix or Vector to a numpy array. Either copies the data or simply
     consturcts a view.
     """
     if matrix.nirrep() > 1:
+
+        # We will copy when we make a large matrix
+        if dense:
+            copy = False
+
         ret = []
         for h in matrix.array_interfaces():
             if 0 in h.__array_interface__["shape"]:
                 ret.append(np.empty(shape = h.__array_interface__["shape"]))
             else:
                 ret.append(np.array(h, copy=copy))
-        return ret
+
+        # Return the list of arrays
+        if dense is False:
+            return ret
+        
+        # Build the dense matrix
+        if isinstance(matrix, psi4.Vector):
+            ret_type = '1D'
+        elif isinstance(matrix, psi4.Matrix):
+            ret_type = '2D'
+        else:
+            raise ValidationError("Array_to_Matrix: type '%s' is not recognized." % type(matrix))
+
+        dim1 = []
+        dim2 = []
+        for h in ret:
+            # Ignore zero dim irreps
+            if 0 in h.shape:
+                dim1.append(0)
+                dim2.append(0)
+            else:
+                dim1.append(h.shape[0])
+                if ret_type == '2D':
+                    dim2.append(h.shape[1])
+
+        ndim1 = np.sum(dim1)
+        ndim2 = np.sum(dim2)
+        if ret_type == '1D':
+            dense_ret = np.empty(shape=(ndim1))
+            start = 0
+            for d1, arr in zip(dim1, ret):
+                if d1 == 0: continue
+                dense_ret[start: start + d1] = arr
+                start += d1
+        else:
+            dense_ret = np.empty(shape=(ndim1, ndim2))
+            start1 = 0
+            start2 = 0
+            for d1, d2, arr in zip(dim1, dim2, ret):
+                if d1 == 0: continue
+                dense_ret[start1: start1 + d1, start2: start2 + d2] = arr
+                start1 += d1
+                start1 += d2
+
+        return dense_ret
 
     else:
         if 0 in matrix.__array_interface__["shape"]:
