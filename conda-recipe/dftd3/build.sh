946d3fdc50aa09f25ddb8a86c667a8492577fe89
Lori A. Burns <lori.burns@gmail.com>
2016-05-16 11:29:59 -0400

    remove unused conda recipes that could deceive readers. mostly to trigger a version increment.
    Former-commit-id: 87d264f8eab2c1b050f49c8218cfea95bf2059e1

--- conda-recipe/dftd3/build.sh
+++
@@ -1,8 +0,0 @@
-# load Intel compilers and mkl
-source /theoryfs2/common/software/intel2015/bin/compilervars.sh intel64
-
-make
-# pseudo "make install"
-mkdir ${PREFIX}/bin
-cp dftd3 ${PREFIX}/bin
-
