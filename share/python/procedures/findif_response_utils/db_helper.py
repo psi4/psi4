0a2b816205e0a5dc5b1380884286a1ba3b90d70e
Andrew Moulton James <amjames2@vt.edu>
2016-05-29 18:51:34 -0400

    Begins reorganization of code from roa.py
    This will facilitate the re-use of common methods between several
    driver methods for computing property derivatives by finite differences.

---
+++ share/python/procedures/findif_response_utils/db_helper.py
@@ -0,0 +1,150 @@
+#
+# @BEGIN LICENSE
+#
+# Psi4: an open-source quantum chemistry software package
+#
+# Copyright (c) 2007-2016 The Psi4 Developers.
+#
+# The copyrights for code used from other parties are included in
+# the corresponding files.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# @END LICENSE
+#
+from __future__ import absolute_import
+from __future__ import print_function
+import collections
+import shelve
+import copy
+import os
+import psi4
+import p4util
+from p4const import *
+
+def generate_inputs(name,prop,db):
+    """
+        Generates the input files in each sub-directory of the
+        distributed finite differences property calculation
+
+    name: ( string ) method name passed to original driver,
+    prop: ( string ) property type being computed.
+      ** Warning this will very soon be depreciated for **
+      ** a more general database structure that contains this **
+      ** information is being developed **
+    db:   (database) The database object associated with this property
+        calculation.
+    """
+    molecule = psi4.get_active_molecule()
+    natom = molecule.natom()
+
+    # get list of displacements
+    displacement_geoms = psi4.atomic_displacements(molecule)
+
+    # Sanity Check
+    # there should be 3 cords * natoms *2 directions (+/-)
+    if not (6*natom ) == len(displacment_geoms):
+        raise Exception('The number of atomic displacements should be 6 times'
+                        ' the number of atoms!')
+
+    displacement_names = db['job_status'].keys()
+
+    for n,entry in enumerate(displacement_names):
+        if not os.path.exists(entry):
+            os.makedirs(entry)
+
+        # Setup up input file string
+        inp_template = 'molecule {molname}_{disp}'
+        inp_template += ' {{\n{molecule_info}\n}}\n{options}\n{jobspec}\n'
+        molecule.set_geometry(displacement_geoms[n])
+        molecule.fix_orientation(True)
+        molecule.fix_com(True)
+        inputfile = open('{0}/input.dat'.format(entry),'w')
+        inputfile.write("# This is a psi4 input file auto-generated for computing properties by finite differences.\n\n")
+        inputfile.write(
+                inp_template.format(
+                    molname=molecule.name(),
+                    disp=entry,
+                    molecule_info=molecule.create_psi4_string_from_molecule(),
+                    options=p4util.format_options_for_input(),
+                    jobspec="property('{0}', properties=['{1}'])".format(
+                        name,prop
+                        )
+                    )
+                )
+        inputfile.close()
+
+def initalize_database(database,prop):
+    """
+        Initialize the database for computation of some property
+        using distributed finite differences driver
+
+    database: (database) the database object passes from the caller
+    prop:  (string) the property that is being computed
+    """
+    registered_props = {
+            "roa":"roa_tensor",
+            "roation":"roation_tensor"
+            }
+    database['inputs_generated']         = False
+    database['jobs_complete']            = False
+    database['{}_computed'.format(prop)] = False
+    database['prop_string']              = registered_props[prop]
+    database['job_status']               = collections.OrderedDict()
+    # Populate the job_status dict
+    molecule = psi4.get_active_molecule()
+    natom    = molecule.natom()
+    coordinates       = ['x','y','z']
+    step_direction    = ['p','m']
+
+    for atom in range(1,natom+1):
+        for coord in coordinates:
+            for step in step_direction:
+                job_name = '{}_{}_{}'.format(atom,coord,step)
+                database['job_status'].update({job_name: 'not_started'})
+
+    # End initalize_database()
+
+
+
+def stat(db):
+    """
+        Checks displacement sub_directories for the status of each
+        displacement computation
+
+    db: (database) the database storing information for this distributed
+        property calculation
+
+    """
+    n_finished=0
+    for job,status in db['job_status'].items():
+        if status == 'finished':
+            n_finished +=1
+        elif status in ('not_started','running'):
+            try:
+                with open("{}/output.dat".format(job)) as outfile:
+                    outfile.seek(-150,2)
+                    for line in outfile:
+                        if 'Psi4 exiting sucessfully' in line:
+                            db['job_status'][job] = 'finished'
+                            n_finished += 1
+                            break
+                        else:
+                            db['job_status'][job] = 'running'
+            except:
+                pass
+    ### check all jobs done?
+    if n_finished == len(db['job_status'].keys()):
+        db['jobs_complete'] = True
---
+++ share/python/procedures/findif_response_utils/tensor_helper.py
@@ -0,0 +1,113 @@
+#
+# @BEGIN LICENSE
+#
+# Psi4: an open-source quantum chemistry software package
+#
+# Copyright (c) 2007-2016 The Psi4 Developers.
+#
+# The copyrights for code used from other parties are included in
+# the corresponding files.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# @END LICENSE
+#
+
+from __future__ import absolute_import
+from __future__ import print_function
+import collections
+import shelve
+import copy
+import os
+import psi4
+import p4util
+from p4const import *
+
+
+def synthesize_displaced_tensor(db,signature,row_dim):
+    """
+        Gathers a list of tensors, one at each displaced geometry.
+
+    db: (database) the database object for this property calculation
+    signature: (string) The string that notifies the matrix reader that the
+        targeted tensor data begins.
+    row_dim: the expected number of rows that this value should be printed
+        across in the file
+
+        Returns a 2d list result[i][j]:
+            i: indexes displacements
+            j: indexes elements of the flattened tensor at some displacement
+    """
+    result = []
+    for job in db['job_status']:
+        with open('{}/output.dat'.format(job)) as outfile:
+            result.append(grab_psi4_matrix(outfile,signature,row_dim))
+
+    return result
+
+
+def grab_psi4_matrix(outfile, matrix_name,row_tot):
+    """
+        Grabs a psi4 matrix printed to the output file
+
+    outfile: ( file ) handle open in read mode, where the data should be found
+    matrix_name: ( string ) that indicates the matrix data is found on the lines
+        below
+    row_tot: ( int ) indicates the number of lines that the matrix data should
+        be printed across in the file
+
+        Returns a flattened matrix (list) should be 3*row_tot
+            Throws an exception (Collecting matrix data failed) if it is not
+            the corect size
+    """
+    collect_matrix = False
+    n_nrows = 0
+    n_tries = 0
+    matrix_data = []
+    for line in outfile:
+        if matrix_name in line:
+            collect_matrix = True
+        if collect_matrix and (n_rows < row_tot):
+            try:
+                n_tries += 1
+                if n_tries > (row_tot +13):
+                    raise Exception('{} Matrix was unreadable. Scanned {}'
+                            'lines.'.format(matrix_name,n_tries))
+                else:
+                    (index, x,y,z) = line.split()
+                    matrix_data.append(float(x))
+                    matrix_data.append(float(y))
+                    matrix_data.append(float(z))
+                    n_rows += 1
+            except:
+                pass
+        if (n_rows == row_tot) and (len(matrix_data) != 3*row_tot):
+            raise Exception('Collecting {} matrix data failed!'
+                    '\nExpected {} elements but only captured {}'.format(
+                         matrix_name,3*row_tot,len(matrix_data)
+                        )
+                    )
+        if len(matrix_data) == 3*row_tot:
+            return matrix_data
+
+
+
+
+
+
+
+
+
+
