b63630515fde15097f2683c705600e04eece38cb
Rollin King <rking@bethel.edu>
2016-06-24 15:05:26 -0500

    tweaked RFO to work when Delta-E is very small for interfragment optimizations
    Former-commit-id: 0b8ddbc48814013893d867597ed94320c3bbdd51

--- src/bin/optking/opt_params.h
+++ src/bin/optking/opt_params.h
@@ -62,6 +62,7 @@ struct OPT_PARAMS {
 
   enum INTERFRAGMENT_MODE {FIXED, PRINCIPAL_AXES} interfragment_mode;
 
+  //std::vector< std::vector<int> > frag_atoms;
   bool intcos_generate_exit;
 
   bool rfo_follow_root; // whether to do root following
