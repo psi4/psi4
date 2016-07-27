3d176e173c3bf32ccf44a55e145ce30901914d94
Andy Simmonett <andy.simmonett@gmail.com>
2016-07-26 19:08:18 -0600

    The bundled Boost is not built in parallel, with debug flags, to speed up testing.
    Former-commit-id: 8bf556250ad932fd0c548e200e3c67ad48d85932

--- .scripts/build_boost.sh
+++ .scripts/build_boost.sh
@@ -6,5 +6,17 @@ then
   exit 1
 fi 
 
-
 echo "using ${1} : ${2} : ${3} : ;" > ${HOME}/user-config.jam
+
+mkdir boost_build
+
+cd boost_build
+tar -xjvf ../external/boost/boost-src/boost_1_57_0.tar.bz2 >& /dev/null
+cd boost_1_57_0
+./bootstrap.sh \
+      --with-libraries="filesystem,python,regex,serialization,system,timer,chrono,thread" \
+      --with-toolset=${1} \
+      --prefix=${TRAVIS_BUILD_DIR}/boost_install
+./b2 -j2 -q --toolset=${1} --variant=debug install >& /dev/null
+cd ..
+cd ..
