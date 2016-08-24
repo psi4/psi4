36960b8e38a4cee154586326d9ae60f3ccdb0b9b
Lori A. Burns <lori.burns@gmail.com>
2016-08-23 21:04:18 -0400

    fix OpenMP that was broken in the presence of Fortran
    Former-commit-id: 85de13bd043843318f30cbffed3f2a03e37c500f

--- cmake/autocmake_omp.cmake
+++ cmake/autocmake_omp.cmake
@@ -2,6 +2,7 @@
 #   https://github.com/coderefinery/autocmake/blob/master/modules/omp.cmake
 # * moved option up
 # * toggled option default to ON
+# * reorganized logic for Fortran + C/CXX, see https://github.com/coderefinery/autocmake/issues/177
 
 #.rst:
 #
@@ -24,25 +25,6 @@
 #   define: "'-DENABLE_OPENMP={0}'.format(arguments['--omp'])"
 
 if(ENABLE_OPENMP)
-    if(DEFINED CMAKE_Fortran_COMPILER_ID)
-        # we do this in a pedestrian way because the Fortran support is relatively recent
-        if(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
-            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fopenmp")
-        endif()
-        if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -openmp")
-        endif()
-        if(CMAKE_Fortran_COMPILER_ID MATCHES PGI)
-            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -mp")
-        endif()
-        if(CMAKE_Fortran_COMPILER_ID MATCHES XL)
-            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qsmp")
-        endif()
-        if(CMAKE_Fortran_COMPILER_ID MATCHES Cray)
-            # do nothing in this case
-        endif()
-        set(OPENMP_FOUND TRUE)
-    endif()
 
     if(NOT OPENMP_FOUND)
         find_package(OpenMP)
@@ -56,5 +38,35 @@ if(ENABLE_OPENMP)
         if(DEFINED CMAKE_CXX_COMPILER_ID)
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
         endif()
+        if(DEFINED CMAKE_Fortran_COMPILER_ID)
+            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_Fortran_FLAGS}")
+        endif()
+    endif()
+
+    if(DEFINED CMAKE_Fortran_COMPILER_ID AND NOT DEFINED OpenMP_Fortran_FLAGS)
+        # we do this in a pedestrian way because the Fortran support is relatively recent
+        if(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
+            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fopenmp")
+        endif()
+        if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
+            if(WIN32)
+                set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Qopenmp")
+            elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "Intel" AND
+                   "${CMAKE_Fortran_COMPILER_VERSION}" VERSION_LESS "15.0.0.20140528")
+                set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -openmp")
+            else()
+                set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp")
+            endif()
+        endif()
+        if(CMAKE_Fortran_COMPILER_ID MATCHES PGI)
+            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -mp")
+        endif()
+        if(CMAKE_Fortran_COMPILER_ID MATCHES XL)
+            set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qsmp")
+        endif()
+        if(CMAKE_Fortran_COMPILER_ID MATCHES Cray)
+            # do nothing in this case
+        endif()
+        set(OPENMP_FOUND TRUE)
     endif()
 endif()
