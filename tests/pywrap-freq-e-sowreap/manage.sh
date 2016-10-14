c3e92658945256f9f000d802011f9906f71efb7f
Lori A. Burns <lori.burns@gmail.com>
2016-10-14 14:17:56 -0400

    fix up sowreap test cases
    Former-commit-id: f3bcdd1703c6663412b6eaf96b108aa1fab26c61

--- tests/pywrap-freq-e-sowreap/manage.sh
+++ tests/pywrap-freq-e-sowreap/manage.sh
@@ -1,5 +1,6 @@
 # modify objdir here. assumes running from test dir
-OBJDIR="objdir-pn"
+OBJDIR="objdir-pn/stage/usr/local/psi4"
+DVRDIR="" #"-l ../../share/"
 
 # clean
 rm -f psi*clean
@@ -19,22 +20,22 @@ rm -f BASIC-*out
 set -x
 
 # run calculation sequence
-../../${OBJDIR}/bin/psi4 -l ../../share/
+../../${OBJDIR}/bin/psi4 ${DVRDIR}
 
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-1.in                   -o FREQ-1.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-2.in                   -o FREQ-2.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-3.in                   -o FREQ-3.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-4.in                   -o FREQ-4.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-5.in                   -o FREQ-5.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-6.in                   -o FREQ-6.out 
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-7.in                   -o FREQ-7.out 
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-8.in                   -o FREQ-8.out 
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-9.in                   -o FREQ-9.out 
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-10.in                  -o FREQ-10.out   
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-11.in                  -o FREQ-11.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-12.in                  -o FREQ-12.out  
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-13.in                  -o FREQ-13.out  
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-1.in                   -o FREQ-1.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-2.in                   -o FREQ-2.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-3.in                   -o FREQ-3.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-4.in                   -o FREQ-4.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-5.in                   -o FREQ-5.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-6.in                   -o FREQ-6.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-7.in                   -o FREQ-7.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-8.in                   -o FREQ-8.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-9.in                   -o FREQ-9.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-10.in                  -o FREQ-10.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-11.in                  -o FREQ-11.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-12.in                  -o FREQ-12.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-13.in                  -o FREQ-13.out
 
 cat tests >> FREQ-master.in
-../../${OBJDIR}/bin/psi4 -l ../../share/ -i FREQ-master.in              -o FREQ-master.out
+../../${OBJDIR}/bin/psi4 ${DVRDIR} -i FREQ-master.in              -o FREQ-master.out
 
