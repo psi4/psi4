c91a04c0a5c4013b3f1287053c3a9384b77d7820
Lori A. Burns <lori.burns@gmail.com>
2017-01-13 14:12:10 -0500

    turn off plugins testing by default. turn on for travis.
    Avoid returning non-const Dimension objects from the Matrix class.
    
    Vector now returns a const Dimension& object instead of an int*.
    
    Remove const_cast.

--- psi4/src/psi4/libmints/vector.cc
+++ psi4/src/psi4/libmints/vector.cc
@@ -58,13 +58,13 @@ Vector::Vector(const Vector &c)
     name_ = c.name_;
 }
 
-Vector::Vector(int nirreps, int *dimpi)
-        : dimpi_(nirreps)
-{
-    nirrep_ = nirreps;
-    dimpi_ = dimpi;
-    alloc();
-}
+//Vector::Vector(int nirreps, int *dimpi)
+//        : dimpi_(nirreps)
+//{
+//    nirrep_ = nirreps;
+//    dimpi_ = dimpi;
+//    alloc();
+//}
 
 Vector::Vector(int dim)
         : dimpi_(1)
@@ -74,16 +74,16 @@ Vector::Vector(int dim)
     alloc();
 }
 
-Vector::Vector(const std::string &name, int nirreps, int *dimpi)
-        : dimpi_(nirreps)
-{
-    nirrep_ = nirreps;
-    dimpi_ = new int[nirrep_];
-    for (int h = 0; h < nirrep_; ++h)
-        dimpi_[h] = dimpi[h];
-    alloc();
-    name_ = name;
-}
+//Vector::Vector(const std::string &name, int nirreps, int *dimpi)
+//        : dimpi_(nirreps)
+//{
+//    nirrep_ = nirreps;
+//    dimpi_ = new int[nirrep_];
+//    for (int h = 0; h < nirrep_; ++h)
+//        dimpi_[h] = dimpi[h];
+//    alloc();
+//    name_ = name;
+//}
 
 Vector::Vector(const std::string &name, int dim)
         : dimpi_(1)
