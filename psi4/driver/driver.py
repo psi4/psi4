117d1572e498019754b8979a1cf07c9144fd0947
Mike Zott <mzott3@gatech.edu>
2017-05-10 17:51:45 -0400

    Fix to block of code where beta density is calculated when alpha/beta density is desired separately. I believe there were just typos from copying the block that computes the alpha density above.

--- psi4/driver/driver.py
+++ psi4/driver/driver.py
@@ -1857,8 +1857,8 @@ def molden(wfn, filename=None, density_a=None, density_b=None, dovirtual=None):
 
         if density_b:
             NO_Rb = core.Matrix("NO Beta Rotation Matrix", nmopi, nmopi)
-            NO_occa = core.Vector(nmopi)
-            density_b.diagonalize(NO_Ra, NO_occa, core.DiagonalizeOrder.Descending)
+            NO_occb = core.Vector(nmopi)
+            density_b.diagonalize(NO_Rb, NO_occb, core.DiagonalizeOrder.Descending)
             NO_Cb = core.Matrix("Cb Natural Orbitals", nsopi, nmopi)
             NO_Cb.gemm(False, False, 1.0, wfn.Cb(), NO_Rb, 0)
 
