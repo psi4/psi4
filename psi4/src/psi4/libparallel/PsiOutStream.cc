afb909ab8b0bae2d5d305998e60cb0d85845affc
Daniel Smith <malorian@me.com>
2017-06-08 17:17:44 -0400

    Cleanup: Rewrite PsiOutStream to not use templates

--- psi4/src/psi4/libparallel/PsiOutStream.cc
+++ psi4/src/psi4/libparallel/PsiOutStream.cc
@@ -1,89 +1,49 @@
-/*
- * @BEGIN LICENSE
- *
- * Psi4: an open-source quantum chemistry software package
- *
- * Copyright (c) 2007-2017 The Psi4 Developers.
- *
- * The copyrights for code used from other parties are included in
- * the corresponding files.
- *
- * This file is part of Psi4.
- *
- * Psi4 is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * Psi4 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with Psi4; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * @END LICENSE
- */
-#include "PsiOutStream.h"
+
+
 #include "psi4/libpsi4util/exception.h"
+#include "PsiOutStream.h"
+#include <cstdio>
+#include <fstream>
 
-#include <stdarg.h>
-#include <stdio.h>
-namespace psi{
-void Destructor(std::ostream* Stream_){}
+namespace psi {
 
-void PsiOutStream::Buffer2Stream() {
-    if (this->ImSpecial()) {
-        (*Stream_) << Buffer_.str();
-        this->Flush();
+PsiOutStream::PsiOutStream(std::string fname, std::ios_base::openmode mode) {
+    if (fname == "") {
+        stream_ = &std::cout;
+        is_cout_ = true;
+    } else {
+        std::ofstream* tmpf = new std::ofstream(fname, mode);
+        if (!tmpf->is_open()) {
+            throw PSIEXCEPTION("PsiOutStream: Failed to open file.");
+        }
+
+        stream_ = tmpf;
+        is_cout_ = false;
     }
-    this->EmptyBuffer();
+
+    buffer_.resize(512);
 }
 
-void PsiOutStream::MakeBanner(const std::string& message, const char delimiter, const int width) {
-    std::string symbols(width, delimiter);
-    (*this) << symbols << std::endl;
-    int size = message.size();
-    int nspaces = 2;  // Number of spaces between message, on each side
-    if (size < width - 2 * (nspaces + 1)) {
-        // Divide message in half, extra char goes left
-        int lsize = (size - (size % 2)) / 2 + (size % 2);
-        int rsize = size - lsize;
-        // Number of times to print character
-        int lchars = width / 2 - nspaces - lsize;
-        int rchars = width / 2 - nspaces - rsize;
-        std::string lsym(lchars, delimiter);
-        std::string rsym(rchars, delimiter);
-        std::string spaces(nspaces, ' ');
-        (*this) << lsym << spaces << message << spaces << rsym << std::endl;
-    }
-    (*this) << symbols << std::endl;
-}
-
-PsiOutStream::PsiOutStream(SharedOutStream Stream) {
-    if (this->ImSpecial()) {
-        Stream_ = (Stream ? Stream : SharedOutStream(&std::cout, Destructor));
+PsiOutStream::~PsiOutStream() {
+    if (!is_cout_) {
+        delete stream_;
     }
 }
 
 void PsiOutStream::Printf(const char* format, ...) {
     // We don't know how long the fully expanded string is so lets guess our average print is about
     // a line
-    const int guess_limit = 512;
-    char* buffer = new char[guess_limit];
     va_list args;
     va_start(args, format);
-    int left = vsnprintf(buffer, guess_limit, format, args);
+    int left = vsnprintf(buffer_.data(), buffer_.size(), format, args);
 
     if (left < 0) {
         // Encoding error?!?
         throw PSIEXCEPTION("PsiOutStream: vsnprintf encoding error!");
-    } else if (left >= guess_limit) {
+    } else if (left >= buffer_.size()) {
         // Buffer was too small! Try again
-        delete[] buffer;
-        buffer = new char[left + 1];
-        left = vsnprintf(buffer, guess_limit, format, args);
+        std::vector<char> tmp_buffer(left + 1);
+        left = vsnprintf(tmp_buffer.data(), left + 1, format, args);
         if (left < 0) {
             throw PSIEXCEPTION("PsiOutStream: vsnprintf encoding error!");
         }
@@ -91,13 +51,12 @@ void PsiOutStream::Printf(const char* format, ...) {
     // Everything is cool
 
     va_end(args);
-    Write2Buffer(buffer);
-    delete[] buffer;
+    (*stream_) << buffer_.data();
 }
 
-std::ostream& PsiOutStream::Write2Buffer(StreamManips fp) {
-    Buffer_ << fp;
-    this->Buffer2Stream();
-    return Buffer_;
+PsiOutStream& PsiOutStream::operator<<(std::string fp) {
+    (*stream_) << fp;
+    return (*this);
 }
-}
+
+} // End Psi Namespace
--- psi4/src/psi4/libparallel/PsiOutStream.h
+++ psi4/src/psi4/libparallel/PsiOutStream.h
@@ -1,172 +1,29 @@
-/*
- * @BEGIN LICENSE
- *
- * Psi4: an open-source quantum chemistry software package
- *
- * Copyright (c) 2007-2017 The Psi4 Developers.
- *
- * The copyrights for code used from other parties are included in
- * the corresponding files.
- *
- * This file is part of Psi4.
- *
- * Psi4 is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * Psi4 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with Psi4; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * @END LICENSE
- */
-#ifndef PSIOUTSTREAM_H_
-#define PSIOUTSTREAM_H_
 
-#include "StreamBase.h"
 
-namespace psi{
+#ifndef _psi_src_lib_libparallel_psioutstream_h_
+#define _psi_src_lib_libparallel_psioutstream_h_
 
-///Specialization of PsiStream to output streams
-class PsiOutStream:public PsiStreamBase<std::ostream>{
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace psi {
+
+class PsiOutStream {
    private:
-      typedef PsiStreamBase<std::ostream> BaseType;
+        std::ostream *stream_;
+        bool is_cout_;
+        std::vector<char> buffer_;
 
-      ///Dumps Buffer to Stream_
-      void Buffer2Stream();
+    public:
+        PsiOutStream(std::string fname = "", std::ios_base::openmode mode = std::ostream::trunc);
+        ~PsiOutStream();
 
-      /** \brief This is the interface for Printf and << operator to
-       *           Buffer_ for most writable objects
-       *
-       *  \param[in] Input An object that can be passed to an ostream
-       */
-      template<typename T>
-      std::ostream& Write2Buffer(const T& Input);
+        void Printf(const char * fmt, ...);
+        PsiOutStream& operator<<(std::string fp);
+        void MakeBanner(std::string header);
 
-      /** \brief This is the interface for Printf and << operator to
-       *     Buffer_ for special iostream functions.
-       *
-       *     \param[in] fp iostream functions like std::endl
-       */
-      std::ostream& Write2Buffer(StreamManips fp);
-
-   public:
-
-      ///Makes an OutStreamBase that defaults to std::cout
-      PsiOutStream(SharedOutStream Stream=SharedOutStream());
-
-      ///Creates this by copying other, via base copy constructor
-      PsiOutStream(const PsiOutStream& other):BaseType(other){}
-
-      ///Allows assignment of other to this, calls base assignment
-      const PsiOutStream& operator=(const PsiOutStream& other){
-         BaseType::operator=(other);
-         return *this;
-      }
-
-      ///No memory to free up (we don't delete cout or cerr)
-      virtual ~PsiOutStream(){}
-
-      ///Flushes the Stream
-      void Flush(){
-         if(this->ImSpecial()){
-            Stream_->flush();
-         }
-      }
-
-      /** \brief Allows printf (or fprintf) like syntax to this object
-       *
-       *   This function takes your format stream and converts it to a 1,000
-       *   character MAXIMUM char* array.  That array is then passed to Buffer_.
-       *   If the current MPI process is rank 0 on MPI_COMM_WORLD, Buffer_ is
-       *   then written to Stream_.  No locks should be used around your Printf
-       *   statements, nor is flushing needed.  For reference the accepted
-       *   format flags are (stolen from cplusplus.com):
-       *
-       *   %[flags][width][.precision][length]specifier
-       *
-       *   specifier    Output
-       *     d or i  Signed decimal integer
-       *      u      Unsigned decimal integer
-       *      o   Unsigned octal integer
-       *      x   Unsigned hexadecimal integer
-       *      X   Unsigned hexadecimal integer (uppercase)
-       *      f   Decimal floating point, lowercase
-       *      F   Decimal floating point, uppercase
-       *      e   Scientific notation (mantissa/exponent), lowercase
-       *      E   Scientific notation (mantissa/exponent), uppercase
-       *      g   Use the shortest representation: %e or %f
-       *      G   Use the shortest representation: %E or %F
-       *      a   Hexadecimal floating point, lowercase
-       *      A   Hexadecimal floating point, uppercase
-       *      c   Character
-       *      s   String of characters
-       *      p   Pointer address
-       *      n   Nothing printed
-       *
-       *
-       *   \param[in] format The message plus format codes you want to pass
-       *   \param[in] ...    The arguments for each format code given in "format"
-       *
-       */
-      void Printf(const char* format,...);
-
-      ///Allows c++ like interface for most objects
-      template<typename T>
-      std::ostream& operator<<(const T& Input){return Write2Buffer(Input);}
-
-      ///Allows c++ like interface for things like std::endl
-      std::ostream& operator<<(StreamManips fp){return Write2Buffer(fp);}
-
-      /** \brief Makes a banner
-       *
-       *   \param[in] message   The message that will go in the banner
-       *   \param[in] delimiter The character that will comprise the banner
-       *   \param[in] width     The number of characters wide the banner will be
-       *
-       *
-       *   Let's say you want to print "Hello World!" in a banner comprised of
-       *   '*' characters, and for simplicity (your banner should be 80 chars
-       *   long) you only wanted a 16 character long banner.  If you passed
-       *   message="Hello World!",delimiter='*', and width=16, you would get
-       *   (hopefully you are viewing this in a monospaced font):
-       *
-       *   0123456789ABCDEF
-       *
-       *   ****************
-       *
-       *   * Hello World! *
-       *
-       *   ****************
-       *
-       *   where if you didn't guess the columns are labeled in hexadecimal, so
-       *   that they are all single characters.  For the time being your message
-       *   must be width-6 characters or shorter because I have mandated that at
-       *   least one delimiter is printed on each side plus two spaces between that
-       *   delimiter and the actual message. The remaining space will be filled by
-       *   delimiters. Consequentially, this means your message must fit on one
-       *   line.  This could of course be remedied by splitting the message
-       *   across multiple lines, but I'm lazy.
-       */
-      void MakeBanner(const std::string& message, const char delimiter='*', const
-            int width=80);
 };
 
-
-template<typename T>
-std::ostream& PsiOutStream::Write2Buffer(const T& Input){
-   Buffer_<<Input;
-   this->Buffer2Stream();
-   return Buffer_;
-}
-
-}//End namespace
-
-
-
-#endif /* PSIOUTSTREAM_H_ */
+} // End Psi namespace
+#endif
