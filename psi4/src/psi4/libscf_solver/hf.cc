e9f7cb4336f4141999d6780bb46d00296a9f7ce1
Lori A. Burns <lori.burns@gmail.com>
2017-11-06 12:34:24 -0500

    SCF: fix dftd3/gcp. stdize some units.

--- psi4/src/psi4/libscf_solver/hf.cc
+++ psi4/src/psi4/libscf_solver/hf.cc
@@ -1189,7 +1189,7 @@ void HF::print_orbitals()
 {
     std::vector<std::string> labels = molecule_->irrep_labels();
 
-    outfile->Printf( "    Orbital Energies (a.u.)\n    -----------------------\n\n");
+    outfile->Printf( "    Orbital Energies [Eh]\n    ---------------------\n\n");
 
     std::string reference = options_.get_str("REFERENCE");
     if((reference == "RHF") || (reference == "RKS")){
