315dd73da31b7e7d4b824253af16e49824f5367c
Daniel Smith <malorian@me.com>
2017-12-06 18:17:47 -0500

    DFT: std::ptrdiff_t too annoying to use

--- psi4/src/psi4/libfock/points.cc
+++ psi4/src/psi4/libfock/points.cc
@@ -692,7 +692,7 @@ void BasisFunctions::compute_functions(std::shared_ptr<BlockOPoints> block) {
         center[2] = v[2];
 
         // Make new pointers, gg computes along rows so we need to skip down `nval` rows.
-        ptrdiff_t row_shift = nvals * npoints;
+        size_t row_shift = nvals * npoints;
         double* phi_start = tmpp + row_shift;
 
         // Copmute collocation
