7de9c578ceb652c1b976a8d2a2c0339e8fffbe7a
Lori A. Burns <lori.burns@gmail.com>
2018-04-16 00:24:02 -0400

    CMake: Set 2017 Intel compiler minimum, since 2016 can't cope with pb11 >=2.1

--- psi4/driver/qcdb/util/misc.py
+++ psi4/driver/qcdb/util/misc.py
@@ -46,8 +46,7 @@ def distance_matrix(a, b):
     assert a.shape[1] == b.shape[1], """Inner dimensions do not match"""
     distm = np.zeros([a.shape[0], b.shape[0]])
     for i in range(a.shape[0]):
-        for j in range(b.shape[0]):
-            distm[i, j] = np.linalg.norm(a[i] - b[j])
+        distm[i] = np.linalg.norm(a[i] - b, axis=1)
     return distm
 
 
