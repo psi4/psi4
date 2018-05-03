9a4575950e74282255c7be53bb72067a94b4d504
bgpeyton <bgpeyton@vt.edu>
2018-05-02 22:31:51 -0400

    Add test case

---
+++ tests/python/ghost_types/input.py
@@ -0,0 +1,33 @@
+#! Testing mass() and Z() for both ghost types: A (Gh(x) or @(x)) and B (extract_subsets())
+import psi4
+
+m_He = 4.00260325415 #TEST
+m_Gh = 0.0           #TEST
+Z_He = 2.0           #TEST
+Z_Gh = 0.0           #TEST
+
+frag = psi4.geometry("""
+    He
+    --
+    He 1 1
+""")
+
+A_frag = psi4.geometry(""" 
+    He
+    --
+    Gh(He) 1 1
+""")
+
+B_frag = A_frag.extract_subsets(1,2)
+
+# mass defaults to zero_ghost = false
+assert psi4.compare_values(m_He, A_frag.mass(1), 9, 'A_frag mass default')
+assert psi4.compare_values(m_Gh, A_frag.mass(1, True), 9, 'A_frag mass True')
+assert psi4.compare_values(m_He, B_frag.mass(1), 9, 'B_frag mass default')
+assert psi4.compare_values(m_Gh, B_frag.mass(1, True), 9, 'B_frag mass True')
+
+# z defaults to zero_ghost = true
+assert psi4.compare_values(Z_Gh, A_frag.Z(1), 9, 'A_frag Z default')
+assert psi4.compare_values(Z_He, A_frag.Z(1, False), 9, 'A_frag Z False')
+assert psi4.compare_values(Z_Gh, B_frag.Z(1), 9, 'B_frag Z default')
+assert psi4.compare_values(Z_He, B_frag.Z(1, False), 9, 'B_frag Z False')
