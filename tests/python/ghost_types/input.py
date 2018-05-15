7e29f642527ecedf5c55ff538756a82767f2cd74
bgpeyton <bgpeyton@vt.edu>
2018-05-15 18:35:13 -0400

    Change test mol to have two atoms

--- tests/python/ghost_types/input.py
+++ tests/python/ghost_types/input.py
@@ -1,28 +1,30 @@
 #! Testing mass() and Z() for both ghost types: A (Gh(x) or @(x)) and B (extract_subsets())
 import psi4
 
+m_H = 1.00782503207      #TEST
 m_He = 4.00260325415     #TEST
 m_Gh = 0.0               #TEST
+Z_H = 1.0                #TEST
 Z_He = 2.0               #TEST
 Z_Gh = 0.0               #TEST
-NRE_real = 2.11670883436 #TEST
+NRE_real = 1.05835441718 #TEST
 NRE_Gh = 0.0             #TEST
 
 frag = psi4.geometry("""
-    He
+    H
     --
     He 1 1
 """)
 
 A_frag = psi4.geometry(""" 
-    He
+    H
     --
     Gh(He) 1 1
 """)
 
 B_frag = frag.extract_subsets(1,2)
 
-mix_frag = A_frag.extract_subsets(2,1) # two ghost atoms: 1 is type B, 2 is type A
+mix_frag = A_frag.extract_subsets(2,1) # two ghost atoms: H is type B, He is type A
 
 def mol_mass(mol,opt): # for testing total mass
     mass = 0
@@ -31,7 +33,7 @@ def mol_mass(mol,opt): # for testing total mass
     return mass
 
 # mass defaults to zero_ghost = false
-# check mass of second He for frag, A_frag, B_frag
+# check mass of He for frag, A_frag, and B_frag
 assert psi4.compare_values(m_He, A_frag.mass(1), 9, 'A_frag mass default')
 assert psi4.compare_values(m_He, A_frag.mass(1, False), 9, 'A_frag mass False')
 assert psi4.compare_values(m_Gh, A_frag.mass(1, True), 9, 'A_frag mass True')
@@ -39,8 +41,14 @@ assert psi4.compare_values(m_He, B_frag.mass(1), 9, 'B_frag mass default')
 assert psi4.compare_values(m_He, B_frag.mass(1, False), 9, 'B_frag mass False')
 assert psi4.compare_values(m_Gh, B_frag.mass(1, True), 9, 'B_frag mass True')
 
+# check masses for mix_frag
+assert psi4.compare_values(m_Gh, mix_frag.mass(0, True), 9, 'mix_frag H mass True')
+assert psi4.compare_values(m_H, mix_frag.mass(0, False), 9, 'mix_frag H mass False')
+assert psi4.compare_values(m_Gh, mix_frag.mass(1, True), 9, 'mix_frag He mass True')
+assert psi4.compare_values(m_He, mix_frag.mass(1, False), 9, 'mix_frag He mass False')
+
 # z defaults to zero_ghost = true
-# check Z of second He for frag, A_frag, B_frag
+# check Z of He for frag, A_frag, and B_frag
 assert psi4.compare_values(Z_Gh, A_frag.Z(1), 9, 'A_frag Z default')
 assert psi4.compare_values(Z_Gh, A_frag.Z(1, True), 9, 'A_frag Z True')
 assert psi4.compare_values(Z_He, A_frag.Z(1, False), 9, 'A_frag Z False')
@@ -48,16 +56,22 @@ assert psi4.compare_values(Z_Gh, B_frag.Z(1), 9, 'B_frag Z default')
 assert psi4.compare_values(Z_Gh, B_frag.Z(1, True), 9, 'B_frag Z True')
 assert psi4.compare_values(Z_He, B_frag.Z(1, False), 9, 'B_frag Z False')
 
+# check Z for mix_frag
+assert psi4.compare_values(Z_Gh, mix_frag.Z(0, True), 9, 'mix_frag H Z True')
+assert psi4.compare_values(Z_H, mix_frag.Z(0, False), 9, 'mix_frag H Z False')
+assert psi4.compare_values(Z_Gh, mix_frag.Z(1, True), 9, 'mix_frag He Z True')
+assert psi4.compare_values(Z_He, mix_frag.Z(1, False), 9, 'mix_frag He Z False')
+
 # check molecular mass of all frag types + mix_frag
-# both He return non-zero for False
-assert psi4.compare_values(2*m_He, mol_mass(frag, False), 9, 'frag mol_mass False')
-assert psi4.compare_values(2*m_He, mol_mass(A_frag, False), 9, 'A_frag mol_mass False') 
-assert psi4.compare_values(2*m_He, mol_mass(B_frag, False), 9, 'B_frag mol_mass False')
-assert psi4.compare_values(2*m_He, mol_mass(mix_frag, False), 9, 'mix_frag mol_mass False')
-# ghost He return zero for True
-assert psi4.compare_values(2*m_He, mol_mass(frag, True), 9, 'frag mol_mass True')
-assert psi4.compare_values(m_He, mol_mass(A_frag, True), 9, 'A_frag mol_mass True')
-assert psi4.compare_values(m_He, mol_mass(B_frag, True), 9, 'B_frag mol_mass True')
+# both atoms return non-zero for False
+assert psi4.compare_values((m_H + m_He), mol_mass(frag, False), 9, 'frag mol_mass False')
+assert psi4.compare_values((m_H + m_He), mol_mass(A_frag, False), 9, 'A_frag mol_mass False') 
+assert psi4.compare_values((m_H + m_He), mol_mass(B_frag, False), 9, 'B_frag mol_mass False')
+assert psi4.compare_values((m_H + m_He), mol_mass(mix_frag, False), 9, 'mix_frag mol_mass False')
+# ghost returns zero mass for True
+assert psi4.compare_values((m_H + m_He), mol_mass(frag, True), 9, 'frag mol_mass True')
+assert psi4.compare_values(m_H, mol_mass(A_frag, True), 9, 'A_frag mol_mass True')
+assert psi4.compare_values(m_H, mol_mass(B_frag, True), 9, 'B_frag mol_mass True')
 assert psi4.compare_values(m_Gh, mol_mass(mix_frag, True), 9, 'mix_frag mol_mass True')
 
 # check nuclear_repulsion_energy of all frag types
