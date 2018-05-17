4cbc93c8ab29bfb08b9a072a72ffa6f8efd24188
Daniel Gibney <dpgibney@gmail.com>
2018-05-17 18:06:28 -0400

    corrected pytest, added a read option and ENABLE_gpu_dfcc

--- tests/pytest/test_addons.py
+++ tests/pytest/test_addons.py
@@ -755,34 +755,31 @@ def test_gpu_dfcc():
     """gpu_dfcc/tests/gpu_dfcc1"""
     #! cc-pvdz (H2O)2 Test DF-CCSD vs GPU-DF-CCSD
     
-    sys.path.insert(0,'../../../.')
-    
-    import gpu_dfcc
-    
-    molecule {
+    import gpu_dfcc 
+ 
+    H20 = psi4.geometry("""
                O          0.000000000000     0.000000000000    -0.068516219310   
                H          0.000000000000    -0.790689573744     0.543701060724   
                H          0.000000000000     0.790689573744     0.543701060724   
-    }
+    """)
     
-    memory 32000 mb
-    set {
-      cc_timings false
-      num_gpus 1
-      cc_type df
-      df_basis_cc  aug-cc-pvdz-ri
-      df_basis_scf aug-cc-pvdz-jkfit
-      basis        aug-cc-pvdz
-      freeze_core true
-      e_convergence 1e-8
-      d_convergence 1e-8
-      r_convergence 1e-8
-      scf_type df
-      maxiter 30
-    }
-    set_num_threads(2) 
-    en_dfcc     = energy('ccsd')
-    en_gpu_dfcc = energy('gpu-df-ccsd')
+    psi4.set_memory(32000000000)
+    psi4.set_options({
+      'cc_timings': False,
+      'num_gpus': 1,
+      'cc_type': 'df',
+      'df_basis_cc':  'aug-cc-pvdz-ri',
+      'df_basis_scf': 'aug-cc-pvdz-jkfit',
+      'basis':        'aug-cc-pvdz',
+      'freeze_core': 'true',
+      'e_convergence': 1e-8,
+      'd_convergence': 1e-8,
+      'r_convergence': 1e-8,
+      'scf_type': 'df',
+      'maxiter': 30})
+    psi4.set_num_threads(2)
+    en_dfcc     = psi4.energy('ccsd', molecule=H20)
+    en_gpu_dfcc = psi4.energy('gpu-df-ccsd', molecule=H20)
     
     assert psi4.compare_values(en_gpu_dfcc, en_dfcc, 8, "CCSD total energy")
     
