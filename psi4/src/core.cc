8d76e377c8caacefb2e48b4ec48c440d6fbf7cde
Lori A. Burns <lori.burns@gmail.com>
2018-06-06 16:46:15 -0400

    Update core.cc

--- psi4/src/core.cc
+++ psi4/src/core.cc
@@ -547,13 +547,11 @@ void py_psi_clean_options() {
     Process::environment.options.clear();
     Process::environment.options.set_read_globals(true);
     read_options("", Process::environment.options, true);
-    Process::environment.options.set_read_globals(false);
     for (std::map<std::string, plugin_info>::iterator it=plugins.begin(); it!=plugins.end(); ++it) {
         // Get the plugin options back into the global space
-        Process::environment.options.set_read_globals(true);
         it->second.read_options(it->second.name, Process::environment.options);
-        Process::environment.options.set_read_globals(false);
     }
+    Process::environment.options.set_read_globals(false);
 }
 
 void py_psi_print_out(std::string s) { (*outfile->stream()) << s; }
