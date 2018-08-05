545a0eb14def657309b9691a6cdd4e07fa700e2e
Lori A. Burns <lori.burns@gmail.com>
2018-08-05 04:17:08 -0400

    Cfour: backport from GH:qcdb/qcdb cfour parsing, new Molecule work, fix v1 parsing, DROPMO

--- psi4/driver/qcdb/cfour.py
+++ psi4/driver/qcdb/cfour.py
@@ -181,6 +181,19 @@ def harvest_outfile_pass(outtext):
             Decimal(mobj.group(2)) + Decimal(mobj.group(3)) + Decimal(mobj.group(4))
         psivar['MP2 TOTAL ENERGY'] = mobj.group(6)
 
+    mobj = re.search(
+        r'^\s+' + r'(?:S-MBPT\(2\))' + r'\s+' + r'(?P<sgl>' + NUMBER + r')' + r'\s+' + NUMBER + r'\s*' +
+        r'^\s+' + r'(?:D-MBPT\(2\))' + r'\s+' + r'(?P<dbl>' + NUMBER + r')' + r'\s+' +
+                                                r'(?P<mp2tot>' + NUMBER + r')' + r'\s*$',
+        outtext, re.MULTILINE)
+    if mobj:
+        print('matched mp2ro2')
+        #psivar['MP2 SAME-SPIN CORRELATION ENERGY'] = Decimal(mobj.group(1)) + Decimal(mobj.group(2))
+        #psivar['MP2 OPPOSITE-SPIN CORRELATION ENERGY'] = mobj.group(3)
+        psivar['MP2 SINGLES ENERGY'] = mobj.group('sgl')
+        psivar['MP2 CORRELATION ENERGY'] = Decimal(mobj.group('sgl')) + Decimal(mobj.group('dbl'))
+        psivar['MP2 TOTAL ENERGY'] = mobj.group('mp2tot')
+
     # Process MP3
     mobj = re.search(
         r'^\s+' + r'(?:D-MBPT\(2\))' + r'\s+' + NUMBER + r'\s+' + NUMBER + r'\s*' +
@@ -305,6 +318,19 @@ def harvest_outfile_pass(outtext):
         psivar['%s CORRELATION ENERGY' % (mobj.group('iterCC'))] = mobj.group(3)
         psivar['%s TOTAL ENERGY' % (mobj.group('iterCC'))] = mobj.group(4)
 
+    mobj = re.search(
+        r'^\s+' + r'(?:\d+)' + r'\s+' + r'(?P<corl>' + NUMBER + r')\s+' +
+                  NUMBER + r'\s+' + NUMBER + r'\s+' + NUMBER + r'\s+' + NUMBER + r'\s*' +
+        r'^\s*' +
+        r'^\s*' + r'(?:\w+ iterations converged .*?)' +
+        r'^\s*' +
+        r'^\s*' + r'(?:Total (?P<iterCC>\w+) energy:)' + r'\s+' + r'(?P<tot>' + NUMBER + ')\s*$',
+        outtext, re.MULTILINE | re.DOTALL)
+    if mobj:
+        print('matched ncc cc iter')
+        psivar['{} CORRELATION ENERGY'.format(mobj.group('iterCC'))] = mobj.group('corl')
+        psivar['{} TOTAL ENERGY'.format(mobj.group('iterCC'))] = mobj.group('tot')
+
     # Process CC(T)
     mobj = re.search(
         r'^\s+' + r'(?:E\(SCF\))' + r'\s+=\s+' + NUMBER + r'\s+a\.u\.\s*' +
@@ -320,6 +346,17 @@ def harvest_outfile_pass(outtext):
         psivar['(T) CORRECTION ENERGY'] = Decimal(mobj.group(3)) - Decimal(mobj.group(2))
         psivar['CCSD(T) CORRELATION ENERGY'] = Decimal(mobj.group(3)) - Decimal(mobj.group(1))
         psivar['CCSD(T) TOTAL ENERGY'] = mobj.group(3)
+
+    mobj = re.search(
+        r'^\s+' + r'(?:E\(CCSD\))' + r'\s+=\s+' + NUMBER + r'\s*' +
+        r'(?:.*?)' +
+        r'^\s+' + r'(?:E\(CCSD\(T\)\))' + r'\s+=\s+' + NUMBER + r'\s*$',
+        outtext, re.MULTILINE | re.DOTALL)
+    if mobj:
+        print('matched ccsd(t) vcc v2')
+        psivar['CCSD TOTAL ENERGY'] = mobj.group(1)
+        psivar['(T) CORRECTION ENERGY'] = Decimal(mobj.group(2)) - Decimal(mobj.group(1))
+        psivar['CCSD(T) TOTAL ENERGY'] = mobj.group(2)
 
     mobj = re.search(
         r'^\s+' + r'(?:E\(SCF\))' + r'\s+=\s*' + NUMBER + r'\s+a\.u\.\s*' +
@@ -368,6 +405,17 @@ def harvest_outfile_pass(outtext):
         psivar['CCSD(T) CORRELATION ENERGY'] = Decimal(mobj.group(2)) - psivar['SCF TOTAL ENERGY']
         psivar['CCSD(T) TOTAL ENERGY'] = mobj.group(2)
 
+    mobj = re.search(
+        r'^\s+' + r'(?:CCSD\(T\) contribution:)\s+' + r'(?P<tcorr>' + NUMBER + ')' + r'\s*'
+        r'^\s*' + r'(?:CCSD\[T\] contribution:)\s+' + r'(?P<bkttcorr>' + NUMBER + ')' + r'\s*'
+        r'^\s*' + r'(?:Total CCSD\(T\) energy:)\s+' + r'(?P<ttot>' + NUMBER + ')' + r'\s*$',
+        outtext, re.MULTILINE | re.DOTALL)
+    if mobj:
+        print('matched ccsd(t) ncc')
+        psivar['(T) CORRECTION ENERGY'] = mobj.group('tcorr')
+        psivar['[T] CORRECTION ENERGY'] = mobj.group('bkttcorr')
+        psivar['CCSD(T) TOTAL ENERGY'] = mobj.group('ttot')
+
     # Process SCS-CC
     mobj = re.search(
         r'^\s+' + r'(?P<fullCC>(?P<iterCC>CC(?:\w+))(?:\(T\))?)' + r'\s+(?:energy will be calculated.)\s*' +
@@ -386,7 +434,7 @@ def harvest_outfile_pass(outtext):
     mobj = re.search(
         r'^\s+' + r'(?P<fullCC>(?P<iterCC>CC(?:\w+))(?:\(T\))?)' + r'\s+(?:energy will be calculated.)\s*' +
         r'(?:.*?)' +
-        r'^\s+' + r'Amplitude equations converged in' + r'\s*\d+\s*' + r'iterations.\s*' +
+        r'^\s+' + r'Amplitude equations converged in' + r'\s*\d+\s*' + r'iterations.' +
         r'(?:.*?)' +
         r'^\s+' + r'The AA contribution to the correlation energy is:\s+' + NUMBER + r'\s+a.u.\s*' +
         r'^\s+' + r'The BB contribution to the correlation energy is:\s+' + NUMBER + r'\s+a.u.\s*' +
@@ -439,7 +487,7 @@ def harvest_outfile_pass(outtext):
             lline = line.split()
             molxyz += '%s %16s %16s %16s\n' % (lline[0], lline[-3], lline[-2], lline[-1])
         # Rather a dinky Molecule as no ghost, charge, or multiplicity
-        psivar_coord = Molecule.init_with_xyz(molxyz, no_com=True, no_reorient=True, contentsNotFilename=True)
+        psivar_coord = Molecule.from_string(molxyz, dtype='xyz+', fix_com=True, fix_orientation=True)
 
     # Process atom geometry
     mobj = re.search(
@@ -450,7 +498,7 @@ def harvest_outfile_pass(outtext):
         print('matched atom')
         # Dinky Molecule
         molxyz = '1 bohr\n\n%s 0.0 0.0 0.0\n' % (mobj.group(1))
-        psivar_coord = Molecule.init_with_xyz(molxyz, no_com=True, no_reorient=True, contentsNotFilename=True)
+        psivar_coord = Molecule.from_string(molxyz, dtype='xyz+', fix_com=True, fix_orientation=True)
 
     # Process error codes
     mobj = re.search(
@@ -629,7 +677,7 @@ def harvest_GRD(grd):
         molxyz += '%s %16s %16s %16s\n' % (el, mline[-3], mline[-2], mline[-1])
         lline = grd[at + 1 + Nat].split()
         grad.append([float(lline[-3]), float(lline[-2]), float(lline[-1])])
-    mol = Molecule.init_with_xyz(molxyz, no_com=True, no_reorient=True, contentsNotFilename=True)
+    mol = Molecule.from_string(molxyz, dtype='xyz+', fix_com=True, fix_orientation=True)
 
     return mol, grad
 
@@ -877,7 +925,7 @@ def muster_modelchem(name, dertype):
     elif lowername == 'c4-ccsdt(q)':
         options['CFOUR']['CFOUR_CALC_LEVEL']['value'] = 'CCSDT(Q)'
         options['CFOUR']['CFOUR_CC_PROGRAM']['value'] = 'NCC'
- 
+
     elif lowername == 'c4-ccsdtq':
         options['CFOUR']['CFOUR_CALC_LEVEL']['value'] = 'CCSDTQ'
         options['CFOUR']['CFOUR_CC_PROGRAM']['value'] = 'NCC'
