6fbe88ec555d2d85a219281da19c4b6f790e7f7e
Andy Simmonett <andy.simmonett@gmail.com>
2018-08-24 14:35:32 -0400

    fmt: clang-format the DETCI module

--- psi4/src/psi4/detci/s2v.cc
+++ psi4/src/psi4/detci/s2v.cc
@@ -30,7 +30,7 @@
     \ingroup DETCI
     \brief Code to compute the sigma2 contribution of sigma
 
-    \sigma_2(Ia, Ib) = 
+    \sigma_2(Ia, Ib) =
       \sum_{Ja} \sum_{kl} <Ja|E^a_{kl}|Ia>
                           \times [ h_{kl} - 0.5*\sum_j (kj|jl) ] C(Ja,Ib)
       + 0.5 * \sum_{Ja} \sum_{ijkl} <Ja|E^a_{ij} E^a_{kl}|Ia>
@@ -47,9 +47,8 @@
 namespace psi {
 namespace detci {
 
-extern void b2brepl(unsigned char **occs, int *Jcnt, int **Jij, int **Joij,
-                    int **Jridx, signed char **Jsgn, struct olsen_graph *Graph,
-                    int Ilist, int Jlist, int len, struct calcinfo *Cinfo);
+extern void b2brepl(unsigned char **occs, int *Jcnt, int **Jij, int **Joij, int **Jridx, signed char **Jsgn,
+                    struct olsen_graph *Graph, int Ilist, int Jlist, int len, struct calcinfo *Cinfo);
 
 #define INDEX(i, j) ((i > j) ? (ioff[(i)] + (j)) : (ioff[(j)] + (i)))
 
@@ -66,87 +65,85 @@ extern void b2brepl(unsigned char **occs, int *Jcnt, int **Jij, int **Joij,
 ** David Sherrill, 18 April 1996
 ** Based on many previous versions by David Sherrill 1994-5
 */
-void s2_block_vfci(struct stringwr **alplist, struct stringwr **betlist,
-                   double **C, double **S, double *oei, double *tei, double *F,
-                   int nlists, int nas, int nbs, int Ia_list, int Ja_list,
-                   int Ja_list_nas) {
-  struct stringwr *Ia, *Ka;
-  size_t Ia_idx, Ib_idx, Ka_idx, Ja_idx;
-  size_t Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
-  size_t *Iaridx, *Karidx;
-  int *Iaij, *Kaij;
-  signed char *Iasgn, *Kasgn;
-  int ij, kl, ijkl;
-  double Ka_sgn, Ja_sgn;
-  double tval;
-  double *Sptr, *Cptr;
+void s2_block_vfci(struct stringwr **alplist, struct stringwr **betlist, double **C, double **S, double *oei,
+                   double *tei, double *F, int nlists, int nas, int nbs, int Ia_list, int Ja_list, int Ja_list_nas) {
+    struct stringwr *Ia, *Ka;
+    size_t Ia_idx, Ib_idx, Ka_idx, Ja_idx;
+    size_t Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
+    size_t *Iaridx, *Karidx;
+    int *Iaij, *Kaij;
+    signed char *Iasgn, *Kasgn;
+    int ij, kl, ijkl;
+    double Ka_sgn, Ja_sgn;
+    double tval;
+    double *Sptr, *Cptr;
 
-  /* loop over all alpha strings Ia that belong to list Ia_list (irrep, block
-   * of alpha strings) */
-  for (Ia = alplist[Ia_list], Ia_idx = 0; Ia_idx < nas; Ia_idx++, Ia++) {
-    Sptr = S[Ia_idx];
-    zero_arr(F, Ja_list_nas);
+    /* loop over all alpha strings Ia that belong to list Ia_list (irrep, block
+     * of alpha strings) */
+    for (Ia = alplist[Ia_list], Ia_idx = 0; Ia_idx < nas; Ia_idx++, Ia++) {
+        Sptr = S[Ia_idx];
+        zero_arr(F, Ja_list_nas);
 
-    /* loop over excitations E^a_{kl} from |A(I_a)> */
+        /* loop over excitations E^a_{kl} from |A(I_a)> */
 
-    /* first loop over the K_a lists to block the excited strings by
-     * irrep or RAS code */
-    for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
-      Iacnt = Ia->cnt[Ka_list];
-      Iaridx = Ia->ridx[Ka_list];
-      Iasgn = Ia->sgn[Ka_list];
-      Iaij = Ia->ij[Ka_list];
+        /* first loop over the K_a lists to block the excited strings by
+         * irrep or RAS code */
+        for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
+            Iacnt = Ia->cnt[Ka_list];
+            Iaridx = Ia->ridx[Ka_list];
+            Iasgn = Ia->sgn[Ka_list];
+            Iaij = Ia->ij[Ka_list];
 
-      /* Now loop over excited strings that belong to the given block Ka_list */
-      for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
-        kl = *Iaij++;
-        Ka_idx = *Iaridx++;
-        Ka_sgn = (double)*Iasgn++;
+            /* Now loop over excited strings that belong to the given block Ka_list */
+            for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
+                kl = *Iaij++;
+                Ka_idx = *Iaridx++;
+                Ka_sgn = (double)*Iasgn++;
 
-        /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
-        Ka = alplist[Ka_list] + Ka_idx;
-        if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[kl];
+                /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
+                Ka = alplist[Ka_list] + Ka_idx;
+                if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[kl];
 
-        /* loop over excitations E^a_{ij} from |A(K_a)> */
-        /* Ja_list pre-determined because of C blocking */
-        Kacnt = Ka->cnt[Ja_list];
-        Karidx = Ka->ridx[Ja_list];
-        Kasgn = Ka->sgn[Ja_list];
-        Kaij = Ka->ij[Ja_list];
-        for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
-          Ja_idx = *Karidx++;
-          Ja_sgn = (double)*Kasgn++;
-          ij = *Kaij++;
-          ijkl = INDEX(ij, kl);
-          F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
-        }
-      } /* end loop over Ia excitations */
-    }   /* end loop over Ka_list */
+                /* loop over excitations E^a_{ij} from |A(K_a)> */
+                /* Ja_list pre-determined because of C blocking */
+                Kacnt = Ka->cnt[Ja_list];
+                Karidx = Ka->ridx[Ja_list];
+                Kasgn = Ka->sgn[Ja_list];
+                Kaij = Ka->ij[Ja_list];
+                for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
+                    Ja_idx = *Karidx++;
+                    Ja_sgn = (double)*Kasgn++;
+                    ij = *Kaij++;
+                    ijkl = INDEX(ij, kl);
+                    F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
+                }
+            } /* end loop over Ia excitations */
+        }     /* end loop over Ka_list */
 
-    /*
-    for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
-       tval = 0.0;
-       for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
-          tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
-          }
-       S[Ia_idx][Ib_idx] += tval;
-       }
-    */
+        /*
+        for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
+           tval = 0.0;
+           for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
+              tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
+              }
+           S[Ia_idx][Ib_idx] += tval;
+           }
+        */
 
-    for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
-      if ((tval = F[Ja_idx]) == 0.0) continue;
-      Cptr = C[Ja_idx];
+        for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
+            if ((tval = F[Ja_idx]) == 0.0) continue;
+            Cptr = C[Ja_idx];
 
 #ifdef USE_BLAS
-      C_DAXPY(nbs, tval, Cptr, 1, Sptr, 1);
+            C_DAXPY(nbs, tval, Cptr, 1, Sptr, 1);
 #else
-      for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
-        Sptr[Ib_idx] += tval * Cptr[Ib_idx];
-      }
+            for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
+                Sptr[Ib_idx] += tval * Cptr[Ib_idx];
+            }
 #endif
-    }
+        }
 
-  } /* end loop over Ia */
+    } /* end loop over Ia */
 }
 
 /*
@@ -168,88 +165,86 @@ void s2_block_vfci(struct stringwr **alplist, struct stringwr **betlist,
 ** Obtained 7/22/95 from s1 routine by changing a's to b's and vice versa
 ** Modified 5/10/96 for more vectorized approach
 */
-void s2_block_vras(struct stringwr **alplist, struct stringwr **betlist,
-                   double **C, double **S, double *oei, double *tei, double *F,
-                   int nlists, int nas, int nbs, int Ia_list, int Ja_list,
-                   int Ja_list_nas) {
-  struct stringwr *Ia, *Ka;
-  size_t Ia_idx, Ib_idx, Ka_idx, Ja_idx;
-  size_t Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
-  size_t *Iaridx, *Karidx;
-  int *Iaij, *Kaij, *Iaoij, *Kaoij;
-  signed char *Iasgn, *Kasgn;
-  int ij, kl, ijkl, oij, okl;
-  double Ka_sgn, Ja_sgn;
-  double tval;
-  double *Sptr, *Cptr;
+void s2_block_vras(struct stringwr **alplist, struct stringwr **betlist, double **C, double **S, double *oei,
+                   double *tei, double *F, int nlists, int nas, int nbs, int Ia_list, int Ja_list, int Ja_list_nas) {
+    struct stringwr *Ia, *Ka;
+    size_t Ia_idx, Ib_idx, Ka_idx, Ja_idx;
+    size_t Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
+    size_t *Iaridx, *Karidx;
+    int *Iaij, *Kaij, *Iaoij, *Kaoij;
+    signed char *Iasgn, *Kasgn;
+    int ij, kl, ijkl, oij, okl;
+    double Ka_sgn, Ja_sgn;
+    double tval;
+    double *Sptr, *Cptr;
 
-  /* loop over I_a */
-  for (Ia = alplist[Ia_list], Ia_idx = 0; Ia_idx < nas; Ia_idx++, Ia++) {
-    Sptr = S[Ia_idx];
-    zero_arr(F, Ja_list_nas);
+    /* loop over I_a */
+    for (Ia = alplist[Ia_list], Ia_idx = 0; Ia_idx < nas; Ia_idx++, Ia++) {
+        Sptr = S[Ia_idx];
+        zero_arr(F, Ja_list_nas);
 
-    /* loop over excitations E^a_{kl} from |A(I_a)> */
-    for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
-      Iacnt = Ia->cnt[Ka_list];
-      Iaridx = Ia->ridx[Ka_list];
-      Iasgn = Ia->sgn[Ka_list];
-      Iaij = Ia->ij[Ka_list];
-      Iaoij = Ia->oij[Ka_list];
-      for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
-        kl = *Iaij++;
-        okl = *Iaoij++;
-        Ka_idx = *Iaridx++;
-        Ka_sgn = (double)*Iasgn++;
+        /* loop over excitations E^a_{kl} from |A(I_a)> */
+        for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
+            Iacnt = Ia->cnt[Ka_list];
+            Iaridx = Ia->ridx[Ka_list];
+            Iasgn = Ia->sgn[Ka_list];
+            Iaij = Ia->ij[Ka_list];
+            Iaoij = Ia->oij[Ka_list];
+            for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
+                kl = *Iaij++;
+                okl = *Iaoij++;
+                Ka_idx = *Iaridx++;
+                Ka_sgn = (double)*Iasgn++;
 
-        /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
-        Ka = alplist[Ka_list] + Ka_idx;
-        /* note okl on next line, not kl */
-        if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[okl];
+                /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
+                Ka = alplist[Ka_list] + Ka_idx;
+                /* note okl on next line, not kl */
+                if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[okl];
 
-        /* loop over excitations E^a_{ij} from |A(K_a)> */
-        /* Ja_list pre-determined because of C blocking */
-        Kacnt = Ka->cnt[Ja_list];
-        Karidx = Ka->ridx[Ja_list];
-        Kasgn = Ka->sgn[Ja_list];
-        Kaij = Ka->ij[Ja_list];
-        Kaoij = Ka->oij[Ja_list];
-        for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
-          Ja_idx = *Karidx++;
-          Ja_sgn = (double)*Kasgn++;
-          ij = *Kaij++;
-          oij = *Kaoij++;
-          ijkl = INDEX(ij, kl);
-          if (oij > okl)
-            F[Ja_idx] += Ka_sgn * Ja_sgn * tei[ijkl];
-          else if (oij == okl)
-            F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
-        }
-      } /* end loop over Ia excitations */
-    }   /* end loop over Ka_list */
+                /* loop over excitations E^a_{ij} from |A(K_a)> */
+                /* Ja_list pre-determined because of C blocking */
+                Kacnt = Ka->cnt[Ja_list];
+                Karidx = Ka->ridx[Ja_list];
+                Kasgn = Ka->sgn[Ja_list];
+                Kaij = Ka->ij[Ja_list];
+                Kaoij = Ka->oij[Ja_list];
+                for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
+                    Ja_idx = *Karidx++;
+                    Ja_sgn = (double)*Kasgn++;
+                    ij = *Kaij++;
+                    oij = *Kaoij++;
+                    ijkl = INDEX(ij, kl);
+                    if (oij > okl)
+                        F[Ja_idx] += Ka_sgn * Ja_sgn * tei[ijkl];
+                    else if (oij == okl)
+                        F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
+                }
+            } /* end loop over Ia excitations */
+        }     /* end loop over Ka_list */
 
-    /*
-    for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
-       tval = 0.0;
-       for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
-          tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
-          }
-       S[Ia_idx][Ib_idx] += tval;
-       }
-    */
+        /*
+        for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
+           tval = 0.0;
+           for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
+              tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
+              }
+           S[Ia_idx][Ib_idx] += tval;
+           }
+        */
 
-    for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
-      if ((tval = F[Ja_idx]) == 0.0) continue;
-      Cptr = C[Ja_idx];
+        for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
+            if ((tval = F[Ja_idx]) == 0.0) continue;
+            Cptr = C[Ja_idx];
 #ifdef USE_BLAS
-      C_DAXPY(nbs, tval, Cptr, 1, Sptr, 1);
+            C_DAXPY(nbs, tval, Cptr, 1, Sptr, 1);
 #else
-      for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
-        Sptr[Ib_idx] += tval * Cptr[Ib_idx];
-      }
+            for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
+                Sptr[Ib_idx] += tval * Cptr[Ib_idx];
+            }
 #endif
-    }
+        }
 
-  } /* end loop over Ia */
+    } /* end loop over Ia */
 }
 
 /*
@@ -274,92 +269,88 @@ void s2_block_vras(struct stringwr **alplist, struct stringwr **betlist,
 ** Modified 5/13/96 for new sparse-F vectorized version
 **
 */
-void s2_block_vras_rotf(int *Cnt[2], int **Ij[2], int **Oij[2], int **Ridx[2],
-                        signed char **Sgn[2], unsigned char **Toccs, double **C,
-                        double **S, double *oei, double *tei, double *F,
-                        int nlists, int nas, int nbs, int Ia_list, int Ja_list,
-                        int Ja_list_nas, struct olsen_graph *AlphaG,
-                        struct olsen_graph *BetaG, struct calcinfo *CIinfo,
-                        unsigned char ***Occs) {
-  int Ia_idx, Ib_idx, Ka_idx, Ja_idx;
-  int Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
-  int *Iaridx, *Karidx;
-  int *Iaij, *Kaij, *Iaoij, *Kaoij;
-  signed char *Iasgn, *Kasgn;
-  int i, ij, kl, ijkl, oij, okl;
-  double Ka_sgn, Ja_sgn;
-  double tval, *Cptr, *Sptr;
+void s2_block_vras_rotf(int *Cnt[2], int **Ij[2], int **Oij[2], int **Ridx[2], signed char **Sgn[2],
+                        unsigned char **Toccs, double **C, double **S, double *oei, double *tei, double *F, int nlists,
+                        int nas, int nbs, int Ia_list, int Ja_list, int Ja_list_nas, struct olsen_graph *AlphaG,
+                        struct olsen_graph *BetaG, struct calcinfo *CIinfo, unsigned char ***Occs) {
+    int Ia_idx, Ib_idx, Ka_idx, Ja_idx;
+    int Iacnt, Kacnt, Ka_list, Ia_ex, Ka_ex;
+    int *Iaridx, *Karidx;
+    int *Iaij, *Kaij, *Iaoij, *Kaoij;
+    signed char *Iasgn, *Kasgn;
+    int i, ij, kl, ijkl, oij, okl;
+    double Ka_sgn, Ja_sgn;
+    double tval, *Cptr, *Sptr;
 
-  for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
-    b2brepl(Occs[Ia_list], Cnt[0], Ij[0], Oij[0], Ridx[0], Sgn[0], BetaG,
-            Ia_list, Ka_list, nas, CIinfo);
+    for (Ka_list = 0; Ka_list < nlists; Ka_list++) {
+        b2brepl(Occs[Ia_list], Cnt[0], Ij[0], Oij[0], Ridx[0], Sgn[0], BetaG, Ia_list, Ka_list, nas, CIinfo);
 
-    /* loop over I_a */
-    for (Ia_idx = 0; Ia_idx < nas; Ia_idx++) {
-      if ((Iacnt = Cnt[0][Ia_idx]) < 0) continue;
-      Sptr = S[Ia_idx];
-      zero_arr(F, Ja_list_nas);
+        /* loop over I_a */
+        for (Ia_idx = 0; Ia_idx < nas; Ia_idx++) {
+            if ((Iacnt = Cnt[0][Ia_idx]) < 0) continue;
+            Sptr = S[Ia_idx];
+            zero_arr(F, Ja_list_nas);
 
-      /* loop over excitations E^a_{kl} from |A(I_a)> */
-      Iaridx = Ridx[0][Ia_idx];
-      Iasgn = Sgn[0][Ia_idx];
-      Iaij = Ij[0][Ia_idx];
-      Iaoij = Oij[0][Ia_idx];
+            /* loop over excitations E^a_{kl} from |A(I_a)> */
+            Iaridx = Ridx[0][Ia_idx];
+            Iasgn = Sgn[0][Ia_idx];
+            Iaij = Ij[0][Ia_idx];
+            Iaoij = Oij[0][Ia_idx];
 
-      for (i = 0; i < Iacnt; i++) Toccs[i] = Occs[Ka_list][Iaridx[i]];
+            for (i = 0; i < Iacnt; i++) Toccs[i] = Occs[Ka_list][Iaridx[i]];
 
-      b2brepl(Toccs, Cnt[1], Ij[1], Oij[1], Ridx[1], Sgn[1], AlphaG, Ka_list,
-              Ja_list, Iacnt, CIinfo);
+            b2brepl(Toccs, Cnt[1], Ij[1], Oij[1], Ridx[1], Sgn[1], AlphaG, Ka_list, Ja_list, Iacnt, CIinfo);
 
-      for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
-        kl = *Iaij++;
-        okl = *Iaoij++;
-        Ka_idx = *Iaridx++;
-        Ka_sgn = (double)*Iasgn++;
+            for (Ia_ex = 0; Ia_ex < Iacnt; Ia_ex++) {
+                kl = *Iaij++;
+                okl = *Iaoij++;
+                Ka_idx = *Iaridx++;
+                Ka_sgn = (double)*Iasgn++;
 
-        /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
-        /* note okl on next line, not kl */
-        if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[okl];
+                /* A(K_a) = sgn(kl) * E^a_{kl} |A(I_a)> */
+                /* note okl on next line, not kl */
+                if (Ka_list == Ja_list) F[Ka_idx] += Ka_sgn * oei[okl];
 
-        /* loop over excitations E^a_{ij} from |A(K_a)> */
-        /* Ja_list pre-determined because of C blocking */
-        Kacnt = Cnt[1][Ia_ex];
-        Karidx = Ridx[1][Ia_ex];
-        Kasgn = Sgn[1][Ia_ex];
-        Kaij = Ij[1][Ia_ex];
-        Kaoij = Oij[1][Ia_ex];
-        for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
-          Ja_idx = *Karidx++;
-          Ja_sgn = (double)*Kasgn++;
-          ij = *Kaij++;
-          oij = *Kaoij++;
-          ijkl = INDEX(ij, kl);
-          if (oij > okl)
-            F[Ja_idx] += Ka_sgn * Ja_sgn * tei[ijkl];
-          else if (oij == okl)
-            F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
-        }
-      } /* end loop over Ia excitations */
+                /* loop over excitations E^a_{ij} from |A(K_a)> */
+                /* Ja_list pre-determined because of C blocking */
+                Kacnt = Cnt[1][Ia_ex];
+                Karidx = Ridx[1][Ia_ex];
+                Kasgn = Sgn[1][Ia_ex];
+                Kaij = Ij[1][Ia_ex];
+                Kaoij = Oij[1][Ia_ex];
+                for (Ka_ex = 0; Ka_ex < Kacnt; Ka_ex++) {
+                    Ja_idx = *Karidx++;
+                    Ja_sgn = (double)*Kasgn++;
+                    ij = *Kaij++;
+                    oij = *Kaoij++;
+                    ijkl = INDEX(ij, kl);
+                    if (oij > okl)
+                        F[Ja_idx] += Ka_sgn * Ja_sgn * tei[ijkl];
+                    else if (oij == okl)
+                        F[Ja_idx] += 0.5 * Ka_sgn * Ja_sgn * tei[ijkl];
+                }
+            } /* end loop over Ia excitations */
 
-      /*
-      for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
-         tval = 0.0;
-         for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
-            tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
+            /*
+            for (Ib_idx=0; Ib_idx < nbs; Ib_idx++) {
+               tval = 0.0;
+               for (Ja_idx=0; Ja_idx < Ja_list_nas; Ja_idx++) {
+                  tval += C[Ja_idx][Ib_idx] * F[Ja_idx];
+                  }
+               S[Ia_idx][Ib_idx] += tval;
+               }
+            */
+
+            for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
+                if ((tval = F[Ja_idx]) == 0.0) continue;
+                Cptr = C[Ja_idx];
+                for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
+                    Sptr[Ib_idx] += tval * Cptr[Ib_idx];
+                }
             }
-         S[Ia_idx][Ib_idx] += tval;
-         }
-      */
 
-      for (Ja_idx = 0; Ja_idx < Ja_list_nas; Ja_idx++) {
-        if ((tval = F[Ja_idx]) == 0.0) continue;
-        Cptr = C[Ja_idx];
-        for (Ib_idx = 0; Ib_idx < nbs; Ib_idx++) {
-          Sptr[Ib_idx] += tval * Cptr[Ib_idx];
-        }
-      }
-
-    } /* end loop over Ia */
-  }   /* end loop over Ka_list */
+        } /* end loop over Ia */
+    }     /* end loop over Ka_list */
 }
-}}  // namespace psi::detci
+}  // namespace detci
+}  // namespace psi
--- psi4/src/psi4/detci/slaterd.cc
+++ psi4/src/psi4/detci/slaterd.cc
@@ -39,131 +39,114 @@
 
 #include "psi4/psi4-dec.h"
 
-namespace psi { namespace detci {
+namespace psi {
+namespace detci {
 
 extern double get_twoel(int i, int j, int k, int l);
 extern double get_onel(int i, int j);
-extern int calc_orb_diff(int cnt, unsigned char *I, unsigned char *J,
-   int *I_alpha_diff, int *J_alpha_diff, int *sign, int *same,
-   int extended);
-extern void common_orbs(int *same_alpha, int *same_beta, int cnt_alpha,
-   int cnt_beta, int *common_docc, int *common_alpha_socc,
-   int *common_beta_socc, int *cnt_docc, int *cnt_alpha_socc,
-   int *cnt_beta_socc);
+extern int calc_orb_diff(int cnt, unsigned char *I, unsigned char *J, int *I_alpha_diff, int *J_alpha_diff, int *sign,
+                         int *same, int extended);
+extern void common_orbs(int *same_alpha, int *same_beta, int cnt_alpha, int cnt_beta, int *common_docc,
+                        int *common_alpha_socc, int *common_beta_socc, int *cnt_docc, int *cnt_alpha_socc,
+                        int *cnt_beta_socc);
 
+void SlaterDeterminant::set(size_t na, unsigned char *alpoccs, size_t nb, unsigned char *betoccs) {
+    int i;
 
-void SlaterDeterminant::set(size_t na, unsigned char *alpoccs,
-      size_t nb, unsigned char *betoccs)
-{
-   int i;
+    if (nalp_ != na) {
+        if (Occs_[0] != nullptr) free(Occs_[0]);
+        Occs_[0] = (unsigned char *)malloc(sizeof(unsigned char) * na);
+        nalp_ = na;
+    }
+    if (nbet_ != nb) {
+        if (Occs_[1] != nullptr) free(Occs_[1]);
+        Occs_[1] = (unsigned char *)malloc(sizeof(unsigned char) * nb);
+        nbet_ = nb;
+    }
 
-   if (nalp_ != na) {
-      if (Occs_[0] != nullptr) free(Occs_[0]);
-      Occs_[0] = (unsigned char *) malloc (sizeof(unsigned char) * na);
-      nalp_ = na;
-      }
-   if (nbet_ != nb) {
-      if (Occs_[1] != nullptr) free(Occs_[1]);
-      Occs_[1] = (unsigned char *) malloc (sizeof(unsigned char) * nb);
-      nbet_ = nb;
-      }
-
-   for (i=0; i<nalp_; i++) {
-      Occs_[0][i] = alpoccs[i];
-      }
-   for (i=0; i<nbet_; i++) {
-      Occs_[1][i] = betoccs[i];
-      }
+    for (i = 0; i < nalp_; i++) {
+        Occs_[0][i] = alpoccs[i];
+    }
+    for (i = 0; i < nbet_; i++) {
+        Occs_[1][i] = betoccs[i];
+    }
 }
 
+void SlaterDeterminant::print() {
+    int i;
 
+    outfile->Printf("Alpha string: ");
+    for (i = 0; i < nalp_; i++) {
+        outfile->Printf("%3d ", Occs_[0][i]);
+    }
+    outfile->Printf("\n");
 
-void SlaterDeterminant::print()
-{
-   int i;
-
-   outfile->Printf( "Alpha string: ");
-   for (i=0; i<nalp_; i++) {
-      outfile->Printf( "%3d ", Occs_[0][i]);
-      }
-   outfile->Printf( "\n");
-
-   outfile->Printf( "Beta string : ");
-   for (i=0; i<nbet_; i++) {
-      outfile->Printf( "%3d ", Occs_[1][i]);
-      }
-   outfile->Printf( "\n");
+    outfile->Printf("Beta string : ");
+    for (i = 0; i < nbet_; i++) {
+        outfile->Printf("%3d ", Occs_[1][i]);
+    }
+    outfile->Printf("\n");
 }
 
+void SlaterDeterminant::print_config() {
+    int i = 0, j = 0;
 
-void SlaterDeterminant::print_config()
-{
-   int i=0, j=0;
+    while ((i < nalp_) && (j < nbet_)) {
+        if (Occs_[0][i] == Occs_[1][j]) {
+            outfile->Printf("%dX ", Occs_[0][i] + 1);
+            i++;
+            j++;
+        } else if (Occs_[0][i] < Occs_[1][j]) {
+            outfile->Printf("%dA ", Occs_[0][i] + 1);
+            i++;
+        } else if (Occs_[0][i] > Occs_[1][j]) {
+            outfile->Printf("%dB ", Occs_[1][j] + 1);
+            j++;
+        }
+    }
 
-   while ((i < nalp_) && (j < nbet_)) {
-      if (Occs_[0][i] == Occs_[1][j]) {
-         outfile->Printf( "%dX ", Occs_[0][i]+1);
-         i++; j++;
-         }
-      else if (Occs_[0][i] < Occs_[1][j]) {
-         outfile->Printf( "%dA ", Occs_[0][i]+1);
-         i++;
-         }
-      else if (Occs_[0][i] > Occs_[1][j]) {
-         outfile->Printf( "%dB ", Occs_[1][j]+1);
-         j++;
-         }
-      }
+    if (i < j) {
+        while (i < nalp_) {
+            outfile->Printf("%dA ", Occs_[0][i] + 1);
+            i++;
+        }
+    } else if (i > j) {
+        while (j < nbet_) {
+            outfile->Printf("%dB ", Occs_[1][j] + 1);
+            j++;
+        }
+    }
 
-   if (i < j) {
-      while (i < nalp_) {
-         outfile->Printf( "%dA ", Occs_[0][i]+1);
-         i++;
-         }
-      }
-   else if (i > j) {
-      while (j < nbet_) {
-         outfile->Printf( "%dB ", Occs_[1][j]+1);
-         j++;
-         }
-      }
-
-   outfile->Printf( "\n") ;
-
+    outfile->Printf("\n");
 }
 
-
-SlaterDeterminant& SlaterDeterminant::operator=(const SlaterDeterminant& s)
-{
-   if (nalp_ != s.nalp_) {
-      if (Occs_[0] != nullptr) free(Occs_[0]);
-      Occs_[0] = (unsigned char *) malloc (sizeof(unsigned char) * s.nalp_);
-      }
-   if (nbet_ != s.nbet_) {
-      if (Occs_[1] != nullptr) free(Occs_[1]);
-      Occs_[1] = (unsigned char *) malloc (sizeof(unsigned char) * s.nbet_);
-      }
-   set(s.nalp_, s.Occs_[0], s.nbet_, s.Occs_[1]);
-   return(*this);
+SlaterDeterminant &SlaterDeterminant::operator=(const SlaterDeterminant &s) {
+    if (nalp_ != s.nalp_) {
+        if (Occs_[0] != nullptr) free(Occs_[0]);
+        Occs_[0] = (unsigned char *)malloc(sizeof(unsigned char) * s.nalp_);
+    }
+    if (nbet_ != s.nbet_) {
+        if (Occs_[1] != nullptr) free(Occs_[1]);
+        Occs_[1] = (unsigned char *)malloc(sizeof(unsigned char) * s.nbet_);
+    }
+    set(s.nalp_, s.Occs_[0], s.nbet_, s.Occs_[1]);
+    return (*this);
 }
 
+int operator==(SlaterDeterminant &s1, SlaterDeterminant &s2) {
+    int i;
 
-int operator ==(SlaterDeterminant& s1, SlaterDeterminant& s2)
-{
-   int i;
+    if (s1.nalp_ != s2.nalp_ || s1.nbet_ != s2.nbet_) return (0);
 
-   if (s1.nalp_ != s2.nalp_ || s1.nbet_ != s2.nbet_) return(0);
+    for (i = 0; i < s1.nalp_; i++) {
+        if (s1.Occs_[0][i] != s2.Occs_[0][i]) return (0);
+    }
+    for (i = 0; i < s1.nbet_; i++) {
+        if (s1.Occs_[1][i] != s2.Occs_[1][i]) return (0);
+    }
 
-   for (i=0; i<s1.nalp_; i++) {
-      if (s1.Occs_[0][i] != s2.Occs_[0][i]) return(0);
-   }
-   for (i=0; i<s1.nbet_; i++) {
-      if (s1.Occs_[1][i] != s2.Occs_[1][i]) return(0);
-   }
-
-   return(1);
+    return (1);
 }
 
-
-
-}} // namespace psi::detci
+}  // namespace detci
+}  // namespace psi
