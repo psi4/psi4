d2ee8ed4875c9b5cee803c724d88c486971bb202
Raimondas Galvelis <r.galvelis@acellera.com>
2018-09-06 11:58:40 +0200

    Remove "update" from OpenMP atomic

--- psi4/src/psi4/libfock/v.cc
+++ psi4/src/psi4/libfock/v.cc
@@ -385,12 +385,12 @@ double VBase::vv10_nlc(SharedMatrix D, SharedMatrix ret) {
             int mg = function_map[ml];
             for (int nl = 0; nl < ml; nl++) {
                 int ng = function_map[nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vp[mg][ng] += V2p[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vp[ng][mg] += V2p[ml][nl];
             }
-#pragma omp atomic update
+#pragma omp atomic
             Vp[mg][mg] += V2p[ml][ml];
         }
         parallel_timer_off("VV10 Fock", rank);
@@ -585,12 +585,12 @@ void RV::compute_V(std::vector<SharedMatrix> ret) {
             int mg = function_map[ml];
             for (int nl = 0; nl < ml; nl++) {
                 int ng = function_map[nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vp[mg][ng] += V2p[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vp[ng][mg] += V2p[ml][nl];
             }
-#pragma omp atomic update
+#pragma omp atomic
             Vp[mg][mg] += V2p[ml][ml];
         }
         parallel_timer_off("V_xc", rank);
@@ -869,12 +869,12 @@ void RV::compute_Vx(std::vector<SharedMatrix> Dx, std::vector<SharedMatrix> ret)
                 int mg = function_map[ml];
                 for (int nl = 0; nl < ml; nl++) {
                     int ng = function_map[nl];
-#pragma omp atomic update
+#pragma omp atomic
                     Vxp[mg][ng] += Vx_localp[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                     Vxp[ng][mg] += Vx_localp[ml][nl];
                 }
-#pragma omp atomic update
+#pragma omp atomic
                 Vxp[mg][mg] += Vx_localp[ml][ml];
             }
             parallel_timer_off("V_XCd", rank);
@@ -1654,18 +1654,18 @@ void UV::compute_V(std::vector<SharedMatrix> ret) {
             int mg = function_map[ml];
             for (int nl = 0; nl < ml; nl++) {
                 int ng = function_map[nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vap[mg][ng] += Va2p[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vap[ng][mg] += Va2p[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vbp[mg][ng] += Vb2p[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                 Vbp[ng][mg] += Vb2p[ml][nl];
             }
-#pragma omp atomic update
+#pragma omp atomic
             Vap[mg][mg] += Va2p[ml][ml];
-#pragma omp atomic update
+#pragma omp atomic
             Vbp[mg][mg] += Vb2p[ml][ml];
         }
         parallel_timer_off("V_xc", rank);
@@ -2153,19 +2153,19 @@ void UV::compute_Vx(std::vector<SharedMatrix> Dx, std::vector<SharedMatrix> ret)
                 for (int nl = 0; nl < ml; nl++) {
                     int ng = function_map[nl];
 
-#pragma omp atomic update
+#pragma omp atomic
                     Vaxp[mg][ng] += Vax_localp[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                     Vaxp[ng][mg] += Vax_localp[ml][nl];
 
-#pragma omp atomic update
+#pragma omp atomic
                     Vbxp[mg][ng] += Vbx_localp[ml][nl];
-#pragma omp atomic update
+#pragma omp atomic
                     Vbxp[ng][mg] += Vbx_localp[ml][nl];
                 }
-#pragma omp atomic update
+#pragma omp atomic
                 Vaxp[mg][mg] += Vax_localp[ml][ml];
-#pragma omp atomic update
+#pragma omp atomic
                 Vbxp[mg][mg] += Vbx_localp[ml][ml];
             }
         }
