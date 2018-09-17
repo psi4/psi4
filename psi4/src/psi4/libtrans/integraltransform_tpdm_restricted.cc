c682ba10257178ff4cc242c883e72f5618d86b65
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-17 11:32:16 -0400

    Remove dpd.gbl file
    Co-authored-by: Andrew M. James <amjames2@vt.edu>

--- psi4/src/psi4/libtrans/integraltransform_tpdm_restricted.cc
+++ psi4/src/psi4/libtrans/integraltransform_tpdm_restricted.cc
@@ -29,20 +29,18 @@
 #include "integraltransform.h"
 #include "mospace.h"
 
-#include "psi4/libpsio/psio.hpp"
 #include "psi4/libciomr/libciomr.h"
+#include "psi4/libdpd/dpd.h"
 #include "psi4/libiwl/iwl.hpp"
-#include "psi4/libqt/qt.h"
 #include "psi4/libmints/matrix.h"
 #include "psi4/libpsi4util/PsiOutStream.h"
+#include "psi4/libpsio/psio.hpp"
+#include "psi4/libqt/qt.h"
 #include "psi4/psifiles.h"
 
 #include <math.h>
 #include <ctype.h>
 #include <stdio.h>
-
-#define EXTERN
-#include "psi4/libdpd/dpd.gbl"
 
 using namespace psi;
 
