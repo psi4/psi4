376c28da2695d9aa3d49e4e01eb968ca4245416c
Lori A. Burns <lori.burns@gmail.com>
2018-11-03 23:15:15 -0400

    mol: cov radii from qcel, test with mol writer, add mol writer to psi4.core.Mol

--- psi4/driver/qcdb/molecule.py
+++ psi4/driver/qcdb/molecule.py
@@ -1853,8 +1853,6 @@ class Molecule(LibmintsMolecule):
 
 # Attach methods to qcdb.Molecule class
 from .parker import xyz2mol as _parker_xyz2mol_yo
-Molecule.format_molecule_for_mol2 = _parker_xyz2mol_yo
-from .parker import bond_profile as _parker_bondprofile_yo
-Molecule.bond_profile = _parker_bondprofile_yo
+Molecule.format_molecule_for_mol = _parker_xyz2mol_yo
 from .interface_gcp import run_gcp as _gcp_qcdb_yo
 Molecule.run_gcp = _gcp_qcdb_yo
