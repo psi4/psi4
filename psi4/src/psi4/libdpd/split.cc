816552014c447fbde760b6a5deadc8a2fe3a905a
Francesco Evangelista <francesco.evangelista@gmail.com>
2018-11-08 23:31:17 -0500

    Make psi4 compatible with C++17

--- psi4/src/psi4/libdpd/split.cc
+++ psi4/src/psi4/libdpd/split.cc
@@ -42,13 +42,13 @@ namespace psi {
 
 // trim from start
 static inline std::string &dpd_ltrim(std::string &s) {
-    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(std::ptr_fun<int, int>(isspace))));
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
     return s;
 }
 
 // trim from end
 static inline std::string &dpd_rtrim(std::string &s) {
-    s.erase(find_if(s.rbegin(), s.rend(), not1(std::ptr_fun<int, int>(isspace))).base(), s.end());
+    s.erase(find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(), s.end());
     return s;
 }
 
