bb42c6413aa545bc31cef157b967e64690459957
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-11-16 15:30:45 -0500

    Require C++14 at least

--- cmake/custom_cxxstandard.cmake
+++ cmake/custom_cxxstandard.cmake
@@ -1,8 +1,13 @@
-# We require C++11 support from the compiler and standard library.
+# We require C++14 support from the compiler and standard library.
+
+list(APPEND _allowed_cxx_standards 14 17)
+if(NOT psi4_CXX_STANDARD IN_LIST _allowed_cxx_standards)
+  message(FATAL_ERROR "Psi4 requires C++14 at least")
+endif()
 
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
-    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
-        message(FATAL_ERROR "GCC version must be at least 4.9!")
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0)
+        message(FATAL_ERROR "GCC version must be at least 5.0!")
     endif()
 
 elseif (CMAKE_CXX_COMPILER_ID MATCHES Intel)
