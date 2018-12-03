886ff2e775946458c33b0e15ca3437ae3a26504e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-12-03 16:47:22 -0500

    Modify miniconda.sh script following MolSSI sample
    https://github.com/MolSSI/QCFractal/blob/master/devtools/travis-ci/before_install.sh

--- .scripts/miniconda.sh
+++ .scripts/miniconda.sh
@@ -1,32 +1,47 @@
-#!/usr/bin/env bash
-
 set -eu -o pipefail
 
+# Temporarily change directory to $HOME to install software
+pushd .
+cd "$HOME"
+
+# Install Miniconda
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    # Make OSX md5 mimic md5sum from linux, alias does not work
+    md5sum () {
+        command md5 -r "$@"
+    }
+    MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
+else
+    MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+fi
+MINICONDA_HOME=$HOME/miniconda
+
 echo "-- Installing latest Miniconda"
-if [ -d "$HOME/Deps/miniconda/bin" ]; then
+if [ -d "$MINICONDA_HOME/bin" ]; then
     echo "-- Miniconda latest version FOUND in cache"
 else
-    cd "$HOME"/Downloads
+    MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
     echo "-- Miniconda latest version NOT FOUND in cache"
-    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-        curl -Ls https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh
-    elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-        curl -Ls https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh
+    wget -q https://repo.continuum.io/miniconda/$MINICONDA
+    if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+        echo "Miniconda MD5 mismatch"
+        exit 1
     fi
     # Travis creates the cached directories for us.
     # This is problematic when wanting to install Anaconda for the first time...
-    rm -rf "$HOME"/Deps/miniconda
-    bash miniconda.sh -b -p "$HOME"/Deps/miniconda > /dev/null 2>&1
-    touch "$HOME"/Deps/miniconda/conda-meta/pinned
-    PATH=$HOME/Deps/miniconda/bin${PATH:+:$PATH}
-    hash -r
-    conda config --set show_channel_urls True > /dev/null 2>&1
-    conda config --set always_yes yes > /dev/null 2>&1
-    conda config --set changeps1 no > /dev/null 2>&1
-    conda update --all --yes > /dev/null 2>&1
-    conda clean -tipy > /dev/null 2>&1
-    conda info -a
-    cd "$TRAVIS_BUILD_DIR"
-    rm -f "$HOME"/Downloads/miniconda.sh
+    rm -rf "$MINICONDA_HOME"
+    bash $MINICONDA -b -p "$MINICONDA_HOME"
+
+    # Configure miniconda
+    export PIP_ARGS="-U"
+    export PATH=$MINICONDA_HOME/bin:$PATH
+
+    conda config --set always_yes yes --set changeps1 no
+    conda update --q conda
+
+    rm -f $MINICONDA
 fi
 echo "-- Done with latest Miniconda"
+
+# Restore original directory
+popd
