a73eb82381179ed6814f163c4fd93c7ad967d5bd
Andrew M. James <amjames2@vt.edu>
2018-12-04 17:47:41 -0500

    TDSCF: add product engines for TDSCF
    - RHF (singlet/triplet)
    - UHF
    - Implement the interface required by the davidson/hamiltonian engine

--- psi4/driver/procrouting/response/scf_products.py
+++ psi4/driver/procrouting/response/scf_products.py
@@ -109,3 +109,444 @@ class SCFProducts:
             ret += vector * (omega**2)
 
         return np.array(ret).ravel()
+
+
+class TDRSCFEngine(object):
+    def __init__(self, wfn, ptype='rpa', triplet=False):
+        self.wfn = wfn
+        self.ptype = _validate_ptype(ptype)
+        self.triplet = triplet
+        self.jk = self.wfn.jk()
+        self.Co = wfn.Ca_subset("SO", "OCC")
+        self.Cv = wfn.Ca_subset("SO", "VIR")
+        self.ei = wfn.epsilon_a_subset("SO", "OCC")
+        self.ea = wfn.epsilon_b_subset("SO", "VIR")
+        # orbital energy differences
+        self.Dia = None
+        # symmetry of vector
+        self.Gx = None
+        self.occpi = self.wfn.nalphapi()
+        self.virpi = self.wfn.nmopi() - self.occpi
+        self.V_pot = wfn.V_potiential()
+        self.func = wfn.functional()
+        self.Vx = []
+        self.Dx = []
+
+    def new_vector(self, name=""):
+        """Obtain a blank matrix object with the correct symmetry"""
+        return core.Matrix(name, self.occpi, self.virpi, self.Gx)
+
+    def build_Dia(self):
+        """Rebuild the (Eps_a - Eps_i) matrix for the currently set Guess vector symmetry"""
+        self.Dia = self.new_vector("ei-ea")
+        for h_i in range(self.wfn.nirrep()):
+            h_a = self.Gx ^ h_i
+            for i in range(self.occpi[h_i]):
+                for a in range(self.virpi[h_a]):
+                    self.Dia.nph[h_i][i, a] = self.ea.nph[H_a][a] - self.ei.nph[H_i][i]
+
+    def reset_symmetry(self, symmetry):
+        """Reset pre-allocated storage will matrices with the correct symemtry"""
+        self.Gx = symmetry
+        self.build_Dia()
+
+    def Fx_product(self, v):
+        Fxi = self.new_vector("Fx")
+        for h in range(self.wfn.nirrep()):
+            Fxi.nph[h] = v.nph[h] * self.Dia.nph[h]
+        return Fxi
+
+    def add_Dx(self, D):
+        self.Dx.append(D)
+        self.Vx.append(self.new_vector("Vx"))
+
+    def compute_products(self, X):
+        if self.ptype == 'rpa':
+            Px = []
+            Mx = []
+            for xi in X:
+                Pxi, Mxi = self.compute_one(xi)
+                Px.append(Pxi)
+                Mx.append(Mxi)
+            return Px, Mx
+        else:
+            Ax = []
+            for xi in X:
+                Ax.append(self.compute_one(xi))
+
+    def compute_one(self, vector):
+        self.jk.C_clear()
+        self.Dx.clear()
+        self.Vx.clear()
+        cl = self.Co
+        cr = core.Matrix.doublet(self.Cv, vector, False, True)
+        self.jk.C_left_add(cl)
+        self.jk.C_right_add(cr)
+        self.jk.compute()
+        if self.func.needs_xc():
+            self.add_Dx(core.Matrix.doublet(cl, cr, False, True))
+            self.V_pot.compute_Vx(self.Dx, self.Vx)
+
+        Fx = self.Fx_product(vector)
+
+        if self.ptype == 'rpa':
+            if self.triplet:
+                P = self.new_vector("Px")
+                M = self.new_vector("Mx")
+            else:
+                P = self.jk.J()[0]
+                M = self.new_vector("Mx")
+                P.scale(4.0)
+                if self.func.needs_xc():
+                    P.axpy(4.0, self.Vx[0])
+
+            if self.func.is_x_hybrid():
+                alpha = self.func.x_alpha()
+                K = self.jk.K()[0]
+                Kt = K.transpose()
+                P.axpy(-alpha, K)
+                P.axpy(-alpha, Kt)
+                M.axpy(alpha, Kt)
+                M.axpy(-alpha, K)
+
+            if self.func.is_x_lrc():
+                beta = self.func.x_beta()
+                wK = self.jk.wK()[0]
+                wKt = wK.transpose()
+                P.axpy(-beta, wK)
+                P.axpy(-beta, wKt)
+                M.axpy(beta, wKt)
+                M.axpy(-beta, wK)
+
+            Pret = core.Matrix.triplet(self.Co, P, self.Cv, True, False, False)
+            Pret.axpy(1.0, Fx)
+            Mret = core.Matrix.triplet(self.Co, M, self.Cv, True, False, False)
+            Mret.axpy(1.0, Fx)
+            return Pret, Mret
+
+        elif self.ptype == 'tda':
+            if self.triplet:
+                A = self.new_vector("Ax")
+            else:
+                A = self.jk.J()[0]
+                A.scale(2.0)
+            if self.func.needs_xc():
+                A.axpy(2.0, self.Vx[0])
+            if self.func.is_x_hybrid():
+                alpha = self.func.x_alpha()
+                K = self.jk.K()[0]
+                A.axpy(-alpha, K)
+            if self.func.is_x_lrc():
+                beta = self.func.x_beta()
+                wK = self.jk.wK()[0]
+                A.axpy(-beta, wK)
+
+            Aret = core.Matrix.triplet(self.Co, A, self.Cv, True, False, False)
+            Aret.axpy(1.0, Fx)
+            return Aret
+
+    def precondition_residual(self, rvec, shift):
+        denom = self.new_vector("preconditioner")
+        denom.set(shift)
+        denom.axpy(-1.0, self.Dia)
+        rvec.apply_denomenator(denom)
+        return rvec
+
+    def approximate_eigvectors(self, ax, ss_vector):
+        """Approximate the eigenvector of the real system:
+
+        Parameters:
+        -----------
+        ax: list, of :py:class:`~psi4.core.Matrix(x_dim, x_sym)` length == ss_dim
+          The matrix times guess vector products.
+        ss_vector: :py:class:`numpy.ndarray` {ss_dim}
+          Subspace eigenvector
+
+        Returns:
+        --------
+        :py:class:`psi4.core.Matrix`
+           The approximate eigenvector: $V_i \approx \sum_j A_{ij} x_j \tilde{V}_{ji}$
+        """
+        ret = self.new_vector("eigenvector")
+        for j in range(len(ax)):
+            ret.axpy(ss_vector[j], ax[j])
+        return ret
+
+    def error(self, x, ss_vector, ss_value):
+        """Compute the error piece of the residual expression:
+        $\sum_{j} X_j \tilde{V}_{ji}\omega_i$
+        """
+        ret = self.new_vector("error")
+        c = ss_vector * ss_value
+        for j in range(len(x)):
+            ret.axpy(c[j], x[j])
+        return ret
+
+    def vector_norm(self, v):
+        return np.sqrt(v.vector_dot(v))
+
+    def orthogonalize_subspace(self, old_X, new_X):
+        X = old_X
+        while len(new_X) != 0:
+            new = new_X.pop()
+            for o in X:
+                dot = new.dot(o)
+                new.axpy(-dot, o)
+            norm = np.sqrt(new.dot(new))
+            if norm >= self.add_tol:
+                new.scale(1.0 / norm)
+                X.append(new)
+        return X, len(X)
+
+    def residual(self, V, x, ss_vector, ss_value):
+        ret = self.error(x, ss_vector, -ss_value)
+        ret.axpy(1.0, V)
+        return ret
+
+    def subspace_project(self, X, Ax):
+        A = np.zeros((len(X), len(X)))
+        for i, xi in enumerate(X):
+            for j, Axj in enumerate(Ax):
+                A[i, j] = xi.vector_dot(Axj)
+        return A
+
+
+class TDUSCFEngine(object):
+    def __init__(self, wfn, ptype='rpa'):
+        self.wfn = wfn
+        self.ptype = _validate_ptype(ptype)
+        self.jk = self.wfn.jk()
+        self.Co = [wfn.Ca_subset("SO", "OCC"), wfn.Cb_subset("SO", "OCC")]
+        self.Cv = [wfn.Ca_subset("SO", "VIR"), wfn.Cb_subset("SO", "VIR")]
+        self.ei = [wfn.epsilon_a_subset("SO", "OCC"), wfn.epsilon_b_subset("SO", "OCC")]
+        self.ea = [wfn.epsilon_a_subset("SO", "VIR"), wfn.epsilon_b_subset("SO", "VIR")]
+        # orbital energy differences
+        self.Dia_a = None
+        # symmetry of vector
+        self.Gx = None
+        self.occpi = [self.wfn.nalphapi(), self.wfn.nbetapi()]
+        self.virpi = [self.wfn.nmopi() - self.occpi[0], self.wfn.nmopi() - self.occpi[1]]
+        self.V_pot = wfn.V_potiential()
+        self.func = wfn.functional()
+        self.Vx = []
+        self.Dx = []
+
+    def new_vector(self, name=""):
+        return [
+            core.Matrix(name + s, odim, vdim, self.Gx) for s, odim, vdim in zip(['a', 'b'], self.occpi, self.virpi)
+        ]
+
+    def build_Dia(self):
+        """Rebuild the (Eps_a - Eps_i) matrix for the currently set Guess vector symmetry"""
+        self.Dia = self.new_vector("ei-ea ")
+        for h_i in range(self.wfn.nirrep()):
+            h_a = self.Gx ^ h_i
+            for a_or_b in (0, 1):
+                for i in range(self.occpi[a_or_b][h_i]):
+                    for a in range(self.virpi[a_or_b][h_a]):
+                        self.Dia[a_or_b].nph[h_i][i, a] = self.ea.nph[a_or_b][H_a][a] - self.ei[a_or_b].nph[H_i][i]
+
+    def reset_symmetry(self, symmetry):
+        self.Gx = symmetry
+        self.build_Dia()
+
+    def Fx_product(self, vector):
+        Fxi = self.new_vector("Fx")
+        for a_or_b in (0, 1):
+            for h in range(self.wfn.nirrep()):
+                Fxi[a_or_b].nph[h] = v[a_or_b].nph[h] * self.Dia[a_or_b].nph[h]
+        return Fxi
+
+    def add_Dx(self, D):
+        self.Dx.extend(D)
+        self.Vx.extend(self.new_vector("Vx"))
+
+    def compute_products(self, X):
+        if self.ptype == 'rpa':
+            Px = []
+            Mx = []
+            for xi in X:
+                Pxi, Mxi = self.compute_one(xi)
+                Px.append(Pxi)
+                Mx.append(Mxi)
+            return Px, Mx
+        else:
+            Ax = []
+            for xi in X:
+                Ax.append(self.compute_one(xi))
+            return Ax
+
+    def compute_one(self, vector):
+        vector_a, vector_b = vector
+        self.jk.C_clear()
+        self.Dx.clear()
+        self.Vx.clear()
+
+        cl_a, cl_b = self.Co
+        cr_a = core.Matrix.doublet(Cv_a, vector_a, False, True)
+        cr_b = core.Matrix.doublet(Cv_b, vector_b, False, True)
+
+        self.jk.C_left_add(cl_a)
+        self.jk.C_right_add(cr_a)
+        self.jk.C_left_add(cl_b)
+        self.jk.C_left_add(cr_b)
+        self.jk.compute()
+        if self.func.needs_xc():
+            self.add_Dx([core.Matrix.doublet(cl_a, cr_a, False, True), core.Matrix.doublet(cl_b, cr_b, False, True)])
+            self.V_pot.compute_Vx(self.Dx, self.Vx)
+
+        Fxa, Fxb = self.Fx_product(vector)
+
+        if self.ptype == 'rpa':
+            P = self.jk.J()
+            P[0].scale(2.0)
+            P[0].axpy(2.0, P[1])
+            P[1].copy(P[0])
+            M = self.new_vector("Mx")
+            if self.func.needs_xc():
+                P[0].axpy(2.0, self.Vx[0])
+                P[1].axpy(2.0, self.Vx[1])
+
+            if self.func.is_x_hybrid():
+                alpha = self.func.x_alpha()
+                K = self.jk.K()
+                P[0].axpy(-alpha, K[0])
+                P[1].axpy(-alpha, K[1])
+                M[0].axpy(-alpha, K[0])
+                M[1].axpy(-alpha, K[1])
+                Kt = [K[0].transpose(), K[1].transpose()]
+                P[0].axpy(-alpha, Kt[0])
+                P[1].axpy(-alpha, Kt[1])
+                M[0].axpy(alpha, Kt[0])
+                M[1].axpy(alpha, Kt[1])
+
+            if self.func.is_x_lrc():
+                beta = self.func.x_beta()
+                wK = self.jk.wK()
+                P[0].axpy(-alpha, wK[0])
+                P[1].axpy(-alpha, wK[1])
+                M[0].axpy(-alpha, wK[0])
+                M[1].axpy(-alpha, wK[1])
+                wKt = [wK[0].transpose(), wK[1].transpose()]
+                P[0].axpy(-alpha, wKt[0])
+                P[1].axpy(-alpha, wKt[1])
+                M[0].axpy(alpha, wKt[0])
+                M[1].axpy(alpha, wKt[1])
+
+            Pret = [
+                core.Matrix.triplet(self.Co[a_or_b], P[a_or_b], self.Cv[a_or_b], True, False, False)
+                for a_or_b in (0, 1)
+            ]
+            Pret[0].axpy(1.0, Fxa)
+            Pret[1].axpy(1.0, Fxb)
+
+            Mret = [
+                core.Matrix.triplet(self.Co[a_or_b], M[a_or_b], self.Cv[a_or_b], True, False, False)
+                for a_or_b in (0, 1)
+            ]
+            Mret[0].axpy(1.0, Fxa)
+            Mret[0].axpy(1.0, Fxb)
+            return Pret, Mret
+
+        elif self.ptype == 'tda':
+            A = self.jk.J()
+            if self.func.needs_xc():
+                A[0].axpy(1.0, self.Vx[0])
+                A[1].axpy(1.0, self.Vx[1])
+            if self.func.is_x_hybrid():
+                alpha = self.func.x_alpha()
+                K = self.jk.K()
+                A[0].axpy(-alpha, K[0])
+                A[1].axpy(-alpha, K[1])
+            if self.func.is_x_lrc():
+                beta = self.func.x_beta()
+                wK = self.jk.wK()
+                A[0].axpy(-beta, wK[0])
+                A[1].axpy(-beta, wK[1])
+
+            Aret = [
+                core.Matrix.triplet(self.Co[a_or_b], A[a_or_b], self.Cv[a_or_b], True, False, False)
+                for a_or_b in (0, 1)
+            ]
+            Aret[0].axpy(1.0, Fxa)
+            Aret[1].axpy(1.0, Fxb)
+            return Aret
+
+    def precondition_residual(self, rvec, shift):
+        rva, rvb = rvec
+        denom_a, denom_b = self.new_vector("preconditioner")
+        denom_a.set(shift)
+        denom_b.set(shift)
+        denom_a.axpy(-1.0, self.Dia[0])
+        denom_b.axpy(-1.0, self.Dia[1])
+        rva.apply_denomenator(denom_a)
+        rvb.apply_denomenator(denom_b)
+        return rva, rvb
+
+    def approximate_eigvector(self, ax, ss_vector):
+        """Approximate the eigenvector of the real system:
+
+        Parameters:
+        -----------
+        ax: list, of :py:class:`~psi4.core.Matrix(x_dim, x_sym)` length == 2*ss_dim (a/b pairs)
+          The matrix times guess vector products.
+        ss_vector: :py:class:`numpy.ndarray` {ss_dim}
+          Eigenvector of ss problem
+
+        Returns:
+        --------
+        pair of :py:class:`psi4.core.Matrix` (a/b pair)
+           The approximate eigenvector: $V_i \approx \sum_j A_{ij} x_j \tilde{V}_{ji}$
+        """
+        ret = self.new_vector("eigenvector")
+        for ax_a, ax_b in ax:
+            ret[0].axpy(ss_vector[j], ax_a)
+            ret[1].axpy(ss_vector[j], ax_b)
+        return ret
+
+    def error(self, x, ss_vector, ss_value):
+        """Compute the error piece of the residual expression:
+        $\Delta_{i} = \sum_{j} X_j \tilde{V}_{ji}\omega_i$
+        """
+        ret = self.new_vector("error")
+        c = ss_vector * ss_value
+        for xa, xb in X:
+            ret[0].axpy(c[j], xa)
+            ret[1].axpy(c[j], xb)
+        return ret
+
+    def orthogonalize_subspace(self, old_X, new_X):
+        X = old_X
+        while len(new_X) != 0:
+            new_a, new_b = new_X
+            for (oa, ob) in X:
+                dot = new_a.vector_dot(oa)
+                dot += new_b.vector_dot(ob)
+                new_a.axpy(-dot, oa)
+                new_b.axpy(-dot, ob)
+            norm = np.sqrt(new_a.vector_dot(new_a))
+            norm += np.sqrt(new_b.vector_dot(new_b))
+            if norm >= self.add_tol:
+                factor = 1.0 / norm
+                new_a.scale(factor)
+                new_b.scale(factor)
+                X.append([new_a, new_b])
+        return X, len(X)
+
+    def vector_norm(self, v):
+        norm = np.sqrt(v[0].vector_dot(v[0]))
+        norm += np.sqrt(v[1].vector_dot(v[1]))
+
+    def residual(self, V, x, ss_vector, ss_value):
+        ret = self.error(x, ss_vector, -ss_value)
+        ret[0].axpy(1.0, V[0])
+        ret[1].axpy(1.0, V[1])
+        return ret
+
+    def subspace_project(self, X, Ax):
+        A = np.zeros((len(X), len(X)))
+        for i, xi in enumerate(X):
+            for j, Axj in enumerate(Ax):
+                A[i, j] = xi[0].vector_dot(Axj[0])
+                A[i, j] += xi[1].vector_dot(Axj[1])
+        return A
