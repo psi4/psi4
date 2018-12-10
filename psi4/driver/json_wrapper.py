2d1937f6737f1c69cdaa9144d8e0da02d9fbf9ba
Daniel Smith <malorian@me.com>
2018-12-09 20:34:06 -0500

    JSON: Deprecates v1.1 json wrapper

--- psi4/driver/json_wrapper.py
+++ psi4/driver/json_wrapper.py
@@ -129,13 +129,16 @@ def _convert_variables(data, include=None):
 
     return ret
 
+
 ## Execution functions
+
 
 def _clean_psi_environ(do_clean):
     if do_clean:
         psi4.core.clean_variables()
         psi4.core.clean_options()
         psi4.core.clean()
+
 
 def run_json(json_data, clean=True):
 
@@ -152,7 +155,6 @@ def run_json(json_data, clean=True):
     if "memory" in json_data:
         psi4.set_memory(json_data["memory"])
 
-
     # Do we return the output?
     return_output = json_data.pop("return_output", False)
     if return_output:
@@ -164,12 +166,8 @@ def run_json(json_data, clean=True):
 
     # Attempt to run the computer
     try:
-        if json_data.get("schema_name", "").startswith("qc_schema"):
-            # qc_schema should be copied
-            json_data = run_json_qc_schema(copy.deepcopy(json_data), clean)
-        else:
-            # Original run updates inplace
-            run_json_original_v1_1(json_data, clean)
+        # qc_schema should be copied
+        json_data = run_json_qc_schema(copy.deepcopy(json_data), clean)
 
     except Exception as error:
         json_data["error"] = repr(error)
@@ -226,7 +224,6 @@ def run_json_qc_schema(json_data, clean):
     # Update molecule geometry as we orient and fix_com
     json_data["molecule"]["geometry"] = mol.geometry().np.ravel().tolist()
 
-
     # Set options
     for k, v in json_data["keywords"].items():
         core.set_global_option(k, v)
@@ -246,7 +243,6 @@ def run_json_qc_schema(json_data, clean):
                 raise KeyError("Did not understand property key %s." % kwargs["properties"])
         else:
             kwargs["properties"] = list(can_do_properties_)
-
 
     # Actual driver run
     val, wfn = methods_dict_[json_data["driver"]](method, **kwargs)
@@ -280,7 +276,6 @@ def run_json_qc_schema(json_data, clean):
     else:
         raise KeyError("Did not understand Driver key %s." % json_data["driver"])
 
-
     props = {
         "calcinfo_nbasis": wfn.nso(),
         "calcinfo_nmo": wfn.nmo(),
@@ -302,178 +297,5 @@ def run_json_qc_schema(json_data, clean):
     _clean_psi_environ(clean)
 
     json_data["schema_name"] = "qc_schema_output"
-
-    return json_data
-
-
-def run_json_original_v1_1(json_data, clean):
-    """
-    Runs and updates the input JSON data.
-
-    This was a trial specification introduced in Psi4 v1.1. This will be deprecated in Psi4 v1.3 in favour of the QC JSON format
-    found here: http://molssi-qc-schema.readthedocs.io/en/latest/index.html#
-
-    Parameters
-    ----------
-    json_data : JSON input
-        Required input fields:
-            - molecule : str
-                A string representation of the molecule. Any valid psi4 synatx is valid.
-            - driver : str
-                The driver method to use valid arguments are "energy", "gradient", "property"
-            - args : str
-                Input arguments to the driver function
-            - kwargs : dict
-                Input dictionary to the driver function
-
-        Optional input fields:
-            - kwargs : dict
-                Additional kwargs to be passed to the driver.
-            - options : dict
-                Global options to set in the Psi4 run.
-            - scratch_location : str
-                Overide the default scratch location.
-            - return_output : bool
-                Return the full string reprsentation of the output or not.
-
-        Output fields:
-            - return_value : float, psi4.core.Vector, psi4.core.Matrix
-                The return value of the input function.
-            - variables : dict
-                The list of Psi4 variables generated from the run.
-            - success : bool
-                Indicates if the run was successful or not.
-            - error : str
-                If an error is raised the result is returned here.
-            - raw_output : str
-                The full Psi4 output if requested.
-
-
-    Notes
-    -----
-    !Warning! This function is experimental and likely to change in the future.
-    Please report any suggestions or uses of this function on github.com/psi4/psi4.
-
-    Examples
-    --------
-
-    # CP corrected Helium dimer energy in the STO-3G basis.
-    >>> json_data = {}
-
-    >>> json_data["molecule"] = "He 0 0 0\n--\nHe 0 0 1"
-    >>> json_data["driver"] = "energy"
-    >>> json_data["method"] = 'SCF'
-    >>> json_data["kwargs"] = {"bsse_type": "cp"}
-    >>> json_data["options"] = {"BASIS": "STO-3G"}
-
-    >>> run_json(json_data)
-    {
-        "raw_output": "Output storing was not requested.",
-        "options": {
-            "BASIS": "STO-3G"
-        },
-        "driver": "energy",
-        "molecule": "He 0 0 0\n--\nHe 0 0 1",
-        "method": "SCF",
-        "variables": {
-            "SCF N ITERS": 2.0,
-            "SCF DIPOLE Y": 0.0,
-            "CURRENT DIPOLE Y": 0.0,
-            "CP-CORRECTED 2-BODY INTERACTION ENERGY": 0.1839360538612116,
-            "HF TOTAL ENERGY": -5.433191881443323,
-            "SCF TOTAL ENERGY": -5.433191881443323,
-            "TWO-ELECTRON ENERGY": 4.124089347186247,
-            "SCF ITERATION ENERGY": -5.433191881443323,
-            "CURRENT DIPOLE X": 0.0,
-            "CURRENT DIPOLE Z": 0.0,
-            "CURRENT REFERENCE ENERGY": -5.433191881443323,
-            "CURRENT ENERGY": 0.1839360538612116,
-            "COUNTERPOISE CORRECTED TOTAL ENERGY": -5.433191881443323,
-            "SCF DIPOLE Z": 0.0,
-            "COUNTERPOISE CORRECTED INTERACTION ENERGY": 0.1839360538612116,
-            "NUCLEAR REPULSION ENERGY": 2.11670883436,
-            "SCF DIPOLE X": 0.0,
-            "ONE-ELECTRON ENERGY": -11.67399006298957
-        },
-        "return_value": 0.1839360538612116,
-        "error": "",
-        "success": true,
-        "provenance": {
-            "creator": "Psi4",
-            "routine": "psi4.run_json",
-            "version": "1.1a1"
-        },
-        "kwargs": {
-            "bsse_type": "cp"
-        }
-    }
-    """
-
-    # Clean a few things
-    _clean_psi_environ(clean)
-
-    # Set a few variables
-    json_data["error"] = ""
-    json_data["success"] = False
-    json_data["raw_output"] = None
-    json_data[
-        "warning"] = "Warning! This format will be deprecated in Psi4 v1.3. Please switch the standard QC JSON format."
-
-    # Add the provenance data
-    prov = {}
-    prov["version"] = psi4.__version__
-    prov["routine"] = "psi4.run_json"
-    prov["creator"] = "Psi4"
-    json_data["provenance"] = prov
-
-    # Check input
-    for check in ["driver", "method", "molecule"]:
-        if check not in list(json_data):
-            json_data["error"] = "Minimum input requires the %s field." % check
-            return json_data
-
-    # Check driver call
-    if json_data["driver"] not in list(methods_dict_):
-        json_data["error"] = "Driver parameters '%s' not recognized" % str(json_data["driver"])
-        return json_data
-
-    # Set options
-    if "options" in json_data.keys() and (json_data["options"] is not None):
-        for k, v in json_data["options"].items():
-            core.set_global_option(k, v)
-
-    # Rework args
-    args = json_data["method"]
-    if not isinstance(args, (list, tuple)):
-        args = [args]
-
-    # Deep copy kwargs
-    if "kwargs" in list(json_data):
-        kwargs = copy.deepcopy(json_data["kwargs"])
-    else:
-        kwargs = {}
-
-    kwargs["molecule"] = molutil.geometry(json_data["molecule"])
-
-    # Full driver call
-    kwargs["return_wfn"] = True
-
-    if json_data["driver"] not in methods_dict_:
-        raise KeyError("Driver type '%s' not recognized." % str(json_data["driver"]))
-
-    val, wfn = methods_dict_[json_data["driver"]](*args, **kwargs)
-
-    if isinstance(val, (float, int)):
-        json_data["return_value"] = val
-    elif isinstance(val, (core.Matrix, core.Vector)):
-        json_data["return_value"] = val.to_serial()
-    else:
-        raise TypeError("Unrecognized return value of type %s\n" % type(val))
-
-    json_data["variables"] = core.scalar_variables()
-    json_data["success"] = True
-
-    # Reset state
-    _clean_psi_environ(clean)
 
     return json_data
