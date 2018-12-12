bd18f5f3140cf7084cce311e581d4f31744b4092
alenaizan <alenaizana@gmail.com>
2018-12-12 17:35:03 -0500

    clean nbody/cbs test

--- tests/python/tasks/test_nbody_cbs.py
+++ tests/python/tasks/test_nbody_cbs.py
@@ -9,7 +9,6 @@ from psi4.driver.driver_cbs import CBSComputer
 molecule = psi4.geometry("H 0 0 -2\nH 0 0 -3\n--\nH 0 0 2\nH 0 0 3")
 
 single = {
-#    "cbs_metadata": [{"wfn": "mp2", "basis": "cc-pV[D,T]Z"}],
     "cbs_metadata": [{'wfn': 'mp2', 'basis': 'cc-pV[D,T]Z'}],
     "driver": "energy",
     "keywords": {}
