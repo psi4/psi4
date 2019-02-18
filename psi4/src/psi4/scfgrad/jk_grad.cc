8fb5be186f1ad9d90cabdef067b65efef7754434
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2019-02-18 22:31:17 +0200

    Default cutoffs should be same
    The default gradient and Hessian screening cutoffs should be the same as for the integrals used in SCF (#1540).

--- psi4/src/psi4/scfgrad/jk_grad.cc
+++ psi4/src/psi4/scfgrad/jk_grad.cc
@@ -115,7 +115,7 @@ void JKGrad::common_init()
     omp_num_threads_ = Process::environment.get_n_threads();
 #endif
 
-    cutoff_ = 0.0;
+    cutoff_ = 1E-12;
 
     do_J_ = true;
     do_K_ = true;
