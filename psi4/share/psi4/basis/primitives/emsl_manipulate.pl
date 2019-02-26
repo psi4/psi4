edd36d493529cc11268fe90427364b3366421a7a
Lori A. Burns <lori.burns@gmail.com>
2019-02-25 19:00:54 -0500

    update emsl_manip and clean for Na/Mg in basis +d regime

--- psi4/share/psi4/basis/primitives/emsl_manipulate.pl
+++ psi4/share/psi4/basis/primitives/emsl_manipulate.pl
@@ -76,7 +76,7 @@ print GBS_OUT "!\n\n\n\n****\n";
 @HELEM = (H,He,Li,Be,B,C,N,O,F,Ne,Na,Mg,Al,Si,P,S,Cl,Ar,K,Ca,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Ga,Ge,As,Se,Br,Kr);
 @HLITE = (H,He);
 #@HHEAV =      (Li,Be,B,C,N,O,F,Ne,Na,Mg,Al,Si,P,S,Cl,Ar,K,Ca,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Ga,Ge,As,Se,Br,Kr);
-@HALAR =                               (Al,Si,P,S,Cl,Ar);
+@HALAR =                         (Na,Mg,Al,Si,P,S,Cl,Ar);
 
 
 # merge basis set info
