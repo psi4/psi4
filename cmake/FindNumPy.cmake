4fe48c473738bbc37b5935b11c7001746b86be7e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-04-10 16:01:43 -0400

    Use FindPythonModule to find NumPy, create target

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -1,16 +1,6 @@
 # - Find the NumPy libraries
-# This module finds if NumPy is installed, and sets the following variables
-# indicating where it is.
-#
-# TODO: Update to provide the libraries and paths for linking npymath lib.
-#
-#  NUMPY_FOUND               - was NumPy found
-#  NUMPY_VERSION             - the version of NumPy found as a string
-#  NUMPY_VERSION_MAJOR       - the major version number of NumPy
-#  NUMPY_VERSION_MINOR       - the minor version number of NumPy
-#  NUMPY_VERSION_PATCH       - the patch version number of NumPy
-#  NUMPY_VERSION_DECIMAL     - e.g. version 1.6.1 is 10601
-#  NUMPY_INCLUDE_DIRS        - path to the NumPy include files
+# This module finds if NumPy is installed, and creates an interface library.
+# For the moment the interface library only carries include directories.
 
 #============================================================================
 # Copyright 2012 Continuum Analytics, Inc.
@@ -38,52 +28,53 @@
 #
 #============================================================================
 
+include(FindPythonModule)
+
 # Finding NumPy involves calling the Python interpreter
 if(NumPy_FIND_REQUIRED)
-    find_package(PythonInterp REQUIRED)
+    find_python_module(numpy REQUIRED QUIET)
 else()
-    find_package(PythonInterp)
+    find_python_module(numpy QUIET)
 endif()
 
-if(NOT PYTHONINTERP_FOUND)
-    set(NUMPY_FOUND FALSE)
+# Get include directories and prepare a target
+if(numpy_FOUND)
+  execute_process(
+    COMMAND
+      "${PYTHON_EXECUTABLE}" "-c"
+      "import numpy as n; print(n.__version__); print(n.get_include());"
+    RESULT_VARIABLE
+      _NUMPY_SEARCH_SUCCESS
+    OUTPUT_VARIABLE
+      _NUMPY_VALUES
+    ERROR_VARIABLE
+      _NUMPY_ERROR_VALUE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+  # Convert the process output into a list
+  string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
+  string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
+  list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
+  list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
+
+  # Make sure all directory separators are '/'
+  string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
+
+  # Get the major and minor version numbers
+  string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
+  list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
+  list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
+  list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
+  string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
+  math(EXPR NUMPY_VERSION_DECIMAL
+      "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+
+  add_library(NumPy INTERFACE)
+  target_include_directories(NumPy
+    INTERFACE
+      ${NUMPY_INCLUDE_DIRS}
+    )
+
+  set(NUMPY_FOUND TRUE)
 endif()
-
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
-    "import numpy as n; print(n.__version__); print(n.get_include());"
-    RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-    OUTPUT_VARIABLE _NUMPY_VALUES
-    ERROR_VARIABLE _NUMPY_ERROR_VALUE
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
-    if(NumPy_FIND_REQUIRED)
-        message(FATAL_ERROR
-            "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
-    endif()
-    set(NUMPY_FOUND FALSE)
-endif()
-
-# Convert the process output into a list
-string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
-string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
-list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
-list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
-
-# Make sure all directory separators are '/'
-string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
-
-# Get the major and minor version numbers
-string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
-list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
-list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
-list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
-string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
-math(EXPR NUMPY_VERSION_DECIMAL
-    "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
-
-find_package_message(NUMPY
-    "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-    "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
-
-set(NUMPY_FOUND TRUE)
