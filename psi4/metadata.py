ecbda834b4bcce794ce2338873962f36436842a5
Lori A. Burns <lori.burns@gmail.com>
2019-05-15 11:05:49 -0400

    v1.3.2

--- psi4/metadata.py
+++ psi4/metadata.py
@@ -1,5 +1,5 @@
-__version__ = '1.3.1'
-__version_long = '1.3.1+zzzzzzz'
+__version__ = '1.3.2'
+__version_long = '1.3.2+zzzzzzz'
 __version_upcoming_annotated_v_tag = '1.4a1'
 
 def version_formatter(dummy):
