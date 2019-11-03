94a76d9ca436ec3e1ff84d723413bded8d0e6bb4
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-11-03 13:37:40 +0100

    Use Ninja on Travis
    Install the Kitware-maintained version to have Fortran support:
    https://github.com/Kitware/ninja

--- devtools/scripts/ci_build.sh
+++ devtools/scripts/ci_build.sh
@@ -2,7 +2,8 @@
 
 # The return code will capture an error from ANY of the functions in the pipe
 set -o pipefail
-cmake --build . -- -j 2 VERBOSE=1 2>&1 | tee build.log | grep "Building"
+export NINJA_STATUS="[Built edge %f of %t in %e sec] "
+cmake --build . -- -j 2 -v -d stats 2>&1 | tee build.log | grep "Built"
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
