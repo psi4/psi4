2a0e759f6c7ffa117c1b1105f8d950c10aed7e0c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-11-16 11:14:31 +0100

    More fixes to get Ninja to work

--- devtools/scripts/ninja.sh
+++ devtools/scripts/ninja.sh
@@ -5,8 +5,10 @@ set -eu -o pipefail
 echo "-- Installing Ninja"
 Ninja_VERSION="1.9.0.g99df1.kitware.dyndep-1.jobserver-1"
 Ninja_URL="https://github.com/Kitware/ninja/releases/download/v${Ninja_VERSION}/ninja-${Ninja_VERSION}_x86_64-linux-gnu.tar.gz"
-cd "$HOME"/Deps
-mkdir -p ninja
-curl -Ls $Ninja_URL | tar -xz -C ninja --strip-components=1
-cd "$TRAVIS_BUILD_DIR"
+mkdir -p "$HOME"/Deps
+(
+    cd "$HOME"/Deps || exit
+    mkdir -p ninja
+    curl -Ls $Ninja_URL | tar -xz -C ninja --strip-components=1
+)
 echo "-- Done with Ninja"
