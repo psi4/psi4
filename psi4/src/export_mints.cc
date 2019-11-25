2967907bfb091e60ba6b5c267b010d17499ab89e
Francesco Evangelista <francesco.evangelista@gmail.com>
2019-11-25 12:14:01 -0500

    Add default value to python interface

--- psi4/src/export_mints.cc
+++ psi4/src/export_mints.cc
@@ -661,7 +661,7 @@ void export_mints(py::module& m) {
              "Ignore reference contributions to the gradient? Default is False", "val"_a = false)
         .def("set_deriv_density_backtransformed", &Deriv::set_deriv_density_backtransformed,
              "Is the deriv_density already backtransformed? Default is False", "val"_a = false)
-        .def("compute", &Deriv::compute, "Compute the gradient");
+        .def("compute", &Deriv::compute, "Compute the gradient", "deriv_calc_type"_a = DerivCalcType::Default);
 
     typedef SharedMatrix (MatrixFactory::*create_shared_matrix)() const;
     typedef SharedMatrix (MatrixFactory::*create_shared_matrix_name)(const std::string&) const;
