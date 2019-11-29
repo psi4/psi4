d7f93f288ca5f368ff9994f8c86e28c116793063
Lori A. Burns <lori.burns@gmail.com>
2019-11-28 21:18:22 -0500

    l2: poke to try windows azure

--- cmake/psi4OptionsTools.cmake
+++ cmake/psi4OptionsTools.cmake
@@ -61,7 +61,6 @@ macro(psi4_add_module binlib libname sources)
   target_link_libraries(${libname}
     PRIVATE
       tgt::lapack
-      Libint2::cxx
     )
 
   # library modules get their headers installed
