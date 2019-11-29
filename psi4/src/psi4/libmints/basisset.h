08900efbd47f24348bff23e8229a273df9355ef7
Lori A. Burns <lori.burns@gmail.com>
2019-11-29 09:50:35 -0500

    l2: poke to try windows azure

--- psi4/src/psi4/libmints/basisset.h
+++ psi4/src/psi4/libmints/basisset.h
@@ -29,6 +29,9 @@
 #ifndef _psi_src_lib_libmints_basisset_h_
 #define _psi_src_lib_libmints_basisset_h_
 
+#ifdef _MSC_VER
+#include <libint2/shell.h>
+#endif
 #include "gshell.h"
 
 #include "psi4/pragma.h"
