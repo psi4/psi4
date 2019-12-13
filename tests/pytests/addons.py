9a646796972a6b69f0102d696c3ca7a978c99df6
Lori A. Burns <lori.burns@gmail.com>
2019-12-13 15:17:30 -0500

    Fix CI (#1767)
    * l2: adjust using after qcng 190
    
    Conflicts:
    	external/upstream/libint2/CMakeLists.txt
    
    * bump qcel to 0.12, try spot check on win
    
    * azure poke
    
    * azure poke

--- tests/pytests/addons.py
+++ tests/pytests/addons.py
@@ -1,7 +1,20 @@
-import subprocess
-
 import pytest
+
 from qcelemental.util import parse_version, which, which_import
+from qcengine.testing import using
+
+__all__ = [
+    'hardware_nvidia_gpu',
+    'using_cppe',
+    'using_dftd3',
+    'using_dftd3_321',
+    'using_gcp',
+    'using_mp2d',
+    'using_memory_profiler',
+    'using_networkx',
+    'using_psi4',
+    'using_qcdb',
+]
 
 
 def is_psi4_new_enough(version_feature_introduced):
@@ -9,24 +22,6 @@ def is_psi4_new_enough(version_feature_introduced):
         return False
     import psi4
     return parse_version(psi4.__version__) >= parse_version(version_feature_introduced)
-
-
-def is_numpy_new_enough(version_feature_introduced):
-    if not which_import('numpy', return_bool=True):
-        return False
-    import numpy
-    return parse_version(numpy.version.version) >= parse_version(version_feature_introduced)
-
-
-def is_dftd3_new_enough(version_feature_introduced):
-    if not which('dftd3', return_bool=True):
-        return False
-    # Note: anything below v3.2.1 will return the help menu here. but that's fine as version compare evals to False.
-    command = [which('dftd3'), '-version']
-    proc = subprocess.run(command, stdout=subprocess.PIPE)
-    candidate_version = proc.stdout.decode('utf-8').strip()
-
-    return parse_version(candidate_version) >= parse_version(version_feature_introduced)
 
 
 def is_nvidia_gpu_present():
@@ -50,7 +45,7 @@ def is_nvidia_gpu_present():
             return ngpu > 0
 
 
-hardware_nvidia_gpu = pytest.mark.skipif(is_nvidia_gpu_present() is False,
+hardware_nvidia_gpu = pytest.mark.skipif(True, #is_nvidia_gpu_present() is False,
                                          reason='Psi4 not detecting Nvidia GPU via `nvidia-smi`. Install one')
 
 using_memory_profiler = pytest.mark.skipif(
@@ -63,28 +58,18 @@ using_psi4 = pytest.mark.skipif(
 using_qcdb = pytest.mark.skipif(
     True, reason='Not detecting common driver. Install package if necessary and add to envvar PYTHONPATH')
 
-using_dftd3 = pytest.mark.skipif(
-    which('dftd3', return_bool=True) is False,
-    reason='Not detecting executable dftd3. Install package if necessary and add to envvar PATH or PSIPATH')
-
-using_dftd3_321 = pytest.mark.skipif(is_dftd3_new_enough("3.2.1") is False,
-                                     reason='DFTD3 does not include 3.2.1 features. Update package and add to PATH')
-
 using_gcp = pytest.mark.skipif(
     which("gcp", return_bool=True) is False,
     reason="Not detecting executable gcp. Install package if necessary and add to envvar PATH")
-
-using_mp2d = pytest.mark.skipif(
-    which('mp2d', return_bool=True) is False,
-    reason='Not detecting executable mp2d. Install package if necessary and add to envvar PATH')
 
 using_cppe = pytest.mark.skipif(
     which_import('cppe', return_bool=True) is False,
     reason="Not detecting module cppe. Rebuild with -DENABLE_cppe")
 
-#using_psi4_libxc = pytest.mark.skipif(is_psi4_new_enough("1.2a1.dev100") is False,
-#                                reason="Psi4 does not include DFT rewrite to use Libxc. Update to development head")
-
 using_networkx = pytest.mark.skipif(
     which_import('networkx', return_bool=True) is False,
     reason='Not detecting module networkx. Install package if necessary and add to envvar PYTHONPATH')
+
+using_dftd3 = using('dftd3')
+using_dftd3_321 = using('dftd3')
+using_mp2d = using('mp2d')
