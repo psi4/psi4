cc033e94eed1888dc6188121424334c5b4f058e9
Francesco Evangelista <francesco.evangelista@gmail.com>
2019-12-18 09:45:31 -0500

    Add missing header

--- psi4/src/psi4/occ/get_moinfo.cc
+++ psi4/src/psi4/occ/get_moinfo.cc
@@ -36,6 +36,7 @@
 #include "psi4/libqt/qt.h"
 #include "psi4/libmints/matrix.h"
 #include "psi4/libmints/molecule.h"
+#include "psi4/libmints/mintshelper.h"
 #include "occwave.h"
 
 using namespace psi;
