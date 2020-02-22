95ecb5b170053c2410f1f1a08749d35cecafe2f8
Susi Lehtola <susi.lehtola@gmail.com>
2020-02-22 06:54:15 +0200

    Add consistency check to Cholesky orthogonalization code

--- psi4/src/psi4/libmints/orthog.cc
+++ psi4/src/psi4/libmints/orthog.cc
@@ -247,6 +247,18 @@ std::vector<std::vector<int>> BasisSetOrthogonalization::sort_indices() const {
 void BasisSetOrthogonalization::compute_partial_cholesky_orthog() {
     // Original dimensions
     const Dimension& nbf = normalized_overlap_->rowspi();
+    // Make sure we got passed the right size data
+    if (rsq_->nirrep() != normalized_overlap_->nirrep()) {
+        throw PSIEXCEPTION("BasisSetOrthogonalization::compute_partial_cholesky_orthog: incorrect size of rsq vector.");
+    }
+    for (int h = 0; h < rsq_->nirrep(); h++) {
+        if (rsq_->dim(h) != normalized_overlap_->rowspi(h)) {
+            std::ostringstream oss;
+            oss << "BasisSetOrthogonalization::compute_partial_cholesky_orthog: incorrect size of symmetry " << h
+                << " of rsq vector.";
+            throw PSIEXCEPTION(oss.str());
+        }
+    }
 
     // Order basis functions from tight to diffuse
     std::vector<std::vector<int>> order = sort_indices();
