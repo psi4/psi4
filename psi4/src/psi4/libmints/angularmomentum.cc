6a3b77e6b0fdd50e222779178d04ed7d5b9f9f24
Kirk C Pearce <kcpearce@vt.edu>
2020-03-25 15:31:23 -0400

    Derivative of Lx wrt Bz

--- psi4/src/psi4/libmints/angularmomentum.cc
+++ psi4/src/psi4/libmints/angularmomentum.cc
@@ -687,6 +687,62 @@ void AngularMomentumInt::compute_pair_deriv1(const GaussianShell& s1, const Gaus
                             buffer_[ao12+xbydisp] += 1.0 * m2 * (2.0 * a2 * (v1 + (A[2] - C[2]) * v2) - m2 * (v3 + (A[2] - C[2]) * v4)) * over_pf;
 
                             //
+                            // Bz derivatives with Lx
+                            //
+
+                            v1 = v2 = v3 = v4 = v5 = 0.0;
+                            // (a+1_y|Lx|b+1_z+1_z)
+                            v1 = x[l1][l2] * y[m1+1][m2] * z[n1][n2+2];
+                            // (a|Lx|b+1_z+1_z)
+                            v2 = x[l1][l2] * y[m1][m2] * z[n1][n2+2];
+                            // (a+1_y|Lx|b+1_z-1_z)
+                            v3 = x[l1][l2] * y[m1+1][m2] * z[n1][n2];
+                            // (a|Lx|b+1_z-1_z)
+                            v4 = x[l1][l2] * y[m1][m2] * z[n1][n2];
+                            buffer_[ao12+xbzdisp] += 2.0 * a2 * (2.0 * a2 * (v1 + (A[1] - C[1]) * v2) - n2 * (v3 + (A[1] - C[1]) * v4)) * over_pf;
+
+                            v1 = v2 = v3 = v4 = v5 = 0.0;
+                            // (a+1_y|Lx|b-1_z+1_z)
+                            v1 = x[l1][l2] * y[m1+1][m2] * z[n1][n2];
+                            // (a|Lx|b-1_z+1_z)
+                            v2 = x[l1][l2] * y[m1][m2] * z[n1][n2];
+                            if (n2 || n2-1) {
+                                // (a+1_y|Lx|b-1_z-1_z)
+                                v3 = x[l1][l2] * y[m1+1][m2] * z[n1][n2-2];
+                                // (a|Lx|b-1_z-1_z)
+                                v4 = x[l1][l2] * y[m1][m2] * z[n1][n2-2];
+                            }
+                            buffer_[ao12+xbzdisp] += -1.0 * n2 * (2.0 * a2 * (v1 + (A[1] - C[1]) * v2) - n2 * (v3 + (A[1] - C[1]) * v4)) * over_pf;
+
+                            v1 = v2 = v3 = v4 = v5 = 0.0;
+                            // (a+1_z|Lx|b+1_y+1_z)
+                            v1 = x[l1][l2] * y[m1][m2+1] * z[n1+1][n2+1];
+                            // (a|Lx|b+1_y+1_z)
+                            v2 = x[l1][l2] * y[m1][m2+1] * z[n1][n2+1];
+                            if (n2) {
+                                // (a+1_z|Lx|b+1_y-1_z)
+                                v3 = x[l1][l2] * y[m1][m2+1] * z[n1+1][n2-1];
+                                // (a|Lx|b+1_y-1_z)
+                                v4 = x[l1][l2] * y[m1][m2+1] * z[n1][n2-1];
+                            }
+                            buffer_[ao12+xbzdisp] += -2.0 * a2 * (2.0 * a2 * (v1 + (A[2] - C[2]) * v2) - n2 * (v3 + (A[2] - C[2]) * v4)) * over_pf;
+
+                            v1 = v2 = v3 = v4 = v5 = 0.0;
+                            if (m2) {
+                                // (a+1_z|Lx|b-1_y+1_z)
+                                v1 = x[l1][l2] * y[m1][m2-1] * z[n1+1][n2+1];
+                                // (a|Lx|b-1_y+1_z)
+                                v2 = x[l1][l2] * y[m1][m2-1] * z[n1][n2+1];
+                                if (n2) {
+                                    // (a+1_z|Lx|b-1_y-1_z)
+                                    v3 = x[l1][l2] * y[m1][m2-1] * z[n1+1][n2-1];
+                                    // (a|Lx|b-1_y-1_z)
+                                    v4 = x[l1][l2] * y[m1][m2-1] * z[n1][n2-1];
+                                }
+                            }
+                            buffer_[ao12+xbzdisp] += 1.0 * m2 * (2.0 * a2 * (v1 + (A[2] - C[2]) * v2) - n2 * (v3 + (A[2] - C[2]) * v4)) * over_pf;
+
+                            //
                             // Ax derivatives with Ly
                             //
 
