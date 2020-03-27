2ab7a1d49a54b3fcfd6c912bebedf8f8dcf6108e
Kirk C Pearce <kcpearce@vt.edu>
2020-03-27 11:16:30 -0400

    adds helper function

--- psi4/src/psi4/libmints/mintshelper.cc
+++ psi4/src/psi4/libmints/mintshelper.cc
@@ -2823,6 +2823,61 @@ std::vector<SharedMatrix> MintsHelper::ao_elec_dip_deriv1_helper(int atom) {
     return grad;
 }
 
+/* 1st derivatives of magnetic dipole integrals in the AO basis */
+std::vector<SharedMatrix> MintsHelper::ao_mag_dip_deriv1_helper(int atom) {
+    std::vector<std::string> cartcomp{ "X", "Y", "Z" };
+
+    std::shared_ptr<OneBodyAOInt> Dint(integral_->ao_angular_momentum(1));
+
+    std::shared_ptr<BasisSet> bs1 = Dint->basis1();
+    std::shared_ptr<BasisSet> bs2 = Dint->basis2();
+
+    int nbf1 = bs1->nbf();
+    int nbf2 = bs2->nbf();
+
+    std::vector<SharedMatrix> grad;
+    for (int p = 0; p < 3; p++) {
+        std::stringstream sstream;
+        sstream << "ao_mag_dip" << cartcomp[p] << "_deriv1_";
+        for (int q = 0; q < 3; q++) {
+            sstream << atom << cartcomp[q];
+            grad.push_back(SharedMatrix(new Matrix(sstream.str(), nbf1, nbf2)));
+        }
+    }
+
+    const double *buffer = Dint->buffer();
+
+    for (int P = 0; P < bs1->nshell(); P++) {
+        for (int Q = 0; Q < bs2->nshell(); Q++) {
+            int nP = basisset_->shell(P).nfunction();
+            int oP = basisset_->shell(P).function_index();
+            int aP = basisset_->shell(P).ncenter();
+
+            int nQ = basisset_->shell(Q).nfunction();
+            int oQ = basisset_->shell(Q).function_index();
+            int aQ = basisset_->shell(Q).ncenter();
+
+            if (aP != atom && aQ != atom) continue;
+
+            Dint->compute_shell_deriv1(P, Q);
+
+            for (int mu_cart = 0; mu_cart < 3; mu_cart++) {
+                for (int atom_cart = 0; atom_cart < 3; atom_cart++) {
+                    int offset = (6 * mu_cart * nP * nQ) + (atom_cart * nP * nQ) + (3 * (atom == aQ) * nP * nQ);
+                    for (int p = 0; p < nP; p++) {
+                        for (int q = 0; q < nQ; q++) {
+                            if (aP == aQ) grad[3 * mu_cart + atom_cart]->add(p + oP, q + oQ, buffer[p * nQ + q + offset - (3 * nP * nQ)]);
+                            grad[3 * mu_cart + atom_cart]->add(p + oP, q + oQ, buffer[p * nQ + q + offset]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return grad;
+}
+
 /* 1st and 2nd derivatives of TEI in AO basis  */
 
 std::vector<SharedMatrix> MintsHelper::ao_tei_deriv1(int atom, double omega,
