961004f4af273f48ead9a33d8c3ede2d57a61132
yxie326 <yxie326@gatech.edu>
2020-07-15 13:16:34 -0400

    Update psi4/src/read_options.cc
    SAPT(DFT) exch-disp scheme docstring
    
    Co-authored-by: Lori A. Burns <lori.burns@gmail.com>

--- psi4/src/read_options.cc
+++ psi4/src/read_options.cc
@@ -1022,10 +1022,10 @@ int read_options(const std::string &name, Options &options, bool suppress_printi
         options.add_bool("SAPT_DFT_DO_DHF", true);
         /*- Enables the hybrid xc kernel in dispersion? !expert -*/
         options.add_bool("SAPT_DFT_DO_HYBRID", true);
-        /*- Scheme of exchange-dispersion? !expert -*/
-        /* "NONE" = Do not scale, use Exch-Disp2,u */
-        /* "FIXED" = Use a fixed factor to scale Exch-Disp2,u */
-        /* "NONE" = Use the ratio of Disp20,r and Disp2,u to scale Exch-Disp2,u */
+        /*- Scheme for approximating exchange-dispersion for SAPT-DFT.
+        ``NONE`` Use unscaled ``Exch-Disp2,u`` .
+        ``FIXED`` Use a fixed factor |sapt__sapt_dft_exch_disp_fixed_scale| to scale ``Exch-Disp2,u`` .
+        ``DISP`` Use the ratio of ``Disp2,r`` and ``Disp2,u`` to scale ``Exch-Disp2,u`` . -*/
         options.add_str("SAPT_DFT_EXCH_DISP_SCALE_SCHEME", "DISP", "NONE FIXED DISP");
         /*- Exch-disp scaling factor for FIXED scheme !expert -*/
         options.add_double("SAPT_DFT_EXCH_DISP_FIXED_SCALE", 0.686);
