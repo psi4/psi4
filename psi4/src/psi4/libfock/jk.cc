424d724dcb26a9026f150b28c368197375874761
Andy Simmonett <andy.simmonett@gmail.com>
2020-09-10 21:02:08 -0400

    Remove unused headers from libfock

--- psi4/src/psi4/libfock/jk.cc
+++ psi4/src/psi4/libfock/jk.cc
@@ -35,7 +35,6 @@
 #include "psi4/libqt/qt.h"
 #include "psi4/psi4-dec.h"
 #include "psi4/psifiles.h"
-#include "psi4/libmints/sieve.h"
 #include "psi4/libiwl/iwl.hpp"
 #include "psi4/libmints/matrix.h"
 #include "psi4/libmints/basisset.h"
