bc6aba83983cffdca540b674616f08688e7baa72
Andy <ajiang10224@gmail.com>
2021-04-08 14:40:11 -0400

    Moved code to DirectJK from libscf_solver

--- psi4/src/psi4/libfock/jk.h
+++ psi4/src/psi4/libfock/jk.h
@@ -260,7 +260,10 @@ class PSI_API JK {
     /// Perform an incremental fock build?
     bool incr_fock_;
 
-    /// Perform an incremental fock build?
+    /// Perform Density Screening for ERIs?
+    bool dens_screen_;
+
+    /// Perform Incremental Fock Build for J and K Matrices?
     bool incr_fock_;
 
     /// Combine (pq|rs) and (pq|w|rs) integrals before contracting?
