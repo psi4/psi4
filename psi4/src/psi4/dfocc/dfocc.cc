f596d56c1a6a6f670bb2289fce7b5e729807449c
stefan <stefan.behnle@uni-tuebingen.de>
2021-06-11 19:27:52 +0200

    fixed a comment typo, added a clarification for myself

--- psi4/src/psi4/dfocc/dfocc.cc
+++ psi4/src/psi4/dfocc/dfocc.cc
@@ -115,7 +115,7 @@ void DFOCC::common_init() {
     // title
     title();
 
-    //   Given default conjugate gradient convergence, set the criteria by what shoud
+    //   Given default conjugate gradient convergence, set the criteria by what should
     //   be necessary to achive the target energy convergence.
     //   This is based solely on standard suite testing to achieve 1e-6 E & G with default convcrit.
     if (options_["PCG_CONVERGENCE"].has_changed()) {
