33350a18c9b9fad9aecb4fbbd9b4c8f3217db9e2
Lori A. Burns <lori.burns@gmail.com>
2021-08-12 10:16:01 -0400

    Update oeprop.h

--- psi4/src/psi4/libmints/oeprop.h
+++ psi4/src/psi4/libmints/oeprop.h
@@ -319,7 +319,7 @@ class PopulationAnalysisCalc : public Prop {
     /// Compute Lowdin Charges
     std::tuple<SharedStdVector, SharedStdVector, SharedStdVector> compute_lowdin_charges(bool print_output = false);
     /// Compute MBIS Multipoles (doi:10.1021/acs.jctc.6b00456)
-    std::tuple<SharedMatrix, SharedMatrix, SharedMatrix, SharedMatrix> compute_mbis_multipoles(bool print_output = false);
+    std::tuple<SharedMatrix, SharedMatrix, SharedMatrix, SharedMatrix> compute_mbis_multipoles(bool free_atom_volumes = false, bool print_output = false);
     /// Compute Mayer Bond Indices (non-orthogoal basis)
     std::tuple<SharedMatrix, SharedMatrix, SharedMatrix, SharedVector> compute_mayer_indices(bool print_output = false);
     /// Compute Wiberg Bond Indices using Lowdin Orbitals (symmetrically orthogonal basis)
@@ -415,7 +415,7 @@ class PSI_API OEProp : public TaskListComputer {
     /// Compute Lowdin Charges
     void compute_lowdin_charges();
     /// Compute MBIS Multipoles (doi:10.1021/acs.jctc.6b00456)
-    void compute_mbis_multipoles();
+    void compute_mbis_multipoles(bool free_atom_volumes = false);
     /// Compute Mayer Bond Indices (non-orthogoal basis)
     void compute_mayer_indices();
     /// Compute Wiberg Bond Indices using Lowdin Orbitals (symmetrically orthogonal basis)
