824fee4511b5b4c45138b4859b91ef3a5595a477
Jonathon Misiewicz <jonathon.paul.misiewicz@emory.edu>
2021-12-15 07:53:32 -0500

    Whitespace

--- psi4/src/psi4/libdiis/diismanager.cc
+++ psi4/src/psi4/libdiis/diismanager.cc
@@ -41,20 +41,20 @@ namespace psi {
  */
 DIISManager::DIISManager(int maxSubspaceSize, const std::string &label, RemovalPolicy removalPolicy,
                          StoragePolicy storagePolicy) {
-          auto diis_file = py::module_::import("psi4").attr("driver").attr("diis");
-          py::object pyRemovalPolicy, pyStoragePolicy;
-         if (removalPolicy == RemovalPolicy::LargestError) {
-             pyRemovalPolicy = diis_file.attr("RemovalPolicy").attr("LargestError");
-         } else {
-             pyRemovalPolicy = diis_file.attr("RemovalPolicy").attr("OldestFirst");
-         }
-         if (storagePolicy == StoragePolicy::InCore) {
-             pyStoragePolicy = diis_file.attr("StoragePolicy").attr("InCore");
-         } else {
-             pyStoragePolicy = diis_file.attr("StoragePolicy").attr("OnDisk");
-         }
-         pydiis = diis_file.attr("DIIS")(maxSubspaceSize, label, pyRemovalPolicy, pyStoragePolicy);
-      }
+    auto diis_file = py::module_::import("psi4").attr("driver").attr("diis");
+    py::object pyRemovalPolicy, pyStoragePolicy;
+    if (removalPolicy == RemovalPolicy::LargestError) {
+        pyRemovalPolicy = diis_file.attr("RemovalPolicy").attr("LargestError");
+    } else {
+        pyRemovalPolicy = diis_file.attr("RemovalPolicy").attr("OldestFirst");
+    }
+    if (storagePolicy == StoragePolicy::InCore) {
+        pyStoragePolicy = diis_file.attr("StoragePolicy").attr("InCore");
+    } else {
+        pyStoragePolicy = diis_file.attr("StoragePolicy").attr("OnDisk");
+    }
+    pydiis = diis_file.attr("DIIS")(maxSubspaceSize, label, pyRemovalPolicy, pyStoragePolicy);
+}
 
 int DIISManager::subspace_size() { return py::len(pydiis.attr("stored_vectors")); }
 
