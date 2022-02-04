6141a691070c2c22aade1dd9e420679f522dfe53
Holger Kruse <mail2holger@gmail.com>
2022-02-04 15:48:08 +0100

    atomic quadrature grid blocking [part 1]  (#2336)
    * initial implementation of atom-wise blocking of quadrature points
    
    * review requests. And shrinking atomic_blocks to correct capacity after distant point removal

--- psi4/src/read_options.cc
+++ psi4/src/read_options.cc
@@ -1602,8 +1602,10 @@ int read_options(const std::string &name, Options &options, bool suppress_printi
         options.add_int("DFT_BLOCK_MIN_POINTS", 100);
         /*- The maximum radius to terminate subdivision of an octree block [au]. !expert -*/
         options.add_double("DFT_BLOCK_MAX_RADIUS", 3.0);
+        /*- Remove points from the quadrature grid that exceed the spatial extend of the basis functions. !expert -*/
+        options.add_bool("DFT_REMOVE_DISTANT_POINTS",true);
         /*- The blocking scheme for DFT. !expert -*/
-        options.add_str("DFT_BLOCK_SCHEME", "OCTREE", "NAIVE OCTREE");
+        options.add_str("DFT_BLOCK_SCHEME", "OCTREE", "NAIVE OCTREE ATOMIC");
         /*- Parameters defining the dispersion correction. See Table
         :ref:`-D Functionals <table:dft_disp>` for default values and Table
         :ref:`Dispersion Corrections <table:dashd>` for the order in which
