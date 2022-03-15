56c9a23ca2a143d651832249801c376547ac14d2
Connor Briggs <cgbriggs99@gmail.com>
2022-03-15 04:59:52 -0400

    Fixed an issue that caused tests to fail.

--- psi4/driver/schema_wrapper.py
+++ psi4/driver/schema_wrapper.py
@@ -588,12 +588,13 @@ def run_json_qcschema(json_data, clean, json_serialization, keep_wfn=False):
 
     # Add in handling of matrix arguments which need to be obtained by a
     # a function call.
-    if "tIjAb" in json_data["extras"]["psi4:arrays"]:
-        json_data["extras"]["psi4:arrays"]["tIjAb"] = wfn.get_amplitudes()["tIjAb"].to_array().tolist()
-    if "tIA" in json_data["extras"]["psi4:arrays"]:
-        json_data["extras"]["psi4:arrays"]["tIA"] = wfn.get_amplitudes()["tIA"].to_array().tolist()
-    if "Da" in json_data["extras"]["psi4:arrays"]:
-        json_data["extras"]["psi4:arrays"]["Da"] = wfn.Da().to_array().tolist()
+    if "psi4:arrays" in json_data["extras"] :
+        if "tIjAb" in json_data["extras"]["psi4:arrays"]:
+            json_data["extras"]["psi4:arrays"]["tIjAb"] = wfn.get_amplitudes()["tIjAb"].to_array().tolist()
+        if "tIA" in json_data["extras"]["psi4:arrays"]:
+            json_data["extras"]["psi4:arrays"]["tIA"] = wfn.get_amplitudes()["tIA"].to_array().tolist()
+        if "Da" in json_data["extras"]["psi4:arrays"]:
+            json_data["extras"]["psi4:arrays"]["Da"] = wfn.Da().to_array().tolist()
 
         
     # Handle the return result
