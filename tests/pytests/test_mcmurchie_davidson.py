5cac83737e13228445ca196ab918cc21b7849b88
Maximilian Scheurer <max.scheurer@me.com>
2022-03-17 18:59:57 +0100

    less over-engineered angmom tests

--- tests/pytests/test_mcmurchie_davidson.py
+++ tests/pytests/test_mcmurchie_davidson.py
@@ -7,7 +7,6 @@ import pytest
 import psi4
 from pathlib import Path
 import json
-from dataclasses import dataclass
 
 
 pytestmark = pytest.mark.quick
@@ -21,19 +20,7 @@ def reference_data():
     return reference_data
 
 
-@dataclass
-class IntegralInfo:
-    dictkey: str
-    function_name: str
-    extra_args: list = None
-
-
-integrals = [
-    IntegralInfo("L", "ao_angular_momentum"),
-]
-
-
-def test_mcmurchie_davidson_consistency(reference_data):
+def test_mcmurchie_davidson_consistency_angmom(reference_data):
     assert reference_data['version'] == '1.5'
     refdata = reference_data['data']
     for molname in refdata:
@@ -43,16 +30,9 @@ def test_mcmurchie_davidson_consistency(reference_data):
             ref = moldata[basis_name]
             basis = psi4.core.BasisSet.build(mol, 'orbital', basis_name)
             mints = psi4.core.MintsHelper(basis)
-            for integral in integrals:
-                shape = ref[f'{integral.dictkey}_shape']
-                integral_ref = np.array(ref[integral.dictkey]).reshape(shape)
-                psi4_fun = getattr(mints, integral.function_name)
-                if integral.extra_args is not None:
-                    ret = psi4_fun(*integral.extra_args)
-                else:
-                    ret = psi4_fun()
-                ret_np = np.array([x.np for x in ret])
-                np.testing.assert_allclose(
-                    ret_np, integral_ref, atol=1e-13,
-                    err_msg=f'Consistency error in {integral.function_name}.'
-                )
+
+            shape = ref[f'L_shape']
+            integral_ref = np.array(ref["L"]).reshape(shape)
+            ret = mints.ao_angular_momentum()
+            ret_np = np.array([x.np for x in ret])
+            np.testing.assert_allclose(ret_np, integral_ref, atol=1e-14)
