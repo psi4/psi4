77475b5a0d0e08d956ba1980d7560d352b1a3e66
Lori A. Burns <lori.burns@gmail.com>
2022-05-19 16:20:34 -0400

    v1.6

--- psi4/metadata.py
+++ psi4/metadata.py
@@ -1,6 +1,6 @@
-__version__ = '1.5'
-__version_long = '1.5+e9f4d6d'
-__version_upcoming_annotated_v_tag = '1.6a1'
+__version__ = '1.6'
+__version_long = '1.6+zzzzzzz'
+__version_upcoming_annotated_v_tag = '1.7a1'
 
 def version_formatter(dummy):
     return '(inplace)'
