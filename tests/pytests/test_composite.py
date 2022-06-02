8d55ccbc9496a9aeaeb0146ddbe3e22d102c66e0
Lori A. Burns <lori.burns@gmail.com>
2022-06-02 18:57:09 -0400

    ddd bugfixes

--- tests/pytests/test_composite.py
+++ tests/pytests/test_composite.py
@@ -26,3 +26,30 @@ def test_allen_focal_point():
 
     # 5 tasks: (t)/tz, (t)/qz, (t)/5z, t/tz, t(q)/dz
     # lowering the basis sets keeps the test within packaged L2's AM bounds
+
+@pytest.mark.cbs
+def test_cbs_with_managed_conditions():
+
+    n = psi4.geometry("""
+            0 4
+            N 0.00 0.00 0.00
+        """)
+
+    psi4.set_options({
+        "scf_type": "direct",
+        "reference": "rohf",
+        "r_convergence": 6,
+        "d_convergence": 7,
+        "e_convergence": 8,
+        "freeze_core": True,
+    })
+
+    e_cbs = psi4.energy('cbs',
+        scf_basis='aug-cc-pV[TQ5]Z',
+        corl_wfn='mp2',
+        corl_basis='aug-cc-pV[TQ]Z',
+        delta_wfn='ccsd(t)',
+        delta_basis='aug-cc-pV[DT]Z')
+
+    assert compare_values(-54.5325027481897, e_cbs, 5, "cbs")
+
