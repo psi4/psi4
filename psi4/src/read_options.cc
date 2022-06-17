a04c807c8bbc9d8d6948f276f319afdd657d0256
lazaroid <github@club-burniston.co.uk>
2022-06-17 22:20:48 +0100

    Tdscf excitations symmetry (#2605)

--- psi4/src/read_options.cc
+++ psi4/src/read_options.cc
@@ -1708,7 +1708,7 @@ int read_options(const std::string &name, Options &options, bool suppress_printi
         options.add_int("TDSCF_MAXITER", 60);
         /*- Verbosity level in TDSCF -*/
         options.add_int("TDSCF_PRINT", 1);
-        /*- Cutoff for printing excitations and de-excitations icontributing to each excited state -*/
+        /*- Cutoff for printing excitations and de-excitations contributing to each excited state -*/
         options.add_double("TDSCF_COEFF_CUTOFF", 0.1);
         /*- Which transition dipole moments to print out:
             - E_TDM_LEN : electric transition dipole moments, length representation
