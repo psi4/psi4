dcca1555e923f96dc3c2730ec842404a4adf5da2
Tibor Győri <tibor.gyori@chem.u-szeged.hu>
2022-07-23 00:17:19 +0200

    auto-format rsp.cc

--- psi4/src/psi4/libciomr/rsp.cc
+++ psi4/src/psi4/libciomr/rsp.cc
@@ -58,9 +58,9 @@ namespace psi {
 **
 ** \ingroup CIOMR
 */
-void rsp(int /*nm*/, const int n, const int nv, const double * const array, double *e_vals, const int matz,
-             double * const * const e_vecs, double /*toler*/){
-    if ((matz > 1) || (matz < 0)){
+void rsp(int /*nm*/, const int n, const int nv, const double* const array, double* e_vals, const int matz,
+         double* const* const e_vecs, double /*toler*/) {
+    if ((matz > 1) || (matz < 0)) {
         outfile->Printf("matz values other than 0 and 1 are no longer supported by rsp(...)");
         exit(PSI_RETURN_FAILURE);
     };
@@ -73,26 +73,26 @@ void rsp(int /*nm*/, const int n, const int nv, const double * const array, doub
     // To do the rearangement we run a column-major loop and calculate the packed row-major index (simpler)
     // If anyone ever needs it, here is the index calculation for packed column-major arrays that start at zero:
     // k = i-j + j*N - (j*(j-1))/2
-    for(int j=0, ij=0; j<n; j++){
-        for(int i=j; i<n; i++,ij++){
-            const int f = j + (i*(i+1))/2;
+    for (int j = 0, ij = 0; j < n; j++) {
+        for (int i = j; i < n; i++, ij++) {
+            const int f = j + (i * (i + 1)) / 2;
             tmp_array[ij] = array[f];
         }
     }
     // LAPACK needs more memory for temporary values
-    double* tmp_work = init_array(3*n);
-    if (eigenvectors){
+    double* tmp_work = init_array(3 * n);
+    if (eigenvectors) {
         // LAPACK needs a 1D array with N*N elements to put the eigenvectors in and cannot work with double**
-        double* tmp_eigvecs = init_array(n*n);
+        double* tmp_eigvecs = init_array(n * n);
         const auto info = C_DSPEV('V', 'L', n, tmp_array, e_vals, tmp_eigvecs, n, tmp_work);
-        if (info != 0){
+        if (info != 0) {
             outfile->Printf("DSPEV failed in a call of rsp(...), WITH eigenvectors");
             exit(PSI_RETURN_FAILURE);
         }
         // The eigenvectors are now in tmp_eigvecs in columns, flattened into 1D column major
         // Copy them to the 2D row major array as columns
-        for(int j=0, ij=0; j<n; j++){
-            for(int i=0; i<n; i++,ij++){
+        for (int j = 0, ij = 0; j < n; j++) {
+            for (int i = 0; i < n; i++, ij++) {
                 e_vecs[i][j] = tmp_eigvecs[ij];
             }
         }
@@ -100,7 +100,7 @@ void rsp(int /*nm*/, const int n, const int nv, const double * const array, doub
     } else {
         // LAPACK promises to not reference the eigenvector array if we request no eigenvectors, so don't allocate
         const auto info = C_DSPEV('N', 'L', n, tmp_array, e_vals, nullptr, n, tmp_work);
-        if (info != 0){
+        if (info != 0) {
             outfile->Printf("DSPEV failed in a call of rsp(...), WITHOUT eigenvectors");
             exit(PSI_RETURN_FAILURE);
         }
@@ -108,4 +108,4 @@ void rsp(int /*nm*/, const int n, const int nv, const double * const array, doub
     free(tmp_work);
     free(tmp_array);
 }
-}
+}  // namespace psi
