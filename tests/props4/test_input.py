0e47d2952da1c1ce7f570fd53ade1447a5412543
philipmnel <philip.nelson97@gmail.com>
2022-07-27 16:56:06 -0400

    More test cleanups

---
+++ tests/props4/test_input.py
@@ -0,0 +1,6 @@
+from addons import *
+
+@ctest_labeler("quick;properties;cart")
+def test_props4():
+    ctest_runner(__file__)
+
--- tests/snsmp2/cc-cc/test_input.py
+++ tests/snsmp2/cc-cc/test_input.py
@@ -1,7 +1,7 @@
 from addons import *
 
 @uusing("snsmp2")
-@ctest_labeler("long;cart")
+@ctest_labeler("longtests;cart")
 def test_snsmp2_cc_cc():
     ctest_runner(__file__)
 
