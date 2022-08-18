8e318dfe2a03e2aaef667d542f1d65100e5e5404
TiborGY <gyori.tibor@stud.u-szeged.hu>
2022-08-18 15:55:18 +0200

    Remove unused Matrix::diagonalize overloads, eliminate pointer arguments

--- psi4/src/psi4/libmints/matrix.cc
+++ psi4/src/psi4/libmints/matrix.cc
@@ -1717,61 +1717,20 @@ double Matrix::vector_dot(const Matrix *const rhs) {
 
 double Matrix::vector_dot(const SharedMatrix &rhs) { return vector_dot(rhs.get()); }
 
-void Matrix::diagonalize(Matrix *eigvectors, Vector *eigvalues, diagonalize_order nMatz) {
+void Matrix::diagonalize(Matrix &eigvectors, Vector &eigvalues, diagonalize_order nMatz/* = ascending*/) {
     if (symmetry_) {
         throw PSIEXCEPTION("Matrix::diagonalize: Matrix is non-totally symmetric.");
     }
     for (int h = 0; h < nirrep_; ++h) {
         if (rowspi_[h]) {
-            sq_rsp(rowspi_[h], colspi_[h], matrix_[h], eigvalues->pointer(h), static_cast<int>(nMatz),
-                   eigvectors->matrix_[h], 1.0e-14);
+            sq_rsp(rowspi_[h], colspi_[h], matrix_[h], eigvalues.pointer(h), static_cast<int>(nMatz),
+                   eigvectors.matrix_[h], 1.0e-14);
         }
     }
 }
 
-void Matrix::diagonalize(SharedMatrix &eigvectors, std::shared_ptr<Vector> &eigvalues, diagonalize_order nMatz) {
-    diagonalize(eigvectors.get(), eigvalues.get(), nMatz);
-}
-
-void Matrix::diagonalize(SharedMatrix &eigvectors, Vector &eigvalues, diagonalize_order nMatz) {
-    diagonalize(eigvectors.get(), &eigvalues, nMatz);
-}
-
-void Matrix::diagonalize(SharedMatrix &metric, SharedMatrix & /*eigvectors*/, std::shared_ptr<Vector> &eigvalues,
-                         diagonalize_order /*nMatz*/) {
-    if (symmetry_) {
-        throw PSIEXCEPTION("Matrix::diagonalize: Matrix non-totally symmetric.");
-    }
-
-    // this and metric are destroyed in the process, so let's make a copy
-    // that we work with.
-    Matrix t(*this);
-    Matrix m(metric);
-
-    int lwork = 3 * max_nrow();
-    std::vector<double> work(lwork);
-
-    for (int h = 0; h < nirrep_; ++h) {
-        if (!rowspi_[h] && !colspi_[h]) continue;
-
-        int err = C_DSYGV(1, 'V', 'U', rowspi_[h], t.matrix_[h][0], rowspi_[h], m.matrix_[h][0], rowspi_[h],
-                          eigvalues->pointer(h), work.data(), lwork);
-
-        if (err != 0) {
-            if (err < 0) {
-                outfile->Printf("Matrix::diagonalize with metric: C_DSYGV: argument %d has invalid parameter.\n", -err);
-
-                abort();
-            }
-            if (err > 0) {
-                outfile->Printf("Matrix::diagonalize with metric: C_DSYGV: error value: %d\n", err);
-
-                abort();
-            }
-        }
-
-        // TODO: Sort the data according to eigenvalues.
-    }
+void Matrix::diagonalize(SharedMatrix &eigvectors, std::shared_ptr<Vector> &eigvalues, diagonalize_order nMatz/* = ascending*/) {
+    diagonalize(*eigvectors, *eigvalues, nMatz);
 }
 
 std::tuple<SharedMatrix, SharedVector, SharedMatrix> Matrix::svd_temps() {
@@ -2800,14 +2759,6 @@ void Matrix::back_transform(const Matrix &transformer) {
 }
 
 double Matrix::vector_dot(const Matrix &rhs) { return vector_dot(&rhs); }
-
-void Matrix::diagonalize(Matrix &eigvectors, Vector &eigvalues, int nMatz) {
-    for (int h = 0; h < nirrep_; ++h) {
-        if (rowspi_[h]) {
-            sq_rsp(rowspi_[h], colspi_[h], matrix_[h], eigvalues.pointer(h), nMatz, eigvectors.matrix_[h], 1.0e-14);
-        }
-    }
-}
 
 void Matrix::write_to_dpdfile2(dpdfile2 *outFile) {
     global_dpd_->file2_mat_init(outFile);
