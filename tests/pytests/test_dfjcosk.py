211ed306443b59b653c34250724a872760916c6d
David Poole <dpoole34@gatech.edu>
2022-12-01 10:56:46 -0500

    Add test for IncFock efficiency to test_dfjcosk.py pytest

--- tests/pytests/test_dfjcosk.py
+++ tests/pytests/test_dfjcosk.py
@@ -35,7 +35,6 @@ units au
 """)
         }
 
-
 @pytest.mark.parametrize(
     "inp",
     [
@@ -86,3 +85,60 @@ def test_dfjcosk(inp, mols):
     psi4.set_options({"scf_type" : "pk"})
     energy_pk = psi4.energy(inp["method"], molecule=molecule, bsse_type=inp["bsse_type"])
     assert compare_values(energy_pk, energy_dfjcosk, atol=1e-4)
+
+@pytest.mark.parametrize(
+    "inp",
+    [
+        pytest.param({"method" : "hf",
+                      "options": {"reference" : "rhf"},
+                      "molecule" : "h2o",
+                      "bsse_type" : None,
+                      "ref" : -76.026780223322},
+                      id="h2o (rhf)"),
+        pytest.param({"method" : "b3lyp",
+                      "options": {"reference" : "rhf"},
+                      "molecule" : "h2o",
+                      "bsse_type" : None,
+                      "ref" : -76.420402720419},
+                      id="h2o (rks)"),
+        pytest.param({"method" : "hf",
+                      "options": {"reference" : "uhf"},
+                      "molecule" : "nh2",
+                      "bsse_type" : None,
+                      "ref" : -55.566890252551},
+                      id="nh2 (uhf)"),
+        pytest.param({"method" : "hf",
+                      "options": {"reference" : "rohf"},
+                      "molecule" : "nh2",
+                      "bsse_type" : None,
+                      "ref" : -55.562689948780},
+                      id="nh2 (rohf)"),
+        pytest.param({"method" : "hf",
+                      "options": {"reference" : "rhf"},
+                      "molecule" : "h2o_nap1",
+                      "bsse_type" : "CP",
+                      "ref" :  -0.040121884077},
+                      id="h2o/na+ (rhf ie)"),
+    ],
+)
+def test_dfjcosk_incfock(inp, mols):
+    """Test the efficiency of IncFock in DFJCOSK JK object via SCF calculations"""
+
+    molecule = mols[inp["molecule"]]
+    psi4.set_options({"scf_type" : "cosx", "basis": "cc-pvdz", "incfock": False})
+    psi4.set_options(inp["options"])
+
+    # compute DFJCOSK energy+wfn without IncFock 
+    energy_dfjcosk_noinc, wfn_dfjcosk_noinc = psi4.energy(inp["method"], molecule=molecule, bsse_type=inp["bsse_type"], return_wfn=True)
+    #assert compare_values(inp["ref"], energy_dfjcosk, atol=1e-6)
+
+    # compute DFJCOSK energy+wfn with Incfock 
+    psi4.set_options({"incfock" : True})
+    energy_dfjcosk_inc, wfn_dfjcosk_inc = psi4.energy(inp["method"], molecule=molecule, bsse_type=inp["bsse_type"], return_wfn=True)
+
+    # how do energies compare?
+    assert compare_values(energy_dfjcosk_noinc, energy_dfjcosk_inc, atol=1e-6)
+    
+    # how do SCF iteration counts compare?
+    if hasattr(wfn_dfjcosk_noinc, "iteration_") and hasattr(wfn_dfjcosk_inc, "iteration_"):
+        assert compare_values(wfn_dfjcosk_noinc.iteration_, wfn_dfjcosk_inc.iteration_, 3)
