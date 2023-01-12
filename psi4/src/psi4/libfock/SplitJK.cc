08a36c6e6babfbfd592ccc94ce9dde1d1e6bb6ad
David Poole <dpoole34@gatech.edu>
2023-01-12 09:19:10 -0500

    Add SplitJK.cc

---
+++ psi4/src/psi4/libfock/SplitJK.cc
@@ -0,0 +1,61 @@
+/*
+ * @BEGIN LICENSE
+ *
+ * Psi4: an open-source quantum chemistry software package
+ *
+ * Copyright (c) 2007-2022 The Psi4 Developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This file is part of Psi4.
+ *
+ * Psi4 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Psi4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Psi4; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
+ */
+
+#include "psi4/libqt/qt.h"
+#include "psi4/libmints/basisset.h"
+#include "psi4/libmints/mintshelper.h"
+#include "jk.h"
+#include "SplitJK.h"
+
+#include <unordered_set>
+#include <vector>
+#include <map>
+#include <algorithm>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+using namespace psi;
+
+namespace psi {
+
+SplitJK::SplitJK(std::shared_ptr<BasisSet> primary, Options& options) : primary_(primary) {
+    if (options["BENCH"].has_changed()) bench_ = options.get_int("BENCH");
+    if (options["INTS_TOLERANCE"].has_changed()) cutoff_ = options.get_double("INTS_TOLERANCE");
+};
+
+SplitJK::~SplitJK() {};
+
+size_t SplitJK::num_computed_shells() {
+    outfile->Printf("WARNING: JK::num_computed_shells() was called, but benchmarking is disabled for the chosen JK algorithm.");
+    outfile->Printf(" Returning 0 as computed shells count.\n");
+
+    return 0;
+}
+
+}
