5c5ce90c0e8928217b68a40fb580421ee57f3be3
Jonathon Misiewicz <jonathon.paul.misiewicz@emory.edu>
2023-01-20 06:20:26 -0500

    Revert silly whitespace change

--- psi4/src/psi4/libscf_solver/sad.cc
+++ psi4/src/psi4/libscf_solver/sad.cc
@@ -287,7 +287,7 @@ void SADGuess::run_atomic_calculations(SharedMatrix& DAO, SharedMatrix& HuckelC,
 
             // Sanity check: can't have more active orbitals than basis functions
             if (nact > nbf - nfzc) {
-                nact = nbf-nfzc;
+                nact = nbf - nfzc;
                 if (nact < 0) {
                     throw PSIEXCEPTION("SAD: Atom " + molecule_->symbol(index) + " has more core electrons than basis functions.");
                 }
