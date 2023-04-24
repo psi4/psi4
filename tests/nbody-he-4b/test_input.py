ad40b0ed3bc55ce8bc43f42896e005044991cee6
Lori A. Burns <lori.burns@gmail.com>
2023-04-24 01:53:38 -0400

    run more tests with qcfractal snowflake

--- tests/nbody-he-4b/test_input.py
+++ tests/nbody-he-4b/test_input.py
@@ -1,6 +1,12 @@
+import pytest
 from addons import *
 
+@pytest.mark.parametrize("distributed", [
+    pytest.param(False, id="internal"),
+    pytest.param(True,  id="snowflake", marks=using("qcfractal")),
+])
 @ctest_labeler("nbody;cart")
-def test_nbody_he_4b():
-    ctest_runner(__file__)
+def test_nbody_he_4b(distributed):
+    setenv = ["_PSI4_USE_QCF"] if distributed else None
+    ctest_runner(__file__, setenv=setenv)
 
