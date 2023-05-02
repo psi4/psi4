6c712df041afe6f65b4dee7e6110d46f147d1ee1
Lori A. Burns <lori.burns@gmail.com>
2023-05-01 21:54:26 -0400

    no long qcf jobs on mac

--- tests/nbody-vmfc-hessian/test_input.py
+++ tests/nbody-vmfc-hessian/test_input.py
@@ -5,7 +5,7 @@ from addons import *
 
 @pytest.mark.parametrize("distributed", [
     pytest.param(False, id="internal"),
-    pytest.param(True,  id="snowflake", marks=using("qcfractal")),
+    pytest.param(True,  id="snowflake", marks=using("qcfractal_next")),
 ])
 def test_nbody_vmfc_hessian(distributed):
     setenv = ["_PSI4_USE_QCF"] if distributed else None
