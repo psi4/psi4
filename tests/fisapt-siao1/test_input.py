dddde568f9df5587ed8a6e45313cf8e773b311f1
Lori A. Burns <lori.burns@gmail.com>
2023-05-08 17:33:01 -0400

    name arg in siao test

--- tests/fisapt-siao1/test_input.py
+++ tests/fisapt-siao1/test_input.py
@@ -4,7 +4,8 @@ from addons import *
 @ctest_labeler("sapt")
 def test_fisapt_siao1():
     fsaptpy_installed = (Path(psi4.core.get_datadir()) / "fsapt" / "fsapt.py").resolve()
-    ctest_runner(__file__, [
+
+    ctest_runner(__file__, extra_infiles=[
         fsaptpy_installed,
     ])
 
