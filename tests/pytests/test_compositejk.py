17ba1f4efd2b207e44fd361fccd715ddd8207330
David Poole <dpoole34@gatech.edu>
2023-07-05 09:41:43 -0400

    Initial CompositeJK SCF_TYPE tests

--- tests/pytests/test_compositejk.py
+++ tests/pytests/test_compositejk.py
@@ -146,3 +146,41 @@ def test_dfjcosk_incfock(inp, mols):
     niter_inc = int(wfn_dfjcosk_inc.variable("SCF ITERATIONS")) if wfn_dfjcosk_inc.has_scalar_variable("SCF ITERATIONS") else 0
     
     assert compare(True, abs(niter_inc - niter_noinc) <= 3, "IncFock efficient?")
+
+@pytest.mark.parametrize(
+    "inp",
+    [
+        pytest.param({"method" : "bp86",
+                      "options": {"reference" : "rhf"},
+                      "molecule" : "h2o"},
+                      id="h2o (bp86)"),
+        pytest.param({"method" : "b3lyp",
+                      "options": {"reference" : "rhf"},
+                      "molecule" : "h2o"},
+                      id="h2o (b3lyp)"),
+    ],
+)
+@pytest.mark.parametrize("scf_type", [ "DFDIRJ", "DFDIRJ+COSX", "DFDIRJ+LINK" ])
+def test_compositejk_scftype(inp, scf_type, mols):
+    """Test the functionality of the SCF_TYPE keyword for CompositeJK methods under varying situations.
+    Using hybrid DFT functionals without specifying a K algorithm should cause an exception to be thrown."""
+
+    molecule = mols[inp["molecule"]]
+    
+    screening = "DENSITY" if "COSX" not in scf_type else "CSAM"
+    psi4.set_options({"scf_type" : scf_type, "basis": "cc-pvdz", "screening": screening}) 
+    psi4.set_options(inp["options"])
+
+    is_hybrid = True if inp["method"] == "b3lyp" else False
+    k_algo_specified = True if "+" in scf_type else False
+
+    # if K algorithm isn't specified, but hybrid functional is used, code should throw...
+    if is_hybrid and not k_algo_specified: 
+        with pytest.raises(Exception) as e_info:
+            E = psi4.energy(inp["method"], molecule=molecule) 
+        #assert compare_values(inp["ref"], energy_dfjcosk, atol=1e-6)
+    
+    # ... else code will run fine
+    else:
+        E = psi4.energy(inp["method"], molecule=molecule) 
+     
