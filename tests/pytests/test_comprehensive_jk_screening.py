71cb141abe8e9d2fc438d9fca0694ad5e0f6d845
David Poole <dpoole34@login-hive-2.pace.gatech.edu>
2023-11-17 14:34:39 -0500

    Throw exceptions for bad JK+SCREENING combos

--- tests/pytests/test_comprehensive_jk_screening.py
+++ tests/pytests/test_comprehensive_jk_screening.py
@@ -56,31 +56,20 @@ def test_comprehensive_jk_screening(scf_type, scf_subtype, screening):
 
     #== certain combinations of SCF_TYPE and SCREENING should throw an exception by design ==#
     should_throw = False
-    #== specifically, non-integral-direct methods and DFDirJ+COSX with SCREENING = DENSITY ==# 
+    #== specifically, non-integral-direct methods and DFDirJ+COSX with SCREENING = DENSITY... ==# 
     should_throw = should_throw or (scf_type not in [ "DIRECT", "DFDIRJ+LINK" ] and screening == "DENSITY")
-
-    #== other combinations error out badly and need to be fixed; skip them here ==#
-    should_error_out = False
-    #== this includes Composite methods with SCREENING=NONE... ==#
-    should_error_out = should_error_out or (scf_type in Eref["Singlet"]["Composite"].keys() and screening == "NONE")
-    #== .. DFDIRJ+LINK with SCREENING=SCHWARZ or CSAM... ==#
-    should_error_out = should_error_out or (scf_type == "DFDIRJ+LINK" and screening in [ "SCHWARZ", "CSAM" ])
-    #== .. and DISK_DF, DIRECT, or PK with SCREENING=NONE ==#
-    should_error_out = should_error_out or (scf_type == "PK" and screening == "NONE")
-    should_error_out = should_error_out or (scf_type == "DISK_DF" and screening == "NONE")
-    should_error_out = should_error_out or (scf_type == "DIRECT" and screening == "NONE")
-  
+    #== ... Composite methods with SCREENING=NONE... ==#
+    should_throw = should_throw or (scf_type in Eref["Singlet"]["Composite"].keys() and screening == "NONE")
+    #== .. DISK_DF, DIRECT, or PK with SCREENING=NONE ==#
+    should_throw = should_throw or (scf_type == "PK" and screening == "NONE")
+    should_throw = should_throw or (scf_type == "DISK_DF" and screening == "NONE")
+    should_throw = should_throw or (scf_type == "DIRECT" and screening == "NONE")
+    #== .. and DFDIRJ+LINK with SCREENING=SCHWARZ or CSAM... ==#
+    should_throw = should_throw or (scf_type == "DFDIRJ+LINK" and screening in [ "SCHWARZ", "CSAM" ])
+ 
     E = 0.0 
     
-    #== check that should_error_out and should_throw are not simultaneously true, for better testing ==# 
-    if should_error_out and should_throw:
-        raise Exception(f'Duplicate checks on {scf_type}({scf_subtype})+{screening}!')
-    #== xfail if current option combo is expected to break ==#
-    elif should_error_out and not should_throw:
-        pytest.xfail(f'Singlet {scf_type}({scf_subtype})+{screening}  xfailed: will error out')
-
-    #== if expected, test if current option combo throws exception ==# 
-    elif not should_error_out and should_throw:
+    if should_throw:
         with pytest.raises(Exception) as e_info:
             E = psi4.energy('scf')
 
