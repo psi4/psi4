022f84c427012199f7278c5ef34e69ee5e278878
David Poole <dpoole34@login-hive-2.pace.gatech.edu>
2024-01-18 16:39:07 -0500

    First hookup of GauXC to Psi4 (must build GauXC internally)

---
+++ psi4/src/psi4/libfunctional/GauXCfunctional.cc
@@ -0,0 +1,67 @@
+/*
+ * @BEGIN LICENSE
+ *
+ * Psi4: an open-source quantum chemistry software package
+ *
+ * Copyright (c) 2007-2023 The Psi4 Developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This file is part of Psi4.
+ *
+ * Psi4 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Psi4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Psi4; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
+ */
+
+#include "functional.h"
+#include "GauXCfunctional.h"
+
+#include "psi4/libmints/vector.h"
+#include "psi4/psi4-dec.h"
+#include "psi4/libqt/qt.h"
+#include "psi4/libpsi4util/PsiOutStream.h"
+#include "psi4/libpsi4util/exception.h"
+
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+using namespace psi;
+
+//-- namespace aliasing --//
+//namespace EBack = ExchCXX::Backend;
+//namespace EFunct = ExchCXX::Functional; 
+//namespace ESpin = ExchCXX::Spin;
+
+namespace psi {
+
+GauXCFunctional::GauXCFunctional(std::string xc_name, bool unpolarized) {
+
+    func_ = std::make_unique<GauXC::functional_type>(ExchCXX::Backend::builtin, ExchCXX::Functional::BLYP, ExchCXX::Spin::Polarized);
+} 
+
+GauXCFunctional::~GauXCFunctional() { return; }
+
+std::shared_ptr<Functional> GauXCFunctional::build_polarized() {
+    return nullptr;
+}
+
+void GauXCFunctional::compute_functional(const std::map<std::string, SharedVector>& in,
+                                         const std::map<std::string, SharedVector>& out, int npoints, int deriv) {
+    return;
+}
+
+}  // namespace psi
