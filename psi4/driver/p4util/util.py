9dbdd59dd474947d64522508476ffba1fb6b59c8
Lori A. Burns <lori.burns@gmail.com>
2024-02-13 23:53:20 -0500

    AM symbol list (#3130)
    * Update util.py
    
    * Update meta.yaml
    
    * Update gshell.h
    
    * Update libmintsgshell.py

--- psi4/driver/p4util/util.py
+++ psi4/driver/p4util/util.py
@@ -359,7 +359,7 @@ def _l2_config_style_eri_llll():
     skel = {"onebody_": [], "eri_c4_": [], "eri_c3_": [], "eri_c2_": []}
     skel_re = {"onebody_": r"onebody_\w_d\d", "eri_c4_": r"eri_\w..._d\d", "eri_c3_":  r"eri_\w.._d\d", "eri_c2_": r"eri_\w._d\d"}
 
-    amstr = "SPDFGHIKLMNOPQRTUVWXYZ"
+    amstr = "SPDFGHIKLMNOQRTUVWXYZ"
     libint2_configuration = core._libint2_configuration()
 
     for k, v in skel_re.items():
