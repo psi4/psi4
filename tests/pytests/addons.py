a69d00b5ec99a6a16f3a34ae1a160dcdb4ba887f
David Poole <dpoole34@atl1-1-01-013-14-1.pace.gatech.edu>
2024-03-13 14:26:53 -0400

    Add test_seminum pytest to test_compositejk.py

--- tests/pytests/addons.py
+++ tests/pytests/addons.py
@@ -92,6 +92,7 @@ _programs = {
     "qcportal": which_import("qcportal", return_bool=True),
     "bse": which_import("basis_set_exchange", return_bool=True),
     "einsums": psi4.addons("einsums"),
+    "gauxc": psi4.addons("gauxc"),
 }
 
 
