6ee288f6767715f7083291181ab80773b4193faa
David Poole <dpoole34@atl1-1-01-013-37-1.pace.gatech.edu>
2024-03-21 11:35:32 -0400

    Test flexible gau2grid dependency GauXC PR, adjust build to match

--- tests/pytests/test_compositejk.py
+++ tests/pytests/test_compositejk.py
@@ -106,29 +106,18 @@ def test_composite_call(j_algo, k_algo, mols, request):
 @pytest.mark.parametrize(
     "scf",
     [
-        #pytest.param({"scf_type" : "dfdirj+cosx",
-        #              "ref" : { 
-        #                  "h2o (rhf)" : -76.026780223322,
-        #                  "h2o (rks)" : -76.420402720419,
-        #                  "nh2 (uhf)" : -55.566890252551,
-        #                  "nh2 (rohf)" : -55.562689948780,
-        #                  "h2o/na+ (rhf ie)" : -0.040121884077,
-        #              },
-        #              },
-        #              id="cosx"),
-        #pytest.param({"scf_type" : "dfdirj+snlink",
-        #              "snlink_force_cartesian": False,
-        #              "ref" : { 
-        #                  "h2o (rhf)" : -76.026788692185, 
-        #                  "h2o (rks)" : -76.420403557357,
-        #                  "nh2 (uhf)" : -55.566911357539,
-        #                  "nh2 (rohf)" : -55.562710424257,
-        #                  "h2o/na+ (rhf ie)" : -0.040118757043,
-        #              },
-        #              },
-        #              id="snlink (spherical)", marks=using("gauxc")),
+        pytest.param({"scf_type" : "dfdirj+cosx",
+                      "ref" : { 
+                          "h2o (rhf)" : -76.026780223322,
+                          "h2o (rks)" : -76.420402720419,
+                          "nh2 (uhf)" : -55.566890252551,
+                          "nh2 (rohf)" : -55.562689948780,
+                          "h2o/na+ (rhf ie)" : -0.040121884077,
+                      },
+                      },
+                      id="cosx"),
         pytest.param({"scf_type" : "dfdirj+snlink",
-                      "snlink_force_cartesian": True,
+                      "snlink_force_cartesian": False,
                       "ref" : { 
                           "h2o (rhf)" : -76.026788692185, 
                           "h2o (rks)" : -76.420403557357,
@@ -137,7 +126,18 @@ def test_composite_call(j_algo, k_algo, mols, request):
                           "h2o/na+ (rhf ie)" : -0.040118757043,
                       },
                       },
-                      id="snlink (cartesian)", marks=using("gauxc")),
+                      id="snlink (spherical)", marks=using("gauxc")),
+        #pytest.param({"scf_type" : "dfdirj+snlink",
+        #              "snlink_force_cartesian": True,
+        #              "ref" : { 
+        #                  "h2o (rhf)" : -76.026788692185, 
+        #                  "h2o (rks)" : -76.420403557357,
+        #                  "nh2 (uhf)" : -55.566911357539,
+        #                  "nh2 (rohf)" : -55.562710424257,
+        #                  "h2o/na+ (rhf ie)" : -0.040118757043,
+        #              },
+        #              },
+        #              id="snlink (cartesian)", marks=using("gauxc")),
  
     ]
 )
@@ -152,6 +152,9 @@ def test_seminum(inp, scf, mols, request):
     if "snlink_force_cartesian" in scf.keys():
         psi4.set_options({"snlink_force_cartesian": scf["snlink_force_cartesian"]})
 
+        #SNLINK_FORCE_CARTESIAN doesnt work with symmetry currently
+        full_molecule = psi4.core.get_active_molecule()
+        full_molecule.reset_point_group("C1")
 
     # does the SCF energy match a pre-computed reference?
     energy_seminum = psi4.energy(inp["method"], molecule=molecule, bsse_type=inp["bsse_type"])
