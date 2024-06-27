862d93cc5a5d193a5b85427c9176b295b1f15fc8
Lori A. Burns <lori.burns@gmail.com>
2024-06-27 12:04:48 -0400

    Update test_threading.py

--- psi4/share/psi4/scripts/test_threading.py
+++ psi4/share/psi4/scripts/test_threading.py
@@ -24,7 +24,7 @@ if sys.version_info <= (3, 0):
 import psi4
 import numpy as np
 
-psi4.set_memory("1 GB")
+psi4.set_memory("4 GB")
 
 def test_threaded_blas(args):
     threads = int(args.nthread)
