079ac2398a57b7f57a0c4694394019c826e94fb0
Lori A. Burns <lori.burns@gmail.com>
2024-08-06 08:00:46 -0400

    guess what, try to fix CI again: Eco (M) (#3194)
    * Update test_adcc.py
    
    * Update generate_reference_data_adcc.py
    
    * Update adcc_reference_data.json
    
    * Update test_addons.py
    
    * Update meta.yaml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update nitpick-exceptions
    
    * Update ecosystem.yml
    
    * Update docs.yml
    
    * Update docs-pr.yml
    
    * Update ecosystem.yml
    
    * Update basisset.cc
    
    * Update codedeps.yaml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml
    
    * Update ecosystem.yml

--- tests/pytests/test_addons.py
+++ tests/pytests/test_addons.py
@@ -1568,3 +1568,28 @@ def test_einsums():
     wfn = psi4.core.Wavefunction(mol, basis)
 
     psi4.core.dummy_einsums(wfn)
+
+
+@uusing("forte")
+def test_forte_fci_1():
+    """Test FCI on Li2/STO-3G"""
+    from forte.solvers import solver_factory, HF, ActiveSpaceSolver
+
+    ref_hf_energy = -14.54873910108353
+    ref_fci_energy = -14.595808852754054
+
+    # setup job
+    xyz = """
+    Li
+    Li 1 3.0
+    units bohr
+    """
+    input = solver_factory(molecule=xyz, basis='sto-3g')
+    state = input.state(charge=0, multiplicity=1, sym='ag')
+    hf = HF(input, state=state)
+    fci = ActiveSpaceSolver(hf, type='FCI', states=state)
+    fci.run()
+
+    # check results
+    assert hf.value('hf energy') == pytest.approx(ref_hf_energy, 1.0e-10)
+    assert fci.value('active space energy')[state] == pytest.approx([ref_fci_energy], 1.0e-10)
