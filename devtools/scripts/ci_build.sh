344df8e5533c5a9fbe4135ca37f4a07b260b5464
Lori A. Burns <lori.burns@gmail.com>
2025-01-07 02:39:07 -0500

    more

--- devtools/scripts/ci_build.sh
+++ devtools/scripts/ci_build.sh
@@ -2,8 +2,7 @@
 
 # The return code will capture an error from ANY of the functions in the pipe
 set -o pipefail
-cmake --build . 2>&1 | tee build.log | grep "Building"
-#cmake --build . -- -j 2 VERBOSE=1 2>&1 | tee build.log | grep "Building"
+cmake --build . -- -j 2 VERBOSE=1 2>&1 | tee build.log | grep "Building"
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
