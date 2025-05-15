0040d9568e144d6e416382e273168be502542e9b
Jonathon Misiewicz <jpmisiewicz@vt.edu>
2025-05-15 19:21:04 -0400

    DePointerDPD Warmup: `Dimension` for `cc` (#3288)
    * Remove unused variadic init
    
    * ccenergy orb spaces to Dimension
    
    * Revamp Dimension use in ccdensity
    
    * EOM Dimensionifying
    
    * Dimensionify cchbar
    
    * Dimensionifying cclambda
    
    * ccresponse gets Dimension
    
    * Dimension for ccresponse
    
    * Don't write dimensions to disk

--- psi4/src/psi4/cc/cceom/MOInfo.h
+++ psi4/src/psi4/cc/cceom/MOInfo.h
@@ -36,8 +36,10 @@
 
 #include <string>
 #include <vector>
+#include "psi4/libmints/dimension.h"
 
 namespace psi {
+
 namespace cceom {
 
 struct MOInfo {
@@ -45,23 +47,23 @@ struct MOInfo {
     int nmo;                           /* no. of molecular orbitals */
     int nso;                           /* no. of symmetry orbitals */
     int iopen;                         /* 0=closed shell; >0=open shell */
-    int *sopi;                         /* no. of SOs per irrep */
+    Dimension sopi;                         /* no. of SOs per irrep */
     int *sosym;                        /* orbital symmetry (Pitzer/SO) */
-    int *orbspi;                       /* no. of MOs per irrep */
-    int *clsdpi;                       /* no. of closed-shells per irrep excl. frdocc */
-    int *openpi;                       /* no. of open-shells per irrep */
-    int *uoccpi;                       /* no. of unoccupied orbitals per irr. ex. fruocc */
-    int *frdocc;                       /* no. of frozen core orbitals per irrep */
-    int *fruocc;                       /* no. of frozen unoccupied orbitals per irrep */
+    Dimension orbspi;                       /* no. of MOs per irrep */
+    Dimension clsdpi;                       /* no. of closed-shells per irrep excl. frdocc */
+    Dimension openpi;                       /* no. of open-shells per irrep */
+    Dimension uoccpi;                       /* no. of unoccupied orbitals per irr. ex. fruocc */
+    Dimension frdocc;                       /* no. of frozen core orbitals per irrep */
+    Dimension fruocc;                       /* no. of frozen unoccupied orbitals per irrep */
     int nvirt;                         /* total no. of (active) virtual orbitals */
     std::vector<std::string> irr_labs; /* irrep labels */
     char **irr_labs_lowercase;         /* irrep labels */
-    int *occpi;                        /* no. of occupied orbs. (incl. open) per irrep */
-    int *aoccpi;                       /* no. of alpha occupied orbs. (incl. open) per irrep */
-    int *boccpi;                       /* no. of beta occupied orbs. (incl. open) per irrep */
-    int *virtpi;                       /* no. of virtual orbs. (incl. open) per irrep */
-    int *avirtpi;                      /* no. of alpha virtual orbs. (incl. open) per irrep */
-    int *bvirtpi;                      /* no. of beta virtual orbs. (incl. open) per irrep */
+    Dimension occpi;                        /* no. of occupied orbs. (incl. open) per irrep */
+    Dimension aoccpi;                       /* no. of alpha occupied orbs. (incl. open) per irrep */
+    Dimension boccpi;                       /* no. of beta occupied orbs. (incl. open) per irrep */
+    Dimension virtpi;                       /* no. of virtual orbs. (incl. open) per irrep */
+    Dimension avirtpi;                      /* no. of alpha virtual orbs. (incl. open) per irrep */
+    Dimension bvirtpi;                      /* no. of beta virtual orbs. (incl. open) per irrep */
     int *occ_sym;                      /* relative occupied index symmetry */
     int *aocc_sym;                     /* relative alpha occupied index symmetry */
     int *bocc_sym;                     /* relative beta occupied index symmetry */
