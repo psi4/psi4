fd23c274b864690aed7a49a478bfd5eda4a9514d
Wallace, Austin <awallace43@gatech.edu>
2025-07-28 11:41:25 -0400

    updated HF-D4 naming

--- psi4/driver/procrouting/dft/hyb_functionals.py
+++ psi4/driver/procrouting/dft/hyb_functionals.py
@@ -160,32 +160,10 @@ funcs.append({
     "description": '    PBE Hybrid based 3C composite method with a small basis set, gCP and D3(BJ)\n',
     "citation": '    Grimme et. al., J. Chem. Phys., 143, 054107, 2015\n',
 })
-funcs.append({
-    "name": "HF-D4M",
-    "c_functionals": {},
-    "x_hf": {
-        "alpha": 1.0
-    },
-    "dispersion": {
-        "type": "d4bjeeqatm",
-        "nlc": False,
-        "params": {
-            'a1': 0.095,
-            'a2': 3.637,
-            's6': 1.000,
-            's8': 0.738,
-            's9': 0.000,
-        },
-    },
-    "description":
-    '    HF with D4 dispersion using Sherrill Group BJ damping parameters\n',
-    "citation":
-    '    Wallace, A. M.; Sherrill, C. D., J. Chem. Phys., 161, 114115, 2024\n',
-    "doi": "10.1063/5.0219185",
-})
 
 funcs.append({
-    "name": "HF-D4",
+    "name": "HF-D4BJEEQATM",
+    "alias": ["HF-D4"],
     "c_functionals": {},
     "x_hf": {
         "alpha": 1.0
@@ -199,6 +177,7 @@ funcs.append({
             'a2':3.35743605,
             's6': 1.000,
             's9': 1.000,
+            'alp': 16.0,
         },
     },
     "description":
@@ -206,6 +185,32 @@ funcs.append({
     "citation":
     '    Caldeweyher, E.; Ehlert, S.; Hansen, A.; Neugebauer, H.; Spicher, S.; Bannwarth, C.; Grimmme, S., J. Chem. Phys. 150, 154122 (2019)\n',
     "doi": "10.1063/1.5090222150",
+})
+
+funcs.append({
+    "name": "HF-D4BJEEQTWO",
+    "alias": ["HF-D4M"],
+    "c_functionals": {},
+    "x_hf": {
+        "alpha": 1.0
+    },
+    "dispersion": {
+        "type": "d4bjeeqatm",
+        "nlc": False,
+        "params": {
+            'a1': 0.095,
+            'a2': 3.637,
+            's6': 1.000,
+            's8': 0.738,
+            's9': 0.000,
+            'alp': 16.0,
+        },
+    },
+    "description":
+    '    HF with D4 dispersion using Sherrill Group BJ damping parameters\n',
+    "citation":
+    '    Wallace, A. M.; Sherrill, C. D., J. Chem. Phys., 161, 114115, 2024\n',
+    "doi": "10.1063/5.0219185",
 })
 
 funcs.append({
