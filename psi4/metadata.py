f2492db10de56b30b188239bdc6b4cfbc4a23bd5
Lori A. Burns <lori.burns@gmail.com>
2025-09-05 10:52:21 -0400

    v1.10

--- psi4/metadata.py
+++ psi4/metadata.py
@@ -1,6 +1,6 @@
-__version__ = '1.9'
-__version_long = '1.9+d99b642'
-__version_upcoming_annotated_v_tag = '1.10a1'
+__version__ = '1.10'
+__version_long = '1.10+zzzzzzz'
+__version_upcoming_annotated_v_tag = '1.11a1'
 
 def version_formatter(dummy):
     return '(inplace)'
