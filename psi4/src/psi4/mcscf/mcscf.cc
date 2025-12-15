7b747997b9ec75ae52e21a9741cbfb49ec596ad1
Connor Briggs <cgbriggs99@gmail.com>
2025-12-15 16:13:17 -0500

    Also fix a seg fault in mcscf that impacted psimrcc.

--- psi4/src/psi4/mcscf/mcscf.cc
+++ psi4/src/psi4/mcscf/mcscf.cc
@@ -72,6 +72,9 @@ SharedWavefunction mcscf(SharedWavefunction ref_wfn, Options& options) {
 
     tstart();
 
+    if (memory_manager != nullptr) {
+        delete memory_manager;
+    }
     memory_manager = new MemoryManager(Process::environment.get_memory());
 
     psio->open(PSIF_MCSCF, PSIO_OPEN_NEW);
@@ -87,6 +90,9 @@ SharedWavefunction mcscf(SharedWavefunction ref_wfn, Options& options) {
 
         // Now, set the reference wavefunction for subsequent codes to use
         wfn = std::make_shared<SCF>(ref_wfn, options, psio);
+        if (moinfo_scf != nullptr) {
+            delete moinfo_scf;
+        }
         moinfo_scf = new psi::MOInfoSCF(*(wfn.get()), options);
         wfn->compute_energy();
 
@@ -98,8 +104,15 @@ SharedWavefunction mcscf(SharedWavefunction ref_wfn, Options& options) {
         throw PSIEXCEPTION("REFERENCE = MCSCF not implemented yet");
     }
 
-    if (moinfo_scf) delete moinfo_scf;
-    if (memory_manager) delete memory_manager;
+    if (moinfo_scf != nullptr) {
+        delete moinfo_scf;
+        moinfo_scf = nullptr;
+    }
+
+    if (memory_manager != nullptr) {
+        delete memory_manager;
+        memory_manager = nullptr;
+    }
 
     close_psi(options);
     psio->close(PSIF_MCSCF, 1);
