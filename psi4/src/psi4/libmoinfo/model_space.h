a2a7baa421edda18b0a7a78a44ac63024362a05e
Tibor Győri <tibor.gyori@chem.u-szeged.hu>
2026-01-08 22:17:33 +0100

    ModelSpace::unique_to_all is only used in inactive code

--- psi4/src/psi4/libmoinfo/model_space.h
+++ psi4/src/psi4/libmoinfo/model_space.h
@@ -56,8 +56,9 @@ class ModelSpace {
     std::vector<SlaterDeterminant> determinants;
     std::vector<int> closed_to_all;  // closed-shell determinants
     std::vector<int> opensh_to_all;  // open-shell   determinants
-    std::vector<int> unique_to_all;  // spin-unique  determinants
     MOInfo* moinfo_obj;
+    //Only used by commented-out code in model_space_build.cc
+    //std::vector<int> unique_to_all;  // spin-unique  determinants
 };
 }  // namespace psi
 
