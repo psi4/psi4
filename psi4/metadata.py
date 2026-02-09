66823cd75e2917a38c626cbd212a95518c0a1306
Filip Brzek <filip.brzek@gmail.com>
2026-02-09 22:43:40 +0100

    ci: ruff - leave metadata.py single-quoted for now

--- psi4/metadata.py
+++ psi4/metadata.py
@@ -1,3 +1,5 @@
+# fmt: off
+# Note: versioner.py parses this file with regex expecting single quotes
 __version__ = '1.10'
 __version_long = '1.10+f2492db'
 __version_upcoming_annotated_v_tag = '1.11a1'
