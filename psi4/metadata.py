4993c860d0dbbf81ef5edf58472753b2856c8521
Filip Brzek <filip.brzek@gmail.com>
2026-02-11 21:54:58 +0100

    style: re-format psi4/metadata.py and remove it from pre-commit ignores

--- psi4/metadata.py
+++ psi4/metadata.py
@@ -1,8 +1,7 @@
-# fmt: off
-# Note: versioner.py parses this file with regex expecting single quotes
-__version__ = '1.10'
-__version_long = '1.10+f2492db'
-__version_upcoming_annotated_v_tag = '1.11a1'
+__version__ = "1.10"
+__version_long = "1.10+f2492db"
+__version_upcoming_annotated_v_tag = "1.11a1"
+
 
 def version_formatter(dummy):
-    return '(inplace)'
+    return "(inplace)"
