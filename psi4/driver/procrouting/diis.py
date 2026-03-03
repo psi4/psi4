3beb41f379efc55fa21949cb0c15d67d8c3de0e6
Ed Hohenstein <ehohenstein@nvidia.com>
2026-03-02 19:56:50 -0800

    Stop unneeded allocations

--- psi4/driver/procrouting/diis.py
+++ psi4/driver/procrouting/diis.py
@@ -161,6 +161,8 @@ class DIIS:
         template_object = self.template[name][item_num]
         if isinstance(template_object, float) or self.storage_policy == StoragePolicy.InCore:
             quantity = self.stored_vectors[entry_num][name][item_num]
+            if force_new == False:
+                return quantity
             try:
                 quantity = quantity.clone()
             except AttributeError:
@@ -195,8 +197,8 @@ class DIIS:
         except KeyError:
             dot_product = 0
             for item_num in range(len(self.template["error"])):
-                Rix = self.load_quantity("error", i, item_num)
-                Rjx = self.load_quantity("error", j, item_num)
+                Rix = self.load_quantity("error", i, item_num, False)
+                Rjx = self.load_quantity("error", j, item_num, False)
                 dot_product += Rix.vector_dot(Rjx)
 
             self.cached_dot_products[key] = dot_product
@@ -415,7 +417,7 @@ class DIIS:
         for j, Tj in enumerate(args):
             Tj.zero()
             for i, ci in enumerate(coeffs):
-                Tij = self.load_quantity("target", i, j)
+                Tij = self.load_quantity("target", i, j, False)
                 axpy(Tj, ci, Tij)
 
         return performed
