8e0ece712fdb9a44da303da405148629ac7cf3d9
Matt :D <mward10@memphis.edu>
2026-04-06 13:59:31 -0500

    Removed extra_infile kwarg from test_input.py in cghf4, cghf5, and cghf6
    - Appears to be reading the NumPy file as a string

--- tests/cghf5/test_input.py
+++ tests/cghf5/test_input.py
@@ -3,5 +3,5 @@ from addons import *
 @uusing("einsums")
 @ctest_labeler("quick;cghf")
 def test_cghf5():
-    ctest_runner(__file__, extra_infiles=["density.dat"])
+    ctest_runner(__file__)
 
--- tests/cghf6/test_input.py
+++ tests/cghf6/test_input.py
@@ -3,5 +3,5 @@ from addons import *
 @uusing("einsums")
 @ctest_labeler("quick;cghf")
 def test_cghf6():
-    ctest_runner(__file__, extra_infiles=["density.dat"])
+    ctest_runner(__file__)
 
