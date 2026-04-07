e7cae8e5143052f7e8089c295e1eafeefe33031a
Matt :D <mward10@memphis.edu>
2026-04-06 20:19:05 -0500

    Modified Pytests to handle binary files

--- tests/cghf4/test_input.py
+++ tests/cghf4/test_input.py
@@ -3,5 +3,5 @@ from addons import *
 @uusing("einsums")
 @ctest_labeler("quick;cghf")
 def test_cghf4():
-    ctest_runner(__file__)
+    ctest_runner(__file__, extra_binary_infiles=["density.dat"])
 
--- tests/cghf6/test_input.py
+++ tests/cghf6/test_input.py
@@ -3,5 +3,5 @@ from addons import *
 @uusing("einsums")
 @ctest_labeler("quick;cghf")
 def test_cghf6():
-    ctest_runner(__file__)
+    ctest_runner(__file__, extra_binary_infiles=["density.dat"])
 
