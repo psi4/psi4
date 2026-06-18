b014a3dbee42c5eeac6a16c75fdbbe77bf3b85bf
jaclark5 <jennifer.clark@openforcefield.org>
2026-06-18 16:29:02 -0400

    Update mbis ecp tests

--- tests/mbis-ecp/test_input.py
+++ tests/mbis-ecp/test_input.py
@@ -1,6 +1,8 @@
 from addons import *
+import pytest
 
 @uusing("ecpint")
 @ctest_labeler("properties;df;quick;ecp")
+@pytest.mark.xfail(reason="MBIS incompatible with ECP: requires all-electron density. Use all-electron basis or reconstruct density with denspart.")
 def test_mbis_ecp():
     ctest_runner(__file__)
