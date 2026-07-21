294e152642a32055b2235f9dd0e568d995f242cb
Nathan Gillispie <nwgllspe@memphis.edu>
2026-07-21 17:35:52 -0500

    Move common getter/setter functions to BaseWavefunction. Tests pass

--- psi4/src/psi4/libmints/basewavefunction.cc
+++ psi4/src/psi4/libmints/basewavefunction.cc
@@ -29,7 +29,10 @@
 #include "basewavefunction.h"
 #include "wavefunction.h"
 
+#include "psi4/libmints/mintshelper.h"
 #include "psi4/liboptions/liboptions.h"
+#include "psi4/libpsi4util/exception.h"
+#include "psi4/libpsi4util/libpsi4util.h"
 #include "psi4/libpsi4util/process.h"
 
 namespace psi {
@@ -58,5 +61,80 @@ BaseWavefunction::BaseWavefunction(Options& options)
     : options_(options), dipole_field_strength_{{0.0, 0.0, 0.0}}, PCM_enabled_(false) {}
 
 BaseWavefunction::~BaseWavefunction() = default;
+
+std::map<std::string, std::shared_ptr<BasisSet>> BaseWavefunction::basissets() const {
+    return mintshelper_->basissets();
+}
+
+std::shared_ptr<BasisSet> BaseWavefunction::get_basisset(std::string label) {
+    return mintshelper_->get_basisset(label);
+}
+
+void BaseWavefunction::set_basisset(std::string label, std::shared_ptr<BasisSet> basis) {
+    return mintshelper_->set_basisset(label, basis);
+}
+
+bool BaseWavefunction::basisset_exists(std::string label) { return mintshelper_->basisset_exists(label); }
+
+void BaseWavefunction::set_frzvpi(const Dimension& frzvpi) {
+    for (int h = 0; h < nirrep_; h++) {
+        frzvpi_[h] = frzvpi[h];
+    }
+}
+
+void BaseWavefunction::set_energy(double ene) { set_scalar_variable("CURRENT ENERGY", ene); }
+
+bool BaseWavefunction::has_scalar_variable(const std::string& key) { return variables_.count(to_upper_copy(key)); }
+
+bool BaseWavefunction::has_potential_variable(const std::string& key) {
+    return potentials_.count(to_upper_copy(key));
+}
+
+double BaseWavefunction::scalar_variable(const std::string& key) {
+    std::string uc_key = to_upper_copy(key);
+
+    auto search = variables_.find(uc_key);
+    if (search != variables_.end()) {
+        return search->second;
+    } else {
+        throw PSIEXCEPTION("BaseWavefunction::scalar_variable: Requested variable " + uc_key + " was not set!\n");
+    }
+}
+
+std::shared_ptr<ExternalPotential> BaseWavefunction::potential_variable(const std::string& key) {
+    std::string uc_key = to_upper_copy(key);
+
+    auto search = potentials_.find(uc_key);
+    if (search != potentials_.end()) {
+        return search->second;
+    } else {
+        throw PSIEXCEPTION("BaseWavefunction::potential_variable: Requested variable " + uc_key + " was not set!\n");
+    }
+}
+
+void BaseWavefunction::set_scalar_variable(const std::string& key, double val) {
+    variables_[to_upper_copy(key)] = val;
+
+    if (to_upper_copy(key) == "CURRENT ENERGY") energy_ = val;
+}
+
+void BaseWavefunction::set_potential_variable(const std::string& key, std::shared_ptr<ExternalPotential> val) {
+    potentials_[to_upper_copy(key)] = val;
+}
+
+int BaseWavefunction::del_scalar_variable(const std::string& key) { return variables_.erase(to_upper_copy(key)); }
+
+int BaseWavefunction::del_potential_variable(const std::string& key) { return potentials_.erase(to_upper_copy(key)); }
+
+std::map<std::string, double> BaseWavefunction::scalar_variables() { return variables_; }
+
+std::map<std::string, std::shared_ptr<ExternalPotential>> BaseWavefunction::potential_variables() {
+    return potentials_;
+}
+
+void BaseWavefunction::set_PCM(const std::shared_ptr<PCM>& pcm) {
+    PCM_ = pcm;
+    PCM_enabled_ = true;
+}
 
 }  // namespace psi
