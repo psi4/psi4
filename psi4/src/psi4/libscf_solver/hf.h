f2b7983da86bf3429000d5dd0bd5969b88b5c1e7
Nathan Gillispie <nwgllspe@memphis.edu>
2026-07-23 17:31:43 -0500

    Add basehf.cc. Move some HF common_init there.

--- psi4/src/psi4/libscf_solver/hf.h
+++ psi4/src/psi4/libscf_solver/hf.h
@@ -87,9 +87,6 @@ class HF : public Wavefunction, public BaseHF {
     // Q: right now, thresholds are removed from Wfn since only appear once, py-side.
     //    should we instead store here the E & D to which SCF was converged?
 
-    /// Table of energy components
-    std::map<std::string, double> energies_;
-
     /// Basis list for SAD
     std::vector<std::shared_ptr<BasisSet>> sad_basissets_;
     std::vector<std::shared_ptr<BasisSet>> sad_fitting_basissets_;
@@ -336,10 +333,6 @@ class HF : public Wavefunction, public BaseHF {
     void set_sad_fitting_basissets(std::vector<std::shared_ptr<BasisSet>> basis_vec) {
         sad_fitting_basissets_ = basis_vec;
     }
-
-    // Energies data
-    void set_energies(std::string key, double value) { energies_[key] = value; }
-    double get_energies(std::string key) { return energies_[key]; }
 
     // External potentials
     void clear_external_potentials() { external_potentials_.clear(); }
