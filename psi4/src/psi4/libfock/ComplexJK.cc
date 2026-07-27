5274d55562a204f80f024e9dcba8f615e831e3f3
Nathan Gillispie <nwgllspe@memphis.edu>
2026-07-27 11:17:15 -0500

    Implement ComplexJK::memory_overhead and allocate_JK.

--- psi4/src/psi4/libfock/ComplexJK.cc
+++ psi4/src/psi4/libfock/ComplexJK.cc
@@ -42,6 +42,7 @@ ComplexJK::~ComplexJK() {}
 
 void ComplexJK::common_init() {
     do_csam_ = false;
+    // Not sure if I need this yet
     // std::shared_ptr<IntegralFactory> integral =
     //     std::make_shared<IntegralFactory>(primary_, primary_, primary_, primary_);
 }
@@ -71,7 +72,7 @@ std::shared_ptr<ComplexJK> ComplexJK::build_JK(std::shared_ptr<BasisSet> primary
 
         return jk;
     } else {
-        throw PSIEXCEPTION("ComplexJK::build_JK: Unknown SCF Type '" + jk_type + "'");
+        throw PSIEXCEPTION("ComplexJK::build_JK: Unknown complex SCF Type '" + jk_type + "'");
     }
 }
 
@@ -91,39 +92,85 @@ void ComplexJK::allocate_JK() {
         J_.clear();
         K_.clear();
 
-        for (size_t i = 0; i < D_.size() && do_J_; i++) {
+        for (size_t N = 0; N < D_.size() && do_J_; N++) {
             std::stringstream s;
-            s << "J " << i << " (SO)";
-            // J_.push_back(std::make_shared<ComplexMatrix>(s.str(), D_[i]->rowspi(), D_[i]->rowspi(),
-            //                                       D_[i]->symmetry()));
+            s << "J " << N << " (SO)";
+            J_.push_back(std::make_shared<ComplexMatrix>(s.str(), D_[N]->block_dims()));
         }
+
+        for (size_t N = 0; N < D_.size() && do_K_; N++) {
+            std::stringstream s;
+            s << "K " << N << " (SO)";
+            K_.push_back(std::make_shared<ComplexMatrix>(s.str(), D_[N]->block_dims()));
+        }
+    }
+}
+
+void ComplexJK::compute_D() {
+    /// Make sure the memory is there
+    bool same = true;
+    if (C_left_.size() != D_.size()) {
+        same = false;
+    }
+
+    if (!same) {
+        D_.clear();
+        for (size_t N = 0; N < C_left_.size(); N++) {
+            std::stringstream s;
+            s << "D " << N << " (SO)";
+            // eventually maybe switch to TiledTensor so C_left_ and C_right_ can
+            // have different number of rows.
+            D_.push_back(std::make_shared<ComplexMatrix>(s.str(), C_left_[N]->block_dims()));
+        }
+    }
+
+    // Form the density, differs from dou
+    for (size_t N = 0; N < D_.size(); ++N) {
+        // int symm = D_[N]->symmetry();
+        D_[N]->zero();
+        auto temp1_ = ComplexMatrix("square C", D_[N]->block_dims());
+        auto temp2_ = ComplexMatrix("square C.H", D_[N]->block_dims());
+
+        for (size_t h = 0; h < D_[N]->num_blocks(); h++) {
+            // C_DGEMM('N', 'T', nsol, nsor, nocc, 1.0, Clp[0], nocc, Crp[0], nocc, 0.0, Dp[0], nsor);
+
+            temp1_[h].zero();
+            temp2_[h].zero();
+
+            /// Yeah, this one isn't ready yet.
+            throw pybind11::attribute_error("DirectJK::compute_D() not implemented!");
+
+            // Fills temp1_ and temp2_ with the occupied (2*nsopi_ x nelecpi_) and conjugate
+            // occupied matrices (e.g. Cocc)
+            // Note that temp1_ and temp2_ are both (2*nsopi_ x 2*nsopi_), but the 'remainder'
+            // are all->zeros and contribute nothing
+            // This is just a minor inefficiency, since these matrices are larger than they should be
+            // NOTE: this is done like this because BlockTensors cannot handle rectangular matrices
+
+            // TODO: get nelecpi_[h] somehow
+            auto nelecpi_ = C_left_[N]->block_dim(h);
+
+            for (int j = 0; j < C_left_[N]->block_dim(h); j++) {
+                for (int k = 0; k < nelecpi_; k++) {
+                    const std::complex<double>& C_jk = C_left_[N]->block(h)(j, k);
+                    temp1_[h](j, k) = C_jk;
+                    temp2_[h](j, k) = std::conj(C_jk);
+                }
+            }
+        }
+
+        // D_ = einsums("ui,vi->uv", temp1_, temp2_)
+        einsums::tensor_algebra::einsum(einsums::Indices{einsums::index::u, einsums::index::v}, D_[N].get(),  // D_uv
+                                        einsums::Indices{einsums::index::u, einsums::index::i}, temp1_,   // Cocc_ui
+                                        einsums::Indices{einsums::index::v, einsums::index::i}, temp2_    // Cocc_vi.conj().T
+        );
 
     }
 }
 
-void ComplexJK::compute_D() { throw pybind11::attribute_error("compute_D not implemented!"); }
-
 size_t ComplexJK::memory_overhead() const {
-    size_t mem = 0L;
-    int JKwKD_factor = 1;
-    if (do_J_) JKwKD_factor++;
-    if (do_K_) JKwKD_factor++;
-
-    int C_factor = 1;
-    if (!lr_symmetric_) C_factor++;
-
-    // if (C1() && C_left_.size() && C_left_[0]->num_blocks() != 1) {
-    //     int nbf = primary_->nbf();
-    //     for (size_t N = 0; N < C_left_.size(); N++) {
-    //         int nocc = 0;
-    //         for (int h = 0; h < C_left_[N]->num_blocks(); h++) {
-    //             nocc += C_left_[N]->row_dims()[h];
-    //         }
-    //         mem += C_factor * (size_t)nocc * nbf + JKwKD_factor * (size_t)nbf * nbf;
-    //     }
-    // }
-
-    return mem;
+    // Only non-zero for USO / AO quantities
+    return 0L;
 }
 
 void ComplexJK::zero() {
@@ -135,6 +182,42 @@ void ComplexJK::zero() {
     }
 }
 
-void ComplexJK::compute() { throw pybind11::attribute_error("ComplexJK::compute not implemented!"); }
+void ComplexJK::compute() {
+    // Is this density symmetric?
+    if (C_left_.size() && !C_right_.size()) {
+        lr_symmetric_ = true;
+        C_right_ = C_left_;
+    } else {
+        throw PSIEXCEPTION("C_left must equal C_right.");
+    }
+
+    // input_symmetry_cast_map_ not needed for C1
+
+    // Construct the densities
+    timer_on("JK: D");
+    compute_D();
+    timer_off("JK: D");
+
+    allocate_JK();
+
+    timer_on("JK: JK");
+    compute_JK();
+    timer_off("JK: JK");
+
+    if (debug_ > 6) {
+        outfile->Printf("\n   WARNING: ComplexMatrix->print() not implemented. "
+                        "No internal JK quantities will be logged.\n");
+        // for (size_t N = 0; N < C_left_.size(); N++) {
+        //     C_left_[N]->print("outfile");
+        //     C_right_[N]->print("outfile");
+        //     D_[N]->print("outfile");
+        //     J_[N]->print("outfile");
+        //     K_[N]->print("outfile");
+        // }
+    }
+
+    // Assuming lr_symmetric_
+    C_right_.clear();
+}
 
 }  // namespace psi
