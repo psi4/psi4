baa1c0f837a6672345b99d548b8c308900b99031
Lori A. Burns <lori.burns@gmail.com>
2026-09-01 05:12:52 -0400

    rebuild

--- tests/dft-psivar/test_input.py
+++ tests/dft-psivar/test_input.py
@@ -4,9 +4,9 @@ from addons import *
 @ctest_labeler("dft;scf")
 @pytest.mark.parametrize("oopkg", [
     pytest.param(False, id="internal"),
-    pytest.param("ooo", id="openorbitaloptimizer", marks=using("ooo")),
-    pytest.param("otr", id="opentrustregion", marks=using("otr")),
+    pytest.param("OOO", id="openorbitaloptimizer", marks=using("ooo")),
+    pytest.param("OTR", id="opentrustregion", marks=using("otr")),
 ])
 def test_dft_psivar(oopkg):
-    setenv = {"ooo": ["_PSI4_USE_OOPKG"], "otr": ["_PSI4_USE_OTRPKG"]}.get(oopkg)
+    setenv = [f"_PSI4_USE_{oopkg}PKG"]
     ctest_runner(__file__, setenv=setenv)
